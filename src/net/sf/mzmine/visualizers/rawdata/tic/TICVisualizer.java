9154ece441ac874ebe990c42ba2dba792c1e8061
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-04-26 11:21:23 +0000

    Changed getFont() to titleLabel.getFont() (for Win32 compatibility)

--- src/net/sf/mzmine/visualizers/rawdata/tic/TICVisualizer.java
+++ src/net/sf/mzmine/visualizers/rawdata/tic/TICVisualizer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2005 VTT Biotechnology
- * 
+ *
  * This file is part of MZmine.
- * 
+ *
  * MZmine is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA 02110-1301 USA
@@ -95,7 +95,7 @@ public class TICVisualizer extends JInternalFrame implements RawDataVisualizer,
 
     /**
      * Constructor for total ion chromatogram visualizer
-     * 
+     *
      */
     public TICVisualizer(RawDataFile rawDataFile, int msLevel) {
 
@@ -113,7 +113,7 @@ public class TICVisualizer extends JInternalFrame implements RawDataVisualizer,
 
         titleLabel = new JLabel(rawDataFile.toString(), JLabel.CENTER);
         titleLabel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        titleLabel.setFont(getFont().deriveFont(11.0f));
+        titleLabel.setFont(titleLabel.getFont().deriveFont(11.0f));
         add(titleLabel, BorderLayout.NORTH);
 
         toolBar = new TICToolBar(this);
