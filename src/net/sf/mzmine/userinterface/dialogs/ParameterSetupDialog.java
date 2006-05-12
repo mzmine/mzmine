d6f2929a59ddbdf60363dad91d10f43f825f3d69
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-05-12 10:31:12 +0000

    Modified constructor to initialize modal dialog.

--- src/net/sf/mzmine/userinterface/dialogs/ParameterSetupDialog.java
+++ src/net/sf/mzmine/userinterface/dialogs/ParameterSetupDialog.java
@@ -1,26 +1,26 @@
 /*
-    Copyright 2005 VTT Biotechnology
-
-    This file is part of MZmine.
-
-    MZmine is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    MZmine is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MZmine; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * Copyright 2006 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package net.sf.mzmine.userinterface.dialogs;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.awt.Frame;
 import java.text.NumberFormat;
 
 import javax.swing.BorderFactory;
@@ -63,8 +63,11 @@ public class ParameterSetupDialog extends JDialog implements ActionListener {
 	/**
 	 * Constructor
 	 */
-	public ParameterSetupDialog(MainWindow _mainWin, String title, String[] labelNames, double[] defaultValues, NumberFormat[] numberFormats) {
-		//super(_mainWin, title, true);
+	public ParameterSetupDialog(Frame owner, String title, String[] labelNames, double[] defaultValues, NumberFormat[] numberFormats) {
+
+		// Make dialog modal
+		super(owner, true);
+
 		exitCode = -1;
 
 		// Allocate arrays
@@ -115,9 +118,9 @@ public class ParameterSetupDialog extends JDialog implements ActionListener {
 
 		setTitle(title);
 
-		//setLocationRelativeTo(_mainWin);
-
 		pack();
+
+		setLocationRelativeTo(owner);
 	}
 
 	/**
