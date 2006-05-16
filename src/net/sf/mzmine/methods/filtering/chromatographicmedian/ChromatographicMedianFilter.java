13356e4781a26d20de1d3bc7a3ef7cc47b69c96e
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-05-16 07:55:16 +0000

    First full implementation of ChromatographicMedianFilter

--- src/net/sf/mzmine/methods/filtering/chromatographicmedian/ChromatographicMedianFilter.java
+++ src/net/sf/mzmine/methods/filtering/chromatographicmedian/ChromatographicMedianFilter.java
@@ -24,20 +24,20 @@ import java.awt.Frame;
 
 import net.sf.mzmine.interfaces.Scan;
 import net.sf.mzmine.io.RawDataFile;
+import net.sf.mzmine.io.MZmineProject;
 import net.sf.mzmine.methods.Method;
 import net.sf.mzmine.methods.MethodParameters;
 import net.sf.mzmine.methods.alignment.AlignmentResult;
+import net.sf.mzmine.taskcontrol.Task;
+import net.sf.mzmine.taskcontrol.TaskController;
+import net.sf.mzmine.taskcontrol.TaskListener;
 import net.sf.mzmine.userinterface.dialogs.ParameterSetupDialog;
 import net.sf.mzmine.userinterface.mainwindow.MainWindow;
+import net.sf.mzmine.util.Logger;
 
 
 
-
-public class ChromatographicMedianFilter implements Method {
-
-	// Labels for parameters
-	private final String[] fieldNames = {	"Tolerance in M/Z tolerance",
-											"One-sided scan window length" };
+public class ChromatographicMedianFilter implements Method, TaskListener {
 
 
 	public String getMethodDescription() {
@@ -49,27 +49,30 @@ public class ChromatographicMedianFilter implements Method {
 		ChromatographicMedianFilterParameters currentParameters = (ChromatographicMedianFilterParameters)parameters;
 		if (currentParameters==null) return false;
 
-		// Show parameter setup dialog
+		// Initialize parameter setup dialog
 		double[] paramValues = new double[2];
 		paramValues[0] = currentParameters.mzTolerance;
 		paramValues[1] = currentParameters.oneSidedWindowLength;
+
+		String[] paramNames = new String[2];
+		paramNames[0] = "Tolerance in M/Z tolerance";
+		paramNames[1] = "One-sided scan window length";
 
 		NumberFormat[] numberFormats = new NumberFormat[2];
 		numberFormats[0] = NumberFormat.getNumberInstance(); numberFormats[0].setMinimumFractionDigits(3);
 		numberFormats[1] = NumberFormat.getIntegerInstance();
 
+		// Show parameter setup dialog
 		MainWindow mainWin = MainWindow.getInstance();
-		ParameterSetupDialog psd = new ParameterSetupDialog((Frame)mainWin, "Please check the parameter values", fieldNames, paramValues, numberFormats);
+		ParameterSetupDialog psd = new ParameterSetupDialog((Frame)mainWin, "Please check the parameter values", paramNames, paramValues, numberFormats);
 		psd.setVisible(true);
-
 
 		// Check if user clicked Cancel-button
 		if (psd.getExitCode()==-1) {
 			return false;
 		}
 
-
-		// Read parameter values
+		// Write values from dialog back to parameters object
 		double d;
 
 		d = psd.getFieldValue(0);
@@ -92,11 +95,44 @@ public class ChromatographicMedianFilter implements Method {
 	}
 
 	public void runMethod(MethodParameters parameters, RawDataFile[] rawDataFiles, AlignmentResult[] alignmentResults) {
+
+		Task filterTask;
+		ChromatographicMedianFilterParameters cmfParam = (ChromatographicMedianFilterParameters)parameters;
+
+		for (RawDataFile rawDataFile: rawDataFiles) {
+			filterTask = new ChromatographicMedianFilterTask(rawDataFile, cmfParam);
+			TaskController.getInstance().addTask(filterTask, this);
+		}
+
 	}
 
+    public void taskStarted(Task task) {
+		// do nothing
+	}
 
+    public void taskFinished(Task task) {
 
+        if (task.getStatus() == Task.TaskStatus.FINISHED) {
 
+			RawDataFile oldFile = (RawDataFile)((Object[])task.getResult())[0];
+			RawDataFile newFile = (RawDataFile)((Object[])task.getResult())[1];
+			ChromatographicMedianFilterParameters cmfParam = (ChromatographicMedianFilterParameters)((Object[])task.getResult())[2];
+
+			// Add filtering to the history of the file
+			newFile.addHistory(oldFile.getCurrentFile(), this, cmfParam.clone());
+
+			// Update MZmineProject about replacement of oldFile by newFile
+			MZmineProject.getCurrentProject().updateFile(oldFile, newFile);
+
+        } else if (task.getStatus() == Task.TaskStatus.ERROR) {
+            /* Task encountered an error */
+            Logger.putFatal("Error while filtering a file: " + task.getErrorMessage());
+            MainWindow.getInstance().displayErrorMessage(
+                    "Error while filtering a file: " + task.getErrorMessage());
+
+        }
+
+	}
 
 }
 
