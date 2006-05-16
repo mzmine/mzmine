26791251d6875aa9799bc7bbb5bdfebc62e0293b
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-05-16 13:01:39 +0000

    Updated strings for parameter setup dialog

--- src/net/sf/mzmine/methods/filtering/chromatographicmedian/ChromatographicMedianFilter.java
+++ src/net/sf/mzmine/methods/filtering/chromatographicmedian/ChromatographicMedianFilter.java
@@ -1,22 +1,21 @@
 /*
-    Copyright 2005 VTT Biotechnology
-
-    This file is part of MZmine.
-
-    MZmine is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    MZmine is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MZmine; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * Copyright 2006 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package net.sf.mzmine.methods.filtering.chromatographicmedian;
 import java.text.NumberFormat;
 import java.util.Vector;
@@ -55,8 +54,8 @@ public class ChromatographicMedianFilter implements Method, TaskListener {
 		paramValues[1] = currentParameters.oneSidedWindowLength;
 
 		String[] paramNames = new String[2];
-		paramNames[0] = "Tolerance in M/Z tolerance";
-		paramNames[1] = "One-sided scan window length";
+		paramNames[0] = "Tolerance in M/Z tolerance (Da)";
+		paramNames[1] = "One-sided window length (scans)";
 
 		NumberFormat[] numberFormats = new NumberFormat[2];
 		numberFormats[0] = NumberFormat.getNumberInstance(); numberFormats[0].setMinimumFractionDigits(3);
