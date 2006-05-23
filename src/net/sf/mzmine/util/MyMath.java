d72af86c1f7fff6be1bdd25eadbbcf7d89c6b6da
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-05-23 06:39:03 +0000

    Added methods for binning and interpolation.

--- src/net/sf/mzmine/util/MyMath.java
+++ src/net/sf/mzmine/util/MyMath.java
@@ -19,7 +19,7 @@
 */
 package net.sf.mzmine.util;
 import java.util.Arrays;
-
+import java.util.ArrayList;
 
 public class MyMath {
 
@@ -102,5 +102,149 @@ public class MyMath {
 		return stdev/avg;
 	}
 
+
+	/**
+	 * This method bins values on x-axis.
+	 * Each bin is assigned biggest y-value of all values in the same bin.
+	 *
+	 * @param	x				X-coordinates of the data
+	 * @param	y				Y-coordinates of the data
+	 * @param	firstBinStart	Value at the "left"-edge of the first bin
+	 * @param	lastBinStop		Value at the "right"-edge of the last bin
+	 * @param	numberOfBins	Number of bins
+	 * @return	Values for each bin
+	 */
+	public double[] binValues(double[] x, double[] y, double firstBinStart, double lastBinStop, int numberOfBins, boolean interpolate) {
+
+		Double[] binValues = new Double[numberOfBins];
+		double binWidth = (lastBinStop-firstBinStart)/numberOfBins;
+
+		// Binnings
+		for (int valueIndex=0; valueIndex<x.length; valueIndex++) {
+
+			if ((x[valueIndex]-firstBinStart)<0) continue;	// Before first bin?
+			if ((lastBinStop-x[valueIndex])<0) continue;	// After last bin?
+
+			int binIndex = (int)((x[valueIndex]-firstBinStart)/numberOfBins);
+
+			if (binValues[binIndex]==null) {
+				binValues[binIndex] = y[valueIndex];
+				continue;
+			}
+
+			if (binValues[binIndex]<y[valueIndex]) {
+				binValues[binIndex] = y[valueIndex];
+			}
+
+		}
+
+		double[] res = new double[binValues.length];
+		for (int binIndex=0; binIndex<binValues.length; binIndex++) {
+			res[binIndex] = binValues[binIndex];
+		}
+		return res;
+
+	}
+
+	/**
+	 * This method fills missing values in an array by interpolation.
+	 * It is assumed that value
+	 * @param	values	Original values, null if value is missing
+	 * @param	valueBefore	Value before the first element in "values". If null, then method tries to extrapolate if interpolation is not possible
+	 * @param	valueAfter	Value after the last element in "values". If null, then method tries to extrapolate if interpolation is not possible
+	 */
+	public void interpolateMissingValues(double x[], Double[] y) {
+
+		for (int index=0; index<y.length; index++) {
+
+			if (y[index]==null) {
+
+				// Find nearest existing left neighbour
+				Double leftY = null;
+				double leftX = 0.0;
+				int leftIndex = -1;
+				for (int anotherIndex=index; anotherIndex>=0; anotherIndex--) {
+					if (y[anotherIndex]!=null) {
+						leftY = y[anotherIndex];
+						leftX = x[anotherIndex];
+						leftIndex = anotherIndex;
+						break;
+					}
+				}
+
+				// Find nearest existing right neighbour
+				Double rightY = null;
+				double rightX = 0.0;
+				int rightIndex = -1;
+				for (int anotherIndex=index; anotherIndex<y.length; anotherIndex++) {
+					if (y[anotherIndex]!=null) {
+						rightY = y[anotherIndex];
+						rightX = x[anotherIndex];
+						rightIndex = anotherIndex;
+						break;
+					}
+				}
+
+				// If both left and right neighbour are missing, then it is not possible to interpolate or extrapolate
+				if ((leftY==null) && (rightY==null)) { continue; }
+
+				// If both neighbours are present, then interpolate
+				if ((leftY!=null) && (rightY!=null)) {
+					double deltaY = rightY-leftY;
+					double deltaX = rightX-leftX;
+					y[index] = new Double(leftY + deltaY/deltaX * (x[index]-leftX) );
+					continue;
+				}
+
+				// If only left neighbour is present, then must look for another left neighbour
+				if ((leftY!=null) && (rightY==null)) {
+					// Find second nearest existing left neighbour
+					Double secondLeftY = null;
+					double secondLeftX = 0.0;
+					for (int anotherIndex=leftIndex-1; anotherIndex>=0; anotherIndex--) {
+						if (y[anotherIndex]!=null) {
+							secondLeftY = y[anotherIndex];
+							secondLeftX = x[anotherIndex];
+							break;
+						}
+					}
+
+					// If did not find another left neightbour, can't interpolate
+					if (secondLeftY==null) continue;
+
+					double deltaY = leftY-secondLeftY;
+					double deltaX = leftX-secondLeftX;
+					y[index] = new Double(leftY + deltaY/deltaX * (x[index]-leftX) );
+
+				}
+
+				// If only right neighbour is present, then must look for another left neighbour
+				if ((leftY==null) && (rightY!=null)) {
+
+					// Find second nearest existing left neighbour
+					Double secondRightY = null;
+					double secondRightX = 0.0;
+					for (int anotherIndex=rightIndex+1; anotherIndex<y.length; anotherIndex++) {
+						if (y[anotherIndex]!=null) {
+							secondRightY = y[anotherIndex];
+							secondRightX = x[anotherIndex];
+							break;
+						}
+					}
+
+					// If did not find another left neightbour, can't interpolate
+					if (secondRightY==null) continue;
+
+					double deltaY = secondRightY-rightY;
+					double deltaX = secondRightX-rightX;
+					y[index] = new Double(rightY - deltaY/deltaX * (rightX-x[index]) );
+
+				}
+
+			}
+
+		}
+
+	}
 
 }
