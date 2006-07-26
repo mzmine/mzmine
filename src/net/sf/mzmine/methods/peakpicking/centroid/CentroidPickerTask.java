30b6260ab99b905647f574f409cf874a8aded32c
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2006-07-26 07:30:57 +0000

    Small changes for slightly faster peak picking.

--- src/net/sf/mzmine/methods/peakpicking/centroid/CentroidPickerTask.java
+++ src/net/sf/mzmine/methods/peakpicking/centroid/CentroidPickerTask.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2006 The MZmine Development Team
- * 
+ *
  * This file is part of MZmine.
- * 
+ *
  * MZmine is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA 02110-1301 USA
@@ -36,7 +36,7 @@ import net.sf.mzmine.util.MathUtils;
 import net.sf.mzmine.util.ScanUtils;
 
 /**
- * 
+ *
  */
 class CentroidPickerTask implements Task {
 
@@ -134,53 +134,65 @@ class CentroidPickerTask implements Task {
                                                     // raw data file
         int numOfBins = (int) (java.lang.Math.ceil((endMZ - startMZ)
                 / parameters.binSize));
-        double[][] binInts = new double[numOfBins][totalScans];
 
-        // Loop through scans and calculate binned maximum intensities
-        for (int i = 0; i < totalScans; i++) {
+		double[] chromatographicThresholds = new double[numOfBins];
 
-            if (status == TaskStatus.CANCELED)
-                return;
+		if (parameters.chromatographicThresholdLevel>0) {
 
-            try {
-                Scan sc = rawDataFile.getScan(scanNumbers[i]);
+			double[][] binInts = new double[numOfBins][totalScans];
 
-                double[] mzValues = sc.getMZValues();
-                double[] intensityValues = sc.getIntensityValues();
-                double[] tmpInts = ScanUtils.binValues(mzValues,
-                        intensityValues, startMZ, endMZ, numOfBins, true,
-                        ScanUtils.BinningType.MAX);
-                for (int bini = 0; bini < numOfBins; bini++) {
-                    binInts[bini][i] = tmpInts[bini];
-                }
+			// Loop through scans and calculate binned maximum intensities
+			for (int i = 0; i < totalScans; i++) {
 
-            } catch (IOException e) {
-                status = TaskStatus.ERROR;
-                errorMessage = e.toString();
-            }
+				if (status == TaskStatus.CANCELED)
+					return;
 
-            processedScans++;
+				try {
+					Scan sc = rawDataFile.getScan(scanNumbers[i]);
 
-        }
+					double[] mzValues = sc.getMZValues();
+					double[] intensityValues = sc.getIntensityValues();
+					double[] tmpInts = ScanUtils.binValues(mzValues,
+							intensityValues, startMZ, endMZ, numOfBins, true,
+							ScanUtils.BinningType.MAX);
+					for (int bini = 0; bini < numOfBins; bini++) {
+						binInts[bini][i] = tmpInts[bini];
+					}
 
-        // Calculate filtering threshold from each RIC
-        double initialThreshold = Double.MAX_VALUE;
-        double[] chromatographicThresholds = new double[numOfBins];
-        for (int bini = 0; bini < numOfBins; bini++) {
+				} catch (IOException e) {
+					status = TaskStatus.ERROR;
+					errorMessage = e.toString();
+				}
 
-            chromatographicThresholds[bini] = MathUtils.calcQuantile(
-                    binInts[bini], parameters.chromatographicThresholdLevel);
-            if (chromatographicThresholds[bini] < initialThreshold) {
-                initialThreshold = chromatographicThresholds[bini];
-            }
-        }
+				processedScans++;
 
-        binInts = null;
-        System.gc();
+			}
+
+			// Calculate filtering threshold from each RIC
+			double initialThreshold = Double.MAX_VALUE;
+
+			for (int bini = 0; bini < numOfBins; bini++) {
+
+				chromatographicThresholds[bini] = MathUtils.calcQuantile(
+						binInts[bini], parameters.chromatographicThresholdLevel);
+				if (chromatographicThresholds[bini] < initialThreshold) {
+					initialThreshold = chromatographicThresholds[bini];
+				}
+			}
+
+			binInts = null;
+			System.gc();
+
+		} else {
+			processedScans += totalScans;
+			for (int bini = 0; bini < numOfBins; bini++)
+				chromatographicThresholds[bini] = 0;
+
+		}
 
         Vector<SimplePeak> underConstructionPeaks = new Vector<SimplePeak>();
         Vector<OneDimPeak> oneDimPeaks = new Vector<OneDimPeak>();
-        
+
         for (int i = 0; i < totalScans; i++) {
 
             if (status == TaskStatus.CANCELED)
@@ -254,6 +266,7 @@ class CentroidPickerTask implements Task {
             int DEBUGcounter = 0;
 
             Iterator<MatchScore> scoreIterator = scores.iterator();
+
             while (scoreIterator.hasNext()) {
                 MatchScore score = scoreIterator.next();
 
@@ -474,7 +487,7 @@ class CentroidPickerTask implements Task {
          * This function check for the shape of the peak in RT direction, and
          * determines if it is possible to add given m/z peak at the end of the
          * peak.
-         * 
+         *
          */
         private double calcScoreForRTShape(SimplePeak uc, OneDimPeak od) {
 
@@ -493,11 +506,6 @@ class CentroidPickerTask implements Task {
 
                 Double[] prevTriplet = triplets.nextElement();
                 double prevIntensity = prevTriplet[2];
-                /*
-                 * Object[] triplets = datapoints.values().toArray(); double
-                 * prevIntensity = ((Double[])(triplets[0]))[2]; triplets =
-                 * null;
-                 */
 
                 // If it goes up, then give minimum (best) score
                 if ((nextIntensity - prevIntensity) >= 0) {
