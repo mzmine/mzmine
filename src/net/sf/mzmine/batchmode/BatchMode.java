6eb5d2b40e8fb881888a67dc62f615942832d63a
Tomas Pluskal <plusik@gmail.com>
2007-04-05 09:00:40 +0000

    Redesign of parameter storage and batch mode. Work in progress.

---
+++ src/net/sf/mzmine/batchmode/BatchMode.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2006 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.batchmode;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+import javax.swing.JDialog;
+import javax.swing.JMenuItem;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import net.sf.mzmine.batchmode.BatchModeDialog.ExitCode;
+import net.sf.mzmine.data.AlignmentResult;
+import net.sf.mzmine.data.ParameterSet;
+import net.sf.mzmine.io.OpenedRawDataFile;
+import net.sf.mzmine.main.MZmineCore;
+import net.sf.mzmine.main.MZmineModule;
+import net.sf.mzmine.methods.Method;
+import net.sf.mzmine.methods.MethodListener;
+import net.sf.mzmine.project.MZmineProject;
+import net.sf.mzmine.userinterface.Desktop;
+import net.sf.mzmine.userinterface.Desktop.MZmineMenu;
+
+/**
+ * Batch mode module
+ */
+public class BatchMode implements MZmineModule, ListSelectionListener,
+        MethodListener, ActionListener {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private MZmineCore core;
+    private Desktop desktop;
+
+    private JMenuItem batchMenuItem;
+
+    private Vector<BatchStep> currentBatchSteps;
+    private boolean batchRunning = false;
+    private int currentStep;
+    private OpenedRawDataFile[] selectedDataFiles;
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#initModule(net.sf.mzmine.main.MZmineCore)
+     */
+    public void initModule(MZmineCore core) {
+
+        this.core = core;
+        this.desktop = core.getDesktop();
+
+        currentBatchSteps = new Vector<BatchStep>();
+
+        batchMenuItem = desktop.addMenuItem(MZmineMenu.BATCH,
+                "Define batch operations", this, null, KeyEvent.VK_D, false,
+                false);
+        desktop.addSelectionListener(this);
+
+    }
+
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e) {
+
+        if (batchRunning) {
+            desktop.displayErrorMessage("Batch is already running");
+            return;
+        }
+
+        selectedDataFiles = desktop.getSelectedDataFiles();
+        if (selectedDataFiles.length == 0) {
+            desktop.displayErrorMessage("Please select at least one data file");
+            return;
+        }
+
+        logger.finest("Showing parameter setup dialog");
+
+        BatchModeDialog setupDialog = new BatchModeDialog(desktop.getMainFrame(), core,
+                currentBatchSteps);
+        setupDialog.setVisible(true);
+
+        if (setupDialog.getExitCode() == ExitCode.OK) {
+            currentStep = 0;
+            runNextStep();
+        }
+
+    }
+
+    /**
+     * @see javax.swing.event.ListSelectionListener#valueChanged(javax.swing.event.ListSelectionEvent)
+     */
+    public void valueChanged(ListSelectionEvent e) {
+        batchMenuItem.setEnabled(desktop.isDataFileSelected());
+    }
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#toString()
+     */
+    public String toString() {
+        return "Batch mode";
+    }
+
+    public void methodFinished(MethodReturnStatus status) {
+
+        logger.finest("Batch mode received methodFinished call");
+
+        if ((status == MethodReturnStatus.ERROR)
+                || (status == MethodReturnStatus.CANCELED)) {
+            desktop.displayErrorMessage("Batch processing canceled.");
+            batchRunning = false;
+            return;
+        }
+
+        if (status == MethodReturnStatus.FINISHED) {
+            if (currentStep < currentBatchSteps.size())
+                runNextStep();
+            else {
+                desktop.displayErrorMessage("Batch processing done.");
+                batchRunning = false;
+            }
+        }
+    }
+
+    private void runNextStep() {
+
+        logger.finest("Batch mode runNextStep");
+
+        BatchStep newStep = currentBatchSteps.get(currentStep);
+        Method method = newStep.getMethod();
+
+        AlignmentResult[] lastResult = null;
+        AlignmentResult[] allResults = MZmineProject.getCurrentProject().getAlignmentResults();
+        if (allResults.length > 0)
+            lastResult = new AlignmentResult[] { allResults[allResults.length - 1] };
+
+        method.runMethod(selectedDataFiles, lastResult,
+                newStep.getParameters(), this);
+
+        currentStep++;
+
+    }
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#getParameterSet()
+     */
+    public ParameterSet getParameterSet() {
+        return null;
+    }
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#setParameters(net.sf.mzmine.data.ParameterSet)
+     */
+    public void setParameters(ParameterSet parameterValues) {
+    }
+
+}
