48aa90fd9a6f97d15bb04827af582e0969fee7fe
Tomas Pluskal <plusik@gmail.com>
2007-04-06 07:26:35 +0000

    Updated filters

---
+++ src/net/sf/mzmine/methods/filtering/chromatographicmedian/CMFilter.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2006-2007 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.methods.filtering.chromatographicmedian;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenuItem;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import net.sf.mzmine.data.AlignmentResult;
+import net.sf.mzmine.data.Parameter;
+import net.sf.mzmine.data.ParameterSet;
+import net.sf.mzmine.data.Parameter.ParameterType;
+import net.sf.mzmine.data.impl.SimpleParameter;
+import net.sf.mzmine.data.impl.SimpleParameterSet;
+import net.sf.mzmine.io.OpenedRawDataFile;
+import net.sf.mzmine.main.MZmineCore;
+import net.sf.mzmine.methods.Method;
+import net.sf.mzmine.methods.MethodListener;
+import net.sf.mzmine.taskcontrol.TaskController;
+import net.sf.mzmine.userinterface.Desktop;
+import net.sf.mzmine.userinterface.Desktop.MZmineMenu;
+import net.sf.mzmine.userinterface.dialogs.ParameterSetupDialog;
+import net.sf.mzmine.userinterface.dialogs.ParameterSetupDialog.ExitCode;
+
+public class CMFilter implements Method, ListSelectionListener, ActionListener {
+
+    public static final Parameter parameterOneSidedWindowLength = new SimpleParameter(
+            ParameterType.INTEGER, "Window length",
+            "One-sided width of the smoothing window", "scans", new Integer(1),
+            new Integer(1), null);
+
+    public static final Parameter parameterMZTolerance = new SimpleParameter(
+            ParameterType.DOUBLE, "M/Z tolerance",
+            "Maximum allowed M/Z difference", "Da", new Double(0.1),
+            new Double(0.0), null);
+
+    private ParameterSet parameters;
+
+    private TaskController taskController;
+    private Desktop desktop;
+    private JMenuItem myMenuItem;
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#initModule(net.sf.mzmine.main.MZmineCore)
+     */
+    public void initModule(MZmineCore core) {
+
+        this.taskController = core.getTaskController();
+        this.desktop = core.getDesktop();
+
+        parameters = new SimpleParameterSet(new Parameter[] {
+                parameterOneSidedWindowLength, parameterMZTolerance });
+
+        myMenuItem = desktop.addMenuItem(MZmineMenu.FILTERING,
+                "Chromatographic median filter", this, null, KeyEvent.VK_H,
+                false, false);
+
+        desktop.addSelectionListener(this);
+
+    }
+
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == myMenuItem) {
+            ParameterSet param = setupParameters(parameters);
+            if (param == null)
+                return;
+            OpenedRawDataFile[] dataFiles = desktop.getSelectedDataFiles();
+            runMethod(dataFiles, null, param);
+        }
+    }
+
+    /**
+     * @see javax.swing.event.ListSelectionListener#valueChanged(javax.swing.event.ListSelectionEvent)
+     */
+    public void valueChanged(ListSelectionEvent e) {
+        myMenuItem.setEnabled(desktop.isDataFileSelected());
+    }
+
+    /**
+     * @see net.sf.mzmine.methods.Method#toString()
+     */
+    public String toString() {
+        return "Chromatographic median filter";
+    }
+
+    /**
+     * @see net.sf.mzmine.methods.Method#setupParameters()
+     */
+    public ParameterSet setupParameters(ParameterSet currentParameters) {
+        ParameterSetupDialog dialog = new ParameterSetupDialog(
+                desktop.getMainFrame(), "Please check parameter values for "
+                        + toString(), currentParameters);
+        dialog.setVisible(true);
+        if (dialog.getExitCode() == ExitCode.CANCEL)
+            return null;
+        return currentParameters.clone();
+    }
+
+    /**
+     * @see net.sf.mzmine.methods.Method#runMethod(net.sf.mzmine.io.OpenedRawDataFile[],
+     *      net.sf.mzmine.data.AlignmentResult[],
+     *      net.sf.mzmine.data.ParameterSet)
+     */
+    public void runMethod(OpenedRawDataFile[] dataFiles,
+            AlignmentResult[] alignmentResults, ParameterSet parameters) {
+
+        runMethod(dataFiles, alignmentResults, parameters, null);
+
+    }
+
+    /**
+     * @see net.sf.mzmine.methods.Method#runMethod(net.sf.mzmine.io.OpenedRawDataFile[],
+     *      net.sf.mzmine.data.AlignmentResult[],
+     *      net.sf.mzmine.data.ParameterSet,
+     *      net.sf.mzmine.methods.MethodListener)
+     */
+    public void runMethod(OpenedRawDataFile[] dataFiles,
+            AlignmentResult[] alignmentResults, ParameterSet parameters,
+            MethodListener methodListener) {
+
+        // prepare a new sequence of tasks
+        CMFilterRun newRun = new CMFilterRun(this, dataFiles, parameters,
+                methodListener, desktop, taskController);
+
+        // execute the sequence
+        newRun.run();
+
+    }
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#getParameterSet()
+     */
+    public ParameterSet getParameterSet() {
+        return parameters;
+    }
+
+    /**
+     * @see net.sf.mzmine.main.MZmineModule#setParameters(net.sf.mzmine.data.ParameterSet)
+     */
+    public void setParameters(ParameterSet parameters) {
+        this.parameters = parameters;
+    }
+
+}
