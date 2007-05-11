9cb9bfe337ba1dcadb339b8f281499f9a26f1898
Tomas Pluskal <plusik@gmail.com>
2007-05-11 04:34:56 +0000

    source reorganization

--- src/net/sf/mzmine/modules/identification/pubchem/PubChemSearch.java
+++ src/net/sf/mzmine/modules/identification/pubchem/PubChemSearch.java
@@ -1,6 +1,22 @@
-/**
+/*
+ * Copyright 2006-2007 The MZmine Development Team
  * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
  */
+
 package net.sf.mzmine.modules.identification.pubchem;
 
 import gov.nih.nlm.ncbi.www.soap.eutils.EUtilsServiceLocator;
@@ -11,9 +27,8 @@ import net.sf.mzmine.data.ParameterSet;
 import net.sf.mzmine.main.MZmineCore;
 import net.sf.mzmine.main.MZmineModule;
 
-
 /**
- *
+ * 
  */
 public class PubChemSearch implements MZmineModule {
 
@@ -29,30 +44,31 @@ public class PubChemSearch implements MZmineModule {
      * @see net.sf.mzmine.main.MZmineModule#initModule(net.sf.mzmine.main.MZmineCore)
      */
     public void initModule(MZmineCore core) {
+
+    }
+
+    public void test() {
         try {
             EUtilsServiceLocator service = new EUtilsServiceLocator();
             EUtilsServiceSoap utils = service.geteUtilsServiceSoap();
             // call NCBI ESearch utility
-            // NOTE: search term should be URL encoded 
+            // NOTE: search term should be URL encoded
             ESearchRequest req = new ESearchRequest();
             req.setDb("pccompound");
             req.setRetMax("20");
             req.setTerm("391.903:391.913[MonoisotopicMass] AND 1:1000000[CID] NOT Cl[Element] NOT Br[Element]");
-            
-            
+
             ESearchResult res = utils.run_eSearch(req);
             // results output
             System.out.println("Found ids: " + res.getCount());
-            System.out.print("First " + res.getRetMax() +" ids: ");
-            for(int i=0; i<res.getIdList().getId().length; i++)
-            {
+            System.out.print("First " + res.getRetMax() + " ids: ");
+            for (int i = 0; i < res.getIdList().getId().length; i++) {
                 System.out.print(res.getIdList().getId()[i] + " ");
             }
-            
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("Finished ");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
