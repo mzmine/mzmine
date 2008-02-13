d8150cb067e8ca9e017760774c2d0d4b0ab1ebef
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2008-02-13 09:46:28 +0000

    Started working on an identification method for qbix internal lipid db.

---
+++ src/net/sf/mzmine/modules/identification/vttcompounddb/QBIXLipidDBSearchParameters.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2006-2008 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.identification.qbixlipiddb;
+
+import net.sf.mzmine.data.Parameter;
+import net.sf.mzmine.data.ParameterType;
+import net.sf.mzmine.data.impl.SimpleParameter;
+import net.sf.mzmine.data.impl.SimpleParameterSet;
+import net.sf.mzmine.main.MZmineCore;
+
+/**
+ * 
+ */
+public class QBIXLipidDBSearchParameters extends SimpleParameterSet {
+
+    public static final String QBIXInternalLipidDatabase = "QBIX internal lipid database";
+	
+	
+	public static final Parameter MZTolerance = new SimpleParameter(
+			ParameterType.FLOAT, "ppm", "m/z tolerance in ppm", "ppm",
+			new Float(0.2), new Float(0.0), null, MZmineCore.getDesktop()
+					.getMZFormat());
+
+	public static final Parameter MassResolution = new SimpleParameter(
+			ParameterType.FLOAT, "Mass resolution", "Mass resolution", "",
+			new Float(0.2), new Float(0.0), null, MZmineCore.getDesktop()
+					.getMZFormat());
+
+
+    public QBIXLipidDBSearchParameters() {
+        super(new Parameter[] { MZTolerance, MassResolution });
+    }
+
+	
+}
---
+++ src/net/sf/mzmine/modules/identification/vttcompounddb/QBIXLipidDBSearchTask.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2006-2007 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.identification.qbixlipiddb;
+
+import java.util.logging.Logger;
+
+import net.sf.mzmine.data.PeakList;
+import net.sf.mzmine.data.PeakListRow;
+import net.sf.mzmine.data.impl.SimpleCompoundIdentity;
+import net.sf.mzmine.taskcontrol.Task;
+
+/**
+ * 
+ */
+class QBIXLipidDBSearchTask implements Task {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private PeakList peakList;
+    private QBIXLipidDBSearchParameters parameters;
+
+    private TaskStatus status;
+    private String errorMessage;
+    private String[][] databaseValues;
+    private int finishedLines = 0;
+
+    QBIXLipidDBSearchTask(PeakList peakList, QBIXLipidDBSearchParameters parameters) {
+        status = TaskStatus.WAITING;
+        this.peakList = peakList;
+        this.parameters = parameters;
+    }
+
+    /**
+     * @see net.sf.mzmine.taskcontrol.Task#cancel()
+     */
+    public void cancel() {
+        status = TaskStatus.CANCELED;
+    }
+
+    /**
+     * @see net.sf.mzmine.taskcontrol.Task#getErrorMessage()
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * @see net.sf.mzmine.taskcontrol.Task#getFinishedPercentage()
+     */
+    public float getFinishedPercentage() {
+        if (databaseValues == null)
+            return 0;
+        return ((float) finishedLines) / databaseValues.length;
+    }
+
+
+    /**
+     * @see net.sf.mzmine.taskcontrol.Task#getStatus()
+     */
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * @see net.sf.mzmine.taskcontrol.Task#getTaskDescription()
+     */
+    public String getTaskDescription() {
+        return "Peak identification of " + peakList + " using QBIX internal lipid database.";
+    }
+
+    /**
+     * @see java.lang.Runnable#run()
+     */
+    public void run() {
+
+        status = TaskStatus.PROCESSING;
+        
+        
+        logger.info("Running " + getTaskDescription());
+        
+        SimpleCompoundIdentity tmpIdentity = new SimpleCompoundIdentity("1", "not yet ready", 
+        																null, "NA", 
+        																null, "QBIX lipid DB");
+
+        
+        for (PeakListRow peakRow : peakList.getRows()) {
+        	
+        	peakRow.addCompoundIdentity(tmpIdentity);
+        }
+
+        logger.info("Finished " + getTaskDescription());
+        
+        status = TaskStatus.FINISHED;
+
+    }
+    
+}
