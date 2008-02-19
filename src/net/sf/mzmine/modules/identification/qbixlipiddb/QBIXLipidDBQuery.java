642ba231dc5f6fdf18115ef2c1bae1ba4a56a9f9
Mikko Katajamaa <Mikko.Katajamaa@vtt.fi>
2008-02-19 07:38:32 +0000

    Fixed package name

---
+++ src/net/sf/mzmine/modules/identification/qbixlipiddb/QBIXLipidDBQuery.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2006-2008 The MZmine Development Team
+ * 
+ * This file is part of MZmine.
+ * 
+ * MZmine is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.identification.qbixlipiddb;
+
+class QBIXLipidDBQuery {
+
+	private String name;
+	private float mz;
+	private float rt;
+	private float tolerancePPM;
+	private String adduct;
+	private float add;
+	private float resolution;
+	private String expected;
+
+	QBIXLipidDBQuery(String name, float mz, float rt, float tolerancePPM,
+			String adduct, float add, float resolution, String expected) {
+
+		this.name = name;
+		this.mz = mz;
+		this.rt = rt;
+		this.tolerancePPM = tolerancePPM;
+		this.adduct = adduct;
+		this.add = add;
+		this.resolution = resolution;
+		this.expected = expected;
+
+	}
+
+	String getName() {
+		return name;
+	}
+
+	float getMZ() {
+		return mz;
+	}
+	
+	float getMinMZ() {
+		return mz - mz * tolerancePPM / 1000000.0f;
+	}
+
+	float getMaxMZ() {
+		return mz + mz * tolerancePPM / 1000000.0f;
+	}
+
+	float getRT() {
+		return rt;
+	}
+	
+	String getAdduct() {
+		return adduct;
+	}
+
+	float getAdd() {
+		return add;
+	}
+
+	String getExpected() {
+		return expected;
+	}
+
+}
