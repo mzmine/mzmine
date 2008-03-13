2eee943cc1614f7630787b7432d89679aa2a8965
Tomas Pluskal <plusik@gmail.com>
2008-03-13 00:22:40 +0000

    About dialog now reads the contents from AboutText.html

--- src/net/sf/mzmine/userinterface/dialogs/AboutDialog.java
+++ src/net/sf/mzmine/userinterface/dialogs/AboutDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 The MZmine Development Team
+ * Copyright 2006-2008 The MZmine Development Team
  * 
  * This file is part of MZmine.
  * 
@@ -24,6 +24,10 @@ import java.awt.Color;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
@@ -40,16 +44,28 @@ import net.sf.mzmine.util.GUIUtils;
  */
 public class AboutDialog extends JDialog implements ActionListener {
 
-    // Contents of the dialog, this string is available (in editable format) in
-    // file AboutText.html
-    private String contentString = "<html>  <center> 	<h1>MZmine 2</h1> 	<h3>under construction</h3>  	<p> 	Copyright (c) 2005-2008 MZmine Development Team 	</p> </center>  <p> Please use mailing list mzmine-devel@lists.sourceforge.net to contact the MZmine Development Team. </p>  <br> <h3>License information for MZmine</h3>  <p> This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. </p>  <p> This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. </p>  <p> You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. </p>  <br> <h3>Information of included third-party libraries</h3>  <p>TODO: update this section.</p>   </body> </html>";
+    public static final String ABOUT_TEXT_FILE = "AboutText.html";
 
     /**
      * Creates new form AboutDialog
      */
     public AboutDialog() {
 
-        super(MZmineCore.getDesktop().getMainFrame(), "About MZmine", true);
+        super(MZmineCore.getDesktop().getMainFrame(), "About MZmine 2", true);
+
+        // Read the contents of the about dialog
+        InputStream aboutTextStream = AboutDialog.class.getResourceAsStream(ABOUT_TEXT_FILE);
+        InputStreamReader isr = new InputStreamReader(aboutTextStream);
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuffer contentString = new StringBuffer();
+        try {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                contentString.append(line);
+            }
+        } catch (IOException e) {
+            // ignore
+        }
 
         JPanel buttonPanel = new JPanel();
         GUIUtils.addButtonInPanel(buttonPanel, "OK", this);
@@ -59,7 +75,7 @@ public class AboutDialog extends JDialog implements ActionListener {
         textPane.setBorder(new MatteBorder(new Insets(0, 10, 10, 10),
                 Color.white));
         textPane.setContentType("text/html");
-        textPane.setText(contentString);
+        textPane.setText(contentString.toString());
         textPane.setEditable(false);
         textPane.setCaretPosition(0);
 
