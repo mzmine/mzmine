4898fdc1d7209fd0d16a4822a3e6d1b6bfa728f8
Alejandro Villar Briones <avillar@oist.jp>
2008-05-01 02:34:35 +0000

    Complete implementation of module io running as a task.

--- rest/startMZmine.bat
+++ rest/startMZmine.bat
@@ -1,3 +1,3 @@
 @echo off
 cls
-java -Dj3d.rend=d3d -Djava.util.logging.config.file=conf/logging.properties -Xms512m -Xmx1384m -cp MZmine.jar net.sf.mzmine.main.MZmineClient
+java -Dj3d.rend=d3d -Djava.library.path=lib -Djava.util.logging.config.file=conf/logging.properties -Xms512m -Xmx1384m -cp MZmine.jar net.sf.mzmine.main.MZmineClient
