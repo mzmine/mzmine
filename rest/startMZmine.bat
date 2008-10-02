d0afabf78ac164fa2e013b17163b895abdb9c55d
Tomas Pluskal <plusik@gmail.com>
2008-10-02 05:00:05 +0000

    Removed default Direct3D rendering

--- rest/startMZmine.bat
+++ rest/startMZmine.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Dj3d.rend=d3d -Djava.util.logging.config.file=conf/logging.properties -Xms1024m -Xmx1384m -cp MZmine2.jar net.sf.mzmine.main.MZmineClient
+java -Djava.util.logging.config.file=conf/logging.properties -Xms1024m -Xmx1384m -cp MZmine2.jar net.sf.mzmine.main.MZmineClient
