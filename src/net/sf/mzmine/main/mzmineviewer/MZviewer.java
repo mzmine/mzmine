fc0b44776ab204355c3ae3220b3aef19a9e8cb37
Alejandro Villar Briones <avillar@oist.jp>
2009-01-21 03:29:55 +0000

    New mzmineviewer package. This contains the core classes to start MZmine as a simple viewer

--- src/net/sf/mzmine/main/mzmineviewer/MZviewer.java
+++ src/net/sf/mzmine/main/mzmineviewer/MZviewer.java
@@ -17,7 +17,7 @@
  * Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-package net.sf.mzmine.modules.lightviewer;
+package net.sf.mzmine.main.mzmineviewer;
 
 import java.util.Vector;
 import java.util.logging.Level;
