ef71b027ee7901b91d6e4d31bbbe23e784558fcf
Tomas Pluskal <plusik@gmail.com>
2009-09-02 04:39:31 +0000

    Fixed number formating of hh:mm:ss values

--- src/net/sf/mzmine/util/NumberFormatter.java
+++ src/net/sf/mzmine/util/NumberFormatter.java
@@ -36,131 +36,137 @@ import org.dom4j.Element;
  */
 public class NumberFormatter extends NumberFormat implements Cloneable {
 
-    public static final String TYPE_ELEMENT_NAME = "type";
-    public static final String PATTERN_ELEMENT_NAME = "pattern";
+	public static final String TYPE_ELEMENT_NAME = "type";
+	public static final String PATTERN_ELEMENT_NAME = "pattern";
 
-    public enum FormatterType {
-        TIME, NUMBER
-    }
+	public enum FormatterType {
+		TIME, NUMBER
+	}
 
-    private FormatterType embeddedFormatterType;
-    private Format embeddedFormatter;
+	private FormatterType embeddedFormatterType;
+	private Format embeddedFormatter;
 
-    public NumberFormatter(FormatterType type, String pattern) {
-        setFormat(type, pattern);
-    }
+	public NumberFormatter(FormatterType type, String pattern) {
+		setFormat(type, pattern);
+	}
 
-    public NumberFormatter(Element xmlElement) {
-        importFromXML(xmlElement);
-    }
+	public NumberFormatter(Element xmlElement) {
+		importFromXML(xmlElement);
+	}
 
-    public void setFormat(FormatterType type, String pattern) {
-        this.embeddedFormatterType = type;
-        switch (type) {
-        case TIME:
-            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-            // important for handling low values, otherwise in different time
-            // zones we may get to negative numbers
-            sdf.setTimeZone(TimeZone.getTimeZone("GMT+0:00"));
-            embeddedFormatter = sdf;
+	public void setFormat(FormatterType type, String pattern) {
+		this.embeddedFormatterType = type;
+		switch (type) {
+		case TIME:
 
-            break;
-        case NUMBER:
-            embeddedFormatter = new DecimalFormat(pattern);
-            break;
-        }
-    }
+			// We want to avoid the 12-hour format ('h') and use 24-hour format
+			// starting with 0 instead ('H')
+			pattern = pattern.replace('h', 'H');
 
-    public FormatterType getType() {
-        return embeddedFormatterType;
-    }
+			SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+			// important for handling low values, otherwise in different time
+			// zones we may get to negative numbers
+			sdf.setTimeZone(TimeZone.getTimeZone("GMT+0:00"));
+			embeddedFormatter = sdf;
 
-    public String getPattern() {
-        switch (embeddedFormatterType) {
-        case TIME:
-            return ((SimpleDateFormat) embeddedFormatter).toPattern();
-        case NUMBER:
-            return ((DecimalFormat) embeddedFormatter).toPattern();
-        }
-        return null;
-    }
+			break;
+		case NUMBER:
+			embeddedFormatter = new DecimalFormat(pattern);
+			break;
+		}
+	}
 
-    public void importFromXML(Element xmlElement) {
-        String pattern = xmlElement.elementText(PATTERN_ELEMENT_NAME);
-        FormatterType type = FormatterType.valueOf(xmlElement.elementText(TYPE_ELEMENT_NAME));
-        setFormat(type, pattern);
-    }
+	public FormatterType getType() {
+		return embeddedFormatterType;
+	}
 
-    public void exportToXML(Element xmlElement) {
-        Element typeElement = xmlElement.addElement(TYPE_ELEMENT_NAME);
-        typeElement.setText(embeddedFormatterType.toString());
-        Element patternElement = xmlElement.addElement(PATTERN_ELEMENT_NAME);
-        patternElement.setText(getPattern());
-    }
+	public String getPattern() {
+		switch (embeddedFormatterType) {
+		case TIME:
+			return ((SimpleDateFormat) embeddedFormatter).toPattern();
+		case NUMBER:
+			return ((DecimalFormat) embeddedFormatter).toPattern();
+		}
+		return null;
+	}
 
-    /**
-     * @see java.text.NumberFormat#format(double, java.lang.StringBuffer,
-     *      java.text.FieldPosition)
+	public void importFromXML(Element xmlElement) {
+		String pattern = xmlElement.elementText(PATTERN_ELEMENT_NAME);
+		FormatterType type = FormatterType.valueOf(xmlElement
+				.elementText(TYPE_ELEMENT_NAME));
+		setFormat(type, pattern);
+	}
+
+	public void exportToXML(Element xmlElement) {
+		Element typeElement = xmlElement.addElement(TYPE_ELEMENT_NAME);
+		typeElement.setText(embeddedFormatterType.toString());
+		Element patternElement = xmlElement.addElement(PATTERN_ELEMENT_NAME);
+		patternElement.setText(getPattern());
+	}
+
+	/**
+	 * @see java.text.NumberFormat#format(double, java.lang.StringBuffer,
+	 *      java.text.FieldPosition)
+	 */
+	public synchronized StringBuffer format(double arg0, StringBuffer arg1,
+			FieldPosition arg2) {
+
+		// conversion to msec
+		if (embeddedFormatterType == FormatterType.TIME)
+			arg0 *= 1000;
+
+		return embeddedFormatter.format(arg0, arg1, arg2);
+	}
+
+	/**
      */
-    public synchronized StringBuffer format(double arg0, StringBuffer arg1,
-            FieldPosition arg2) {
+	public synchronized StringBuffer format(Range range, StringBuffer arg1,
+			FieldPosition arg2) {
 
-        // conversion to msec
-        if (embeddedFormatterType == FormatterType.TIME)
-            arg0 *= 1000;
+		embeddedFormatter.format(range.getMin(), arg1, new FieldPosition(0));
+		arg1.append(" - ");
+		embeddedFormatter.format(range.getMax(), arg1, new FieldPosition(0));
+		return arg1;
+	}
 
-        return embeddedFormatter.format(arg0, arg1, arg2);
-    }
-    
-    /**
-     */
-    public synchronized StringBuffer format(Range range, StringBuffer arg1,
-            FieldPosition arg2) {
+	/**
+	 * @see java.text.NumberFormat#format(long, java.lang.StringBuffer,
+	 *      java.text.FieldPosition)
+	 */
+	public synchronized StringBuffer format(long arg0, StringBuffer arg1,
+			FieldPosition arg2) {
 
-        embeddedFormatter.format(range.getMin(), arg1, new FieldPosition(0));
-        arg1.append(" - ");
-        embeddedFormatter.format(range.getMax(), arg1, new FieldPosition(0));
-        return arg1;
-    }
+		// conversion to msec
+		if (embeddedFormatterType == FormatterType.TIME)
+			arg0 *= 1000;
 
-    /**
-     * @see java.text.NumberFormat#format(long, java.lang.StringBuffer,
-     *      java.text.FieldPosition)
-     */
-    public synchronized StringBuffer format(long arg0, StringBuffer arg1,
-            FieldPosition arg2) {
+		return embeddedFormatter.format(arg0, arg1, arg2);
+	}
 
-        // conversion to msec
-        if (embeddedFormatterType == FormatterType.TIME)
-            arg0 *= 1000;
+	/**
+	 * @see java.text.NumberFormat#parse(java.lang.String,
+	 *      java.text.ParsePosition)
+	 */
+	public synchronized Number parse(String str, ParsePosition pos) {
+		try {
+			switch (embeddedFormatterType) {
+			case TIME:
+				SimpleDateFormat sdf = (SimpleDateFormat) embeddedFormatter;
+				return ((sdf.parse(str, pos).getTime()) / 1000l);
+			case NUMBER:
+				DecimalFormat df = (DecimalFormat) embeddedFormatter;
+				return df.parse(str, pos);
+			}
+		} catch (Exception e) {
+		}
+		return null;
+	}
 
-        return embeddedFormatter.format(arg0, arg1, arg2);
-    }
-
-    /**
-     * @see java.text.NumberFormat#parse(java.lang.String,
-     *      java.text.ParsePosition)
-     */
-    public synchronized Number parse(String str, ParsePosition pos) {
-        try {
-            switch (embeddedFormatterType) {
-            case TIME:
-                SimpleDateFormat sdf = (SimpleDateFormat) embeddedFormatter;
-                return ((sdf.parse(str, pos).getTime()) / 1000l);
-            case NUMBER:
-                DecimalFormat df = (DecimalFormat) embeddedFormatter;
-                return df.parse(str, pos);
-            }
-        } catch (Exception e) {
-        }
-        return null;
-    }
-
-    /**
-     * @see java.text.NumberFormat#clone()
-     */
-    public NumberFormatter clone() {
-        return new NumberFormatter(embeddedFormatterType, getPattern());
-    }
+	/**
+	 * @see java.text.NumberFormat#clone()
+	 */
+	public NumberFormatter clone() {
+		return new NumberFormatter(embeddedFormatterType, getPattern());
+	}
 
 }
