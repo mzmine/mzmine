681061d27dde3f534b4383bc6975fecdadb7a1e3
Tomas Pluskal <plusik@gmail.com>
2010-06-08 05:38:58 +0000

    Marco's patch

--- src/net/sf/mzmine/modules/visualization/threed/ThreeDVisualizer.java
+++ src/net/sf/mzmine/modules/visualization/threed/ThreeDVisualizer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2006-2010 The MZmine 2 Development Team
- * 
+ *
  * This file is part of MZmine 2.
- * 
+ *
  * MZmine 2 is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * MZmine 2; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA 02110-1301 USA
@@ -145,10 +145,17 @@ public class ThreeDVisualizer implements MZmineModule, ActionListener {
 		int mzRes = (Integer) myInstance.parameters
 				.getParameterValue(ThreeDVisualizerParameters.mzResolution);
 
-		ThreeDVisualizerWindow newWindow = new ThreeDVisualizerWindow(dataFile,
-				msLevel, rtRange, rtRes, mzRange, mzRes);
+		try {
+			ThreeDVisualizerWindow newWindow = new ThreeDVisualizerWindow(
+					dataFile, msLevel, rtRange, rtRes, mzRange, mzRes);
+			MZmineCore.getDesktop().addInternalFrame(newWindow);
+		} catch (UnsatisfiedLinkError e) {
+			MZmineCore
+					.getDesktop()
+					.displayErrorMessage(
+							"It seems that Java3D is not installed. Please install Java3D and try again.");
+		}
 
-		MZmineCore.getDesktop().addInternalFrame(newWindow);
 
 	}
 
