bf736197e008a2a102b30099f9318ecfe146feba
Tomas Pluskal <plusik@gmail.com>
2012-02-02 03:03:34 +0000

    Fixed toString in Range class

--- src/net/sf/mzmine/util/Range.java
+++ src/net/sf/mzmine/util/Range.java
@@ -209,7 +209,7 @@ public class Range {
 	 * 
 	 * @return This range as string
 	 */
-	public String getName() {
+	public String toString() {
 		return String.valueOf(min) + "~" + String.valueOf(max);
 	}
 
