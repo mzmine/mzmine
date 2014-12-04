54a17aadc24d4a95079aa17286678c2fd51f235d
Tomas Pluskal <plusik@gmail.com>
2014-12-04 06:41:18 +0000

    Added a logger.info() call to update the status bar when tasks are finished

--- src/main/java/net/sf/mzmine/taskcontrol/impl/WorkerThread.java
+++ src/main/java/net/sf/mzmine/taskcontrol/impl/WorkerThread.java
@@ -33,82 +33,91 @@ import net.sf.mzmine.util.ExceptionUtils;
  */
 class WorkerThread extends Thread {
 
-	private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Logger logger = Logger.getLogger(this.getClass().getName());
 
-	private WrappedTask wrappedTask;
-	private boolean finished = false;
+    private WrappedTask wrappedTask;
+    private boolean finished = false;
 
-	WorkerThread(WrappedTask wrappedTask) {
-		super("Thread executing task " + wrappedTask);
-		this.wrappedTask = wrappedTask;
-		wrappedTask.assignTo(this);
+    WorkerThread(WrappedTask wrappedTask) {
+	super("Thread executing task " + wrappedTask);
+	this.wrappedTask = wrappedTask;
+	wrappedTask.assignTo(this);
+    }
+
+    /**
+     * @see java.lang.Runnable#run()
+     */
+    public void run() {
+
+	Task actualTask = wrappedTask.getActualTask();
+
+	try {
+
+	    // Log the start (INFO level events go to the Status bar, too)
+	    logger.info("Starting processing of task "
+		    + actualTask.getTaskDescription());
+
+	    // Process the actual task
+	    actualTask.run();
+
+	    // Check if task finished with an error
+	    if (actualTask.getStatus() == TaskStatus.ERROR) {
+
+		String errorMsg = actualTask.getErrorMessage();
+		if (errorMsg == null)
+		    errorMsg = "Unspecified error";
+
+		// Log the error
+		logger.severe("Error of task "
+			+ actualTask.getTaskDescription() + ": " + errorMsg);
+
+		MZmineCore.getDesktop().displayErrorMessage(
+			"Error of task " + actualTask.getTaskDescription(),
+			errorMsg);
+	    } else {
+		// Log the finish
+		logger.info("Processing of task "
+			+ actualTask.getTaskDescription() + " done, status "
+			+ actualTask.getStatus());
+	    }
+
+	    /*
+	     * This is important to allow the garbage collector to remove the
+	     * task, while keeping the task description in the
+	     * "Tasks in progress" window
+	     */
+	    wrappedTask.removeTaskReference();
+
+	} catch (Throwable e) {
+
+	    /*
+	     * This should never happen, it means the task did not handle its
+	     * exception properly, or there was some severe error, like
+	     * OutOfMemoryError
+	     */
+
+	    logger.log(Level.SEVERE,
+		    "Unhandled exception " + e + " while processing task "
+			    + actualTask.getTaskDescription(), e);
+
+	    e.printStackTrace();
+
+	    MZmineCore.getDesktop().displayErrorMessage(
+		    "Unhandled exception in task "
+			    + actualTask.getTaskDescription() + ": "
+			    + ExceptionUtils.exceptionToString(e));
+
 	}
 
-	/**
-	 * @see java.lang.Runnable#run()
+	/*
+	 * Mark this thread as finished
 	 */
-	public void run() {
+	finished = true;
 
-		Task actualTask = wrappedTask.getActualTask();
+    }
 
-		logger.finest("Starting processing of task: "
-				+ actualTask.getTaskDescription());
-
-		try {
-
-			// Process the actual task
-			actualTask.run();
-
-			// Check if task finished with an error
-			if (actualTask.getStatus() == TaskStatus.ERROR) {
-				logger.severe("Task error: " + actualTask.getErrorMessage());
-
-				String errorMsg = actualTask.getErrorMessage();
-				if (errorMsg == null)
-					errorMsg = "Unspecified error";
-
-				MZmineCore.getDesktop().displayErrorMessage(
-						"Error of task " + actualTask.getTaskDescription(),
-						errorMsg);
-			}
-
-			/*
-			 * This is important to allow the garbage collector to remove the
-			 * task, while keeping the task description in the
-			 * "Tasks in progress" window
-			 */
-			wrappedTask.removeTaskReference();
-
-		} catch (Throwable e) {
-
-			/*
-			 * This should never happen, it means the task did not handle its
-			 * exception properly, or there was some severe error, like
-			 * OutOfMemoryError
-			 */
-
-			logger.log(Level.SEVERE,
-					"Unhandled exception " + e + " while processing task "
-							+ actualTask.getTaskDescription(), e);
-
-			e.printStackTrace();
-
-			MZmineCore.getDesktop().displayErrorMessage(
-					"Unhandled exception in task "
-							+ actualTask.getTaskDescription() + ": "
-							+ ExceptionUtils.exceptionToString(e));
-
-		}
-
-		/*
-		 * Mark this thread as finished
-		 */
-		finished = true;
-
-	}
-
-	boolean isFinished() {
-		return finished;
-	}
+    boolean isFinished() {
+	return finished;
+    }
 
 }
