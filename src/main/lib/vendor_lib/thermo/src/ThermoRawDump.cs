2377a4a39f90ea1f02a7aa12d913280dbacef54a
Tomas Pluskal <plusik@gmail.com>
2015-02-05 01:48:55 +0000

    Added handling of exceptions to the Thermo reader
    Former-commit-id: f231cbf6d70900dc263141e898bf9cb7f14644c8

--- src/main/lib/vendor_lib/thermo/src/ThermoRawDump.cs
+++ src/main/lib/vendor_lib/thermo/src/ThermoRawDump.cs
@@ -70,166 +70,174 @@ namespace ThermoRawDump
         static void Main(string[] args)
         {
 
-            if (args.Length != 1)
-            {
-                Console.Write("ERROR: This program accepts exactly 1 argument: a RAW file path\n");
-                Environment.Exit(1);
-            }
-
-            string filename = args[0];
-
-            // Test whether the file exists, because the rawFile.Open()
-            // function does not check that
-            if (!File.Exists(filename))
-            {
-                Console.Write("ERROR: Unable to read RAW file " + filename + "\n");
-                Environment.Exit(1);
-            }
-
-            // Open the raw file
-            IXRawfile4 rawFile = (IXRawfile4)new MSFileReader_XRawfile();
-            rawFile.Open(filename);
-
-            // Set the controller number, otherwise no data can be accessed
-            int nControllerType = 0; // 0 == mass spec device
-            int nContorllerNumber = 1; // first MS device
-            rawFile.SetCurrentController(nControllerType, nContorllerNumber);
-
-            // Number of scans
-            int totalNumScans = 0;
-            rawFile.GetNumSpectra(ref totalNumScans);
-            Console.Write("NUMBER OF SCANS: " + totalNumScans + "\n");
-
-            // Number of first and last scan
-            int firstScanNumber = 0, lastScanNumber = 0;
-            rawFile.GetFirstSpectrumNumber(ref firstScanNumber);
-            rawFile.GetLastSpectrumNumber(ref lastScanNumber);
-
-            // Open the stdout stream and prepare a buffer
-            Stream stdout = Console.OpenStandardOutput();
-            byte[] byteBuffer = new byte[1000000];
-
-            for (int curScanNum = firstScanNumber; curScanNum <= lastScanNumber; curScanNum++)
+            try
             {
 
-                // Scan number
-                Console.Write("SCAN NUMBER: " + curScanNum + "\n");
-
-                // Scan filter line
-                string scanFilter = null;
-                rawFile.GetFilterForScanNum(curScanNum, ref scanFilter);
-                if (scanFilter == null)
+                if (args.Length != 1)
                 {
-                    Console.Write("ERROR: Could not extract scan filter line for scan #" + curScanNum + "\n");
+                    Console.Write("ERROR: This program accepts exactly 1 argument: a RAW file path\n");
                     Environment.Exit(1);
                 }
-                Console.Write("SCAN ID: " + scanFilter + "\n");
 
-                // Polarity
-                char polarity;
-                if (scanFilter.Contains(" - ")) polarity = '-';
-                else if (scanFilter.Contains(" + ")) polarity = '+';
-                else polarity = '?';
-                Console.Write("POLARITY: " + polarity + "\n");
+                string filename = args[0];
 
-                // MS level
-                int msLevel = -1;
-                rawFile.GetMSOrderForScanNum(curScanNum, ref msLevel);
-                if (msLevel < -1) msLevel = 2; // e.g., neutral gain scan returns -3, see MSFileReader doc
-                if (msLevel < 1) msLevel = 1; // e.g., parent scan scan returns -1, see MSFileReader doc
-                Console.Write("MS LEVEL: " + msLevel + "\n");
-
-                int numDataPoints = -1; // points in both the m/z and intensity arrays
-                double retentionTimeInMinutes = -1;
-                double minObservedMZ = -1;
-                double maxObservedMZ = -1;
-                double totalIonCurrent = -1;
-                double basePeakMZ = -1;
-                double basePeakIntensity = -1;
-                int channel = 0; // unused
-                int uniformTime = 0; // unused
-                double frequency = 0; // unused
-                rawFile.GetScanHeaderInfoForScanNum(curScanNum,
-                                                    ref numDataPoints,
-                                                    ref retentionTimeInMinutes,
-                                                    ref minObservedMZ,
-                                                    ref maxObservedMZ,
-                                                    ref totalIonCurrent,
-                                                    ref basePeakMZ,
-                                                    ref basePeakIntensity,
-                                                    ref channel, // unused
-                                                    ref uniformTime, // unused
-                                                    ref frequency // unused
-                                                    );
-
-                // Retention time
-                Console.Write("RETENTION TIME: " + retentionTimeInMinutes + "\n");
-
-                // m/z range
-                Console.Write("MZ RANGE: " + minObservedMZ + " - " + maxObservedMZ + "\n");
-
-                // Precursor
-                if (msLevel > 1)
+                // Test whether the file exists, because the rawFile.Open()
+                // function does not check that
+                if (!File.Exists(filename))
                 {
-                    object precursorMz = null;
-                    object precursorCharge = null;
-                    rawFile.GetTrailerExtraValueForScanNum(curScanNum, "Monoisotopic M/Z:", ref precursorMz);
-                    rawFile.GetTrailerExtraValueForScanNum(curScanNum, "Charge State:", ref precursorCharge);
-                    Console.Write("PRECURSOR: " + precursorMz + " " + precursorCharge + "\n");
+                    Console.Write("ERROR: Unable to read RAW file " + filename + "\n");
+                    Environment.Exit(1);
                 }
 
-                // Scan raw data points
-                int arraySize = -1;
-                object rawData = null; // rawData wil come as Double[,]
-                object peakFlags = null;
-                int scanNum = curScanNum;
-                string szFilter = null;        // No filter
-                int intensityCutoffType = 0;        // No cutoff
-                int intensityCutoffValue = 0;    // No cutoff
-                int maxNumberOfPeaks = 0;        // 0 : return all data peaks
-                double centroidPeakWidth = 0;        // No centroiding
-                int centroidThisScan = 0; // No centroiding
+                // Open the raw file
+                IXRawfile4 rawFile = (IXRawfile4)new MSFileReader_XRawfile();
+                rawFile.Open(filename);
 
-                rawFile.GetMassListFromScanNum(
-                                                ref scanNum,
-                                                szFilter,             // filter
-                                                intensityCutoffType, // intensityCutoffType
-                                                intensityCutoffValue, // intensityCutoffValue
-                                                maxNumberOfPeaks,     // maxNumberOfPeaks
-                                                centroidThisScan,        // centroid result?
-                                                ref centroidPeakWidth,    // centroidingPeakWidth
-                                                ref rawData,        // daw data
-                                                ref peakFlags,        // peakFlags
-                                                ref arraySize);        // array size
+                // Set the controller number, otherwise no data can be accessed
+                int nControllerType = 0; // 0 == mass spec device
+                int nContorllerNumber = 1; // first MS device
+                rawFile.SetCurrentController(nControllerType, nContorllerNumber);
 
-                // Print data points
-                Console.Write("MASS VALUES: " + arraySize + " x " + sizeof(double) + " BYTES\n");
+                // Number of scans
+                int totalNumScans = 0;
+                rawFile.GetNumSpectra(ref totalNumScans);
+                Console.Write("NUMBER OF SCANS: " + totalNumScans + "\n");
+
+                // Number of first and last scan
+                int firstScanNumber = 0, lastScanNumber = 0;
+                rawFile.GetFirstSpectrumNumber(ref firstScanNumber);
+                rawFile.GetLastSpectrumNumber(ref lastScanNumber);
+
+                // Open the stdout stream and prepare a buffer
+                Stream stdout = Console.OpenStandardOutput();
+                byte[] byteBuffer = new byte[1000000];
+
+                for (int curScanNum = firstScanNumber; curScanNum <= lastScanNumber; curScanNum++)
+                {
+
+                    // Scan number
+                    Console.Write("SCAN NUMBER: " + curScanNum + "\n");
+
+                    // Scan filter line
+                    string scanFilter = null;
+                    rawFile.GetFilterForScanNum(curScanNum, ref scanFilter);
+                    if (scanFilter == null)
+                    {
+                        Console.Write("ERROR: Could not extract scan filter line for scan #" + curScanNum + "\n");
+                        Environment.Exit(1);
+                    }
+                    Console.Write("SCAN ID: " + scanFilter + "\n");
+
+                    // Polarity
+                    char polarity;
+                    if (scanFilter.Contains(" - ")) polarity = '-';
+                    else if (scanFilter.Contains(" + ")) polarity = '+';
+                    else polarity = '?';
+                    Console.Write("POLARITY: " + polarity + "\n");
+
+                    // MS level
+                    int msLevel = -1;
+                    rawFile.GetMSOrderForScanNum(curScanNum, ref msLevel);
+                    if (msLevel < -1) msLevel = 2; // e.g., neutral gain scan returns -3, see MSFileReader doc
+                    if (msLevel < 1) msLevel = 1; // e.g., parent scan scan returns -1, see MSFileReader doc
+                    Console.Write("MS LEVEL: " + msLevel + "\n");
+
+                    int numDataPoints = -1; // points in both the m/z and intensity arrays
+                    double retentionTimeInMinutes = -1;
+                    double minObservedMZ = -1;
+                    double maxObservedMZ = -1;
+                    double totalIonCurrent = -1;
+                    double basePeakMZ = -1;
+                    double basePeakIntensity = -1;
+                    int channel = 0; // unused
+                    int uniformTime = 0; // unused
+                    double frequency = 0; // unused
+                    rawFile.GetScanHeaderInfoForScanNum(curScanNum,
+                                                        ref numDataPoints,
+                                                        ref retentionTimeInMinutes,
+                                                        ref minObservedMZ,
+                                                        ref maxObservedMZ,
+                                                        ref totalIonCurrent,
+                                                        ref basePeakMZ,
+                                                        ref basePeakIntensity,
+                                                        ref channel, // unused
+                                                        ref uniformTime, // unused
+                                                        ref frequency // unused
+                                                        );
+
+                    // Retention time
+                    Console.Write("RETENTION TIME: " + retentionTimeInMinutes + "\n");
+
+                    // m/z range
+                    Console.Write("MZ RANGE: " + minObservedMZ + " - " + maxObservedMZ + "\n");
+
+                    // Precursor
+                    if (msLevel > 1)
+                    {
+                        object precursorMz = null;
+                        object precursorCharge = null;
+                        rawFile.GetTrailerExtraValueForScanNum(curScanNum, "Monoisotopic M/Z:", ref precursorMz);
+                        rawFile.GetTrailerExtraValueForScanNum(curScanNum, "Charge State:", ref precursorCharge);
+                        Console.Write("PRECURSOR: " + precursorMz + " " + precursorCharge + "\n");
+                    }
+
+                    // Scan raw data points
+                    int arraySize = -1;
+                    object rawData = null; // rawData wil come as Double[,]
+                    object peakFlags = null;
+                    int scanNum = curScanNum;
+                    string szFilter = null;        // No filter
+                    int intensityCutoffType = 0;        // No cutoff
+                    int intensityCutoffValue = 0;    // No cutoff
+                    int maxNumberOfPeaks = 0;        // 0 : return all data peaks
+                    double centroidPeakWidth = 0;        // No centroiding
+                    int centroidThisScan = 0; // No centroiding
+
+                    rawFile.GetMassListFromScanNum(
+                                                    ref scanNum,
+                                                    szFilter,             // filter
+                                                    intensityCutoffType, // intensityCutoffType
+                                                    intensityCutoffValue, // intensityCutoffValue
+                                                    maxNumberOfPeaks,     // maxNumberOfPeaks
+                                                    centroidThisScan,        // centroid result?
+                                                    ref centroidPeakWidth,    // centroidingPeakWidth
+                                                    ref rawData,        // daw data
+                                                    ref peakFlags,        // peakFlags
+                                                    ref arraySize);        // array size
+
+                    // Print data points
+                    Console.Write("MASS VALUES: " + arraySize + " x " + sizeof(double) + " BYTES\n");
 
 
-                // Calculate the byte size of rawData.
-                // rawData contains arraySize of doubles for mz values,
-                // followed by arraySize of doubles for intensity values
-                int numOfBytes = sizeof(double) * arraySize;
+                    // Calculate the byte size of rawData.
+                    // rawData contains arraySize of doubles for mz values,
+                    // followed by arraySize of doubles for intensity values
+                    int numOfBytes = sizeof(double) * arraySize;
 
-                // Make sure our buffer is big enough
-                if (byteBuffer.Length < numOfBytes)
-                    byteBuffer = new byte[numOfBytes * 2];
+                    // Make sure our buffer is big enough
+                    if (byteBuffer.Length < numOfBytes)
+                        byteBuffer = new byte[numOfBytes * 2];
 
-                // Dump the binary data
-                Buffer.BlockCopy((Array)rawData, 0, byteBuffer, 0, numOfBytes);
-                stdout.Write(byteBuffer, 0, numOfBytes);
+                    // Dump the binary data
+                    Buffer.BlockCopy((Array)rawData, 0, byteBuffer, 0, numOfBytes);
+                    stdout.Write(byteBuffer, 0, numOfBytes);
 
-                Console.Write("INTENSITY VALUES: " + arraySize + " x " + sizeof(double) + " BYTES\n");
+                    Console.Write("INTENSITY VALUES: " + arraySize + " x " + sizeof(double) + " BYTES\n");
 
-                // Dump the binary data
-                Buffer.BlockCopy((Array)rawData, numOfBytes, byteBuffer, 0, numOfBytes);
-                stdout.Write(byteBuffer, 0, numOfBytes);
+                    // Dump the binary data
+                    Buffer.BlockCopy((Array)rawData, numOfBytes, byteBuffer, 0, numOfBytes);
+                    stdout.Write(byteBuffer, 0, numOfBytes);
 
+
+                }
+
+                rawFile.Close();
 
             }
-
-            rawFile.Close();
-
+            catch (Exception e)
+            {
+                Console.Write("ERROR: " + e.ToString() + "\n");
+            }
         }
 
     }
