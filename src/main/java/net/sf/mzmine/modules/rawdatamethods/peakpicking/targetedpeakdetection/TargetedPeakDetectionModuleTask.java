3567e54414a588535382ec0a470adaefbb03cf83
Thomas F. Dyrlund <thomas@dyrlund.dk>
2015-03-31 14:37:14 +0200

    Removed import java.util.Arrays;

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/targetedpeakdetection/TargetedPeakDetectionModuleTask.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/peakpicking/targetedpeakdetection/TargetedPeakDetectionModuleTask.java
@@ -21,7 +21,6 @@ package net.sf.mzmine.modules.rawdatamethods.peakpicking.targetedpeakdetection;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
