0b1c7855e2baecf4f92bda6784c812efd7ba3869
Tomáš Pluskal <plusik@gmail.com>
2015-04-09 09:45:20 +0900

    Fixed the path to the version number file

--- src/main/java/net/sf/mzmine/main/MZmineCore.java
+++ src/main/java/net/sf/mzmine/main/MZmineCore.java
@@ -287,7 +287,7 @@ public final class MZmineCore {
 	try {
 	    ClassLoader myClassLoader = MZmineCore.class.getClassLoader();
 	    InputStream inStream = myClassLoader
-		    .getResourceAsStream("META-INF/maven/net.sf.mzmine/MZmine/pom.properties");
+		    .getResourceAsStream("META-INF/maven/net.sf.mzmine/mzmine/pom.properties");
 	    if (inStream == null)
 		return "0.0";
 	    Properties properties = new Properties();
