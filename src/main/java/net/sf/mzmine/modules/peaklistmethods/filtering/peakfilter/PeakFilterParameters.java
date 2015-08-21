a7fac4281c394a2a862b6d32eb2728e6b6a9519a
Thomas F. Dyrlund <thomas@dyrlund.dk>
2015-08-21 10:25:23 +0200

    Added a peak filter module which allows filtering of individual peaks in a peak list

---
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/filtering/peakfilter/PeakFilterParameters.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2006-2015 The MZmine 2 Development Team
+ * 
+ * This file is part of MZmine 2.
+ * 
+ * MZmine 2 is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine 2; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.peaklistmethods.filtering.peakfilter;
+
+import java.awt.Window;
+
+import net.sf.mzmine.main.MZmineCore;
+import net.sf.mzmine.parameters.Parameter;
+import net.sf.mzmine.parameters.UserParameter;
+import net.sf.mzmine.parameters.dialogs.ParameterSetupDialog;
+import net.sf.mzmine.parameters.impl.SimpleParameterSet;
+import net.sf.mzmine.parameters.parametertypes.BooleanParameter;
+import net.sf.mzmine.parameters.parametertypes.OptionalParameter;
+import net.sf.mzmine.parameters.parametertypes.StringParameter;
+import net.sf.mzmine.parameters.parametertypes.ranges.IntRangeParameter;
+import net.sf.mzmine.parameters.parametertypes.ranges.DoubleRangeParameter;
+import net.sf.mzmine.parameters.parametertypes.selectors.PeakListsParameter;
+import net.sf.mzmine.util.ExitCode;
+
+import com.google.common.collect.Range;
+
+public class PeakFilterParameters extends SimpleParameterSet {
+
+    public static final PeakListsParameter PEAK_LISTS = new PeakListsParameter();
+
+    public static final StringParameter SUFFIX = new StringParameter(
+            "Name suffix", "Suffix to be added to peak list name", "filtered");
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_DURATION = new OptionalParameter<>(
+            new DoubleRangeParameter("Duration",
+                    "Permissible range of peak durations", MZmineCore
+                            .getConfiguration().getRTFormat(), Range.closed(
+                            0.0, 10.0)));
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_AREA = new OptionalParameter<>(
+            new DoubleRangeParameter("Area",
+                    "Permissible range of peak area", MZmineCore
+                            .getConfiguration().getIntensityFormat(),
+                    Range.closed(0.0, 10000000.0)));
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_HEIGHT = new OptionalParameter<>(
+            new DoubleRangeParameter("Height",
+                    "Permissible range of peak height", MZmineCore
+                            .getConfiguration().getIntensityFormat(),
+                    Range.closed(0.0, 10000000.0)));
+
+    public static final OptionalParameter<IntRangeParameter> PEAK_DATAPOINTS = new OptionalParameter<>(
+            new IntRangeParameter(
+                    "# data points",
+                    "Permissible range of the number of data points over a peak",
+                    false, Range.closed(8, 30)));
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_FWHM = new OptionalParameter<>(
+            new DoubleRangeParameter(
+                    "FWHM",
+                    "Permissible range of the full width half minimum (FWHM) for a peak",
+                    MZmineCore.getConfiguration().getRTFormat(), Range.closed(
+                            0.0, 2.0)));
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_TAILINGFACTOR = new OptionalParameter<>(
+            new DoubleRangeParameter("Tailing factor",
+                    "Permissible range of the tailing factor for a peak",
+                    MZmineCore.getConfiguration().getRTFormat(), Range.closed(
+                            0.5, 2.0)));
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_ASYMMETRYFACTOR = new OptionalParameter<>(
+            new DoubleRangeParameter("Asymmetry factor",
+                    "Permissible range of the asymmetry factor for a peak",
+                    MZmineCore.getConfiguration().getRTFormat(), Range.closed(
+                            0.5, 2.0)));
+
+    public static final BooleanParameter AUTO_REMOVE = new BooleanParameter(
+            "Remove source peak list after filtering",
+            "If checked, the original peak list will be removed leaving only the filtered version");
+
+    public PeakFilterParameters() {
+        super(new Parameter[] { PEAK_LISTS, SUFFIX, PEAK_DURATION, PEAK_AREA,
+                PEAK_HEIGHT, PEAK_DATAPOINTS, PEAK_FWHM, PEAK_TAILINGFACTOR,
+                PEAK_ASYMMETRYFACTOR, AUTO_REMOVE });
+    }
+
+    @Override
+    public ExitCode showSetupDialog(Window parent, boolean valueCheckRequired) {
+
+        // Update the parameter choices
+        UserParameter<?, ?> newChoices[] = MZmineCore.getProjectManager()
+                .getCurrentProject().getParameters();
+        String[] choices;
+        if (newChoices == null || newChoices.length == 0) {
+            choices = new String[1];
+            choices[0] = "No parameters defined";
+        } else {
+            choices = new String[newChoices.length + 1];
+            choices[0] = "Ignore groups";
+            for (int i = 0; i < newChoices.length; i++) {
+                choices[i + 1] = "Filtering by " + newChoices[i].getName();
+            }
+        }
+
+        ParameterSetupDialog dialog = new ParameterSetupDialog(parent,
+                valueCheckRequired, this);
+        dialog.setVisible(true);
+        return dialog.getExitCode();
+    }
+}
---
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/filtering/peakfilter/PeakFilterTask.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright 2006-2015 The MZmine 2 Development Team
+ * 
+ * This file is part of MZmine 2.
+ * 
+ * MZmine 2 is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine 2; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.peaklistmethods.filtering.peakfilter;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.sf.mzmine.datamodel.Feature;
+import net.sf.mzmine.datamodel.MZmineProject;
+import net.sf.mzmine.datamodel.PeakList;
+import net.sf.mzmine.datamodel.PeakListRow;
+import net.sf.mzmine.datamodel.RawDataFile;
+import net.sf.mzmine.datamodel.impl.SimpleFeature;
+import net.sf.mzmine.datamodel.impl.SimplePeakList;
+import net.sf.mzmine.datamodel.impl.SimplePeakListRow;
+import net.sf.mzmine.modules.peaklistmethods.filtering.rowsfilter.RowsFilterParameters;
+import net.sf.mzmine.parameters.ParameterSet;
+import net.sf.mzmine.taskcontrol.AbstractTask;
+import net.sf.mzmine.taskcontrol.TaskStatus;
+import net.sf.mzmine.util.PeakUtils;
+
+import com.google.common.collect.Range;
+
+/**
+ * Filters out peaks from peak list.
+ */
+public class PeakFilterTask extends AbstractTask {
+
+    // Logger
+    private static final Logger LOG = Logger.getLogger(PeakFilterTask.class
+            .getName());
+
+    // Peak lists
+    private final MZmineProject project;
+    private final PeakList origPeakList;
+    private PeakList filteredPeakList;
+
+    // Processed rows counter
+    private int processedRows, totalRows;
+
+    // Parameters
+    private final ParameterSet parameters;
+
+    /**
+     * Create the task.
+     *
+     * @param list
+     *            peak list to process.
+     * @param parameterSet
+     *            task parameters.
+     */
+    public PeakFilterTask(final MZmineProject project, final PeakList list,
+            final ParameterSet parameterSet) {
+
+        // Initialize
+        this.project = project;
+        parameters = parameterSet;
+        origPeakList = list;
+        filteredPeakList = null;
+        processedRows = 0;
+        totalRows = 0;
+    }
+
+    @Override
+    public double getFinishedPercentage() {
+        return totalRows == 0 ? 0.0 : (double) processedRows
+                / (double) totalRows;
+    }
+
+    @Override
+    public String getTaskDescription() {
+        return "Filtering peak list";
+    }
+
+    @Override
+    public void run() {
+
+        if (isCanceled()) {
+            return;
+        }
+
+        try {
+            setStatus(TaskStatus.PROCESSING);
+            LOG.info("Filtering peak list");
+
+            // Filter the peak list
+            filteredPeakList = filterPeakList(origPeakList);
+
+            if (!isCanceled()) {
+
+                // Add new peaklist to the project
+                project.addPeakList(filteredPeakList);
+
+                // Remove the original peaklist if requested
+                if (parameters.getParameter(PeakFilterParameters.AUTO_REMOVE)
+                        .getValue()) {
+                    project.removePeakList(origPeakList);
+                }
+
+                setStatus(TaskStatus.FINISHED);
+                LOG.info("Finished peak list filter");
+            }
+        } catch (Throwable t) {
+
+            setErrorMessage(t.getMessage());
+            setStatus(TaskStatus.ERROR);
+            LOG.log(Level.SEVERE, "Peak list filter error", t);
+        }
+
+    }
+
+    /**
+     * Filter the peak list.
+     *
+     * @param peakList
+     *            peak list to filter.
+     * @return a new peak list with entries of the original peak list that pass
+     *         the filtering.
+     */
+    private PeakList filterPeakList(final PeakList peakList) {
+
+        // Make a copy of the peakList
+        final PeakList newPeakList = new SimplePeakList(peakList.getName()
+                + ' '
+                + parameters.getParameter(RowsFilterParameters.SUFFIX)
+                        .getValue(), peakList.getRawDataFiles());
+
+        // Get parameters - which filters are active
+        final boolean filterByDuration = parameters.getParameter(
+                PeakFilterParameters.PEAK_DURATION).getValue();
+        final boolean filterByArea = parameters.getParameter(
+                PeakFilterParameters.PEAK_AREA).getValue();
+        final boolean filterByHeight = parameters.getParameter(
+                PeakFilterParameters.PEAK_HEIGHT).getValue();
+        final boolean filterByDatapoints = parameters.getParameter(
+                PeakFilterParameters.PEAK_DATAPOINTS).getValue();
+        final boolean filterByFWHM = parameters.getParameter(
+                PeakFilterParameters.PEAK_FWHM).getValue();
+        final boolean filterByTailingFactor = parameters.getParameter(
+                PeakFilterParameters.PEAK_TAILINGFACTOR).getValue();
+        final boolean filterByAsymmetryFactor = parameters.getParameter(
+                PeakFilterParameters.PEAK_ASYMMETRYFACTOR).getValue();
+
+        // Loop through all rows in peak list
+        final PeakListRow[] rows = peakList.getRows();
+        totalRows = rows.length;
+        for (processedRows = 0; !isCanceled() && processedRows < totalRows; processedRows++) {
+            final PeakListRow row = rows[processedRows];
+            final RawDataFile[] rawdatafiles = row.getRawDataFiles();
+            int totalRawDataFiles = rawdatafiles.length;
+            boolean[] keepPeak = new boolean[totalRawDataFiles];
+
+            for (int i = 0; i < totalRawDataFiles; i++) {
+                // Peak values
+                keepPeak[i] = true;
+                final Feature peak = row.getPeak(rawdatafiles[i]);
+                final double peakDuration = peak.getRawDataPointsRTRange()
+                        .upperEndpoint()
+                        - peak.getRawDataPointsRTRange().lowerEndpoint();
+                final double peakArea = peak.getArea();
+                final double peakHeight = peak.getHeight();
+                final int peakDatapoints = peak.getScanNumbers().length;
+                final double peakFWHM = peak.getFWHM();
+                final double peakTailingFactor = peak.getTailingFactor();
+                final double peakAsymmetryFactor = peak.getAsymmetryFactor();
+
+                // Check Duration
+                if (filterByDuration) {
+                    final Range<Double> durationRange = parameters
+                            .getParameter(PeakFilterParameters.PEAK_DURATION)
+                            .getEmbeddedParameter().getValue();
+                    if (!durationRange.contains(peakDuration)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check Area
+                if (filterByArea) {
+                    final Range<Double> areaRange = parameters
+                            .getParameter(PeakFilterParameters.PEAK_AREA)
+                            .getEmbeddedParameter().getValue();
+                    if (!areaRange.contains(peakArea)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check Height
+                if (filterByHeight) {
+                    final Range<Double> heightRange = parameters
+                            .getParameter(PeakFilterParameters.PEAK_HEIGHT)
+                            .getEmbeddedParameter().getValue();
+                    if (!heightRange.contains(peakHeight)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check # Data Points
+                if (filterByDatapoints) {
+                    final Range<Integer> datapointsRange = parameters
+                            .getParameter(PeakFilterParameters.PEAK_DATAPOINTS)
+                            .getEmbeddedParameter().getValue();
+                    System.out.println(datapointsRange);
+                    System.out.println(peakDatapoints);
+                    if (!datapointsRange.contains(peakDatapoints)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check FWHM
+                if (filterByFWHM) {
+                    final Range<Double> fwhmRange = parameters
+                            .getParameter(PeakFilterParameters.PEAK_FWHM)
+                            .getEmbeddedParameter().getValue();
+                    if (!fwhmRange.contains(peakFWHM)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check Tailing Factor
+                if (filterByTailingFactor) {
+                    final Range<Double> tailingRange = parameters
+                            .getParameter(
+                                    PeakFilterParameters.PEAK_TAILINGFACTOR)
+                            .getEmbeddedParameter().getValue();
+                    if (!tailingRange.contains(peakTailingFactor)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+                // Check height
+                if (filterByAsymmetryFactor) {
+                    final Range<Double> asymmetryRange = parameters
+                            .getParameter(
+                                    PeakFilterParameters.PEAK_ASYMMETRYFACTOR)
+                            .getEmbeddedParameter().getValue();
+                    if (!asymmetryRange.contains(peakAsymmetryFactor)) {
+                        // Mark peak to be removed
+                        keepPeak[i] = false;
+                    }
+                }
+
+            }
+
+            newPeakList.addRow(copyPeakRow(row, keepPeak));
+
+        }
+
+        return newPeakList;
+    }
+
+    /**
+     * Create a copy of a peak list row.
+     */
+    private static PeakListRow copyPeakRow(final PeakListRow row,
+            final boolean[] keepPeak) {
+
+        // Copy the peak list row.
+        final PeakListRow newRow = new SimplePeakListRow(row.getID());
+        PeakUtils.copyPeakListRowProperties(row, newRow);
+
+        // Copy the peaks.
+        int i = 0;
+        for (final Feature peak : row.getPeaks()) {
+
+            // Only keep peak if it fulfills the filter criteria
+            if (keepPeak[i]) {
+                final Feature newPeak = new SimpleFeature(peak);
+                PeakUtils.copyPeakProperties(peak, newPeak);
+                newRow.addPeak(peak.getDataFile(), newPeak);
+            }
+            i++;
+        }
+
+        return newRow;
+    }
+}
