7692d4acba1e978c340f79e7751eccd533e12d71
timfallon <tfallon@mit.edu>
2016-02-15 19:38:52 -0500

    Progress for peak comparison filter

---
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/filtering/peakcomparisonrowfilter/PeakComparisonRowFilterParameters.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2006-2015 The MZmine 2 Development Team
+ * 
+ * This file is part of MZmine 2.
+ * 
+ * MZmine 2 is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine 2; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.peaklistmethods.filtering.peakcomparisonrowfilter;
+
+import java.awt.Window;
+
+import net.sf.mzmine.main.MZmineCore;
+import net.sf.mzmine.parameters.Parameter;
+import net.sf.mzmine.parameters.UserParameter;
+import net.sf.mzmine.parameters.dialogs.ParameterSetupDialog;
+import net.sf.mzmine.parameters.impl.SimpleParameterSet;
+import net.sf.mzmine.parameters.parametertypes.BooleanParameter;
+import net.sf.mzmine.parameters.parametertypes.ComboParameter;
+import net.sf.mzmine.parameters.parametertypes.IntegerParameter;
+import net.sf.mzmine.parameters.parametertypes.OptionalParameter;
+import net.sf.mzmine.parameters.parametertypes.StringParameter;
+import net.sf.mzmine.parameters.parametertypes.ranges.MZRangeParameter;
+import net.sf.mzmine.parameters.parametertypes.ranges.RTRangeParameter;
+import net.sf.mzmine.parameters.parametertypes.ranges.DoubleRangeParameter;
+import net.sf.mzmine.parameters.parametertypes.selectors.PeakListsParameter;
+import net.sf.mzmine.util.ExitCode;
+
+import com.google.common.collect.Range;
+
+public class PeakComparisonRowFilterParameters extends SimpleParameterSet {
+
+    public static final PeakListsParameter PEAK_LISTS = new PeakListsParameter();
+
+    public static final StringParameter SUFFIX = new StringParameter(
+            "Name suffix", "Suffix to be added to peak list name", "filtered");
+    
+    public static final OptionalParameter<IntegerParameter> COLUMN1 = new OptionalParameter<>(
+            new IntegerParameter("1st peak comparison column (zero indexed)",
+                    "index of 1st column for comparison", 1, 0,
+                    null));
+    
+    public static final OptionalParameter<IntegerParameter> COLUMN2 = new OptionalParameter<>(
+            new IntegerParameter("2nd peak comparison column (zero indexed)",
+                    "index of second column for comparison", 1, 0,
+                    null));
+
+    public static final OptionalParameter<IntegerParameter> MIN_ISOTOPE_PATTERN_COUNT = new OptionalParameter<>(
+            new IntegerParameter("Minimum peaks in an isotope pattern",
+                    "Minimum number of peaks required in an isotope pattern"));
+
+    public static final OptionalParameter<MZRangeParameter> MZ_RANGE = new OptionalParameter<>(
+            new MZRangeParameter());
+
+    public static final OptionalParameter<RTRangeParameter> RT_RANGE = new OptionalParameter<>(
+            new RTRangeParameter());
+
+    public static final OptionalParameter<DoubleRangeParameter> PEAK_DURATION = new OptionalParameter<>(
+            new DoubleRangeParameter("Peak duration range",
+                    "Permissible range of (average) peak durations per row",
+                    MZmineCore.getConfiguration().getRTFormat(), Range.closed(
+                            0.0, 10.0)));
+
+    public static final ComboParameter<Object> GROUPSPARAMETER = new ComboParameter<Object>(
+            "Parameter", "Paremeter defining the group of each sample.",
+            new Object[0]);
+
+    public static final BooleanParameter HAS_IDENTITIES = new BooleanParameter(
+            "Only identified?", "Select to filter only identified compounds");
+
+    public static final OptionalParameter<StringParameter> IDENTITY_TEXT = new OptionalParameter<>(
+            new StringParameter(
+                    "Text in identity",
+                    "Only rows that contain this text in their peak identity field will be retained."));
+
+    public static final OptionalParameter<StringParameter> COMMENT_TEXT = new OptionalParameter<>(
+            new StringParameter("Text in comment",
+                    "Only rows that contain this text in their comment field will be retained."));
+
+    public static final BooleanParameter AUTO_REMOVE = new BooleanParameter(
+            "Remove source peak list after filtering",
+            "If checked, the original peak list will be removed leaving only the filtered version");
+
+    public PeakComparisonRowFilterParameters() {
+        super(new Parameter[] { PEAK_LISTS, SUFFIX,
+                MIN_ISOTOPE_PATTERN_COUNT, MZ_RANGE, RT_RANGE, PEAK_DURATION,
+                GROUPSPARAMETER, HAS_IDENTITIES, IDENTITY_TEXT,
+                COMMENT_TEXT, AUTO_REMOVE });
+    }
+
+    @Override
+    public ExitCode showSetupDialog(Window parent, boolean valueCheckRequired) {
+
+        // Update the parameter choices
+        UserParameter<?, ?> newChoices[] = MZmineCore.getProjectManager()
+                .getCurrentProject().getParameters();
+        String[] choices;
+        if (newChoices == null || newChoices.length == 0) {
+            choices = new String[1];
+            choices[0] = "No parameters defined";
+        } else {
+            choices = new String[newChoices.length + 1];
+            choices[0] = "Ignore groups";
+            for (int i = 0; i < newChoices.length; i++) {
+                choices[i + 1] = "Filtering by " + newChoices[i].getName();
+            }
+        }
+
+        getParameter(PeakComparisonRowFilterParameters.GROUPSPARAMETER).setChoices(choices);
+        ParameterSetupDialog dialog = new ParameterSetupDialog(parent,
+                valueCheckRequired, this);
+        dialog.setVisible(true);
+        return dialog.getExitCode();
+    }
+}
---
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/filtering/peakcomparisonrowfilter/PeakComparisonRowFilterTask.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright 2006-2015 The MZmine 2 Development Team
+ * 
+ * This file is part of MZmine 2.
+ * 
+ * MZmine 2 is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * MZmine 2; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package net.sf.mzmine.modules.peaklistmethods.filtering.peakcomparisonrowfilter;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.sf.mzmine.datamodel.Feature;
+import net.sf.mzmine.datamodel.IsotopePattern;
+import net.sf.mzmine.datamodel.MZmineProject;
+import net.sf.mzmine.datamodel.PeakList;
+import net.sf.mzmine.datamodel.PeakList.PeakListAppliedMethod;
+import net.sf.mzmine.datamodel.PeakListRow;
+import net.sf.mzmine.datamodel.RawDataFile;
+import net.sf.mzmine.datamodel.impl.SimpleFeature;
+import net.sf.mzmine.datamodel.impl.SimplePeakList;
+import net.sf.mzmine.datamodel.impl.SimplePeakListAppliedMethod;
+import net.sf.mzmine.datamodel.impl.SimplePeakListRow;
+import net.sf.mzmine.parameters.ParameterSet;
+import net.sf.mzmine.parameters.UserParameter;
+import net.sf.mzmine.taskcontrol.AbstractTask;
+import net.sf.mzmine.taskcontrol.TaskStatus;
+import net.sf.mzmine.util.PeakUtils;
+import net.sf.mzmine.util.RangeUtils;
+
+import com.google.common.collect.Range;
+
+/**
+ * Filters out peak list rows.
+ */
+public class PeakComparisonRowFilterTask extends AbstractTask {
+
+    // Logger.
+    private static final Logger LOG = Logger.getLogger(PeakComparisonRowFilterTask.class
+            .getName());
+    // Peak lists.
+    private final MZmineProject project;
+    private final PeakList origPeakList;
+    private PeakList filteredPeakList;
+    // Processed rows counter
+    private int processedRows, totalRows;
+    // Parameters.
+    private final ParameterSet parameters;
+
+    /**
+     * Create the task.
+     *
+     * @param list
+     *            peak list to process.
+     * @param parameterSet
+     *            task parameters.
+     */
+    public PeakComparisonRowFilterTask(final MZmineProject project, final PeakList list,
+            final ParameterSet parameterSet) {
+
+        // Initialize.
+        this.project = project;
+        parameters = parameterSet;
+        origPeakList = list;
+        filteredPeakList = null;
+        processedRows = 0;
+        totalRows = 0;
+    }
+
+    @Override
+    public double getFinishedPercentage() {
+
+        return totalRows == 0 ? 0.0 : (double) processedRows
+                / (double) totalRows;
+    }
+
+    @Override
+    public String getTaskDescription() {
+
+        return "Filtering peak list rows";
+    }
+
+    @Override
+    public void run() {
+
+        if (!isCanceled()) {
+
+            try {
+                setStatus(TaskStatus.PROCESSING);
+                LOG.info("Filtering peak list rows");
+
+                // Filter the peak list.
+                filteredPeakList = filterPeakListRows(origPeakList);
+
+                if (!isCanceled()) {
+
+                    // Add new peaklist to the project
+                    project.addPeakList(filteredPeakList);
+
+                    // Remove the original peaklist if requested
+                    if (parameters.getParameter(
+                            PeakComparisonRowFilterParameters.AUTO_REMOVE).getValue()) {
+                        project.removePeakList(origPeakList);
+                    }
+
+                    setStatus(TaskStatus.FINISHED);
+                    LOG.info("Finished peak comparison rows filter");
+                }
+            } catch (Throwable t) {
+
+                setErrorMessage(t.getMessage());
+                setStatus(TaskStatus.ERROR);
+                LOG.log(Level.SEVERE, "Peak comparison row filter error", t);
+            }
+        }
+    }
+
+    /**
+     * Filter the peak list rows.
+     *
+     * @param peakList
+     *            peak list to filter.
+     * @return a new peak list with rows of the original peak list that pass the
+     *         filtering.
+     */
+    private PeakList filterPeakListRows(final PeakList peakList) {
+
+        // Create new peak list.
+        final PeakList newPeakList = new SimplePeakList(peakList.getName()
+                + ' '
+                + parameters.getParameter(PeakComparisonRowFilterParameters.SUFFIX)
+                        .getValue(), peakList.getRawDataFiles());
+
+        // Copy previous applied methods.
+        for (final PeakListAppliedMethod method : peakList.getAppliedMethods()) {
+
+            newPeakList.addDescriptionOfAppliedTask(method);
+        }
+
+        // Add task description to peakList.
+        newPeakList
+                .addDescriptionOfAppliedTask(new SimplePeakListAppliedMethod(
+                        getTaskDescription(), parameters));
+
+        // Get parameters.
+        final boolean onlyIdentified = parameters.getParameter(
+                PeakComparisonRowFilterParameters.HAS_IDENTITIES).getValue();
+        final boolean filterByIdentityText = parameters.getParameter(
+                PeakComparisonRowFilterParameters.IDENTITY_TEXT).getValue();
+        final boolean filterByCommentText = parameters.getParameter(
+                PeakComparisonRowFilterParameters.COMMENT_TEXT).getValue();
+        final String groupingParameter = (String) parameters.getParameter(
+                PeakComparisonRowFilterParameters.GROUPSPARAMETER).getValue();
+        final boolean filterByMinIsotopePatternSize = parameters.getParameter(
+                PeakComparisonRowFilterParameters.MIN_ISOTOPE_PATTERN_COUNT).getValue();
+        final boolean filterByMzRange = parameters.getParameter(
+                PeakComparisonRowFilterParameters.MZ_RANGE).getValue();
+        final boolean filterByRtRange = parameters.getParameter(
+                PeakComparisonRowFilterParameters.RT_RANGE).getValue();
+        final boolean filterByDuration = parameters.getParameter(
+                PeakComparisonRowFilterParameters.PEAK_DURATION).getValue();
+
+        // Filter rows.
+        final PeakListRow[] rows = peakList.getRows();
+        totalRows = rows.length;
+        for (processedRows = 0; !isCanceled() && processedRows < totalRows; processedRows++) {
+
+            final PeakListRow row = rows[processedRows];
+
+            final int peakCount = getPeakCount(row, groupingParameter);
+
+            // Check number of peaks.
+
+            // Check identities.
+
+
+            // Check average m/z.
+
+            // Check average RT.
+
+
+            // Search peak identity text.
+
+
+            // Search peak comment text.
+
+
+            // Calculate average duration and isotope pattern count.
+           
+
+            // Check isotope pattern count.
+            
+
+            // Check average duration.
+           
+
+            // Good row?
+            newPeakList.addRow(copyPeakRow(row));
+        }
+
+        return newPeakList;
+    }
+
+    /**
+     * Create a copy of a peak list row.
+     *
+     * @param row
+     *            the row to copy.
+     * @return the newly created copy.
+     */
+    private static PeakListRow copyPeakRow(final PeakListRow row) {
+
+        // Copy the peak list row.
+        final PeakListRow newRow = new SimplePeakListRow(row.getID());
+        PeakUtils.copyPeakListRowProperties(row, newRow);
+
+        // Copy the peaks.
+        for (final Feature peak : row.getPeaks()) {
+
+            final Feature newPeak = new SimpleFeature(peak);
+            PeakUtils.copyPeakProperties(peak, newPeak);
+            newRow.addPeak(peak.getDataFile(), newPeak);
+        }
+
+        return newRow;
+    }
+
+    private int getPeakCount(PeakListRow row, String groupingParameter) {
+        if (groupingParameter.contains("Filtering by ")) {
+            HashMap<String, Integer> groups = new HashMap<String, Integer>();
+            for (RawDataFile file : project.getDataFiles()) {
+                UserParameter<?, ?> params[] = project.getParameters();
+                for (UserParameter<?, ?> p : params) {
+                    groupingParameter = groupingParameter.replace(
+                            "Filtering by ", "");
+                    if (groupingParameter.equals(p.getName())) {
+                        String parameterValue = String.valueOf(project
+                                .getParameterValue(p, file));
+                        if (row.hasPeak(file)) {
+                            if (groups.containsKey(parameterValue)) {
+                                groups.put(parameterValue,
+                                        groups.get(parameterValue) + 1);
+                            } else {
+                                groups.put(parameterValue, 1);
+                            }
+                        } else {
+                            groups.put(parameterValue, 0);
+                        }
+                    }
+                }
+            }
+
+            Set<String> ref = groups.keySet();
+            Iterator<String> it = ref.iterator();
+            int min = Integer.MAX_VALUE;
+            while (it.hasNext()) {
+                String name = (String) it.next();
+                int val = groups.get(name);
+                if (val < min) {
+                    min = val;
+                }
+            }
+            return min;
+
+        } else {
+            return row.getNumberOfPeaks();
+        }
+    }
+}
