699969d107bbfec68c460a98bbbb271a156e6ff2
golgauth <gauthier.boaglio@gmail.com>
2016-04-13 11:37:47 +0200

    Fixed bug in CSVExport: peaklist name substitution

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/io/csvexport/CSVExportTask.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/io/csvexport/CSVExportTask.java
@@ -100,11 +100,12 @@ class CSVExportTask extends AbstractTask {
             // Filename
             File curFile = fileName;
             if (substitute) {
+                // Cleanup from illegal filename characters
+                String cleanPlName = peakList.getName().replaceAll(
+                        "[^a-zA-Z0-9.-]", "_");
                 // Substitute
                 String newFilename = fileName.getPath().replaceAll(
-                        Pattern.quote(plNamePattern), peakList.getName());
-                // Cleanup from illegal filename characters
-                newFilename = newFilename.replaceAll("[^a-zA-Z0-9.-]", "_");
+                        Pattern.quote(plNamePattern), cleanPlName);
                 curFile = new File(newFilename);
             }
 
