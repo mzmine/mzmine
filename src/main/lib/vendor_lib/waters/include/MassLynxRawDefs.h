eb762109233b0a3ec29b59ae9b1f209016f5929c
Owen Myers <omyers2@uncc.edu>
2017-03-27 10:09:38 -0400

    More corrupted files in src/main/lib/vendor_lib/waters/include from merge. I have merged these files keeping everything from HEAD (the most current mzmine 2)

--- src/main/lib/vendor_lib/waters/include/MassLynxRawDefs.h
+++ src/main/lib/vendor_lib/waters/include/MassLynxRawDefs.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //-----------------------------------------------------------------------------------------------------
 // FILE:			MassLynxRawDefs.h
 // COPYRIGHT(C):	Waters Corporation
@@ -9,18 +8,4 @@
 #define MLYNX_RAW_API __declspec(dllexport)
 #else
 #define MLYNX_RAW_API __declspec(dllimport)
-=======
-//-----------------------------------------------------------------------------------------------------
-// FILE:			MassLynxRawDefs.h
-// DATE:			Nov 2009
-// COPYRIGHT(C):	Waters Corporation
-//					
-//-----------------------------------------------------------------------------------------------------
-#ifdef MASSLYNX_RAW_EXPORTS
-#define MLYNX_RAW_API __declspec(dllexport)
-#define EXPIMP_TEMPLATE
-#else
-#define MLYNX_RAW_API __declspec(dllimport)
-#define EXPIMP_TEMPLATE extern
->>>>>>> tmpsrc/master
 #endif
