0d03d0d787868b4795ac7b6922d08c54a2419d61
Tomas Pluskal <plusik@gmail.com>
2017-05-30 10:50:27 -0400

    Removed the unique TMP_FILE_DIRECTORY, due to issue #249

--- src/main/scripts/startMZmine_Windows.bat
+++ src/main/scripts/startMZmine_Windows.bat
@@ -89,22 +89,6 @@ echo Java maximum heap size set to %HEAP_SIZE% MB
 echo.
 
 
-:: *************************************************
-:: Make a unique temp folder for the MZmine instance
-:: *************************************************
-
-:: DOS make sure to use 'short name' representation for temp directory
-:: and forward slashed (required by features not supporting spaces or '\').
-for %%f in ("%TMP_FILE_DIRECTORY%") do (set TMP_FILE_DIRECTORY=%%~sf)
-for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%-%ldt:~10,2%-%ldt:~12,6%
-set MZMINE_UNID=MZmine%ldt%
-
-set TMP_FILE_DIRECTORY=%TMP_FILE_DIRECTORY%\\%MZMINE_UNID%
-mkdir %TMP_FILE_DIRECTORY%
-
-
-
 :: **********************
 :: Java specific commands
 :: **********************
