397925dad365bb4dd634b97a33570f2ab495a0d7
abisarvepalli <abineshsarvepalli@Abis-MacBook-Pro.local>
2017-06-05 19:45:40 -0700

    Fix File Comment for GNPSExportTask.java
    # Start comments on new line after `/*`

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/io/gnpsexport/GNPSExportTask.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/io/gnpsexport/GNPSExportTask.java
@@ -1,5 +1,5 @@
-
-/* This module was prepared by Abi Sarvepalli, Christopher Jensen, and Zheng Zhang 
+/*
+ * This module was prepared by Abi Sarvepalli, Christopher Jensen, and Zheng Zhang 
  * at the Dorrestein Lab (University of California, San Diego). 
  * 
  * It is freely available under the GNU GPL licence of MZmine2.
