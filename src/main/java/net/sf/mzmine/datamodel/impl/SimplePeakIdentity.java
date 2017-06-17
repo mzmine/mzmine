a30f971b3838942089998cacecf277ff7b119b80
zheng zhang <zhz077@ucsd.edu>
2017-06-17 01:43:37 -0700

    Revert "Merge remote-tracking branch 'mzmine/master'"
    This reverts commit b0614fa2daaeb8f39352fe09314c844dd13035c7, reversing
    changes made to 4605c6b9a0d64770adb36e64eac58bda2953eac4.

--- src/main/java/net/sf/mzmine/datamodel/impl/SimplePeakIdentity.java
+++ src/main/java/net/sf/mzmine/datamodel/impl/SimplePeakIdentity.java
@@ -131,9 +131,9 @@ public class SimplePeakIdentity implements PeakIdentity {
     }
 
     @Override
-    // Removed @Nonnull in front because the function may return null
-    public String getPropertyValue(final String property) {
-		return properties.get(property);
+    public @Nonnull String getPropertyValue(final String property) {
+
+	return properties.get(property);
     }
 
     public void setPropertyValue(final String property, final String value) {
