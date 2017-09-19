9e3b88f004cca6b8878bd3cbf257c17f336b1aae
Aleksandr Smirnov <asmirno1@uncc.edu>
2017-09-18 20:08:04 -0400

    Reverted CombinedXIC changes and updated the category name for spectral deconvolution

--- src/main/java/net/sf/mzmine/util/components/CombinedXICComponent.java
+++ src/main/java/net/sf/mzmine/util/components/CombinedXICComponent.java
@@ -76,11 +76,9 @@ public class CombinedXICComponent extends JComponent {
             maxIntensity = Math.max(maxIntensity,
                     peak.getRawDataPointsIntensityRange().upperEndpoint());
             if (rtRange == null)
-//                rtRange = peak.getDataFile().getDataRTRange();
-                rtRange = peak.getRawDataPointsRTRange();
+                rtRange = peak.getDataFile().getDataRTRange();
             else
-//                rtRange = rtRange.span(peak.getDataFile().getDataRTRange());
-                rtRange = rtRange.span(peak.getRawDataPointsRTRange());
+                rtRange = rtRange.span(peak.getDataFile().getDataRTRange());
         }
 
         this.maxIntensity = maxIntensity;
