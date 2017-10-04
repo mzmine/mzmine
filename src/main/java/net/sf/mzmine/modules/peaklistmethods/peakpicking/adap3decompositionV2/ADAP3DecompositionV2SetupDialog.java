537c673ac2e20dc45f5d845c7b6175f42222e01f
Aleksandr Smirnov <asmirno1@uncc.edu>
2017-10-04 07:39:34 -0400

    Implemented NMF-initialization based on clustering

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/peakpicking/adap3decompositionV2/ADAP3DecompositionV2SetupDialog.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/peakpicking/adap3decompositionV2/ADAP3DecompositionV2SetupDialog.java
@@ -372,7 +372,7 @@ public class ADAP3DecompositionV2SetupDialog extends ParameterSetupDialog
 
         List<BetterComponent> components = null;
         try {
-            components = new ComponentSelector(cluster, chromatograms, shapeTolerance, durationRange).run();
+            components = new ComponentSelector(cluster, chromatograms, shapeTolerance).run();
         } catch (Exception e) {
             e.printStackTrace();
         }
