666bb454ba82f78e4648f44657b97b25a3a09dbd
Steffen <Steffen@10.67.9.126>
2018-03-19 15:26:30 +0100

    test

---
+++ src/main/java/net/sf/mzmine/mytest/TestCDK.java
@@ -0,0 +1,14 @@
+package net.sf.mzmine.mytest;
+
+public class TestCDK {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hey");
+		Element e = new Element();
+		Element f = new Element();
+		
+		e.value = 2;
+	}
+
+}
