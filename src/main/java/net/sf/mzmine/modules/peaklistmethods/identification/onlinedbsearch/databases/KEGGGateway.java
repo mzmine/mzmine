6ea4361829696f7e89cecc4c3ba3deaf5e949efa
Tomas Pluskal <plusik@gmail.com>
2018-07-15 13:07:22 -0400

    Fixed the KEGG 2D structure URL

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/onlinedbsearch/databases/KEGGGateway.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/identification/onlinedbsearch/databases/KEGGGateway.java
@@ -41,7 +41,7 @@ public class KEGGGateway implements DBGateway {
     private static final String keggGetAddress = "http://rest.kegg.jp/get/";
     private static final String keggEntryAddress = "http://www.kegg.jp/entry/";
 
-    private static final String kegg2DStructureAddress = "http://www.genome.jp/dbget-bin/www_bget?-f+m+";
+    private static final String kegg2DStructureAddress = "https://www.genome.jp/dbget-bin/www_bget?-f+m+compound+";
     private static final String met3DStructureAddress1 = "http://www.3dmet.dna.affrc.go.jp/pdb/";
     private static final String met3DStructureAddress2 = ".pdb";
 
