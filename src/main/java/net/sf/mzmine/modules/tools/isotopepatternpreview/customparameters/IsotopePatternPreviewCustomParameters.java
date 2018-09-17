049b1b31a405caa8c587191121074ccccaf960f8
SteffenHeu <steffen.heuckeroth@gmx.de>
2018-09-17 19:22:11 +0200

    cleaned parameter description

--- src/main/java/net/sf/mzmine/modules/tools/isotopepatternpreview/customparameters/IsotopePatternPreviewCustomParameters.java
+++ src/main/java/net/sf/mzmine/modules/tools/isotopepatternpreview/customparameters/IsotopePatternPreviewCustomParameters.java
@@ -34,17 +34,16 @@ import net.sf.mzmine.parameters.parametertypes.PercentParameter;
 public class IsotopePatternPreviewCustomParameters  extends SimpleParameterSet{
 
   public static final PercentParameter minAbundance = new PercentParameter("Minimum abundance",
-      "The minimum abundance (%) of Isotopes. Small values ",
-      0.01);
+      "The minimum abundance (%) of Isotopes", 0.01);
   
   public static final DoubleParameter mergeWidth = new DoubleParameter("Merge width(Da)",
-      "This will be used to merge peaks in the calculated isotope pattern if they overlap.",
+      "This will be used to merge isotope compositions in the calculated isotope pattern if they overlap.",
       MZmineCore.getConfiguration().getMZFormat(), 0.0005, 0.0d, 10.0d);
   
   public static final DoubleParameter minPatternIntensity =
       new DoubleParameter("Min. pattern intensity",
-          "The minimum normalized intensity of a peak in the final calculated isotope pattern. "
-              + "Depends on the sensitivity of your MS.\nMin = 0.0, Max = 0.99...",
+          "The minimum normalized intensity of a peak in the final calculated isotope pattern.\n"
+              + "Min = 0.0, Max = 0.99...",
           new DecimalFormat("0.####"), 0.01, 0.0d, 0.99999);
   
   public IsotopePatternPreviewCustomParameters(){
