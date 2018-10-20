5c553aed4c1545e2b71d9a32dc033b3588a5a167
Steffen <steffen.heuckeroth@gmx.de>
2018-10-20 14:10:00 +0200

    added small demonstration

---
+++ src/main/java/net/sf/mzmine/modules/tools/isotopepatternpreview/Test.java
@@ -0,0 +1,20 @@
+package net.sf.mzmine.modules.tools.isotopepatternpreview;
+
+import java.util.logging.Logger;
+import net.sf.mzmine.datamodel.PolarityType;
+import net.sf.mzmine.modules.peaklistmethods.isotopes.isotopepeakscanner.ExtendedIsotopePattern;
+
+public class Test {
+  public static void main(String[] args) {
+    ExtendedIsotopePattern pattern = new ExtendedIsotopePattern();
+    Logger logger = Logger.getLogger("Test");
+    
+    pattern.setUpFromFormula("C6Cl2", 0.01, 0.02, 0.01);
+    pattern.applyCharge(1, PolarityType.NEGATIVE);
+    
+    
+    logger.info(pattern.getDetailedPeakDescription(2));
+    logger.info("----------------");
+    pattern.print();
+  }
+}
