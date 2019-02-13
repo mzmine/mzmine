126695ef5008f27b33aab54dabe357a3336864f2
SteffenHeu <steffen.heuckeroth@gmx.de>
2019-02-13 17:26:56 +0100

    Module uses TaskStatusListener

--- src/main/java/net/sf/mzmine/modules/datapointprocessing/DataPointProcessingModule.java
+++ src/main/java/net/sf/mzmine/modules/datapointprocessing/DataPointProcessingModule.java
@@ -3,10 +3,12 @@ package net.sf.mzmine.modules.datapointprocessing;
 import net.sf.mzmine.datamodel.DataPoint;
 import net.sf.mzmine.modules.MZmineProcessingModule;
 import net.sf.mzmine.modules.visualization.spectra.simplespectra.SpectraPlot;
+import net.sf.mzmine.taskcontrol.TaskStatusListener;
 
 public interface DataPointProcessingModule extends MZmineProcessingModule {
   
-  public DataPointProcessingTask createTask(DataPoint[] dataPoints, SpectraPlot plot, DataPointProcessingListener listener);
+//  public DataPointProcessingTask createTask(DataPoint[] dataPoints, SpectraPlot plot, DataPointProcessingListener listener);
+  public DataPointProcessingTask createTask(DataPoint[] dataPoints, SpectraPlot plot, TaskStatusListener listener);
   
   // TODO: runModule() is not needed for this. what do we do with it?
 }
