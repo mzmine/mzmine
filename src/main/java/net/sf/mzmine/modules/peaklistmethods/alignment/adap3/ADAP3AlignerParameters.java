33b18de6abacf44d8576bb9a32f5c682fffbe4a5
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-02-28 11:50:08 -0500

    Change "compounds" to "features" in the help file and parameter descriptions. Clean up the source code.

--- src/main/java/net/sf/mzmine/modules/peaklistmethods/alignment/adap3/ADAP3AlignerParameters.java
+++ src/main/java/net/sf/mzmine/modules/peaklistmethods/alignment/adap3/ADAP3AlignerParameters.java
@@ -24,7 +24,6 @@ import net.sf.mzmine.parameters.Parameter;
 import net.sf.mzmine.parameters.impl.SimpleParameterSet;
 import net.sf.mzmine.parameters.parametertypes.ComboParameter;
 import net.sf.mzmine.parameters.parametertypes.DoubleParameter;
-import net.sf.mzmine.parameters.parametertypes.ParameterSetParameter;
 import net.sf.mzmine.parameters.parametertypes.StringParameter;
 import net.sf.mzmine.parameters.parametertypes.selectors.PeakListsParameter;
 import net.sf.mzmine.parameters.parametertypes.tolerances.MZToleranceParameter;
@@ -47,9 +46,9 @@ public class ADAP3AlignerParameters extends SimpleParameterSet {
 
     public static final DoubleParameter SAMPLE_COUNT_RATIO =
             new DoubleParameter("Min confidence (between 0 and 1)",
-                    "A fraction of the total number of samples. An aligned compound must be detected at " +
+                    "A fraction of the total number of samples. An aligned feature must be detected at " +
                             "least in several samples. This parameter determines the minimum number of samples where a " +
-                            "compound must be detected",
+                            "feature must be detected.",
                     NumberFormat.getInstance(), 0.7, 0.0, 1.0);
 
     public static final RTToleranceParameter RET_TIME_RANGE = new RTToleranceParameter();
@@ -58,12 +57,12 @@ public class ADAP3AlignerParameters extends SimpleParameterSet {
 
     public static final DoubleParameter SCORE_TOLERANCE =
             new DoubleParameter("Score threshold (between 0 and 1)",
-                    "The minimum value of the similarity function required for compounds to be aligned together.",
+                    "The minimum value of the similarity function required for features to be aligned together.",
                     NumberFormat.getInstance(), 0.75, 0.0, 1.0);
 
     public static final DoubleParameter SCORE_WEIGHT =
             new DoubleParameter("Score weight (between 0 and 1)",
-                    "The weight w that is used in the similarity function",
+                    "The weight w that is used in the similarity function. See the help file for details.",
                     NumberFormat.getInstance(), 0.1, 0.0, 1.0);
 
     public static final ComboParameter<String> EIC_SCORE =
@@ -72,12 +71,6 @@ public class ADAP3AlignerParameters extends SimpleParameterSet {
                             "(fast) is preferred method.",
                     EIC_SCORE_TYPES,
                     AlignmentParameters.RT_DIFFERENCE);
-
-    public static final ParameterSetParameter OPTIM_PARAMS =
-            new ParameterSetParameter("Optimization Parameters",
-                    "These parameters control optimization algorithm for finding "
-                            + "maximum of the convolution integral",
-                    new ADAP3AlignerOptimizationParameters());
 
     public ADAP3AlignerParameters() {
         super(new Parameter[]{PEAK_LISTS, SAMPLE_COUNT_RATIO, RET_TIME_RANGE, MZ_RANGE,
