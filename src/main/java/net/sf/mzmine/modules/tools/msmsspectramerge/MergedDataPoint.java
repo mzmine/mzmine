f586aaaaffe3eedfcefc2d8264eee092f132e9b0
Kai Dührkop <kai.duehrkop@uni-jena.de>
2019-05-09 16:58:51 -0700

    add licence text

--- src/main/java/net/sf/mzmine/modules/tools/msmsspectramerge/MergedDataPoint.java
+++ src/main/java/net/sf/mzmine/modules/tools/msmsspectramerge/MergedDataPoint.java
@@ -1,7 +1,31 @@
+/*
+ * Copyright 2006-2018 The MZmine 2 Development Team
+ *
+ * This file is part of MZmine 2.
+ *
+ * MZmine 2 is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine 2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine 2; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ *
+ * It is freely available under the GNU GPL licence of MZmine2.
+ *
+ * For any questions or concerns, please refer to:
+ * https://groups.google.com/forum/#!forum/molecular_networking_bug_reports
+ */
+
 package net.sf.mzmine.modules.tools.msmsspectramerge;
 
 import net.sf.mzmine.datamodel.DataPoint;
 import net.sf.mzmine.util.scans.ScanUtils;
+
 import java.util.Arrays;
 import java.util.Locale;
 
