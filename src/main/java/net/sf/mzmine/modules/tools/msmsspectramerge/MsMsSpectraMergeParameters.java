7a2442a3fba73a028280f70f1000c159c821ba57
Tomas Pluskal <plusik@gmail.com>
2019-05-11 13:24:15 -0400

    Added units and m/z formatting to parameters

--- src/main/java/net/sf/mzmine/modules/tools/msmsspectramerge/MsMsSpectraMergeParameters.java
+++ src/main/java/net/sf/mzmine/modules/tools/msmsspectramerge/MsMsSpectraMergeParameters.java
@@ -23,6 +23,7 @@
 
 package net.sf.mzmine.modules.tools.msmsspectramerge;
 
+import net.sf.mzmine.main.MZmineCore;
 import net.sf.mzmine.parameters.Parameter;
 import net.sf.mzmine.parameters.impl.SimpleParameterSet;
 import net.sf.mzmine.parameters.parametertypes.ComboParameter;
@@ -35,28 +36,47 @@ import java.util.Locale;
 
 public class MsMsSpectraMergeParameters extends SimpleParameterSet {
 
-    public MsMsSpectraMergeParameters() {
-        super(
-                new Parameter[]{MERGE_MODE, MZ_MERGE_MODE, INTENSITY_MERGE_MODE, MASS_ACCURACY, COSINE_PARAMETER, PEAK_COUNT_PARAMETER, ISOLATION_WINDOW_OFFSET, ISOLATION_WINDOW_WIDTH}
-        );
-    }
+  public MsMsSpectraMergeParameters() {
+    super(new Parameter[] {MERGE_MODE, MZ_MERGE_MODE, INTENSITY_MERGE_MODE, MASS_ACCURACY,
+        COSINE_PARAMETER, PEAK_COUNT_PARAMETER, ISOLATION_WINDOW_OFFSET, ISOLATION_WINDOW_WIDTH});
+  }
 
-    public static final PercentParameter COSINE_PARAMETER = new PercentParameter("Cosine threshold (%)", "Threshold for the cosine similarity between two spectra for merging. Set to 0 if the spectra may have different collision energy!", 0.7d, 0d, 1d);
+  public static final PercentParameter COSINE_PARAMETER = new PercentParameter(
+      "Cosine threshold (%)",
+      "Threshold for the cosine similarity between two spectra for merging. Set to 0 if the spectra may have different collision energy!",
+      0.7d, 0d, 1d);
 
-    public static final PercentParameter PEAK_COUNT_PARAMETER = new PercentParameter("Peak count threshold ", "After merging, remove all peaks which occur in less than X % of the merged spectra.", 0.2d, 0d, 1d);
+  public static final PercentParameter PEAK_COUNT_PARAMETER =
+      new PercentParameter("Peak count threshold (%)",
+          "After merging, remove all peaks which occur in less than X % of the merged spectra.",
+          0.2d, 0d, 1d);
 
-    public static final MZToleranceParameter MASS_ACCURACY = new MZToleranceParameter("Expected mass deviation", "Expected mass deviation of your measurement in ppm (parts per million) and Dalton. We recommend to use a rather large value, e.g. 10 for Orbitrap, 15 for Q-ToF, 100 for QQQ.");
+  public static final MZToleranceParameter MASS_ACCURACY = new MZToleranceParameter(
+      "Expected mass deviation",
+      "Expected mass deviation of your measurement in ppm (parts per million) or Da (larger value is used). We recommend to use a rather large value, e.g. 10 ppm for Orbitrap, 15 ppm for Q-ToF, 100 ppm for QQQ.");
 
-    public static final ComboParameter<MzMergeMode> MZ_MERGE_MODE = new ComboParameter<MzMergeMode>("m/z merge mode", "How to merge the m/z values of peaks from different spectra with similar mass. Choose 'most intense' to pick always the m/z of the best peak - this is a very conservative and safe option. However, 'weighted average (cuttoff outliers)' will often have better results.", MzMergeMode.values(), MzMergeMode.WEIGHTED_AVERAGE_CUTOFF_OUTLIERS);
+  public static final ComboParameter<MzMergeMode> MZ_MERGE_MODE = new ComboParameter<MzMergeMode>(
+      "m/z merge mode",
+      "How to merge the m/z values of peaks from different spectra with similar mass. Choose 'most intense' to pick always the m/z of the best peak - this is a very conservative and safe option. However, 'weighted average (cuttoff outliers)' will often have better results.",
+      MzMergeMode.values(), MzMergeMode.WEIGHTED_AVERAGE_CUTOFF_OUTLIERS);
 
-    public static final ComboParameter<IntensityMergeMode> INTENSITY_MERGE_MODE = new ComboParameter<IntensityMergeMode>("intensity merge mode", "How to merge the intensity values of peaks from different spectra with similar mass. 'sum intensities' is a convenient option that will increase the intensities of peaks that occur consistently in many fragment scans. However, this will make intensities between merged and unmerged spectra incomparable. Use 'max intensitiy' if you want to preserve intensity values.", IntensityMergeMode.values(), IntensityMergeMode.SUM);
+  public static final ComboParameter<IntensityMergeMode> INTENSITY_MERGE_MODE =
+      new ComboParameter<IntensityMergeMode>("intensity merge mode",
+          "How to merge the intensity values of peaks from different spectra with similar mass. 'sum intensities' is a convenient option that will increase the intensities of peaks that occur consistently in many fragment scans. However, this will make intensities between merged and unmerged spectra incomparable. Use 'max intensitiy' if you want to preserve intensity values.",
+          IntensityMergeMode.values(), IntensityMergeMode.SUM);
 
-    public static final ComboParameter<MergeMode> MERGE_MODE = new ComboParameter<MergeMode>("Select spectra to merge", "'across samples' is a convenient option that will merge all MS/MS which belong to the same feature. Note that a clustering is performed automatically to filter out MS/MS which are wrongly associated with the same feature. However, 'same sample' might sometimes be the safer option if you do not thrust your alignment algorithm.",
-                    MergeMode.values(), MergeMode.ACROSS_SAMPLES);
+  public static final ComboParameter<MergeMode> MERGE_MODE = new ComboParameter<MergeMode>(
+      "Select spectra to merge",
+      "'across samples' is a convenient option that will merge all MS/MS which belong to the same feature. Note that a clustering is performed automatically to filter out MS/MS which are wrongly associated with the same feature. However, 'same sample' might sometimes be the safer option if you do not thrust your alignment algorithm.",
+      MergeMode.values(), MergeMode.ACROSS_SAMPLES);
 
-    public static final DoubleParameter ISOLATION_WINDOW_OFFSET = new DoubleParameter("Isolation window offset", "isolation window offset from the precursor m/z", NumberFormat.getNumberInstance(Locale.US), 0d);
-    
-    public static final DoubleParameter ISOLATION_WINDOW_WIDTH = new DoubleParameter("Isolation window width", "width (left and right from offset) of the isolation window", NumberFormat.getNumberInstance(Locale.US), 3d);
+  public static final DoubleParameter ISOLATION_WINDOW_OFFSET = new DoubleParameter(
+      "Isolation window offset (m/z)", "isolation window offset from the precursor m/z",
+      MZmineCore.getConfiguration().getMZFormat(), 0d);
+
+  public static final DoubleParameter ISOLATION_WINDOW_WIDTH = new DoubleParameter(
+      "Isolation window width (m/z)", "width (left and right from offset) of the isolation window",
+      MZmineCore.getConfiguration().getMZFormat(), 3d);
 
 
 }
