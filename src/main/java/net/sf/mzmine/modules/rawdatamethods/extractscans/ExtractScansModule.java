31ecbeaf2190b0227db0af2c936c79a817c13a40
Robin Schmid <robinschmid@uni-muenster.de>
2019-07-10 17:15:26 +0200

    Rename package

--- src/main/java/net/sf/mzmine/modules/rawdatamethods/extractscans/ExtractScansModule.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/extractscans/ExtractScansModule.java
@@ -16,7 +16,7 @@
  * USA
  */
 
-package net.sf.mzmine.modules.rawdatamethods.exportscans;
+package net.sf.mzmine.modules.rawdatamethods.extractscans;
 
 import java.util.Collection;
 import javax.annotation.Nonnull;
--- src/main/java/net/sf/mzmine/modules/rawdatamethods/extractscans/ExtractScansParameters.java
+++ src/main/java/net/sf/mzmine/modules/rawdatamethods/extractscans/ExtractScansParameters.java
@@ -16,7 +16,7 @@
  * USA
  */
 
-package net.sf.mzmine.modules.rawdatamethods.exportscans;
+package net.sf.mzmine.modules.rawdatamethods.extractscans;
 
 import java.text.DecimalFormat;
 import net.sf.mzmine.parameters.Parameter;
