1b086031dcaafa45e4480459c3a085a3a35dbaf9
Tomas Pluskal <plusik@gmail.com>
2019-12-15 22:22:21 -0500

    Removed warning when using version 0.0

--- src/main/java/io/github/mzmine/modules/io/projectload/ProjectOpeningTask.java
+++ src/main/java/io/github/mzmine/modules/io/projectload/ProjectOpeningTask.java
@@ -7,9 +7,9 @@
  * General Public License as published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  *
- * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with MZmine; if not,
  * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
@@ -336,12 +336,14 @@ public class ProjectOpeningTask extends AbstractTask {
     }
 
     // Check if project was saved with a newer version
-    if ((projectMajorVersion > mzmineMajorVersion) || ((projectMajorVersion == mzmineMajorVersion)
-        && (projectMinorVersion > mzmineMinorVersion))) {
-      String warning = "Warning: this project was saved with a newer version of MZmine ("
-          + projectVersionString + "). Opening this project in MZmine " + mzmineVersionString
-          + " may result in errors or loss of information.";
-      MZmineCore.getDesktop().displayMessage(MZmineCore.getDesktop().getMainWindow(), warning);
+    if (mzmineMajorVersion > 0) {
+      if ((projectMajorVersion > mzmineMajorVersion) || ((projectMajorVersion == mzmineMajorVersion)
+          && (projectMinorVersion > mzmineMinorVersion))) {
+        String warning = "Warning: this project was saved with a newer version of MZmine ("
+            + projectVersionString + "). Opening this project in MZmine " + mzmineVersionString
+            + " may result in errors or loss of information.";
+        MZmineCore.getDesktop().displayMessage(MZmineCore.getDesktop().getMainWindow(), warning);
+      }
     }
 
     // Default opening handler for MZmine.5 and higher
