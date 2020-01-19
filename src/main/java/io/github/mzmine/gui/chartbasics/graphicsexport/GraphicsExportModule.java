6d678178e6fb3ef45134afb7a2edd3b8219ba0ad
SteffenHeu <steffen.heuckeroth@gmx.de>
2020-01-19 12:10:36 +0000

    added missing license

--- src/main/java/io/github/mzmine/gui/chartbasics/graphicsexport/GraphicsExportModule.java
+++ src/main/java/io/github/mzmine/gui/chartbasics/graphicsexport/GraphicsExportModule.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2006-2020 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
 package io.github.mzmine.gui.chartbasics.graphicsexport;
 
 import java.util.Collection;
@@ -13,7 +31,7 @@ public class GraphicsExportModule implements MZmineRunnableModule {
 
   private static final String MODULE_NAME = "Graphics Export";
   private static final String MODULE_DESCR = "Exports a plot to a file.";
-  
+
   @Override
   public String getName() {
     return MODULE_NAME;
@@ -30,7 +48,7 @@ public class GraphicsExportModule implements MZmineRunnableModule {
   }
 
   public ExitCode openDialog(JFreeChart chart, ParameterSet parameters) {
-    return ((GraphicsExportParameters)parameters).showSetupDialog(true, chart);
+    return ((GraphicsExportParameters) parameters).showSetupDialog(true, chart);
   }
 
   @Override
