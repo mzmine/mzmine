2f6fdfabb2e14945ebe93cd24c9e1dc37901885d
knakul853 <knakul853@gmail.com>
2020-03-18 18:03:43 +0530

    added basic template for javafx

---
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_sirius/ResultWindowFX.java
@@ -0,0 +1,16 @@
+package io.github.mzmine.modules.dataprocessing.id_sirius;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ResultWindowFX extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
