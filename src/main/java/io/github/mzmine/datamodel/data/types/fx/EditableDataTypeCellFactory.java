2c8343627435c89a1b52659be9945096c5fd1cae
Tomas Pluskal <plusik@gmail.com>
2020-06-20 21:27:56 +0200

    Changed descriptorString to toString to maintain Java 11 compatibility

--- src/main/java/io/github/mzmine/datamodel/data/types/fx/EditableDataTypeCellFactory.java
+++ src/main/java/io/github/mzmine/datamodel/data/types/fx/EditableDataTypeCellFactory.java
@@ -1,16 +1,16 @@
 /*
  * Copyright 2006-2020 The MZmine Development Team
- * 
+ *
  * This file is part of MZmine.
- * 
+ *
  * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with MZmine; if not,
  * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
  * USA
@@ -32,7 +32,7 @@ import javafx.util.Callback;
 
 /**
  * Default cell factory for a DataType
- * 
+ *
  * @author Robin Schmid (robinschmid@uni-muenster.de)
  *
  * @param <T>
@@ -66,8 +66,8 @@ public class EditableDataTypeCellFactory implements
       cell.setAlignment(Pos.CENTER);
       return cell;
     } else {
-      logger.log(Level.SEVERE, "Class in editable CellFactory is no StringParser: "
-          + type.getClass().descriptorString());
+      logger.log(Level.SEVERE,
+          "Class in editable CellFactory is no StringParser: " + type.getClass().toString());
       return null;
     }
   }
