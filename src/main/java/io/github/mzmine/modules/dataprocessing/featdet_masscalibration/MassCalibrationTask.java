8f0bfa4a692fdc16d932ecdbe6f72d581bbebb77
Tomas Pluskal <plusik@gmail.com>
2020-11-06 12:00:20 +0100

    Updated mass calibration help, renamed folder for consistency

---
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_masscalibration/MassCalibrationTask.java
@@ -0,0 +1,447 @@
+/*
+ * Copyright 2006-2020 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+package io.github.mzmine.modules.dataprocessing.featdet_masscalibration;
+
+import com.google.common.collect.Range;
+import io.github.mzmine.datamodel.DataPoint;
+import io.github.mzmine.datamodel.MassList;
+import io.github.mzmine.datamodel.RawDataFile;
+import io.github.mzmine.datamodel.Scan;
+import io.github.mzmine.datamodel.impl.SimpleMassList;
+import io.github.mzmine.main.MZmineCore;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.MassCalibrationParameters.BiasEstimationChoice;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.MassCalibrationParameters.MassPeakMatchingChoice;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.MassCalibrationParameters.RangeExtractionChoice;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.charts.ArithmeticMeanKnnTrend;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.charts.OLSRegressionTrend;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.charts.Trend2D;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.errormodeling.DistributionRange;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.standardslist.StandardsList;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.standardslist.StandardsListExtractor;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.standardslist.StandardsListExtractorFactory;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.standardslist.UniversalCalibrantsListCsvExtractor;
+import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.parameters.parametertypes.combonested.NestedCombo;
+import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
+import io.github.mzmine.parameters.parametertypes.tolerances.RTTolerance;
+import io.github.mzmine.taskcontrol.AbstractTask;
+import io.github.mzmine.taskcontrol.TaskPriority;
+import io.github.mzmine.taskcontrol.TaskStatus;
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.jfree.data.xy.XYSeries;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+/**
+ * Mass calibration task with preview run flag
+ */
+public class MassCalibrationTask extends AbstractTask {
+
+  protected static boolean runCalibrationOnPreview = false;
+  
+  private final Logger logger = Logger.getLogger(this.getClass().getName());
+  private final ParameterSet parameters;
+  private final RawDataFile dataFile;
+
+  // User parameters
+  private final String massListName;
+  private final String suffix;
+  private final boolean autoRemove;
+
+  // scan counter
+  protected int processedScans = 0, totalScans;
+  protected int[] scanNumbers;
+
+  // task timer
+  protected Long startMillis;
+  protected Long endMillis;
+
+  // method parameters
+  // standards list
+  protected String standardsListFilename = null;
+  protected StandardsListExtractor standardsListExtractor;
+  protected StandardsList standardsList;
+  // tolerances
+  protected MZTolerance mzRatioTolerance = null;
+  protected RTTolerance rtTolerance = null;
+  // error trend
+  protected Trend2D errorTrend = null;
+
+  // mass calibrator and data passed between it
+  protected MassCalibrator massCalibrator;
+  protected ArrayList<MassPeakMatch> massPeakMatches = new ArrayList<>();
+  protected ArrayList<Double> errors = new ArrayList<>();
+  protected HashMap<String, DistributionRange> errorRanges = new HashMap<>();
+  protected double biasEstimate;
+
+  protected boolean previewRun = false;
+  protected Runnable afterHook = null;
+
+  /**
+   * @param dataFile
+   * @param parameters
+   * @param previewRun
+   */
+  public MassCalibrationTask(RawDataFile dataFile, ParameterSet parameters, boolean previewRun) {
+    this.dataFile = dataFile;
+    this.parameters = parameters;
+    this.previewRun = previewRun;
+
+    this.massListName = parameters.getParameter(MassCalibrationParameters.massList).getValue();
+    this.suffix = parameters.getParameter(MassCalibrationParameters.suffix).getValue();
+    this.autoRemove = parameters.getParameter(MassCalibrationParameters.autoRemove).getValue();
+  }
+
+  public MassCalibrationTask(RawDataFile dataFile, ParameterSet parameters) {
+    this(dataFile, parameters, false);
+  }
+
+  /**
+   * @see io.github.mzmine.taskcontrol.Task#getTaskDescription()
+   */
+  public String getTaskDescription() {
+    return "Calibrating mass in " + dataFile;
+  }
+
+  /**
+   * @see io.github.mzmine.taskcontrol.Task#getFinishedPercentage()
+   */
+  public double getFinishedPercentage() {
+    if (totalScans == 0)
+      return 0;
+    else
+      // processed scans are added twice, when errors are obtain and when mass lists are shifted
+      // so to get finished percentage of the task, divide processed scans by double total scans
+      return (double) processedScans / totalScans / 2;
+  }
+
+  public RawDataFile getDataFile() {
+    return dataFile;
+  }
+
+  @Override
+  public TaskPriority getTaskPriority() {
+    if (previewRun) {
+      return TaskPriority.HIGH;
+    }
+    return TaskPriority.NORMAL;
+  }
+
+  public Runnable getAfterHook() {
+    return afterHook;
+  }
+
+  public void setAfterHook(Runnable afterHook) {
+    this.afterHook = afterHook;
+  }
+
+  /**
+   * @see Runnable#run()
+   */
+  public void run() {
+    runTask();
+    if (afterHook != null && isCanceled() == false) {
+      afterHook.run();
+    }
+  }
+
+  /**
+   * @see Runnable#run()
+   */
+  public void runTask() {
+    setStatus(TaskStatus.PROCESSING);
+    logger.info("Started mass calibration on " + dataFile);
+    startMillis = System.currentTimeMillis();
+
+    boolean extractionOk = extractStandardsList();
+    if (extractionOk == false) {
+      endMillis = System.currentTimeMillis();
+      return;
+    }
+
+    Double intensityThreshold = parameters.getParameter(MassCalibrationParameters.intensityThreshold).getValue();
+    Boolean filterDuplicates = parameters.getParameter(MassCalibrationParameters.filterDuplicates).getValue();
+    extractToleranceParameters();
+
+    extractErrorTrend();
+
+    massCalibrator = null;
+    NestedCombo rangeExtractionMethod = parameters.
+            getParameter(MassCalibrationParameters.rangeExtractionMethod).getValue();
+    ParameterSet rangeParameterSet = rangeExtractionMethod.getCurrentChoiceParameterSet();
+
+    if (rangeExtractionMethod.isCurrentChoice(RangeExtractionChoice.RANGE_METHOD)) {
+      Double tolerance = rangeParameterSet.getParameter(MassCalibrationParameters.tolerance).getValue();
+      Double rangeSize = rangeParameterSet.getParameter(MassCalibrationParameters.rangeSize).getValue();
+      massCalibrator = new MassCalibrator(rtTolerance, mzRatioTolerance,
+              tolerance, rangeSize, standardsList, errorTrend);
+    } else if (rangeExtractionMethod.isCurrentChoice(RangeExtractionChoice.PERCENTILE_RANGE)) {
+      Range<Double> percentileRange = rangeParameterSet.getParameter(MassCalibrationParameters.percentileRange)
+              .getValue();
+      massCalibrator = new MassCalibrator(rtTolerance, mzRatioTolerance, percentileRange, standardsList, errorTrend);
+    }
+
+
+    scanNumbers = dataFile.getScanNumbers();
+    totalScans = scanNumbers.length;
+
+    // Check if we have at least one scan with a mass list of given name
+    boolean haveMassList = false;
+    for (int i = 0; i < totalScans; i++) {
+      Scan scan = dataFile.getScan(scanNumbers[i]);
+      MassList massList = scan.getMassList(massListName);
+      if (massList != null) {
+        haveMassList = true;
+        break;
+      }
+    }
+    if (!haveMassList) {
+      setStatus(TaskStatus.ERROR);
+      setErrorMessage(dataFile.getName() + " has no mass list called '" + massListName + "'");
+      endMillis = System.currentTimeMillis();
+      return;
+    }
+
+    // obtain errors from all scans
+    for (int i = 0; i < totalScans; i++) {
+
+      if (isCanceled()) {
+        endMillis = System.currentTimeMillis();
+        return;
+      }
+
+      Scan scan = dataFile.getScan(scanNumbers[i]);
+
+      MassList massList = scan.getMassList(massListName);
+
+      // Skip those scans which do not have a mass list of given name
+      if (massList == null) {
+        processedScans++;
+        continue;
+      }
+
+      DataPoint[] mzPeaks = massList.getDataPoints();
+
+      /*List<Double> massListErrors = massCalibrator.findMassListErrors(mzPeaks, scan.getRetentionTime(),
+              massPeakMatches);
+      errors.addAll(massListErrors);*/
+
+      massCalibrator.addMassList(mzPeaks, scan.getRetentionTime(), scanNumbers[i], intensityThreshold);
+
+      processedScans++;
+    }
+
+    massPeakMatches = massCalibrator.getAllMassPeakMatches();
+    Collections.sort(massPeakMatches, MassPeakMatch.measuredMzComparator);
+    errors = massCalibrator.getAllMzErrors();
+    Collections.sort(errors);
+
+    if (errors.size() == 0) {
+      String warningMessage = "No matches were made between the extracted standards list and the mass lists" +
+              " in the selected raw datafile. The module will continue to calibrate mass lists using" +
+              " no matches, the bias estimate is zero, so the mass peaks will be shifted by zero.";
+      logger.warning("Mass calibration warning: " + warningMessage);
+      if (previewRun == false) {
+        MZmineCore.getDesktop().displayMessage("Mass calibration warning", warningMessage);
+      }
+    }
+
+//    Collections.sort(errors);
+//    biasEstimate = massCalibrator.estimateBiasFromErrors(errors, filterDuplicates, errorRanges);
+    biasEstimate = massCalibrator.estimateBias(filterDuplicates);
+    errorRanges = massCalibrator.getErrorRanges();
+
+    if (runCalibrationOnPreview == false && previewRun) {
+      endMillis = System.currentTimeMillis();
+      setStatus(TaskStatus.FINISHED);
+      logger.info("Finished mass calibration on " + dataFile + ", running time: " + getRunningTimeString());
+      return;
+    }
+
+    // mass calibrate all mass lists
+    for (int i = 0; i < totalScans; i++) {
+
+      if (isCanceled()) {
+        endMillis = System.currentTimeMillis();
+        return;
+      }
+
+      Scan scan = dataFile.getScan(scanNumbers[i]);
+
+      MassList massList = scan.getMassList(massListName);
+
+      // Skip those scans which do not have a mass list of given name
+      if (massList == null) {
+        processedScans++;
+        continue;
+      }
+
+      DataPoint[] mzPeaks = massList.getDataPoints();
+
+//      DataPoint[] newMzPeaks = massCalibrator.calibrateMassList(mzPeaks, biasEstimate);
+      DataPoint[] newMzPeaks = massCalibrator.calibrateMassList(mzPeaks);
+
+      SimpleMassList newMassList =
+              new SimpleMassList(massListName + " " + suffix, scan, newMzPeaks);
+
+      scan.addMassList(newMassList);
+
+      // Remove old mass list
+      if (autoRemove && previewRun == false)
+        scan.removeMassList(massList);
+
+      processedScans++;
+    }
+
+    endMillis = System.currentTimeMillis();
+    setStatus(TaskStatus.FINISHED);
+    logger.info("Finished mass calibration on " + dataFile + ", running time: " + getRunningTimeString());
+
+  }
+
+  protected boolean extractStandardsList() {
+    NestedCombo massPeakMatchingMethod = parameters.
+            getParameter(MassCalibrationParameters.massPeakMatchingMethod).getValue();
+    try {
+      if (massPeakMatchingMethod.isCurrentChoice(MassPeakMatchingChoice.UNIVERSAL_CALIBRANTS)) {
+        String universalCalibrantsIonizationMode = massPeakMatchingMethod.getCurrentChoiceParameterSet().
+                getParameter(MassCalibrationParameters.ionizationMode).getValue();
+        String universalCalibrantsFilename = MassCalibrationParameters.ionizationModeChoices.get(universalCalibrantsIonizationMode);
+        URL universalCalibrantsPath = getClass().getClassLoader().getResource(universalCalibrantsFilename);
+        standardsListFilename = universalCalibrantsPath.getPath();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(universalCalibrantsFilename);
+        UniversalCalibrantsListCsvExtractor extractor = new UniversalCalibrantsListCsvExtractor(standardsListFilename,
+                inputStream);
+        standardsListExtractor = extractor;
+      } else {
+        standardsListFilename = massPeakMatchingMethod.getChoices().
+                get(MassPeakMatchingChoice.STANDARDS_LIST.toString()).
+                getParameter(MassCalibrationParameters.standardsList).getValue().getAbsolutePath();
+        standardsListExtractor = StandardsListExtractorFactory.createFromFilename(standardsListFilename, false);
+      }
+      standardsList = standardsListExtractor.extractStandardsList();
+
+      if (standardsList.getStandardMolecules().size() == 0) {
+        throw new RuntimeException("Empty standards list extracted, make sure the file adheres to the expected" +
+                " format, first column is retention time given in minutes, second column is ion formula string.");
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      logger.warning("Exception when extracting standards list from " + standardsListFilename);
+      logger.warning(e.toString());
+      setStatus(TaskStatus.ERROR);
+      setErrorMessage("Exception when extracting standards list from " + standardsListFilename + "\n" + e.toString());
+      return false;
+    }
+    return true;
+  }
+
+  protected void extractToleranceParameters() {
+    NestedCombo massPeakMatchingMethod = parameters.
+            getParameter(MassCalibrationParameters.massPeakMatchingMethod).getValue();
+    ParameterSet massPeakMatchingParameterSet = massPeakMatchingMethod.getCurrentChoiceParameterSet();
+    if (massPeakMatchingMethod.isCurrentChoice(MassPeakMatchingChoice.STANDARDS_LIST)) {
+      mzRatioTolerance = massPeakMatchingParameterSet.getParameter(MassCalibrationParameters.mzRatioTolerance)
+              .getValue();
+      rtTolerance = massPeakMatchingParameterSet.getParameter(MassCalibrationParameters.retentionTimeTolerance)
+              .getValue();
+    } else if (massPeakMatchingMethod.isCurrentChoice(MassPeakMatchingChoice.UNIVERSAL_CALIBRANTS)) {
+      mzRatioTolerance = massPeakMatchingParameterSet.
+              getParameter(MassCalibrationParameters.mzRatioToleranceUniversalCalibrants).getValue();
+      rtTolerance = null;
+    }
+  }
+
+  protected void extractErrorTrend() {
+    NestedCombo trendMethod = parameters.getParameter(MassCalibrationParameters.biasEstimationMethod).getValue();
+    ParameterSet trendParameterSet = trendMethod.getCurrentChoiceParameterSet();
+
+    if (trendMethod.isCurrentChoice(BiasEstimationChoice.KNN_REGRESSION)) {
+      Double percentageNeighbors =
+              trendParameterSet.getParameter(MassCalibrationParameters.nearestNeighborsPercentage).getValue();
+      errorTrend = new ArithmeticMeanKnnTrend(percentageNeighbors / 100.0);
+    } else if (trendMethod.isCurrentChoice(BiasEstimationChoice.OLS_REGRESSION)) {
+      Integer polynomialDegree = trendParameterSet.getParameter(MassCalibrationParameters.polynomialDegree).getValue();
+      Boolean exponentialFeature = trendParameterSet.getParameter(MassCalibrationParameters.exponentialFeature)
+              .getValue();
+      Boolean logarithmicFeature = trendParameterSet.getParameter(MassCalibrationParameters.logarithmicFeature)
+              .getValue();
+      errorTrend = new OLSRegressionTrend(polynomialDegree, exponentialFeature, logarithmicFeature);
+    }
+  }
+
+  /**
+   * Get running time of this task in milliseconds
+   * if it was not started and then finished, then returns null
+   *
+   * @return
+   */
+  public Long getRunningTimeMillis() {
+    if (startMillis != null && endMillis != null) {
+      return endMillis - startMillis;
+    }
+    return null;
+  }
+
+
+  /**
+   * Get running time of this task formatted as a string
+   * if it was not started and then finished, then returns null
+   *
+   * @return
+   */
+  public String getRunningTimeString() {
+    Long runningTimeMillis = getRunningTimeMillis();
+    if (runningTimeMillis == null) {
+      return null;
+    }
+    return DurationFormatUtils.formatDuration(runningTimeMillis, "ss.S's'");
+  }
+
+  public ArrayList<MassPeakMatch> getMassPeakMatches() {
+    return massPeakMatches;
+  }
+
+  public ArrayList<Double> getErrors() {
+    return errors;
+  }
+
+  public HashMap<String, DistributionRange> getErrorRanges() {
+    return errorRanges;
+  }
+
+  public double getBiasEstimate() {
+    return biasEstimate;
+  }
+
+  public Trend2D getErrorVsMzTrend() {
+    return massCalibrator.errorVsMzTrend;
+  }
+
+  public XYSeries getErrorVsMzSeries() {
+    return massCalibrator.errorVsMzSeries;
+  }
+
+}
---
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_masscalibration/charts/ErrorVsMzChart.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2006-2020 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+package io.github.mzmine.modules.dataprocessing.featdet_masscalibration.charts;
+
+
+import com.google.common.collect.Range;
+import io.github.mzmine.gui.chartbasics.gui.javafx.EChartViewer;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.MassPeakMatch;
+import io.github.mzmine.modules.dataprocessing.featdet_masscalibration.errormodeling.DistributionRange;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.XYTextAnnotation;
+import org.jfree.chart.axis.AxisLocation;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.XYToolTipGenerator;
+import org.jfree.chart.plot.DatasetRenderingOrder;
+import org.jfree.chart.plot.ValueMarker;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.ui.Layer;
+import org.jfree.data.general.DatasetUtils;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Chart for error size vs mz ratio plots (xy scatter plot of error values vs mz ratio)
+ * with additional extraction ranges and bias estimates lines shown up
+ * plus estimated trend
+ */
+public class ErrorVsMzChart extends EChartViewer {
+
+  class ErrorVsMzTooltipGenerator implements XYToolTipGenerator {
+    @Override
+    public String generateToolTip(XYDataset dataset, int series, int item) {
+      return ChartUtils.generateTooltipText(matches, item);
+    }
+  }
+
+  private final JFreeChart chart;
+  private final XYPlot plot;
+
+  protected List<MassPeakMatch> matches;
+  protected ArrayList<ValueMarker> rangeMarkers = new ArrayList<>();
+  protected XYTextAnnotation trendNameAnnotation;
+
+  protected ErrorVsMzChart(JFreeChart chart) {
+    super(chart);
+    this.chart = chart;
+    this.plot = chart.getXYPlot();
+    plot.getRenderer(0).setDefaultToolTipGenerator(new ErrorVsMzTooltipGenerator());
+  }
+
+  public ErrorVsMzChart(String title) {
+    this(createEmptyChart(title));
+  }
+
+  public ErrorVsMzChart() {
+    this("Error size vs mz ratio");
+  }
+
+  public static JFreeChart createEmptyChart(String title) {
+    NumberAxis xAxis = new NumberAxis("Measured m/z ratio");
+    NumberAxis yAxis = new NumberAxis("PPM error");
+    XYPlot plot = new XYPlot(null, xAxis, yAxis, null);
+    plot.setRangeAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+    plot.setDomainCrosshairVisible(false);
+    plot.setRangeCrosshairVisible(false);
+
+    plot.setRenderer(0, ChartUtils.createErrorsRenderer());
+    plot.setRenderer(1, ChartUtils.createTrendRenderer());
+    plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+
+//    JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+    JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, false);
+    chart.setTitle((String) null);
+    return chart;
+  }
+
+  public void cleanPlot() {
+    ChartUtils.cleanPlot(plot);
+  }
+
+  public void cleanPlotLabels() {
+    ChartUtils.cleanPlotLabels(plot);
+  }
+
+  public void updatePlot(List<MassPeakMatch> matches, Map<String, DistributionRange> errorRanges,
+                         double biasEstimate, Trend2D errorVsMzTrend) {
+//    this.matches = matches;
+//    updateChartDataset(matches, errorVsMzTrend);
+    this.matches = new ArrayList<>(matches);
+    Collections.sort(this.matches, MassPeakMatch.measuredMzComparator);
+    rangeMarkers.clear();
+    trendNameAnnotation = null;
+    updateChartDataset(this.matches, errorVsMzTrend);
+
+    for (String label : errorRanges.keySet()) {
+      DistributionRange errorRange = errorRanges.get(label);
+      Range<Double> errorValueRange = errorRange.getValueRange();
+
+      ValueMarker valueMarkerLower = ChartUtils.createValueMarker(label + " lower", errorValueRange.lowerEndpoint());
+      ValueMarker valueMarkerUpper = ChartUtils.createValueMarker(label + " upper", errorValueRange.upperEndpoint());
+      rangeMarkers.add(valueMarkerLower);
+      rangeMarkers.add(valueMarkerUpper);
+    }
+    rangeMarkers.add(ChartUtils.createValueMarker("Bias estimate", biasEstimate));
+  }
+
+  public void displayPlotLabels(boolean display) {
+    cleanPlotLabels();
+    if (display) {
+      for (ValueMarker valueMarker : rangeMarkers) {
+//        plot.addRangeMarker(valueMarker);
+        plot.addRangeMarker(0, valueMarker, Layer.FOREGROUND, false);
+      }
+      if (trendNameAnnotation != null) {
+//        plot.addAnnotation(trendNameAnnotation);
+        plot.addAnnotation(trendNameAnnotation, true);
+      }
+    }
+  }
+
+  protected void updateChartDataset(List<MassPeakMatch> matches, Trend2D trend) {
+    XYSeries errorsXY = new XYSeries("PPM errors");
+    for (MassPeakMatch match : matches) {
+      errorsXY.add(match.getMeasuredMzRatio(), match.getMzError());
+    }
+
+    XYSeriesCollection dataset = new XYSeriesCollection(errorsXY);
+    plot.setDataset(0, dataset);
+
+    if (trend != null) {
+      XYSeries trendSeries = DatasetUtils.sampleFunction2DToSeries(trend, dataset.getDomainLowerBound(false),
+              dataset.getDomainUpperBound(false), 1000, "trend series");
+      XYSeriesCollection trendDataset = new XYSeriesCollection(trendSeries);
+      plot.setDataset(1, trendDataset);
+      trendNameAnnotation = new XYTextAnnotation("Trend: " + trend.getName(),
+              plot.getDomainAxis().getRange().getCentralValue(),
+              plot.getRangeAxis().getLowerBound() + plot.getRangeAxis().getRange().getLength() / 10);
+    }
+  }
+}
