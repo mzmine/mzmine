fc9525650fa0bdf06f6b3e546d784a299abe148e
murra668 <murra668@CBS-ES13514641.ad.umn.edu>
2020-11-11 15:03:44 -0600

    Removed deprecated parameters.

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_nist/NistMsSearchParameters.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_nist/NistMsSearchParameters.java
@@ -27,21 +27,17 @@ package io.github.mzmine.modules.dataprocessing.id_nist;
 import java.io.File;
 import java.util.Collection;
 
-import io.github.mzmine.datamodel.IonizationType;
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
-import io.github.mzmine.parameters.parametertypes.BooleanParameter;
-import io.github.mzmine.parameters.parametertypes.ComboParameter;
 import io.github.mzmine.parameters.parametertypes.IntegerParameter;
 import io.github.mzmine.parameters.parametertypes.filenames.DirectoryParameter;
 import io.github.mzmine.parameters.parametertypes.selectors.PeakListsParameter;
-import io.github.mzmine.parameters.parametertypes.tolerances.RTToleranceParameter;
 
 /**
  * Holds NIST MS Search parameters.
  *
  * @author $Author$
- * @version $Revision$
+ * @version 2.0
  */
 public class NistMsSearchParameters extends SimpleParameterSet {
 
@@ -56,50 +52,18 @@ public class NistMsSearchParameters extends SimpleParameterSet {
   public static final DirectoryParameter NIST_MS_SEARCH_DIR =
       new DirectoryParameter("NIST MS Search directory",
           "Full path of the directory containing the NIST MS Search executable (nistms$.exe)");
-
-  /**
-   * Ionization method.
-   */
-  public static final ComboParameter<IonizationType> IONIZATION_METHOD =
-      new ComboParameter<IonizationType>("Ionization method",
-          "Type of ion used to calculate the neutral mass", IonizationType.values());
-
-  /**
-   * Spectrum RT width.
-   */
-  public static final RTToleranceParameter SPECTRUM_RT_WIDTH =
-      new RTToleranceParameter("Spectrum RT tolerance",
-          "The RT tolerance (>= 0) to use when forming search spectra; include all other"
-              + "\ndetected peaks whose RT is within the specified tolerance of a given peak");
-
-  /**
-   * Maximum number of peaks per spectrum.
-   */
-  public static final IntegerParameter MAX_NUM_PEAKS =
-      new IntegerParameter("Max. peaks per spectrum",
-          "The maximum number of peaks to include in a spectrum (0 -> unlimited)", 10, 0, null);
-
-  /**
-   * Must have same identities.
-   */
-
-  public static final BooleanParameter SAME_IDENTITIES = new BooleanParameter(
-      "Must have same identities",
-      "Two peaks will only be included in the same spectrum if they have the same identity (for use with CAMERA pseudo-spectra)",
-      true);
-
   /**
    * Match factor cut-off.
    */
   public static final IntegerParameter MIN_MATCH_FACTOR = new IntegerParameter("Min. match factor",
-      "The minimum match factor (0 .. 1000) that search hits must have", 800, 0, 1000);
+      "The minimum match factor (0 .. 1000) that search hits must have", 700, 0, 1000);
 
   /**
-   * Match factor cut-off.
+   * Reverse match factor cut-off.
    */
   public static final IntegerParameter MIN_REVERSE_MATCH_FACTOR =
       new IntegerParameter("Min. reverse match factor",
-          "The minimum reverse match factor (0 .. 1000) that search hits must have", 800, 0, 1000);
+          "The minimum reverse match factor (0 .. 1000) that search hits must have", 700, 0, 1000);
 
   // NIST MS Search executable.
   private static final String NIST_MS_SEARCH_EXE = "nistms$.exe";
@@ -108,8 +72,8 @@ public class NistMsSearchParameters extends SimpleParameterSet {
    * Construct the parameter set.
    */
   public NistMsSearchParameters() {
-    super(new Parameter[] {PEAK_LISTS, NIST_MS_SEARCH_DIR, IONIZATION_METHOD, SPECTRUM_RT_WIDTH,
-        MAX_NUM_PEAKS, SAME_IDENTITIES, MIN_MATCH_FACTOR, MIN_REVERSE_MATCH_FACTOR});
+    super(new Parameter[] {PEAK_LISTS, NIST_MS_SEARCH_DIR, MIN_MATCH_FACTOR,
+        MIN_REVERSE_MATCH_FACTOR});
   }
 
   @Override
@@ -117,7 +81,7 @@ public class NistMsSearchParameters extends SimpleParameterSet {
 
     // Unsupported OS.
     if (!isWindows()) {
-      errorMessages.add("NIST MS Search is only supported on the Windows operating system.");
+      errorMessages.add("NIST MS Search is only supported on Windows operating systems.");
       return false;
     }
 
