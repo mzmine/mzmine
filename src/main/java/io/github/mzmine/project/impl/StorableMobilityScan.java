52388eafc055a0125591c8229e349cd4da46bbc2
SteffenHeu <steffen.heuckeroth@gmx.de>
2020-12-19 16:33:20 +0100

    added mass list to MobilityScan

--- src/main/java/io/github/mzmine/project/impl/StorableMobilityScan.java
+++ src/main/java/io/github/mzmine/project/impl/StorableMobilityScan.java
@@ -22,11 +22,16 @@ import com.google.common.collect.Range;
 import io.github.mzmine.datamodel.DataPoint;
 import io.github.mzmine.datamodel.Frame;
 import io.github.mzmine.datamodel.ImsMsMsInfo;
+import io.github.mzmine.datamodel.MassList;
 import io.github.mzmine.datamodel.MassSpectrumType;
 import io.github.mzmine.datamodel.MobilityScan;
 import io.github.mzmine.datamodel.MobilityType;
 import io.github.mzmine.util.scans.ScanUtils;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -42,6 +47,7 @@ public class StorableMobilityScan implements MobilityScan {
   private final int spectrumNumber;
   private final int storageId;
   private Range<Double> dataPointsMzRange;
+  private Set<MassList> massLists;
 
   public StorableMobilityScan(MobilityScan originalSpectrum,
       final int storageId) {
@@ -50,6 +56,7 @@ public class StorableMobilityScan implements MobilityScan {
     this.highestDataPoint = originalSpectrum.getHighestDataPoint();
     this.spectrumNumber = originalSpectrum.getSpectrumNumber();
     this.storageId = storageId;
+    this.massLists = new HashSet<>();
   }
 
   @Nonnull
@@ -134,5 +141,64 @@ public class StorableMobilityScan implements MobilityScan {
   @Override
   public ImsMsMsInfo getMsMsInfo() {
     return frame.getImsMsMsInfoForMobilityScan(spectrumNumber);
+  }
+
+  @Override
+  public synchronized void addMassList(final @Nonnull MassList massList) {
+
+    // Remove all mass lists with same name, if there are any
+    MassList currentMassLists[] = massLists.toArray(new MassList[0]);
+    for (MassList ml : currentMassLists) {
+      if (ml.getName().equals(massList.getName())) {
+        removeMassList(ml);
+      }
+    }
+
+    StorableMassList storedMassList;
+    if (massList instanceof StorableMassList) {
+      storedMassList = (StorableMassList) massList;
+    } else {
+      DataPoint massListDataPoints[] = massList.getDataPoints();
+      try {
+        int mlStorageID = ((RawDataFileImpl) getFrame().getDataFile())
+            .storeDataPoints(massListDataPoints);
+        storedMassList = new StorableMassList((RawDataFileImpl) getFrame().getDataFile(),
+            mlStorageID, massList.getName(), null);
+      } catch (IOException e) {
+        //logger.severe("Could not write data to temporary file " + e.toString());
+        return;
+      }
+    }
+
+    // Add the new mass list
+    massLists.add(storedMassList);
+  }
+
+  @Override
+  public synchronized void removeMassList(final @Nonnull MassList massList) {
+
+    // Remove the mass list
+    massLists.remove(massList);
+    if (massList instanceof StorableMassList) {
+      StorableMassList storableMassList = (StorableMassList) massList;
+      storableMassList.removeStoredData();
+    }
+
+  }
+
+  @Override
+  @Nonnull
+  public Set<MassList> getMassLists() {
+    return Objects.requireNonNullElse(massLists, Collections.emptySet());
+  }
+
+  @Override
+  public MassList getMassList(@Nonnull String name) {
+    for (MassList ml : massLists) {
+      if (ml.getName().equals(name)) {
+        return ml;
+      }
+    }
+    return null;
   }
 }
