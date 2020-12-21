87684b36ed6acf406f6c0317b6b48139526732e8
SteffenHeu <steffen.heuckeroth@gmx.de>
2020-12-21 19:00:25 +0100

    Deconv: Rename, Set->Collection, license

--- src/main/java/io/github/mzmine/util/deconvolution/impl/LocalMinimumResolver.java
+++ src/main/java/io/github/mzmine/util/deconvolution/impl/LocalMinimumResolver.java
@@ -1,14 +1,33 @@
+/*
+ * Copyright 2006-2020 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
 package io.github.mzmine.util.deconvolution.impl;
 
 import com.google.common.collect.Range;
-import io.github.mzmine.util.deconvolution.IIndexBasedXYResolver;
-import io.github.mzmine.util.deconvolution.IXYResolver;
+import io.github.mzmine.util.deconvolution.IndexBasedXYResolver;
+import io.github.mzmine.util.deconvolution.XYResolver;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class LocalMinimumResolver implements IXYResolver<Set<Double>, double[], double[]>,
-    IIndexBasedXYResolver<double[], double[]> {
+public class LocalMinimumResolver implements XYResolver<Set<Double>, double[], double[]>,
+    IndexBasedXYResolver<double[], double[]> {
 
   final Range<Double> xRange;
   final float searchXRange;
@@ -186,7 +205,7 @@ public class LocalMinimumResolver implements IXYResolver<Set<Double>, double[], 
    * @return Collection of a Set of indices for each resolved peak.
    */
   @Override
-  public Set<Set<Integer>> resolveToIndices(double[] x, double[] y, int[] indices) {
+  public Collection<? extends Collection<Integer>> resolveToIndices(double[] x, double[] y, int[] indices) {
     if (x.length != y.length || x.length != indices.length) {
       throw new AssertionError("Length of x, y and indices array does not match.");
     }
