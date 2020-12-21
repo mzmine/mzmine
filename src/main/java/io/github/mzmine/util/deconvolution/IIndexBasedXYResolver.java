4d2c0f48b922ab9b9cc8b07272a1433ff16394f8
SteffenHeu <steffen.heuckeroth@gmx.de>
2020-12-21 10:37:58 +0100

    updated javadoc

--- src/main/java/io/github/mzmine/util/deconvolution/IIndexBasedXYResolver.java
+++ src/main/java/io/github/mzmine/util/deconvolution/IIndexBasedXYResolver.java
@@ -2,7 +2,19 @@ package io.github.mzmine.util.deconvolution;
 
 import java.util.Set;
 
+/**
+ *
+ * @param <X> X-value type
+ * @param <Y> Y-value type
+ */
 public interface IIndexBasedXYResolver<X, Y> {
 
+  /**
+   * See implementing classes for more detailed information on possible restrictions on x and y data
+   * such as ordering.
+   *
+   * @param x domain values of the data to be resolved.
+   * @param y range values of the data to be resolved.
+   */
   public Set<Set<Integer>> resolveToIndices(X x, Y y, int[] indices);
 }
