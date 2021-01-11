e79557432ca8d07402911fe63ee4a679a990a5eb
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-01-11 10:17:32 +0100

    c+p msdk mzml import task

---
+++ src/main/java/io/github/mzmine/modules/io/rawdataimport/fileformats/mzml_msdk/msdk/data/MzMLPrecursorActivation.java
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2015-2016 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1 as published by the Free
+ * Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by the Eclipse Foundation.
+ */
+
+package io.github.mzmine.modules.io.rawdataimport.fileformats.mzml_msdk.msdk.data;
+
+import io.github.msdk.io.mzml.data.MzMLCVGroup;
+
+/**
+ * <p>MzMLPrecursorActivation class.</p>
+ *
+ */
+public class MzMLPrecursorActivation extends MzMLCVGroup {
+
+}
---
+++ src/main/java/io/github/mzmine/modules/io/rawdataimport/fileformats/mzml_msdk/msdk/data/MzMLScanList.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright 2015-2016 by MSDK Development Team
+ *
+ * This software is dual-licensed under either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1 as published by the Free
+ * Software Foundation
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by the Eclipse Foundation.
+ */
+
+package io.github.mzmine.modules.io.rawdataimport.fileformats.mzml_msdk.msdk.data;
+
+import io.github.msdk.io.mzml.data.MzMLCVGroup;
+import io.github.msdk.io.mzml.data.MzMLScan;
+import java.util.ArrayList;
+
+/**
+ * <p>MzMLScanList class.</p>
+ *
+ */
+public class MzMLScanList extends MzMLCVGroup {
+  private ArrayList<MzMLScan> scans;
+
+  /**
+   * <p>Constructor for MzMLScanList.</p>
+   */
+  public MzMLScanList() {
+    this.scans = new ArrayList<>();
+  }
+
+  /**
+   * <p>Getter for the field <code>scans</code>.</p>
+   *
+   * @return a {@link ArrayList} object.
+   */
+  public ArrayList<MzMLScan> getScans() {
+    return scans;
+  }
+
+  /**
+   * <p>addScan.</p>
+   *
+   * @param scan a {@link MzMLScan} object.
+   */
+  public void addScan(MzMLScan scan) {
+    scans.add(scan);
+  }
+
+}
