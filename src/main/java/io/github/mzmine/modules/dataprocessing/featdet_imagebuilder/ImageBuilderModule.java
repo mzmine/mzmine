a62d38678a0f15975c05abb7cd61afcfc465c265
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-02-25 08:36:47 +0100

    ported more modules

--- src/main/java/io/github/mzmine/modules/dataprocessing/featdet_imagebuilder/ImageBuilderModule.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_imagebuilder/ImageBuilderModule.java
@@ -20,8 +20,6 @@
 
 package io.github.mzmine.modules.dataprocessing.featdet_imagebuilder;
 
-import java.util.Collection;
-import javax.annotation.Nonnull;
 import io.github.mzmine.datamodel.ImagingRawDataFile;
 import io.github.mzmine.datamodel.MZmineProject;
 import io.github.mzmine.datamodel.RawDataFile;
@@ -31,6 +29,9 @@ import io.github.mzmine.modules.MZmineProcessingModule;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.taskcontrol.Task;
 import io.github.mzmine.util.ExitCode;
+import io.github.mzmine.util.MemoryMapStorage;
+import java.util.Collection;
+import javax.annotation.Nonnull;
 
 /*
  * @author Ansgar Korf (ansgar.korf@uni-muenster.de)
@@ -59,11 +60,13 @@ public class ImageBuilderModule implements MZmineProcessingModule {
     RawDataFile[] files = parameters.getParameter(ImageBuilderParameters.rawDataFiles).getValue()
         .getMatchingRawDataFiles();
 
+    MemoryMapStorage storage = MemoryMapStorage.forFeatureList();
+
     for (RawDataFile file : files) {
       if (!(file instanceof ImagingRawDataFile)) {
         continue;
       }
-      ImageBuilderTask task = new ImageBuilderTask(project, file, parameters);
+      ImageBuilderTask task = new ImageBuilderTask(project, file, parameters, storage);
       MZmineCore.getTaskController().addTask(task);
     }
 
