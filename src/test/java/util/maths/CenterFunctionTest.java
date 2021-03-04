bf3bea724a74c24aefe1fd02c56ad8500882b568
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-03-04 12:21:26 +0100

    added test to center function

---
+++ src/test/java/util/maths/CenterFunctionTest.java
@@ -0,0 +1,35 @@
+package util.maths;
+
+import io.github.mzmine.util.maths.CenterFunction;
+import io.github.mzmine.util.maths.CenterMeasure;
+import io.github.mzmine.util.maths.Weighting;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CenterFunctionTest {
+
+  private static final Logger logger = Logger.getLogger(CenterFunctionTest.class.getName());
+
+  @Test
+  public void testCalcCenter() {
+    // 9 values
+    final double[] mzsNoZeros = {522.0200, 522.0290, 522.0210, 522.0000, 521.9910, 522.0200,
+        522.0200, 522.0200, 522.0200};
+    final double[] intensitiesNoZeros = {1E5, 4E3, 8E4, 9E4, 3E3, 5E5, 3E5, 1E5, 1E5};
+
+    final double[] mzsZeros = {522.0200, 522.0290, 522.0210, 522.0000, 521.9910, 522.0200,
+        522.0200, 522.0200, 522.0200, 0, 0, 0, 0, 0};
+    final double[] intensitiesZeros = {1E5, 4E3, 8E4, 9E4, 3E3, 5E5, 3E5, 1E5, 1E5, 0, 0, 0, 0, 0};
+
+    for(Weighting weighting : Weighting.values()) {
+      final CenterFunction cf = new CenterFunction(CenterMeasure.AVG, Weighting.LINEAR);
+      final double centerNoZeros = cf.calcCenter(mzsNoZeros, intensitiesNoZeros);
+      final double centerZeros = cf.calcCenter(mzsZeros, intensitiesZeros);
+
+      logger.info(weighting.toString() + " Center no zeros: " + centerNoZeros);
+      logger.info(weighting.toString() + " Center zeros: " + centerZeros);
+      Assertions.assertEquals(centerNoZeros, centerZeros);
+    }
+  }
+}
