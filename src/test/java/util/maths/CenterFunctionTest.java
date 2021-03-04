5cb3b5eada82edee6edccdaa571230d9652c460e
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-03-04 13:11:19 +0100

    added test to center function

--- src/test/java/util/maths/CenterFunctionTest.java
+++ src/test/java/util/maths/CenterFunctionTest.java
@@ -11,6 +11,10 @@ public class CenterFunctionTest {
 
   private static final Logger logger = Logger.getLogger(CenterFunctionTest.class.getName());
 
+  /**
+   * This test exists to test if calculation of m/zs is consistent even iv leading/trailing 0s are
+   * added to a feature.
+   */
   @Test
   public void testCalcCenter() {
     // 9 values
@@ -22,7 +26,7 @@ public class CenterFunctionTest {
         522.0200, 522.0200, 522.0200, 0, 0, 0, 0, 0};
     final double[] intensitiesZeros = {1E5, 4E3, 8E4, 9E4, 3E3, 5E5, 3E5, 1E5, 1E5, 0, 0, 0, 0, 0};
 
-    for(Weighting weighting : Weighting.values()) {
+    for (Weighting weighting : Weighting.values()) {
       final CenterFunction cf = new CenterFunction(CenterMeasure.AVG, Weighting.LINEAR);
       final double centerNoZeros = cf.calcCenter(mzsNoZeros, intensitiesNoZeros);
       final double centerZeros = cf.calcCenter(mzsZeros, intensitiesZeros);
