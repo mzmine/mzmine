685582a7190bfc1ae743374bb84236d417d9b850
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-03-26 09:50:59 +0100

    removed XIC and spectrum options from feature table parameters

--- src/main/java/io/github/mzmine/modules/visualization/featurelisttable_modular/FeatureTableFXParameters.java
+++ src/main/java/io/github/mzmine/modules/visualization/featurelisttable_modular/FeatureTableFXParameters.java
@@ -2,15 +2,9 @@ package io.github.mzmine.modules.visualization.featurelisttable_modular;
 
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
-import io.github.mzmine.parameters.parametertypes.BooleanParameter;
 import io.github.mzmine.parameters.parametertypes.datatype.DataTypeCheckListParameter;
 
 public class FeatureTableFXParameters extends SimpleParameterSet {
-
-  public static final BooleanParameter showXIC = new BooleanParameter("Show XIC",
-      "Shows the XIC of the selected feature.", false);
-  public static final BooleanParameter showSpectrum = new BooleanParameter("Show spectrum",
-      "Shows the spectrum of the selected feature.", false);
 
   public static final DataTypeCheckListParameter showRowTypeColumns = new DataTypeCheckListParameter(
       "Row type columns",
@@ -21,7 +15,7 @@ public class FeatureTableFXParameters extends SimpleParameterSet {
       "Specify which data type columns shall be displayed in the feature list table");
 
   public FeatureTableFXParameters() {
-    super(new Parameter[]{showXIC, showSpectrum, showRowTypeColumns, showFeatureTypeColumns});
+    super(new Parameter[]{showRowTypeColumns, showFeatureTypeColumns});
   }
 
 }
