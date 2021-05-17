ed6149d95dcb15f43822812cf93ff0fafca6eb51
Tomas Pluskal <plusik@gmail.com>
2021-05-17 14:55:50 +0200

    Updated the Thermo RAW reader

--- src/main/java/io/github/mzmine/modules/io/import_thermo_raw/ThermoRawImportTask.java
+++ src/main/java/io/github/mzmine/modules/io/import_thermo_raw/ThermoRawImportTask.java
@@ -18,27 +18,29 @@
 
 package io.github.mzmine.modules.io.import_thermo_raw;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.Range;
+import com.sun.jna.Platform;
+import io.github.msdk.datamodel.MsScan;
 import io.github.mzmine.datamodel.MZmineProject;
-import io.github.mzmine.datamodel.MassSpectrumType;
 import io.github.mzmine.datamodel.PolarityType;
 import io.github.mzmine.datamodel.RawDataFile;
-import io.github.mzmine.datamodel.impl.SimpleScan;
+import io.github.mzmine.datamodel.Scan;
+import io.github.mzmine.modules.io.import_mzml_msdk.ConversionUtils;
+import io.github.mzmine.modules.io.import_mzml_msdk.msdk.MzMLFileImportMethod;
+import io.github.mzmine.modules.io.import_mzml_msdk.msdk.data.MzMLMsScan;
 import io.github.mzmine.taskcontrol.AbstractTask;
 import io.github.mzmine.taskcontrol.TaskStatus;
 import io.github.mzmine.util.ExceptionUtils;
-import io.github.mzmine.util.TextUtils;
-import io.github.mzmine.util.scans.ScanUtils;
+import io.github.mzmine.util.ZipUtils;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.nio.file.Files;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.zip.ZipInputStream;
+import org.apache.commons.io.FileUtils;
+
 
 /**
  * This module binds spawns a separate process that dumps the native format's data in a text+binary
@@ -48,12 +50,13 @@ public class ThermoRawImportTask extends AbstractTask {
 
   private Logger logger = Logger.getLogger(this.getClass().getName());
 
-  private File file;
+  private File fileToOpen;
   private MZmineProject project;
   private RawDataFile newMZmineFile;
 
   private Process dumper = null;
 
+  private String taskDescription;
   private int totalScans = 0, parsedScans = 0;
 
   /*
@@ -66,10 +69,13 @@ public class ThermoRawImportTask extends AbstractTask {
   private float retentionTime = 0;
   private double precursorMZ = 0;
 
+  private MzMLFileImportMethod msdkTask;
+
   public ThermoRawImportTask(MZmineProject project, File fileToOpen, RawDataFile newMZmineFile) {
     super(null); // storage in raw data file
     this.project = project;
-    this.file = fileToOpen;
+    this.fileToOpen = fileToOpen;
+    taskDescription = "Opening file " + fileToOpen;
     this.newMZmineFile = newMZmineFile;
   }
 
@@ -88,46 +94,84 @@ public class ThermoRawImportTask extends AbstractTask {
   public void run() {
 
     setStatus(TaskStatus.PROCESSING);
-    logger.info("Opening file " + file);
+    logger.info("Opening file " + fileToOpen);
 
-    // Check the OS we are running
-    String osName = System.getProperty("os.name").toUpperCase();
-    String rawDumpPath = System.getProperty("user.dir") + File.separator + "lib" + File.separator
-        + "vendor_lib" + File.separator + "thermo" + File.separator + "ThermoRawDump.exe";
-    String cmdLine[];
-
-    if (osName.toUpperCase().contains("WINDOWS")) {
-      cmdLine = new String[] {rawDumpPath, file.getPath()};
-    } else {
-      cmdLine = new String[] {"wine", rawDumpPath, file.getPath()};
-    }
-
+    // Unzip ThermoRawFileParser
     try {
+      final File thermoRawFileParserDir = unzipThermoRawFileParser();
+      String thermoRawFileParserCommand;
 
-      // Create a separate process and execute RAWdump.exe
-      dumper = Runtime.getRuntime().exec(cmdLine);
+      if (Platform.isWindows()) {
+        thermoRawFileParserCommand =
+            thermoRawFileParserDir + File.separator
+                + "ThermoRawFileParser.exe";
+      } else if (Platform.isLinux()) {
+        thermoRawFileParserCommand =
+            thermoRawFileParserDir + File.separator
+                + "ThermoRawFileParserLinux";
+      } else if (Platform.isMac()) {
+        thermoRawFileParserCommand =
+            thermoRawFileParserDir + File.separator
+                + "ThermoRawFileParserMac";
+      } else {
+        setStatus(TaskStatus.ERROR);
+        setErrorMessage("Unsupported platform: JNA ID " + Platform.getOSType());
+        return;
+      }
 
-      // Get the stdout of RAWdump.exe process as InputStream
-      InputStream dumpStream = dumper.getInputStream();
-      BufferedInputStream bufStream = new BufferedInputStream(dumpStream);
+      final String cmdLine[] = new String[]{ //
+          thermoRawFileParserCommand, // program to run
+          "-s", // output mzML to stdout
+          "-i", // input RAW file name coming next
+          fileToOpen.getPath() // input RAW file name
+      };
 
-      // Read the dump data
-      readRAWDump(bufStream);
+      // Create a separate process and execute ThermoRawFileParser.
+      // Use thermoRawFileParserDir as working directory; this is essential, otherwise the process will fail.
+      dumper = Runtime.getRuntime().exec(cmdLine, null, thermoRawFileParserDir);
+
+      // Get the stdout of ThermoRawFileParser process as InputStream
+      InputStream mzMLStream = dumper.getInputStream();
+      BufferedInputStream bufStream = new BufferedInputStream(mzMLStream);
+
+      msdkTask = new MzMLFileImportMethod(bufStream);
+      msdkTask.execute();
+      io.github.msdk.datamodel.RawDataFile msdkFile = msdkTask.getResult();
+
+      if (msdkFile == null) {
+        setStatus(TaskStatus.ERROR);
+        setErrorMessage("MSDK returned null");
+        return;
+      }
+      totalScans = msdkFile.getScans().size();
+
+      for (MsScan scan : msdkFile.getScans()) {
+
+        if (isCanceled()) {
+          bufStream.close();
+          dumper.destroy();
+          return;
+        }
+
+        Scan newScan = ConversionUtils.msdkScanToSimpleScan(newMZmineFile, (MzMLMsScan) scan);
+
+        newMZmineFile.addScan(newScan);
+        parsedScans++;
+        taskDescription =
+            "Importing " + fileToOpen.getName() + ", parsed " + parsedScans + "/" + totalScans
+                + " scans";
+      }
 
       // Finish
       bufStream.close();
-
-      if (isCanceled()) {
-        dumper.destroy();
-        return;
-      }
+      dumper.destroy();
 
       if (parsedScans == 0) {
         throw (new Exception("No scans found"));
       }
 
       if (parsedScans != totalScans) {
-        throw (new Exception("RAW dump process crashed before all scans were extracted ("
+        throw (new Exception("ThermoRawFileParser process crashed before all scans were extracted ("
             + parsedScans + " out of " + totalScans + ")"));
       }
 
@@ -149,214 +193,62 @@ public class ThermoRawImportTask extends AbstractTask {
       return;
     }
 
-    logger.info("Finished parsing " + file + ", parsed " + parsedScans + " scans");
+    logger.info("Finished parsing " + fileToOpen + ", parsed " + parsedScans + " scans");
     setStatus(TaskStatus.FINISHED);
 
   }
 
   @Override
   public String getTaskDescription() {
-    return "Opening file " + file;
+    return taskDescription;
   }
 
-  /**
-   * This method reads the dump of the RAW data file produced by RAWdump.exe utility (see
-   * RAWdump.cpp source for details).
-   */
-  private void readRAWDump(InputStream dumpStream) throws IOException {
+  private File unzipThermoRawFileParser() throws IOException {
+    final String tmpPath = System.getProperty("java.io.tmpdir");
+    File thermoRawFileParserFolder = new File(tmpPath, "mzmine_thermo_raw_parser");
+    final File thermoRawFileParserExe = new File(thermoRawFileParserFolder,
+        "ThermoRawFileParser.exe");
 
-    String line;
-    byte byteBuffer[] = new byte[100000];
-    double mzValuesBuffer[] = new double[10000];
-    double intensityValuesBuffer[] = new double[10000];
-
-    while ((line = TextUtils.readLineFromStream(dumpStream)) != null) {
-
-      if (isCanceled()) {
-        return;
-      }
-
-      if (line.startsWith("ERROR: ")) {
-        throw (new IOException(line.substring("ERROR: ".length())));
-      }
-
-      if (line.startsWith("NUMBER OF SCANS: ")) {
-        totalScans = Integer.parseInt(line.substring("NUMBER OF SCANS: ".length()));
-      }
-
-      if (line.startsWith("SCAN NUMBER: ")) {
-        scanNumber = Integer.parseInt(line.substring("SCAN NUMBER: ".length()));
-      }
-
-      if (line.startsWith("SCAN ID: ")) {
-        scanId = line.substring("SCAN ID: ".length());
-      }
-
-      if (line.startsWith("MS LEVEL: ")) {
-        msLevel = Integer.parseInt(line.substring("MS LEVEL: ".length()));
-      }
-
-      if (line.startsWith("POLARITY: ")) {
-        if (line.contains("-")) {
-          polarity = PolarityType.NEGATIVE;
-        } else if (line.contains("+")) {
-          polarity = PolarityType.POSITIVE;
-        } else {
-          polarity = PolarityType.UNKNOWN;
-        }
-
-        // For Thermo RAW files, the polarity is sometimes not recognized. In such case, we can
-        // parse it from the scan filter line (scanId).
-        if ((polarity == PolarityType.UNKNOWN) && (!Strings.isNullOrEmpty(scanId))) {
-          if (scanId.startsWith("-")) {
-            polarity = PolarityType.NEGATIVE;
-          } else if (scanId.startsWith("+")) {
-            polarity = PolarityType.POSITIVE;
-          }
-        }
-
-      }
-
-      if (line.startsWith("RETENTION TIME: ")) {
-        // Retention time is reported in minutes.
-        retentionTime = (float) Double.parseDouble(line.substring("RETENTION TIME: ".length()));
-      }
-
-      if (line.startsWith("PRECURSOR: ")) {
-        String tokens[] = line.split(" ");
-        double token2 = Double.parseDouble(tokens[1]);
-        int token3 = Integer.parseInt(tokens[2]);
-        precursorMZ = token2;
-        precursorCharge = token3;
-
-        // For Thermo RAW files, the MSFileReader library sometimes
-        // returns 0.0 for precursor m/z. In such case, we can parse
-        // the precursor m/z from the scan filter line (scanId).
-        // Examples:
-        // + c ESI SRM ms2 165.000 [118.600-119.600]
-        // FTMS + p ESI d Full ms2 279.16@hcd25.00 [50.00-305.00]
-        if (precursorMZ == 0.0 && (!Strings.isNullOrEmpty(scanId))) {
-          Pattern precursorPattern = Pattern.compile(".* ms\\d+ (\\d+\\.\\d+)[@ ]");
-          Matcher m = precursorPattern.matcher(scanId);
-          if (m.find()) {
-            String precursorMzString = m.group(1);
-            try {
-              precursorMZ = Double.parseDouble(precursorMzString);
-            } catch (Exception e) {
-              e.printStackTrace();
-              // ignore
-            }
-          }
-        }
-      }
-
-      if (line.startsWith("MASS VALUES: ")) {
-        Pattern p = Pattern.compile("MASS VALUES: (\\d+) x (\\d+) BYTES");
-        Matcher m = p.matcher(line);
-        if (!m.matches()) {
-          throw new IOException("Could not parse line " + line);
-        }
-        numOfDataPoints = Integer.parseInt(m.group(1));
-        final int byteSize = Integer.parseInt(m.group(2));
-
-        final int numOfBytes = numOfDataPoints * byteSize;
-        if (byteBuffer.length < numOfBytes) {
-          byteBuffer = new byte[numOfBytes * 2];
-        }
-        dumpStream.read(byteBuffer, 0, numOfBytes);
-
-        ByteBuffer mzByteBuffer =
-            ByteBuffer.wrap(byteBuffer, 0, numOfBytes).order(ByteOrder.LITTLE_ENDIAN);
-        if (mzValuesBuffer.length < numOfDataPoints) {
-          mzValuesBuffer = new double[numOfDataPoints * 2];
-        }
-
-        for (int i = 0; i < numOfDataPoints; i++) {
-          double newValue;
-          if (byteSize == 8) {
-            newValue = mzByteBuffer.getDouble();
-          } else {
-            newValue = mzByteBuffer.getFloat();
-          }
-          mzValuesBuffer[i] = newValue;
-        }
-
-      }
-
-      if (line.startsWith("INTENSITY VALUES: ")) {
-        Pattern p = Pattern.compile("INTENSITY VALUES: (\\d+) x (\\d+) BYTES");
-        Matcher m = p.matcher(line);
-        if (!m.matches()) {
-          throw new IOException("Could not parse line " + line);
-        }
-        // numOfDataPoints must be same for MASS VALUES and INTENSITY
-        // VALUES
-        if (numOfDataPoints != Integer.parseInt(m.group(1))) {
-          throw new IOException("Scan " + scanNumber + " contained " + numOfDataPoints
-              + " mass values, but " + m.group(1) + " intensity values");
-        }
-        final int byteSize = Integer.parseInt(m.group(2));
-
-        final int numOfBytes = numOfDataPoints * byteSize;
-        if (byteBuffer.length < numOfBytes) {
-          byteBuffer = new byte[numOfBytes * 2];
-        }
-        dumpStream.read(byteBuffer, 0, numOfBytes);
-
-        ByteBuffer intensityByteBuffer =
-            ByteBuffer.wrap(byteBuffer, 0, numOfBytes).order(ByteOrder.LITTLE_ENDIAN);
-        if (intensityValuesBuffer.length < numOfDataPoints) {
-          intensityValuesBuffer = new double[numOfDataPoints * 2];
-        }
-        for (int i = 0; i < numOfDataPoints; i++) {
-          double newValue;
-          if (byteSize == 8) {
-            newValue = intensityByteBuffer.getDouble();
-          } else {
-            newValue = intensityByteBuffer.getFloat();
-          }
-          intensityValuesBuffer[i] = newValue;
-        }
-
-        // INTENSITY VALUES was the last item of the scan, so now we can
-        // convert the data to DataPoint[] array and create a new scan
-
-        double mzValues[] = new double[numOfDataPoints];
-        double intensityValues[] = new double[numOfDataPoints];
-        System.arraycopy(mzValuesBuffer, 0, mzValues, 0, numOfDataPoints);
-        System.arraycopy(intensityValuesBuffer, 0, intensityValues, 0, numOfDataPoints);
-
-
-        // Auto-detect whether this scan is centroided
-        MassSpectrumType spectrumType = ScanUtils.detectSpectrumType(mzValues, intensityValues);
-
-        SimpleScan newScan = new SimpleScan(newMZmineFile, scanNumber, msLevel, retentionTime,
-            precursorMZ, precursorCharge, mzValues, intensityValues, spectrumType, polarity, scanId,
-            mzRange);
-        newMZmineFile.addScan(newScan);
-
-        parsedScans++;
-
-        // Clean the variables for next scan
-        scanNumber = 0;
-        scanId = null;
-        polarity = null;
-        mzRange = null;
-        msLevel = 0;
-        retentionTime = 0;
-        precursorMZ = 0;
-        precursorCharge = 0;
-        numOfDataPoints = 0;
-
-      }
-
+    // Check if it has already been unzipped
+    if (thermoRawFileParserFolder.exists() && thermoRawFileParserFolder.isDirectory()
+        && thermoRawFileParserFolder.canRead() && thermoRawFileParserExe.exists()
+        && thermoRawFileParserExe.isFile() && thermoRawFileParserExe.canExecute()) {
+      logger.finest("ThermoRawFileParser found in folder " + thermoRawFileParserFolder);
+      return thermoRawFileParserFolder;
     }
 
+    // In case the folder already exists, unzip to a different folder
+    if (thermoRawFileParserFolder.exists()) {
+      logger.finest("Folder " + thermoRawFileParserFolder + " exists, creating a new one");
+      thermoRawFileParserFolder = Files.createTempDirectory("mzmine_thermo_raw_parser").toFile();
+    }
+
+    logger.finest("Unpacking ThermoRawFileParser to folder " + thermoRawFileParserFolder);
+    InputStream zipStream = getClass()
+        .getResourceAsStream("/vendorlib/thermo/ThermoRawFileParser.zip");
+    if (zipStream == null) {
+      throw new IOException(
+          "Failed to open the resource /vendorlib/thermo/ThermoRawFileParser.zip");
+    }
+    ZipInputStream zipInputStream = new ZipInputStream(zipStream);
+    ZipUtils.unzipStream(zipInputStream, thermoRawFileParserFolder);
+    zipInputStream.close();
+
+    // Delete the temporary folder on application exit
+    FileUtils.forceDeleteOnExit(thermoRawFileParserFolder);
+
+    return thermoRawFileParserFolder;
   }
+
 
   @Override
   public void cancel() {
     super.cancel();
+
+    if (msdkTask != null) {
+      msdkTask.cancel();
+    }
+
     // Try to destroy the dumper process
     if (dumper != null) {
       dumper.destroy();
