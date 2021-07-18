e37a58b795fc8c61bf643d1da72ecd1518bdaeca
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-07-18 16:36:37 +0200

    changed default ms parameters for orbitrap

--- src/main/java/io/github/mzmine/modules/tools/batchwizard/defaults/DefaultMsParameters.java
+++ src/main/java/io/github/mzmine/modules/tools/batchwizard/defaults/DefaultMsParameters.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2006-2020 The MZmine Development Team
+ *
+ *  This file is part of MZmine.
+ *
+ *  MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ *  Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MZmine; if not,
+ *  write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ *  USA
+ */
+
 package io.github.mzmine.modules.tools.batchwizard.defaults;
 
 import io.github.mzmine.modules.tools.batchwizard.BatchWizardMassSpectrometerParameters;
@@ -13,12 +31,12 @@ public class DefaultMsParameters {
       1E2, 1E3, new MZTolerance(0.005, 10), new MZTolerance(0.005, 3), new MZTolerance(0.005, 8));
 
   public static final DefaultMsParameters defaultOrbitrapPositiveParameters = new DefaultMsParameters(
-      1E4, 5E3, 5E4, new MZTolerance(0.005, 5), new MZTolerance(0.005, 3),
-      new MZTolerance(0.005, 5));
+      1E5, 3E3, 3E5, new MZTolerance(0.0015, 10), new MZTolerance(0.0015, 5),
+      new MZTolerance(0.0015, 10));
 
   public static final DefaultMsParameters defaultOrbitrapNegativeParameters = new DefaultMsParameters(
-      1E4, 5E3, 5E4, new MZTolerance(0.005, 5), new MZTolerance(0.005, 3),
-      new MZTolerance(0.005, 5));
+      1E4, 3E3, 5E4, new MZTolerance(0.0015, 10), new MZTolerance(0.0015, 5),
+      new MZTolerance(0.0015, 10));
 
   private final double ms1NoiseLevel;
   private final double ms2NoiseLevel;
