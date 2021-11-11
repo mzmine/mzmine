23ee596fc700ea3435c61f050c07bd85272ed492
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2021-11-11 14:54:44 +0100

    fix exception in ims expander

--- src/main/java/io/github/mzmine/datamodel/impl/SimpleFrame.java
+++ src/main/java/io/github/mzmine/datamodel/impl/SimpleFrame.java
@@ -109,7 +109,6 @@ public class SimpleFrame extends SimpleScan implements Frame {
   @NotNull
   @Override
   public MobilityScan getMobilityScan(int num) {
-    System.out.println("SimpleFrame.getMobilityScan");
     return getMobilityScanStorage().getMobilityScan(num);
   }
 
@@ -119,7 +118,6 @@ public class SimpleFrame extends SimpleScan implements Frame {
   @NotNull
   @Override
   public List<MobilityScan> getMobilityScans() {
-    System.out.println("SimpleFrame.getMobilityScans");
     return getMobilityScanStorage().getMobilityScans();
   }
 
