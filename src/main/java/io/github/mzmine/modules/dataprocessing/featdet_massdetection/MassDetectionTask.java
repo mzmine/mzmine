75c9a2ee9f5b421e71f15b43454e75949300c648
Robin Schmid <rschmid1789@gmail.com>
2021-11-15 09:30:21 -0800

    Fix MassDetectionTask

--- src/main/java/io/github/mzmine/modules/dataprocessing/featdet_massdetection/MassDetectionTask.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_massdetection/MassDetectionTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2021 The MZmine Development Team
+ * Copyright 2006-2020 The MZmine Development Team
  *
  * This file is part of MZmine.
  *
@@ -8,12 +8,11 @@
  * License, or (at your option) any later version.
  *
  * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with MZmine; if not,
- * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 package io.github.mzmine.modules.dataprocessing.featdet_massdetection;
@@ -153,9 +152,11 @@ public class MassDetectionTask extends AbstractTask {
         // [mzs, intensities]
         double[][] mzPeaks = detector.getMassValues(data, massDetector.getParameterSet());
 
+        // add mass list to scans and frames
+        scan.addMassList(new SimpleMassList(getMemoryMapStorage(), mzPeaks[0], mzPeaks[1]));
+
         if (scan instanceof SimpleFrame frame) {
           // for ion mobility, detect subscans, too
-          scan.addMassList(new SimpleMassList(getMemoryMapStorage(), mzPeaks[0], mzPeaks[1]));
           frame.getMobilityScanStorage()
               .generateAndAddMobilityScanMassLists(getMemoryMapStorage(), detector,
                   massDetector.getParameterSet());
