7dfe73a3b68c410beea34aa4f3dae5d7d8680fbb
Robin Schmid <rschmid1789@gmail.com>
2021-11-23 19:42:36 -0800

    Sort column types in checklist parameter

--- src/main/java/io/github/mzmine/parameters/parametertypes/datatype/DataTypeCheckListComponent.java
+++ src/main/java/io/github/mzmine/parameters/parametertypes/datatype/DataTypeCheckListComponent.java
@@ -57,7 +57,7 @@ public class DataTypeCheckListComponent extends StackPane {
 
   public void setValue(Map<String, Boolean> map) {
     dataTypes.clear();
-    map.keySet().forEach(dt -> dataTypes.add(dt));
+    map.keySet().stream().sorted().forEach(dt -> dataTypes.add(dt));
     map.forEach((dt, b) -> {
       if (b) {
         checkList.getCheckModel().check(dt);
