87db34d66d7e4a0fe4cdf61c15bd54f6f1d86a92
Robin Schmid <rschmid1789@gmail.com>
2021-12-16 18:15:06 -0800

    Small optimization of gap fill and ScanUtils.findBasePeak

--- src/main/java/io/github/mzmine/util/scans/ScanUtils.java
+++ src/main/java/io/github/mzmine/util/scans/ScanUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2020 The MZmine Development Team
+ * Copyright 2006-2021 The MZmine Development Team
  *
  * This file is part of MZmine.
  *
@@ -8,11 +8,12 @@
  * License, or (at your option) any later version.
  *
  * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with MZmine; if not,
- * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 package io.github.mzmine.util.scans;
@@ -180,10 +181,14 @@ public class ScanUtils {
    *
    * @param scan    Scan to search
    * @param mzRange mz range to search in
-   * @return double[2] containing base peak m/z and intensity
+   * @return data point containing base peak m/z and intensity
    */
   @Nullable
   public static DataPoint findBasePeak(@NotNull Scan scan, @NotNull Range<Double> mzRange) {
+    final Double scanBasePeakMz = scan.getBasePeakMz();
+    if (mzRange.contains(scanBasePeakMz)) {
+      return new SimpleDataPoint(scanBasePeakMz, scan.getBasePeakIntensity());
+    }
 
     final double lower = mzRange.lowerEndpoint();
     final double upper = mzRange.upperEndpoint();
