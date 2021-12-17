b6fdb9ea23f8ffe8830b64721d4feafd68baa9ab
SteffenHeu <steffen.heuckeroth@gmx.de>
2021-12-17 18:11:09 +0100

    format ResultWindowFX and controller

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_onlinecompounddb/ResultWindowController.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_onlinecompounddb/ResultWindowController.java
@@ -23,6 +23,7 @@ import io.github.mzmine.datamodel.RawDataFile;
 import io.github.mzmine.datamodel.Scan;
 import io.github.mzmine.datamodel.features.Feature;
 import io.github.mzmine.datamodel.features.FeatureListRow;
+import io.github.mzmine.datamodel.features.compoundannotations.SimpleCompoundDBAnnotation;
 import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.modules.visualization.molstructure.MolStructureViewer;
 import io.github.mzmine.modules.visualization.spectra.simplespectra.SpectraVisualizerModule;
@@ -44,178 +45,177 @@ import javafx.scene.control.TableView;
 public class ResultWindowController {
 
 
-    private Logger logger = Logger.getLogger(this.getClass().getName());
-    private final NumberFormat massFormat = MZmineCore.getConfiguration().getMZFormat();
-    private final NumberFormat percentFormat = NumberFormat.getPercentInstance();
-
-    private FeatureListRow peakListRow;
-    private Task searchTask;
-    private double searchedMass;
-
-
-    private final ObservableList<DBCompound> compoundList = FXCollections.observableArrayList();
-
-    @FXML
-    private TableView<DBCompound> IDList;
-    @FXML
-    private TableColumn<DBCompound, String> colID;
-    @FXML
-    private TableColumn <DBCompound, String> colName ;
-    @FXML
-    private TableColumn <DBCompound, String> colFormula ;
-    @FXML
-    private TableColumn <DBCompound, String> colMassDiff ;
-    @FXML
-    private TableColumn <DBCompound, String> colIPS ;
+  private final NumberFormat massFormat = MZmineCore.getConfiguration().getMZFormat();
+  private final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+  private final ObservableList<SimpleCompoundDBAnnotation> compoundList = FXCollections.observableArrayList();
+  private Logger logger = Logger.getLogger(this.getClass().getName());
+  private FeatureListRow peakListRow;
+  private Task searchTask;
+  private double searchedMass;
+  @FXML
+  private TableView<SimpleCompoundDBAnnotation> IDList;
+  @FXML
+  private TableColumn<SimpleCompoundDBAnnotation, String> colID;
+  @FXML
+  private TableColumn<SimpleCompoundDBAnnotation, String> colName;
+  @FXML
+  private TableColumn<SimpleCompoundDBAnnotation, String> colFormula;
+  @FXML
+  private TableColumn<SimpleCompoundDBAnnotation, String> colMassDiff;
+  @FXML
+  private TableColumn<SimpleCompoundDBAnnotation, String> colIPS;
 
 
-    @FXML
-    private void initialize(){
-        colID.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(String.valueOf(cell.getValue().getDatabaseMatchInfo())));
-        colName.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().getCompundName()));
-        colFormula.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().getFormula()));
-        colMassDiff.setCellValueFactory(cell-> {
-            String compFormula = cell.getValue().getFormula();
-            String cellVar = "";
-            if (compFormula != null) {
-                double compMass = FormulaUtils.calculateExactMass(compFormula);
-                double massDifference = Math.abs(searchedMass - compMass);
-                cellVar =  massFormat.format(massDifference);
-            }
-            return new ReadOnlyObjectWrapper<>(cellVar);
-        });
-        colIPS.setCellValueFactory(cell->{
-            Float score = cell.getValue().getIsotopePatternScore();
-            String cellVar ="";
-            if (score != null)
-                cellVar = percentFormat.format(score);
-            return new ReadOnlyObjectWrapper<>(cellVar);
-        });
-        IDList.setItems(compoundList);
+  @FXML
+  private void initialize() {
+    colID.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(
+        String.valueOf(cell.getValue().getDatabaseMatchInfo())));
+    colName.setCellValueFactory(
+        cell -> new ReadOnlyObjectWrapper<>(cell.getValue().getCompundName()));
+    colFormula.setCellValueFactory(
+        cell -> new ReadOnlyObjectWrapper<>(cell.getValue().getFormula()));
+    colMassDiff.setCellValueFactory(cell -> {
+      String compFormula = cell.getValue().getFormula();
+      String cellVar = "";
+      if (compFormula != null) {
+        double compMass = FormulaUtils.calculateExactMass(compFormula);
+        double massDifference = Math.abs(searchedMass - compMass);
+        cellVar = massFormat.format(massDifference);
+      }
+      return new ReadOnlyObjectWrapper<>(cellVar);
+    });
+    colIPS.setCellValueFactory(cell -> {
+      Float score = cell.getValue().getIsotopePatternScore();
+      String cellVar = "";
+        if (score != null) {
+            cellVar = percentFormat.format(score);
+        }
+      return new ReadOnlyObjectWrapper<>(cellVar);
+    });
+    IDList.setItems(compoundList);
+  }
+
+  /**
+   * Initialize values for calculations used in buttons
+   *
+   * @param peakListRow
+   * @param searchTask
+   * @param searchedMass
+   */
+  public void initValues(FeatureListRow peakListRow, Task searchTask, double searchedMass) {
+    this.peakListRow = peakListRow;
+    this.searchTask = searchTask;
+    this.searchedMass = searchedMass;
+  }
+
+  /**
+   * Add compound to the list for viewing in the Table
+   *
+   * @param compound
+   */
+  public void addNewListItem(final SimpleCompoundDBAnnotation compound) {
+    assert Platform.isFxApplicationThread();
+    compoundList.add(compound);
+  }
+
+  /**
+   * Hide the TableView, cancel Task
+   */
+  public void dispose() {
+
+    // Cancel the search task if it is still running
+    TaskStatus searchStatus = searchTask.getStatus();
+    if ((searchStatus == TaskStatus.WAITING) || (searchStatus == TaskStatus.PROCESSING)) {
+      searchTask.cancel();
+    }
+    IDList.getScene().getWindow().hide();
+
+  }
+
+  @FXML
+  public void handleAddAction(ActionEvent actionEvent) {
+    try {
+      SimpleCompoundDBAnnotation compound = IDList.getSelectionModel().getSelectedItem();
+
+      if (compound == null) {
+        MZmineCore.getDesktop()
+            .displayMessage(null, "Select one result to add as compound identity");
+        return;
+
+      }
+
+      peakListRow.addCompoundAnnotation(compound);
+      dispose();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @FXML
+  public void handleViewStructureAction(ActionEvent actionEvent) {
+
+    SimpleCompoundDBAnnotation compound = IDList.getSelectionModel().getSelectedItem();
+
+    if (compound == null) {
+      MZmineCore.getDesktop()
+          .displayMessage(null, "Select one result to display molecule structure");
+      return;
     }
 
-    /**
-     * Initialize values for calculations used in buttons
-     *
-     * @param peakListRow
-     * @param searchTask
-     * @param searchedMass
-     */
-    public void initValues(FeatureListRow peakListRow, Task searchTask, double searchedMass){
-        this.peakListRow = peakListRow;
-        this.searchTask = searchTask;
-        this.searchedMass = searchedMass;
+    URL url2D = compound.get2DStructureURL();
+    URL url3D = compound.get3DStructureURL();
+    String name = compound.getCompundName() + " (" + compound.getDatabaseMatchInfo() + ")";
+    MolStructureViewer viewer = new MolStructureViewer(name, url2D, url3D);
+    viewer.show();
+  }
+
+  @FXML
+  public void handleViewIPAction(ActionEvent actionEvent) {
+    SimpleCompoundDBAnnotation compound = IDList.getSelectionModel().getSelectedItem();
+
+    if (compound == null) {
+      MZmineCore.getDesktop()
+          .displayMessage(null, "Select one result to display the isotope pattern");
+      return;
     }
 
-    /**
-     * Add compound to the list for viewing in the Table
-     *
-     * @param compound
-     */
-    public void addNewListItem(final DBCompound compound) {
-        assert Platform.isFxApplicationThread();
-        compoundList.add(compound);
+    final IsotopePattern predictedPattern = compound.getIsotopePattern();
+
+      if (predictedPattern == null) {
+          return;
+      }
+
+    Feature peak = peakListRow.getBestFeature();
+
+    RawDataFile dataFile = peak.getRawDataFile();
+    Scan scanNumber = peak.getRepresentativeScan();
+    SpectraVisualizerModule.addNewSpectrumTab(dataFile, scanNumber, null, peak.getIsotopePattern(),
+        predictedPattern);
+  }
+
+  @FXML
+  public void handleOpenBrowserAction(ActionEvent actionEvent) {
+    SimpleCompoundDBAnnotation compound = IDList.getSelectionModel().getSelectedItem();
+
+    if (compound == null) {
+      MZmineCore.getDesktop()
+          .displayMessage(null, "Select one compound to display in a web browser");
+      return;
     }
 
-    /**
-     * Hide the TableView, cancel Task
-     */
-    public void dispose() {
+    logger.finest("Launching default browser to display compound details");
 
-        // Cancel the search task if it is still running
-        TaskStatus searchStatus = searchTask.getStatus();
-        if ((searchStatus == TaskStatus.WAITING) || (searchStatus == TaskStatus.PROCESSING)){
-            searchTask.cancel();
-        }
-        IDList.getScene().getWindow().hide();
+    String urlString = compound.getDatabaseUrl();
 
+      if ((urlString == null) || (urlString.length() == 0)) {
+          return;
+      }
+
+    try {
+      MZmineCore.getDesktop().openWebPage(new URL(urlString));
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    @FXML
-    public void handleAddAction(ActionEvent actionEvent) {
-        try {
-            DBCompound compound = IDList.getSelectionModel().getSelectedItem();
-
-            if (compound == null) {
-                MZmineCore.getDesktop().displayMessage(null,
-                        "Select one result to add as compound identity");
-                return;
-
-            }
-
-            peakListRow.addCompoundAnnotation(compound);
-            dispose();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    public void handleViewStructureAction(ActionEvent actionEvent) {
-
-        DBCompound compound = IDList.getSelectionModel().getSelectedItem();
-
-        if (compound==null) {
-            MZmineCore.getDesktop().displayMessage(null,
-                    "Select one result to display molecule structure");
-            return;
-        }
-
-        URL url2D = compound.get2DStructureURL();
-        URL url3D = compound.get3DStructureURL();
-        String name =
-                compound.getCompundName() + " (" + compound.getDatabaseMatchInfo() + ")";
-        MolStructureViewer viewer = new MolStructureViewer(name, url2D, url3D);
-        viewer.show();
-    }
-
-    @FXML
-    public void handleViewIPAction(ActionEvent actionEvent) {
-        DBCompound compound = IDList.getSelectionModel().getSelectedItem();
-
-        if (compound == null) {
-            MZmineCore.getDesktop().displayMessage(null,
-                    "Select one result to display the isotope pattern");
-            return;
-        }
-
-        final IsotopePattern predictedPattern =compound.getIsotopePattern();
-
-        if (predictedPattern == null)
-            return;
-
-        Feature peak = peakListRow.getBestFeature();
-
-        RawDataFile dataFile = peak.getRawDataFile();
-        Scan scanNumber = peak.getRepresentativeScan();
-        SpectraVisualizerModule.addNewSpectrumTab(dataFile, scanNumber, null,
-                peak.getIsotopePattern(), predictedPattern);
-    }
-
-    @FXML
-    public void handleOpenBrowserAction(ActionEvent actionEvent) {
-        DBCompound compound = IDList.getSelectionModel().getSelectedItem();
-
-        if (compound == null) {
-            MZmineCore.getDesktop().displayMessage(null,
-                    "Select one compound to display in a web browser");
-            return;
-        }
-
-        logger.finest("Launching default browser to display compound details");
-
-        String urlString = compound.getDatabaseUrl();
-
-        if ((urlString == null) || (urlString.length() == 0))
-            return;
-
-        try{
-            MZmineCore.getDesktop().openWebPage(new URL(urlString));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+  }
 
 }
