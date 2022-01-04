4578168dbb10cb0ccc1e9ac8ed7a8fc84cd91cb2
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2022-01-04 16:38:47 +0100

    Made import raw data files true by default, added description.

--- src/main/java/io/github/mzmine/modules/io/import_features_mztabm/MzTabmImportParameters.java
+++ src/main/java/io/github/mzmine/modules/io/import_features_mztabm/MzTabmImportParameters.java
@@ -37,7 +37,8 @@ public class MzTabmImportParameters extends SimpleParameterSet {
 
   public static final BooleanParameter importRawFiles = new BooleanParameter(
       "Import raw files too?",
-      "If selected, raw data files will also be imported if they are available (in the msrun location in mzTab-m file or same folder as mzTab-m file in normal, or as zip or gz form). If some raw data files cannot be found, empty files will be generated instead."
+      "If selected, raw data files will also be imported if they are available (in the msrun location in mzTab-m file or same folder as mzTab-m file in normal, or as zip or gz form). If some raw data files cannot be found, empty files will be generated instead. Please note that missing files may lead to issues with other modules that need access to scan data.",
+      Boolean.TRUE
   );
 
   public MzTabmImportParameters() {
