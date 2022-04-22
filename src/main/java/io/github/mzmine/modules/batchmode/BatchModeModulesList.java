1a94198e8d2ece88431bd3d4c963e569b6735f64
Kirill Ponomarov <kirill.ponomarov.work@gmail.com>
2022-04-22 14:42:11 +0200

    id_SIRIUS module is deleted; gradle was updated and some other files previously using id_SIRIUS were updated

--- src/main/java/io/github/mzmine/modules/batchmode/BatchModeModulesList.java
+++ src/main/java/io/github/mzmine/modules/batchmode/BatchModeModulesList.java
@@ -93,7 +93,6 @@ import io.github.mzmine.modules.dataprocessing.id_ms2search.Ms2SearchModule;
 import io.github.mzmine.modules.dataprocessing.id_nist.NistMsSearchModule;
 import io.github.mzmine.modules.dataprocessing.id_onlinecompounddb.OnlineDBSearchModule;
 import io.github.mzmine.modules.dataprocessing.id_precursordbsearch.PrecursorDBSearchModule;
-import io.github.mzmine.modules.dataprocessing.id_sirius.SiriusIdentificationModule;
 import io.github.mzmine.modules.dataprocessing.id_spectral_library_match.SpectralLibrarySearchModule;
 import io.github.mzmine.modules.dataprocessing.norm_linear.LinearNormalizerModule;
 import io.github.mzmine.modules.dataprocessing.norm_rtcalibration.RTCalibrationModule;
@@ -282,7 +281,6 @@ public class BatchModeModulesList {
       NistMsSearchModule.class, //
       OnlineDBSearchModule.class, //
       PrecursorDBSearchModule.class, //
-      SiriusIdentificationModule.class, //
       SpectralLibrarySearchModule.class, //
 
       /*
