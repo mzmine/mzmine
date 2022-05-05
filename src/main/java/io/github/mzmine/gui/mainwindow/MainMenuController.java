782a312ec0dec6436c1cb6efd1a7ec75eaa9bcd4
Robin Schmid <rschmid1789@gmail.com>
2022-05-05 02:37:38 +0200

    add scan mirror to menu

--- src/main/java/io/github/mzmine/gui/mainwindow/MainMenuController.java
+++ src/main/java/io/github/mzmine/gui/mainwindow/MainMenuController.java
@@ -29,6 +29,7 @@ import io.github.mzmine.modules.MZmineRunnableModule;
 import io.github.mzmine.modules.io.projectload.ProjectOpeningTask;
 import io.github.mzmine.modules.tools.batchwizard.BatchWizardModule;
 import io.github.mzmine.modules.visualization.spectra.msn_tree.MSnTreeVisualizerModule;
+import io.github.mzmine.modules.visualization.spectra.simplespectra.mirrorspectra.MirrorScanWindowFXML;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.project.parameterssetup.ProjectParametersSetupDialog;
 import io.github.mzmine.util.ExitCode;
@@ -228,6 +229,11 @@ public class MainMenuController {
 
   public void showMSnTreeTab(ActionEvent actionEvent) {
     MSnTreeVisualizerModule.showNewTab();
+  }
+
+  public void showSpectralMirrorDialog(ActionEvent event) {
+    MirrorScanWindowFXML window = new MirrorScanWindowFXML();
+    window.show();
   }
 }
 
--- src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/mirrorspectra/MirrorScanModule.java
+++ src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/mirrorspectra/MirrorScanModule.java
@@ -18,22 +18,46 @@
 
 package io.github.mzmine.modules.visualization.spectra.simplespectra.mirrorspectra;
 
-import io.github.mzmine.modules.MZmineModule;
+import io.github.mzmine.datamodel.MZmineProject;
+import io.github.mzmine.modules.MZmineModuleCategory;
+import io.github.mzmine.modules.MZmineRunnableModule;
+import io.github.mzmine.modules.tools.isotopepatternpreview.IsotopePatternPreviewParameters;
 import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.taskcontrol.Task;
+import io.github.mzmine.util.ExitCode;
+import java.time.Instant;
+import java.util.Collection;
 import org.jetbrains.annotations.NotNull;
 
-public class MirrorScanModule implements MZmineModule {
+public class MirrorScanModule implements MZmineRunnableModule {
 
-  private static final String MODULE_NAME = "Spectral mirror chart";
+  private static final String MODULE_NAME = "Spectral mirror plot (USI enabled)";
+  private static final String MODULE_DESCRIPTION = "Compare spectra";
 
   @Override
   public @NotNull String getName() {
     return MODULE_NAME;
   }
 
+  public @NotNull MZmineModuleCategory getModuleCategory() {
+    return MZmineModuleCategory.TOOLS;
+  }
+
   @Override
   public @NotNull Class<? extends ParameterSet> getParameterSetClass() {
-    return MirrorScanParameters.class;
+    return IsotopePatternPreviewParameters.class;
+  }
+
+  public @NotNull String getDescription() {
+    return MODULE_DESCRIPTION;
+  }
+
+  @Override
+  public @NotNull ExitCode runModule(@NotNull MZmineProject project,
+      @NotNull ParameterSet parameters, @NotNull Collection<Task> tasks,
+      @NotNull Instant moduleCallDate) {
+
+    return ExitCode.OK;
   }
 
 }
