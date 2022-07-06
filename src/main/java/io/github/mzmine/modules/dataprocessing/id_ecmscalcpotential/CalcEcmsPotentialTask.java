5e627cb14eb0ed9fd2fa27a7cc606f6636af3f1e
m_wesn01 <wesner.mark@web.de>
2022-07-06 10:01:43 +0200

    Review No.3

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_ecmscalcpotential/CalcEcmsPotentialTask.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_ecmscalcpotential/CalcEcmsPotentialTask.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2006-2022 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package io.github.mzmine.modules.dataprocessing.id_ecmscalcpotential;
 
 import io.github.mzmine.datamodel.FeatureStatus;
@@ -53,8 +70,7 @@ public class CalcEcmsPotentialTask extends AbstractTask {
     tubingId = parameters.getValue(CalcEcmsPotentialParameters.tubingIdMM);
     flowRate = parameters.getValue(CalcEcmsPotentialParameters.flowRateMicroLiterPerMin);
     potentialRampSpeed = parameters.getValue(CalcEcmsPotentialParameters.potentialRampSpeed);
-    potentialAssignmentPercentage = parameters.getValue(
-        CalcEcmsPotentialParameters.potentialAssignmentIntensityPercentage);
+    potentialAssignmentPercentage = parameters.getValue(CalcEcmsPotentialParameters.potentialAssignmentIntensityPercentage);
     numRows = Arrays.stream(flists).mapToInt(ModularFeatureList::getNumberOfRows).sum();
   }
 
--- src/main/java/io/github/mzmine/modules/dataprocessing/id_ecmscalcpotential/EcmsUtils.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_ecmscalcpotential/EcmsUtils.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2006-2022 The MZmine Development Team
+ *
+ * This file is part of MZmine.
+ *
+ * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MZmine; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package io.github.mzmine.modules.dataprocessing.id_ecmscalcpotential;
 
 public class EcmsUtils {
@@ -27,8 +44,7 @@ public class EcmsUtils {
    * @param delayTime          Delay time in s.
    * @return The potential at the given retention time.
    */
-  public static double getPotentialAtRt(final float rt, final double delayTime,
-      final double potentialRampSpeed) {
+  public static double getPotentialAtRt(final float rt, final double delayTime, final double potentialRampSpeed) {
     return (rt * 60d - delayTime) * potentialRampSpeed;
   }
 
@@ -38,8 +54,7 @@ public class EcmsUtils {
    * @param potential   Potential in mV.
    * @return            The retention time of a given potential.
    */
-  public static float getRtAtPotential(final double delayTime, final double rampSpeed,
-      final double potential) {
+  public static float getRtAtPotential(final double delayTime, final double rampSpeed, final double potential) {
     return (float) ((float) delayTime + (potential / (rampSpeed * 60d)));
   }
 }
