50eafc79fa5fb16eb1a05c04dd3f0cbaa45c9f0d
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2022-07-30 15:54:19 +0200

    added distance check for msms spots

--- src/main/java/io/github/mzmine/modules/tools/timstofmaldiacq/imaging/TimsTOFImageMsMsTask.java
+++ src/main/java/io/github/mzmine/modules/tools/timstofmaldiacq/imaging/TimsTOFImageMsMsTask.java
@@ -18,8 +18,6 @@
 
 package io.github.mzmine.modules.tools.timstofmaldiacq.imaging;
 
-import com.google.common.collect.Range;
-import com.google.common.io.Files;
 import io.github.mzmine.datamodel.ImagingFrame;
 import io.github.mzmine.datamodel.MZmineProject;
 import io.github.mzmine.datamodel.Scan;
@@ -36,11 +34,7 @@ import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.taskcontrol.AbstractTask;
 import io.github.mzmine.taskcontrol.TaskStatus;
 import io.github.mzmine.util.MemoryMapStorage;
-import io.github.mzmine.util.RangeUtils;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -48,9 +42,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.ObservableList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -137,11 +129,13 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
     }
 
     final int numMsMs = 3;
-    final double minDistance = 30;
+    final double minDistance = 50;
     final double minMsMsIntensity = 10_000;
 
-    final Map<ImagingFrame, ImagingSpot> spotMap = new HashMap<>();
-    ObservableList<FeatureListRow> rows = flist.getRows();
+    final Map<ImagingFrame, ImagingSpot> frameSpotMap = new HashMap<>();
+    final Map<Feature, List<MaldiSpotInfo>> featureSpotMap = new HashMap<>();
+    List<FeatureListRow> rows = new ArrayList<>(flist.getRows());
+    rows.sort(Comparator.comparingDouble(FeatureListRow::getAverageHeight).reversed());
     for (int i = 0; i < rows.size(); i++) {
       progress = 0.1 * i / (double) rows.size();
 
@@ -163,8 +157,8 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
       final IonTimeSeries<? extends ImagingFrame> imagingData = (IonTimeSeries<? extends ImagingFrame>) data;
 
       // check existing msms spots first
-      int createdMsMsEntries = addEntriesToExistingSpots(minMsMsIntensity, spotMap, precursor,
-          imagingData, numMsMs);
+      int createdMsMsEntries = addEntriesToExistingSpots(minMsMsIntensity, frameSpotMap, precursor,
+          imagingData, numMsMs, featureSpotMap, minDistance);
 
       // we have all needed entries
       if (createdMsMsEntries >= numMsMs) {
@@ -172,13 +166,16 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
       }
 
       // find new entries
-      createdMsMsEntries = createNewMsMsSpots(spotMap, minMsMsIntensity, imagingData, precursor,
-          numMsMs, createdMsMsEntries);
+      createdMsMsEntries = createNewMsMsSpots(frameSpotMap, minMsMsIntensity, imagingData,
+          precursor, numMsMs, createdMsMsEntries, featureSpotMap, minDistance);
 
-      logger.finest(() -> "Did not find enough MSMS spots for feature " + f.toString());
+      if (createdMsMsEntries < numMsMs) {
+        logger.finest(() -> "Did not find enough MSMS spots for feature " + f.toString());
+        continue;
+      }
     }
 
-    final List<ImagingSpot> sortedSpots = spotMap.entrySet().stream().sorted((e1, e2) -> {
+    final List<ImagingSpot> sortedSpots = frameSpotMap.entrySet().stream().sorted((e1, e2) -> {
       int xCompare = Integer.compare(e1.getKey().getMaldiSpotInfo().xIndexPos(),
           e2.getValue().spotInfo().xIndexPos());
       if (xCompare != 0) {
@@ -216,10 +213,12 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
 
   private int createNewMsMsSpots(Map<ImagingFrame, ImagingSpot> spotMap, double minMsMsIntensity,
       IonTimeSeries<? extends ImagingFrame> imagingData, MaldiTimsPrecursor precursor, int numMsMs,
-      int currentNumSpots) {
+      int currentNumSpots, Map<Feature, List<MaldiSpotInfo>> featureSpotMap, double minDistance) {
     final IntensitySortedSeries<IonTimeSeries<? extends ImagingFrame>> imagingSorted = new IntensitySortedSeries<>(
         imagingData);
 
+    final List<MaldiSpotInfo> spots = featureSpotMap.computeIfAbsent(precursor.feature(),
+        f -> new ArrayList<>());
     while (imagingSorted.hasNext() && currentNumSpots < numMsMs) {
       final Integer nextIndex = imagingSorted.next();
       final ImagingFrame frame = imagingData.getSpectrum(nextIndex);
@@ -227,6 +226,11 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
       // sorted by intensity, if we are below the threshold, don't add a new scan
       if (imagingData.getIntensity(nextIndex) < minMsMsIntensity) {
         break;
+      }
+
+      // check if we meet the minimum distance requirement
+      if (!checkDistanceForSpots(minDistance, spots, frame.getMaldiSpotInfo())) {
+        continue;
       }
 
       final MaldiSpotInfo spotInfo = frame.getMaldiSpotInfo();
@@ -245,13 +249,25 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
 
   private int addEntriesToExistingSpots(double minMsMsIntensity,
       Map<ImagingFrame, ImagingSpot> spotMap, MaldiTimsPrecursor precursor,
-      IonTimeSeries<? extends ImagingFrame> imagingData, final int numMsMs) {
+      IonTimeSeries<? extends ImagingFrame> imagingData, final int numMsMs,
+      Map<Feature, List<MaldiSpotInfo>> featureSpotMap, double minDistance) {
     int createdMsMsEntries = 0;
     final List<ImagingFrame> usedFrames = checkExistingSpots(spotMap, imagingData,
         minMsMsIntensity);
+
+    final List<MaldiSpotInfo> spots = featureSpotMap.computeIfAbsent(precursor.feature(),
+        f -> new ArrayList<>());
     for (ImagingFrame usedFrame : usedFrames) {
       final ImagingSpot imagingSpot = spotMap.get(usedFrame);
+
+      // check if we meet the minimum distance requirement
+      if (!checkDistanceForSpots(minDistance, spots, imagingSpot.spotInfo())) {
+        continue;
+      }
+
+      // check if the entry fits into the precursor ramp at that spot
       if (imagingSpot.addPrecursor(precursor)) {
+        spots.add(imagingSpot.spotInfo());
         createdMsMsEntries++;
       }
       if (createdMsMsEntries >= numMsMs) {
@@ -278,128 +294,22 @@ public class TimsTOFImageMsMsTask extends AbstractTask {
     return frames;
   }
 
-  private boolean acquire(final File acqControl, final String spot,
-      final List<MaldiTimsPrecursor> precursorList, final int initialOffsetY,
-      final int incrementOffsetX, int precursorListCounter, int spotIncrement,
-      final File savePathDir, String name, File currentCeFile) {
-    List<String> cmdLine = new ArrayList<>();
-
-    cmdLine.add(acqControl.toString());
-    cmdLine.add("--spot");
-    cmdLine.add(spot);
-
-    cmdLine.add("--xoffset");
-    cmdLine.add(String.valueOf(incrementOffsetX * spotIncrement));
-    cmdLine.add("--yoffset");
-    cmdLine.add(String.valueOf(initialOffsetY));
-
-    cmdLine.add("--path");
-    cmdLine.add(savePathDir.toString().replace(File.separatorChar, '/'));
-
-    cmdLine.add("--name");
-    cmdLine.add(name);
-
-    cmdLine.add("--acqtype");
-    cmdLine.add("accumulate");
-
-    if (enableCeStepping && currentCeFile != null && currentCeFile.exists()) {
-      cmdLine.add("--cetable");
-      cmdLine.add(currentCeFile.toPath().toString());
-    }
-
-    replacePrecursorCsv(precursorList, true, spot, precursorListCounter);
-
-    if (!exportOnly) {
-      final ProcessBuilder builder = new ProcessBuilder(cmdLine).inheritIO();
-      final Process process;
-
-      try {
-        process = builder.start();
-        process.waitFor();
-      } catch (IOException | InterruptedException e) {
-        logger.log(Level.WARNING,
-            "Could not acquire spot " + spot + ". Process finished irregularly.", e);
-
+  public boolean checkDistanceForSpots(double minDistance, List<MaldiSpotInfo> spots,
+      MaldiSpotInfo maldiSpotInfo) {
+    for (MaldiSpotInfo spot : spots) {
+      if (!checkDistanceForSpot(spot, maldiSpotInfo, minDistance)) {
+        logger.finest("distance too low");
         return false;
       }
     }
     return true;
   }
 
-  private void replacePrecursorCsv(List<MaldiTimsPrecursor> precursorList, boolean createCopy,
-      String spot, int counter) {
+  public boolean checkDistanceForSpot(MaldiSpotInfo spot1, MaldiSpotInfo spot2,
+      double minDistance) {
+    var dx = spot2.xIndexPos() - spot1.xIndexPos();
+    var dy = spot2.yIndexPos() - spot1.yIndexPos();
 
-    // if we only export, just create the export file
-    final File csv =
-        !exportOnly ? new File("C:\\BDALSystemData\\timsTOF\\maldi\\maldi_tims_precursors.csv")
-            : new File(savePathDir, "maldi_tims_precursors_" + spot + "_msms_" + counter + ".csv");
-    if (csv.exists()) {
-      final boolean deleted = csv.delete();
-      if (!deleted) {
-        throw new IllegalStateException("Cannot delete maldi_tims_precursors.csv file.");
-      }
-    }
-
-    try {
-      csv.createNewFile();
-    } catch (IOException e) {
-      logger.log(Level.WARNING, "Cannot create maldi_tims_precursors.csv file", e);
-      return;
-    }
-
-    try (var writer = new FileWriter(csv)) {
-
-      BufferedWriter w = new BufferedWriter(writer);
-      w.write("1");  // activate CE settings from the MALDI MS/MS tab
-      w.newLine();
-
-      // make sure the precursors are sorted
-      precursorList.sort(Comparator.comparingDouble(p -> p.oneOverK0().lowerEndpoint()));
-
-      for (final MaldiTimsPrecursor precursor : precursorList) {
-        w.write(
-            String.format("%.4f,%.3f,%.3f", precursor.mz(), precursor.oneOverK0().lowerEndpoint(),
-                precursor.oneOverK0().upperEndpoint()));
-        w.newLine();
-      }
-
-      w.flush();
-      w.close();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-
-    // only copy the file, if it was an actual acquisition
-    if (createCopy && !exportOnly) {
-      try {
-        Files.copy(csv,
-            new File(savePathDir, "maldi_tims_precursors_" + spot + "_msms_" + counter + ".csv"));
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    }
+    return minDistance < Math.sqrt(dx * dx + dy * dy);
   }
-
-  private Range<Float> adjustMobilityRange(Float mobility, Range<Float> initial,
-      Double minMobilityWidth, Double maxMobilityWidth) {
-
-    final Float initialLength = RangeUtils.rangeLength(initial);
-
-    if (initialLength <= maxMobilityWidth && initialLength >= minMobilityWidth) {
-      return initial;
-    } else if (initialLength < minMobilityWidth) {
-      return Range.closed((float) (mobility - minMobilityWidth / 2),
-          (float) (mobility + minMobilityWidth / 2));
-    } else if (initialLength > maxMobilityWidth) {
-      return Range.closed((float) (mobility - maxMobilityWidth / 2),
-          (float) (mobility + maxMobilityWidth / 2));
-    }
-
-    logger.fine(
-        () -> String.format("Unexpected mobility range length: %.3f. Min = %.3f, Max = %.3f",
-            initialLength, minMobilityWidth, maxMobilityWidth));
-    return initial;
-  }
-
-
 }
