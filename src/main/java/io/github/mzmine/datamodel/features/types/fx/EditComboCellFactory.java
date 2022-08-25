306f4afe64ca3f00668485aefd938eca2c80a08f
SteffenHeu <steffen.heuckeroth@gmx.de>
2022-08-25 20:35:28 +0200

    fix cell factory going crazy in jfx 17

--- src/main/java/io/github/mzmine/datamodel/features/types/fx/EditComboCellFactory.java
+++ src/main/java/io/github/mzmine/datamodel/features/types/fx/EditComboCellFactory.java
@@ -1,19 +1,19 @@
 /*
- * Copyright 2006-2021 The MZmine Development Team
+ *  Copyright 2006-2022 The MZmine Development Team
  *
- * This file is part of MZmine.
+ *  This file is part of MZmine.
  *
- * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ *  MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
  *
- * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ *  MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ *  Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with MZmine; if not,
- * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ *  You should have received a copy of the GNU General Public License along with MZmine; if not,
+ *  write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ *  USA
  */
 
 package io.github.mzmine.datamodel.features.types.fx;
@@ -90,6 +90,10 @@ public class EditComboCellFactory implements
       @Override
       public void updateItem(Object item, boolean empty) {
         super.updateItem(item, empty);
+        if(!getTableRow().isVisible()) {
+          // fix to make the cell factory not go crazy and create invisible nodes
+          return;
+        }
 
         if (item == null || empty) {
           setGraphic(null);
@@ -139,7 +143,7 @@ public class EditComboCellFactory implements
        * @return
        */
       private List getTypeList() {
-        ModularFeatureListRow row = getTreeTableRow().getItem();
+        ModularFeatureListRow row = getTableRow().getItem();
         ModularDataModel model = raw == null ? row : row.getFeature(raw);
         final Object value;
         if (parentType instanceof DataType pt) {
