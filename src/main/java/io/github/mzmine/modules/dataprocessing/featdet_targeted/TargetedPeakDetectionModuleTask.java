24c96d7e81261c243840d47510c1f575dfa21137
SteffenHeu <steffen.heuckeroth@gmx.de>
2022-10-05 23:43:54 +0200

    update TargetedPeakDetectionModuleTask

--- src/main/java/io/github/mzmine/modules/dataprocessing/featdet_targeted/TargetedPeakDetectionModuleTask.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_targeted/TargetedPeakDetectionModuleTask.java
@@ -1,26 +1,19 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ *  Copyright 2006-2022 The MZmine Development Team
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
+ *  This file is part of MZmine.
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ *  MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ *  MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ *  Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MZmine; if not,
+ *  write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ *  USA
  */
 package io.github.mzmine.modules.dataprocessing.featdet_targeted;
 
@@ -28,15 +21,17 @@ import com.Ostermiller.util.CSVParser;
 import com.google.common.collect.Range;
 import io.github.mzmine.datamodel.MZmineProject;
 import io.github.mzmine.datamodel.RawDataFile;
-import io.github.mzmine.datamodel.Scan;
+import io.github.mzmine.datamodel.data_access.EfficientDataAccess;
+import io.github.mzmine.datamodel.data_access.EfficientDataAccess.ScanDataType;
+import io.github.mzmine.datamodel.data_access.ScanDataAccess;
 import io.github.mzmine.datamodel.features.FeatureList;
 import io.github.mzmine.datamodel.features.FeatureListRow;
 import io.github.mzmine.datamodel.features.ModularFeatureList;
 import io.github.mzmine.datamodel.features.ModularFeatureListRow;
 import io.github.mzmine.datamodel.features.SimpleFeatureListAppliedMethod;
-import io.github.mzmine.datamodel.impl.SimpleFeatureIdentity;
-import io.github.mzmine.modules.dataprocessing.featdet_massdetection.centroid.CentroidMassDetectorParameters;
+import io.github.mzmine.modules.dataprocessing.gapfill_peakfinder.Gap;
 import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.parameters.parametertypes.selectors.ScanSelection;
 import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
 import io.github.mzmine.parameters.parametertypes.tolerances.RTTolerance;
 import io.github.mzmine.taskcontrol.AbstractTask;
@@ -46,8 +41,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jetbrains.annotations.NotNull;
@@ -72,7 +69,6 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
   private boolean ignoreFirstLine;
   private int finishedLines = 0;
   private int ID = 1;
-  private double noiseLevel;
 
   TargetedPeakDetectionModuleTask(MZmineProject project, ParameterSet parameters,
       RawDataFile dataFile, @Nullable MemoryMapStorage storage, @NotNull Instant moduleCallDate) {
@@ -84,15 +80,14 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
     suffix = parameters.getParameter(TargetedPeakDetectionParameters.suffix).getValue();
     msLevel = parameters.getParameter(TargetedPeakDetectionParameters.msLevel).getValue();
     peakListFile = parameters.getParameter(TargetedPeakDetectionParameters.peakListFile).getValue();
-    fieldSeparator =
-        parameters.getParameter(TargetedPeakDetectionParameters.fieldSeparator).getValue();
-    ignoreFirstLine =
-        parameters.getParameter(TargetedPeakDetectionParameters.ignoreFirstLine).getValue();
+    fieldSeparator = parameters.getParameter(TargetedPeakDetectionParameters.fieldSeparator)
+        .getValue();
+    ignoreFirstLine = parameters.getParameter(TargetedPeakDetectionParameters.ignoreFirstLine)
+        .getValue();
 
     intTolerance = parameters.getParameter(TargetedPeakDetectionParameters.intTolerance).getValue();
     mzTolerance = parameters.getParameter(TargetedPeakDetectionParameters.MZTolerance).getValue();
     rtTolerance = parameters.getParameter(TargetedPeakDetectionParameters.RTTolerance).getValue();
-    noiseLevel = parameters.getParameter(CentroidMassDetectorParameters.noiseLevel).getValue();
 
     this.dataFile = dataFile;
   }
@@ -102,10 +97,11 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
     setStatus(TaskStatus.PROCESSING);
 
     // Calculate total number of scans in all files
-    totalScans = dataFile.getNumOfScans(1);
+    totalScans = dataFile.getNumOfScans(msLevel);
 
     // Create new feature list
-    processedPeakList = new ModularFeatureList(dataFile.getName() + " " + suffix, getMemoryMapStorage(), dataFile);
+    processedPeakList = new ModularFeatureList(dataFile.getName() + " " + suffix,
+        getMemoryMapStorage(), dataFile);
 
     List<PeakInformation> peaks = this.readFile();
 
@@ -114,67 +110,56 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
       setErrorMessage("Could not read file or the file is empty ");
       return;
     }
+
+    final Map<FeatureListRow, Gap> rowGapMap = new HashMap<>();
+    List<io.github.mzmine.modules.dataprocessing.gapfill_peakfinder.Gap> gaps = new ArrayList<>();
     // Fill new feature list with empty rows
     for (int row = 0; row < peaks.size(); row++) {
-      FeatureListRow newRow = new ModularFeatureListRow((ModularFeatureList) processedPeakList, ID++);
-      processedPeakList.addRow(newRow);
-    }
+      FeatureListRow newRow = new ModularFeatureListRow((ModularFeatureList) processedPeakList,
+          ID++);
 
-    // Process all raw data files
+      Range<Double> mzRange = mzTolerance.getToleranceRange(peaks.get(row).getMZ());
+      Range<Float> rtRange = rtTolerance.getToleranceRange((float) peaks.get(row).getRT());
+      newRow.addCompoundAnnotation(peaks.get(row).toCompountDBAnnotation());
+
+      io.github.mzmine.modules.dataprocessing.gapfill_peakfinder.Gap newGap = new io.github.mzmine.modules.dataprocessing.gapfill_peakfinder.Gap(
+          newRow, dataFile, mzRange, rtRange, intTolerance);
+      gaps.add(newGap);
+
+      rowGapMap.put(newRow, newGap);
+    }
 
     // Canceled?
     if (isCanceled()) {
       return;
     }
 
-    List<Gap> gaps = new ArrayList<Gap>();
-
-    // Fill each row of this raw data file column, create new empty
-    // gaps if necessary
-    for (int row = 0; row < peaks.size(); row++) {
-      FeatureListRow newRow = processedPeakList.getRow(row);
-      // Create a new gap
-
-      Range<Double> mzRange = mzTolerance.getToleranceRange(peaks.get(row).getMZ());
-      Range<Float> rtRange = rtTolerance.getToleranceRange((float) peaks.get(row).getRT());
-      newRow.addFeatureIdentity(new SimpleFeatureIdentity(peaks.get(row).getName()), true);
-
-      Gap newGap = new Gap(newRow, dataFile, mzRange, rtRange, intTolerance, noiseLevel);
-      gaps.add(newGap);
-    }
-
-    // Stop processing this file if there are no gaps
-    if (gaps.isEmpty()) {
-      processedScans += dataFile.getNumOfScans();
-    }
-
     // Get all scans of this data file
-    Scan scanNumbers[] = dataFile.getScanNumbers(msLevel).toArray(Scan[]::new);
-    if (scanNumbers == null) {
-      logger.log(Level.WARNING, "Could not read file with the MS level of " + msLevel);
-      setStatus(TaskStatus.ERROR);
-      return;
-    }
+    final ScanDataAccess access = EfficientDataAccess.of(dataFile, ScanDataType.CENTROID,
+        new ScanSelection(msLevel));
 
-    // Process each scan
-    for (Scan scan : scanNumbers) {
-
+    while (access.hasNextScan()) {
+      access.nextScan();
       // Canceled?
       if (isCanceled()) {
         return;
       }
 
       // Feed this scan to all gaps
-      for (Gap gap : gaps) {
-        gap.offerNextScan(scan);
+      for (io.github.mzmine.modules.dataprocessing.gapfill_peakfinder.Gap gap : gaps) {
+        gap.offerNextScan(access);
       }
 
       processedScans++;
     }
 
     // Finalize gaps
-    for (Gap gap : gaps) {
-      gap.noMoreOffers();
+    for (Entry<FeatureListRow, Gap> entry : rowGapMap.entrySet()) {
+      final FeatureListRow row = entry.getKey();
+      final Gap gap = entry.getValue();
+      if (gap.noMoreOffers()) {
+        processedPeakList.addRow(row);
+      }
     }
 
     // Append processed feature list to the project
@@ -194,10 +179,8 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
   }
 
   public List<PeakInformation> readFile() {
-    FileReader dbFileReader = null;
-    try {
-      List<PeakInformation> list = new ArrayList<PeakInformation>();
-      dbFileReader = new FileReader(peakListFile);
+    try(final FileReader dbFileReader = new FileReader(peakListFile)) {
+      List<PeakInformation> list = new ArrayList<>();
 
       String[][] peakListValues = CSVParser.parse(dbFileReader, fieldSeparator.charAt(0));
 
@@ -220,9 +203,7 @@ class TargetedPeakDetectionModuleTask extends AbstractTask {
           // ignore incorrect lines
         }
       }
-      dbFileReader.close();
       return list;
-
     } catch (Exception e) {
       e.printStackTrace();
       logger.log(Level.WARNING, "Could not read file " + peakListFile, e);
--- src/main/java/io/github/mzmine/modules/dataprocessing/featdet_targeted/TargetedPeakDetectionParameters.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_targeted/TargetedPeakDetectionParameters.java
@@ -1,34 +1,25 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ *  Copyright 2006-2022 The MZmine Development Team
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
+ *  This file is part of MZmine.
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ *  MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ *  MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ *  Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MZmine; if not,
+ *  write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ *  USA
  */
 package io.github.mzmine.modules.dataprocessing.featdet_targeted;
 
-import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
 import io.github.mzmine.parameters.parametertypes.BooleanParameter;
-import io.github.mzmine.parameters.parametertypes.DoubleParameter;
 import io.github.mzmine.parameters.parametertypes.IntegerParameter;
 import io.github.mzmine.parameters.parametertypes.PercentParameter;
 import io.github.mzmine.parameters.parametertypes.StringParameter;
@@ -54,15 +45,12 @@ public class TargetedPeakDetectionParameters extends SimpleParameterSet {
       new BooleanParameter("Ignore first line", "Ignore the first line of database file");
   public static final PercentParameter intTolerance = new PercentParameter("Intensity tolerance",
       "Maximum allowed deviation from expected /\\ shape of a peak in chromatographic direction");
-  public static final DoubleParameter noiseLevel = new DoubleParameter("Noise level",
-      "Intensities less than this value are interpreted as noise",
-      MZmineCore.getConfiguration().getIntensityFormat());
   public static final MZToleranceParameter MZTolerance = new MZToleranceParameter();
   public static final RTToleranceParameter RTTolerance = new RTToleranceParameter();
 
   public TargetedPeakDetectionParameters() {
     super(new Parameter[] {rawDataFile, msLevel, suffix, peakListFile, fieldSeparator,
-        ignoreFirstLine, intTolerance, noiseLevel, MZTolerance, RTTolerance},
+        ignoreFirstLine, intTolerance, MZTolerance, RTTolerance},
         "https://mzmine.github.io/mzmine_documentation/module_docs/lc-ms_featdet/targeted_featdet/targeted-featdet.html");
   }
 }
