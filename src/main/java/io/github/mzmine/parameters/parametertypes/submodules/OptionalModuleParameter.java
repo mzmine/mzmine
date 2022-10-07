fb24877f6e5dfa7011d5ec3999b9d8707d63a993
robinschmid <robinschmid@uni-muenster.de>
2022-10-07 15:50:52 -0700

    update copyright

--- src/main/java/io/github/mzmine/parameters/parametertypes/submodules/OptionalModuleParameter.java
+++ src/main/java/io/github/mzmine/parameters/parametertypes/submodules/OptionalModuleParameter.java
@@ -1,19 +1,26 @@
 /*
- *  Copyright 2006-2022 The MZmine Development Team
+ * Copyright (c) 2004-2022 The MZmine Development Team
  *
- *  This file is part of MZmine.
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
  *
- *  MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
- *  General Public License as published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- *  MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- *  Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with MZmine; if not,
- *  write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
- *  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package io.github.mzmine.parameters.parametertypes.submodules;
@@ -35,7 +42,8 @@ import org.w3c.dom.Element;
 public class OptionalModuleParameter<T extends ParameterSet> implements
     UserParameter<Boolean, OptionalModuleComponent>, ParameterContainer, EmbeddedParameterSet {
 
-  private String name, description;
+  private final String name;
+  private final String description;
   private T embeddedParameters;
   private Boolean value;
 
@@ -80,8 +88,7 @@ public class OptionalModuleParameter<T extends ParameterSet> implements
     // parameters set
     if ((value != null) && (value)) {
       for (Parameter<?> p : embeddedParameters.getParameters()) {
-        if (p instanceof UserParameter) {
-          UserParameter<?, ?> up = (UserParameter<?, ?>) p;
+        if (p instanceof UserParameter<?, ?> up) {
           Object upValue = up.getValue();
           if (upValue == null) {
             return null;
@@ -137,7 +144,7 @@ public class OptionalModuleParameter<T extends ParameterSet> implements
       errorMessages.add(name + " is not set properly");
       return false;
     }
-    if (value == true) {
+    if (value) {
       return embeddedParameters.checkParameterValues(errorMessages);
     }
     return true;
@@ -159,7 +166,7 @@ public class OptionalModuleParameter<T extends ParameterSet> implements
       return false;
     }
 
-    return RawDataSavingUtils
-        .parameterSetsEqual(getEmbeddedParameters(), thatOpt.getEmbeddedParameters(), false, false);
+    return RawDataSavingUtils.parameterSetsEqual(getEmbeddedParameters(),
+        thatOpt.getEmbeddedParameters(), false, false);
   }
 }
