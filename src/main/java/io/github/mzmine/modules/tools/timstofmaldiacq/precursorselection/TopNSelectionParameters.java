af8974731249f01219f55cec602047f23fe1ef30
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2022-10-14 12:59:58 +0200

    add prefer identified option

--- src/main/java/io/github/mzmine/modules/tools/timstofmaldiacq/precursorselection/TopNSelectionParameters.java
+++ src/main/java/io/github/mzmine/modules/tools/timstofmaldiacq/precursorselection/TopNSelectionParameters.java
@@ -1,25 +1,33 @@
 /*
- * Copyright 2006-2022 The MZmine Development Team
+ * Copyright (c) 2004-2022 The MZmine Development Team
  *
- * This file is part of MZmine.
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
  *
- * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with MZmine; if not,
- * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package io.github.mzmine.modules.tools.timstofmaldiacq.precursorselection;
 
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
+import io.github.mzmine.parameters.parametertypes.BooleanParameter;
 import io.github.mzmine.parameters.parametertypes.IntegerParameter;
 
 public class TopNSelectionParameters extends SimpleParameterSet {
@@ -27,7 +35,12 @@ public class TopNSelectionParameters extends SimpleParameterSet {
   public static final IntegerParameter numPrecursors = new IntegerParameter("Number of precursors",
       "The number of precursors to be fragmented.", 5);
 
+  public static final BooleanParameter preferAnnotated = new BooleanParameter(
+      "Prefer annotated precursors",
+      "If selected, annotated precursors will be added to the fragmentation list with preference.\n"
+          + "If there are more annotated features than the set number of precursors, all annotated precursors will be fragmented anyway.");
+
   public TopNSelectionParameters() {
-    super(new Parameter[]{numPrecursors});
+    super(new Parameter[]{numPrecursors, preferAnnotated});
   }
 }
