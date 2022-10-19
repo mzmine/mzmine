6aab77928b938ce5130a1485b4e0463332b2ee66
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2022-10-19 16:10:58 +0200

    fix npe precursor mz = null in spectra plot

--- src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/SpectraVisualizerTab.java
+++ src/main/java/io/github/mzmine/modules/visualization/spectra/simplespectra/SpectraVisualizerTab.java
@@ -392,7 +392,9 @@ public class SpectraVisualizerTab extends MZmineTab {
       if (scan != null && scan.getMSLevel() > 1) {
         // add all precursors
         final Double prmz = scan.getPrecursorMz();
-        spectrumPlot.getXYPlot().addDomainMarker(new ValueMarker(prmz));
+        if (prmz != null) {
+          spectrumPlot.getXYPlot().addDomainMarker(new ValueMarker(prmz));
+        }
       }
 
     });
