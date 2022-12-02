d9c402399e52ee998757b0227225bcd83cf5c621
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2022-12-02 13:35:36 +0100

    add javadoc to ADAPChromatogramBuilderParameters

--- src/main/java/io/github/mzmine/modules/dataprocessing/featdet_adapchromatogrambuilder/ADAPChromatogramBuilderParameters.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/featdet_adapchromatogrambuilder/ADAPChromatogramBuilderParameters.java
@@ -45,24 +45,26 @@ import java.util.Map;
 import javafx.scene.control.ButtonType;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Important Note: when changing any of the parameter names, reflect the changes in the
+ * {@link io.github.mzmine.modules.dataprocessing.featdet_imagebuilder.ImageBuilderParameters} to
+ * keep the compatibility.
+ */
 public class ADAPChromatogramBuilderParameters extends SimpleParameterSet {
 
   public static final RawDataFilesParameter dataFiles = new RawDataFilesParameter();
 
-  public static final ScanSelectionParameter scanSelection = new ScanSelectionParameter(
-      new ScanSelection(1));
+  public static final ScanSelectionParameter scanSelection = new ScanSelectionParameter(new ScanSelection(1));
 
   public static final IntegerParameter minimumScanSpan = new IntegerParameter(
       "Min group size in # of scans",
       "Minimum scan span over which some feature in the chromatogram must have (continuous) points above the noise level\n"
-          + "to be recognized as a chromatogram.\n"
-          + "The optimal value depends on the chromatography system setup. The best way to set this parameter\n"
+          + "to be recognized as a chromatogram.\n" + "The optimal value depends on the chromatography system setup. The best way to set this parameter\n"
           + "is by studying the raw data and determining what is the typical time span of chromatographic features.",
       5, true, 1, null);
 
   public static final MZToleranceParameter mzTolerance = new MZToleranceParameter(
-      "Scan to scan accuracy (m/z)", "m/z tolerance of the same compound between two scans.\n"
-      + "This does not describe the deviation of the accurate mass (measured) from the exact mass (calculated),\n"
+      "Scan to scan accuracy (m/z)", "m/z tolerance of the same compound between two scans.\n" + "This does not describe the deviation of the accurate mass (measured) from the exact mass (calculated),\n"
       + "but the fluctuation of the accurate between two scans.", 0.002, 10);
 
   public static final StringParameter suffix = new StringParameter("Suffix",
@@ -81,8 +83,7 @@ public class ADAPChromatogramBuilderParameters extends SimpleParameterSet {
 
   public static final HiddenParameter<OptOutParameter, Map<String, Boolean>> allowSingleScans = new HiddenParameter<>(
       new OptOutParameter("Allow single scan chromatograms",
-          "Allows selection of single scans as chromatograms. This is useful for "
-              + "feature table generation if MALDI point measurements."));
+          "Allows selection of single scans as chromatograms. This is useful for " + "feature table generation if MALDI point measurements."));
 
   public ADAPChromatogramBuilderParameters() {
     super(new Parameter[]{dataFiles, scanSelection, minimumScanSpan, minGroupIntensity,
@@ -92,8 +93,7 @@ public class ADAPChromatogramBuilderParameters extends SimpleParameterSet {
 
   @Override
   public ExitCode showSetupDialog(boolean valueCheckRequired) {
-    String message = "<html>ADAP Module Disclaimer:"
-        + "<br> If you use the ADAP Chromatogram Builder Module, please cite the "
+    String message = "<html>ADAP Module Disclaimer:" + "<br> If you use the ADAP Chromatogram Builder Module, please cite the "
         + "<a href=\"https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-11-395\">MZmine2 paper</a> and the following article:"
         + "<br><a href=\"http://pubs.acs.org/doi/abs/10.1021/acs.analchem.7b00947\"> Myers OD, Sumner SJ, Li S, Barnes S, Du X: One Step Forward for Reducing False Positive and False Negative "
         + "<br>Compound Identifications from Mass Spectrometry Metabolomics Data: New Algorithms for Constructing Extracted "
@@ -106,8 +106,7 @@ public class ADAPChromatogramBuilderParameters extends SimpleParameterSet {
 
   @Override
   public String getRestrictedIonMobilitySupportMessage() {
-    return "ADAP chromatogram builder will build two-dimensional chromatograms based on summed "
-        + "frame data (if there is any). Thus, the mobility dimension is not taken into account. "
+    return "ADAP chromatogram builder will build two-dimensional chromatograms based on summed " + "frame data (if there is any). Thus, the mobility dimension is not taken into account. "
         + "The mobility dimension can be added by the IMS expander module after feature resolving. "
         + "Do you wish to continue?";
   }
@@ -124,15 +123,12 @@ public class ADAPChromatogramBuilderParameters extends SimpleParameterSet {
       return false;
     }
 
-    final Boolean singleScansOkOptOut = getParameter(allowSingleScans).getValue()
-        .get("optoutsinglescancheck");
+    final Boolean singleScansOkOptOut = getParameter(allowSingleScans).getValue().get("optoutsinglescancheck");
 
-    if (getParameter(minimumScanSpan).getValue() <= 1 && (singleScansOkOptOut == null
-        || singleScansOkOptOut == false)) {
+    if (getParameter(minimumScanSpan).getValue() <= 1 && (singleScansOkOptOut == null || singleScansOkOptOut == false)) {
       ButtonType buttonType = MZmineCore.getDesktop()
           .createAlertWithOptOut("Confirmation", "Single consecutive scan selected.",
-              "The number of consecutive scans was set to <= 1.\nThis can lead to more noise"
-                  + " detected as EICs.\nDo you want to proceed?", "Do not show again.",
+              "The number of consecutive scans was set to <= 1.\nThis can lead to more noise" + " detected as EICs.\nDo you want to proceed?", "Do not show again.",
               b -> this.getParameter(allowSingleScans).getValue().put("optoutsinglescancheck", b));
       if (buttonType.equals(ButtonType.YES)) {
         return true;
