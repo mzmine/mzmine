8535dbb2fc84fd106d400069a5706623487a7424
Toan Nguyen <tnguy271@uncc.edu>
2023-01-12 09:42:20 -0500

    add gcms parameters

--- src/main/java/io/github/mzmine/modules/tools/batchwizard/subparameters/WizardChromatographyParameters.java
+++ src/main/java/io/github/mzmine/modules/tools/batchwizard/subparameters/WizardChromatographyParameters.java
@@ -26,20 +26,82 @@
 package io.github.mzmine.modules.tools.batchwizard.subparameters;
 
 import com.google.common.collect.Range;
+import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.modules.tools.batchwizard.WizardPreset.ChromatographyDefaults;
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
-import io.github.mzmine.parameters.parametertypes.BooleanParameter;
-import io.github.mzmine.parameters.parametertypes.ComboParameter;
-import io.github.mzmine.parameters.parametertypes.HiddenParameter;
-import io.github.mzmine.parameters.parametertypes.IntegerParameter;
+import io.github.mzmine.parameters.parametertypes.*;
+import io.github.mzmine.parameters.parametertypes.ranges.DoubleRangeParameter;
 import io.github.mzmine.parameters.parametertypes.ranges.RTRangeParameter;
+import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
+import io.github.mzmine.parameters.parametertypes.tolerances.MZToleranceParameter;
 import io.github.mzmine.parameters.parametertypes.tolerances.RTTolerance;
 import io.github.mzmine.parameters.parametertypes.tolerances.RTTolerance.Unit;
 import io.github.mzmine.parameters.parametertypes.tolerances.RTToleranceParameter;
 
-public class WizardChromatographyParameters extends SimpleParameterSet {
+import java.text.NumberFormat;
 
+public class WizardChromatographyParameters extends SimpleParameterSet {
+  /**************************************************ADAP CHROMATOGRAMBUILDER***************************************************************/
+  public static final DoubleParameter minHighestPoint = new DoubleParameter("Min highest intensity",
+          "Points below this intensity will not be considered in starting a new chromatogram",
+          MZmineCore.getConfiguration().getIntensityFormat());
+
+  public static final MZToleranceParameter mzTolerance = new MZToleranceParameter(
+          "Scan to scan accuracy (m/z)", """
+      m/z tolerance of the same compound between two scans.
+      This does not describe the deviation of the accurate mass (measured) from the exact mass (calculated),
+      but the fluctuation of the accurate between two scans.""", 0.002, 10);
+
+  public static final IntegerParameter minimumScanSpan = new IntegerParameter(
+          "Min group size in # of scans", """
+      Minimum scan span over which some feature in the chromatogram must have (continuous) points above the noise level
+      to be recognized as a chromatogram.
+      The optimal value depends on the chromatography system setup. The best way to set this parameter
+      is by studying the raw data and determining what is the typical time span of chromatographic features.""",
+          5, true, 1, null);
+
+  /**************************************************ADAP RESOLVER***************************************************************/
+  public static final DoubleParameter SN_THRESHOLD = new DoubleParameter("S/N threshold",
+          "Signal to noise ratio threshold", NumberFormat.getNumberInstance(), 10.0, 0.0, null);
+
+  public static final DoubleRangeParameter RT_FOR_CWT_SCALES_DURATION = new DoubleRangeParameter(
+          "RT wavelet range",
+          "Upper and lower bounds of retention times to be used for setting the wavelet scales. Choose a range that that simmilar to the range of peak widths expected to be found from the data.",
+          MZmineCore.getConfiguration().getRTFormat(), true, true, Range.closed(0.001, 0.1));
+
+  /**************************************************MULTICURVE RESOLUTION***************************************************************/
+  public static final DoubleParameter PREF_WINDOW_WIDTH = new DoubleParameter(
+          "Deconvolution window width (min)", "Preferred width of deconvolution windows (in minutes).",
+          NumberFormat.getNumberInstance(), 0.2);
+
+  public static final DoubleParameter RET_TIME_TOLERANCE = new DoubleParameter(
+          "Retention time tolerance (min)",
+          "Retention time tolerance value (between 0 and 1) is used for determine the number of components"
+                  + " in a window. The larger tolerance, the smaller components are determined.",
+          NumberFormat.getNumberInstance(), 0.05, 0.0, Double.MAX_VALUE);
+
+  public static final IntegerParameter MIN_CLUSTER_SIZE = new IntegerParameter(
+          "Minimum Number of Peaks", "Minimum number of peaks that can form a component", 1);
+
+  /**************************************************ADAP ALIGNER***************************************************************/
+  public static final DoubleParameter SAMPLE_COUNT_RATIO = new DoubleParameter(
+          "Min confidence (between 0 and 1)",
+          "A fraction of the total number of samples. An aligned feature must be detected at "
+                  + "least in several samples.\nThis parameter determines the minimum number of samples where a "
+                  + "feature must be detected.",
+          NumberFormat.getInstance(), 0.7, 0.0, 1.0);
+
+  public static final RTToleranceParameter RET_TIME_RANGE = new RTToleranceParameter();
+
+  public static final MZToleranceParameter MZ_RANGE = new MZToleranceParameter();
+
+  public static final DoubleParameter SCORE_TOLERANCE = new DoubleParameter(
+          "Score threshold (between 0 and 1)",
+          "The minimum value of the similarity function required for features to be aligned together.",
+          NumberFormat.getInstance(), 0.75, 0.0, 1.0);
+
+  //others
   public static final RTToleranceParameter approximateChromatographicFWHM = new RTToleranceParameter(
       "Approximate feature FWHM",
       "The approximate feature width (chromatograpic peak width) in retention time (full-width-at-half-maximum, FWHM). ");
@@ -106,6 +168,38 @@ public class WizardChromatographyParameters extends SimpleParameterSet {
     setParameter(interSampleRTTolerance, interSampleTolerance);
   }
 
+  public WizardChromatographyParameters(final ChromatographyWorkflow workflowPreset,
+                                         final Double minHighest, final MZTolerance mzToleranceValue,
+                                         final Integer minScanSpan, final Double snThreshold,
+                                         final Range<Double> rtforCWT, final Double windowWidth,
+                                         final Double rtTolderance, final Integer minClusterSize,
+                                         final Double sampleCountRatio, final RTTolerance rtRange,
+                                         final MZTolerance mzRange, final Double scoreTolerance
+                                         ) {
+    setParameter(workflow, workflowPreset);
+
+    //adap chromatogram builder
+    setParameter(minHighestPoint, minHighest);
+    setParameter(mzTolerance, mzToleranceValue);
+    setParameter(minimumScanSpan, minScanSpan);
+
+    //adap resolver
+    setParameter(SN_THRESHOLD, snThreshold);
+    setParameter(RT_FOR_CWT_SCALES_DURATION, rtforCWT);
+
+    //multicurve
+    setParameter(PREF_WINDOW_WIDTH, windowWidth);
+    setParameter(RET_TIME_TOLERANCE, rtTolderance);
+    setParameter(MIN_CLUSTER_SIZE, minClusterSize);
+
+    //adap aligner
+    setParameter(SAMPLE_COUNT_RATIO, sampleCountRatio);
+    setParameter(RET_TIME_RANGE, rtRange);
+    setParameter(MZ_RANGE, mzRange);
+    setParameter(SCORE_TOLERANCE, scoreTolerance);
+
+
+  }
   /**
    * Create parameters from defaults
    *
@@ -124,6 +218,8 @@ public class WizardChromatographyParameters extends SimpleParameterSet {
           case HILIC -> new WizardChromatographyParameters(ChromatographyWorkflow.LC, true, 15, 5,
               Range.closed(0.3, 30d), new RTTolerance(0.05f, Unit.MINUTES),
               new RTTolerance(3, Unit.SECONDS), new RTTolerance(3, Unit.SECONDS));
+
+          //TODO: update default values for GC
           case GC -> new WizardChromatographyParameters(ChromatographyWorkflow.GC, true, 30, 6,
               Range.closed(0.3, 30d), new RTTolerance(0.05f, Unit.MINUTES),
               new RTTolerance(0.04f, Unit.MINUTES), new RTTolerance(0.1f, Unit.MINUTES));
