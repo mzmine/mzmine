7a28ad478d05b912264a49177db3b3a115c08c0b
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2023-03-06 17:28:13 +0100

    fix ms2 mass detection for tdf import

--- src/main/java/io/github/mzmine/modules/io/import_rawdata_bruker_tdf/TDFImportTask.java
+++ src/main/java/io/github/mzmine/modules/io/import_rawdata_bruker_tdf/TDFImportTask.java
@@ -40,10 +40,7 @@ import io.github.mzmine.datamodel.impl.masslist.ScanPointerMassList;
 import io.github.mzmine.datamodel.msms.PasefMsMsInfo;
 import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.modules.MZmineModule;
-import io.github.mzmine.modules.dataprocessing.featdet_massdetection.MassDetectionParameters;
 import io.github.mzmine.modules.dataprocessing.featdet_massdetection.MassDetector;
-import io.github.mzmine.modules.dataprocessing.featdet_massdetection.centroid.CentroidMassDetector;
-import io.github.mzmine.modules.dataprocessing.featdet_massdetection.centroid.CentroidMassDetectorParameters;
 import io.github.mzmine.modules.io.import_rawdata_all.AdvancedSpectraImportParameters;
 import io.github.mzmine.modules.io.import_rawdata_bruker_tdf.datamodel.BrukerScanMode;
 import io.github.mzmine.modules.io.import_rawdata_bruker_tdf.datamodel.sql.BuildingPASEFMsMsInfo;
@@ -131,8 +128,8 @@ public class TDFImportTask extends AbstractTask {
   /**
    * @param project
    * @param file
-   * @param newMZmineFile needs to be created as {@link IMSRawDataFileImpl} via {@link
-   *                      MZmineCore#createNewIMSFile}.
+   * @param newMZmineFile needs to be created as {@link IMSRawDataFileImpl} via
+   *                      {@link MZmineCore#createNewIMSFile}.
    */
   public TDFImportTask(MZmineProject project, File file, IMSRawDataFile newMZmineFile,
       @NotNull final Class<? extends MZmineModule> module, @NotNull final ParameterSet parameters,
@@ -158,33 +155,19 @@ public class TDFImportTask extends AbstractTask {
       ms1DetectorParam = advancedParam.getParameter(AdvancedSpectraImportParameters.msMassDetection)
           .getEmbeddedParameter().getValue().getParameterSet();
     } else {
-      if (denoising) {
-        ms1Detector = MassDetectionParameters.centroid;
-        ms1DetectorParam = MZmineCore.getConfiguration()
-            .getModuleParameters(CentroidMassDetector.class).cloneParameterSet();
-        ms1DetectorParam.getParameter(CentroidMassDetectorParameters.noiseLevel)
-            .setValue(NOISE_THRESHOLD);
-        ms1DetectorParam.setParameter(CentroidMassDetectorParameters.detectIsotopes, false);
-      } else {
-        ms1Detector = null;
-        ms1DetectorParam = null;
-      }
+      ms1Detector = null;
+      ms1DetectorParam = null;
     }
     if (advancedParam != null && advancedParam.getParameter(
-        AdvancedSpectraImportParameters.msMassDetection).getValue()) {
+        AdvancedSpectraImportParameters.ms2MassDetection).getValue()) {
       ms2Detector = advancedParam.getParameter(AdvancedSpectraImportParameters.ms2MassDetection)
           .getEmbeddedParameter().getValue().getModule();
       ms2DetectorParam = advancedParam.getParameter(
               AdvancedSpectraImportParameters.ms2MassDetection).getEmbeddedParameter().getValue()
           .getParameterSet();
     } else {
-      if (denoising) {
-        ms2Detector = ms1Detector;
-        ms2DetectorParam = ms1DetectorParam;
-      } else {
-        ms2Detector = null;
-        ms2DetectorParam = null;
-      }
+      ms2Detector = null;
+      ms2DetectorParam = null;
     }
   }
 
@@ -292,7 +275,7 @@ public class TDFImportTask extends AbstractTask {
     // collect average spectra for each frame
     Set<SimpleFrame> frames = new LinkedHashSet<>();
 
-   final boolean importProfile = MZmineCore.getInstance().isTdfPseudoProfile();
+    final boolean importProfile = MZmineCore.getInstance().isTdfPseudoProfile();
 
     try {
       for (int i = 0; i < numFrames; i++) {
@@ -317,7 +300,7 @@ public class TDFImportTask extends AbstractTask {
           frame.addMassList(new ScanPointerMassList(frame));
         }
 
-        if(isMaldi && frame instanceof ImagingFrame imgFrame) {
+        if (isMaldi && frame instanceof ImagingFrame imgFrame) {
           final MaldiSpotInfo maldiSpotInfo = maldiFrameInfoTable.getMaldiSpotInfo(
               frame.getFrameId());
           imgFrame.setMaldiSpotInfo(maldiSpotInfo);
@@ -549,7 +532,7 @@ public class TDFImportTask extends AbstractTask {
 
   @Nullable
   private Frame getParentFrame(IMSRawDataFile file, Integer parentFrameNumber) {
-    if(parentFrameNumber == null) {
+    if (parentFrameNumber == null) {
       return null;
     }
     Optional<Frame> optionalFrame = (Optional<Frame>) file.getFrames().stream()
