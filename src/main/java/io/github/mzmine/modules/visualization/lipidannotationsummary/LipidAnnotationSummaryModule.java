8fc9cf7f28aefa00e17435eefd8d50f4d08f2ca8
kyrasolaris01 <kyrasolaris01@gmail.com>
2023-11-23 13:51:00 +0100

    Only allow LipidAnnotationSummaryModule for feature lists with Lipid ID

--- src/main/java/io/github/mzmine/modules/visualization/lipidannotationsummary/LipidAnnotationSummaryModule.java
+++ src/main/java/io/github/mzmine/modules/visualization/lipidannotationsummary/LipidAnnotationSummaryModule.java
@@ -2,16 +2,22 @@ package io.github.mzmine.modules.visualization.lipidannotationsummary;
 
 import io.github.mzmine.datamodel.MZmineProject;
 import io.github.mzmine.datamodel.features.FeatureList;
+import io.github.mzmine.datamodel.features.FeatureListRow;
+import io.github.mzmine.datamodel.features.types.annotations.LipidMatchListType;
 import io.github.mzmine.gui.mainwindow.MZmineTab;
 import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.modules.MZmineModuleCategory;
 import io.github.mzmine.modules.MZmineRunnableModule;
+import io.github.mzmine.modules.dataprocessing.id_lipididentification.common.lipididentificationtools.matchedlipidannotations.MatchedLipid;
 import io.github.mzmine.modules.dataprocessing.id_lipididentification.lipidannotationmodules.LipidAnnotationParameters;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.taskcontrol.Task;
 import io.github.mzmine.util.ExitCode;
 import java.time.Instant;
 import java.util.Collection;
+import java.util.List;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -42,9 +48,19 @@ public class LipidAnnotationSummaryModule implements MZmineRunnableModule {
             parameters.getParameter(LipidAnnotationParameters.featureLists)).getValue()
         .getMatchingFeatureLists();
     for (FeatureList featureList : featureLists) {
-      MZmineTab tab = new LipidAnnotationSummaryTab("Lipid annotation summary", parameters,
-          featureList);
-      MZmineCore.getDesktop().addTab(tab);
+      List<FeatureListRow> rowsWithLipidID = featureList.getRows().stream()
+          .filter(this::rowHasMatchedLipidSignals).toList();
+      if (!rowsWithLipidID.isEmpty()) {
+        MZmineTab tab = new LipidAnnotationSummaryTab("Lipid Annotation summary", parameters,
+            featureList);
+        MZmineCore.getDesktop().addTab(tab);
+      } else {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("No Lipid Annotations found in " + featureList.getName());
+        alert.setContentText("No Lipid Annotations found in " + featureList.getName()
+            + ". Did you run the Lipid Annotation module?");
+        alert.showAndWait();
+      }
     }
 
     return ExitCode.OK;
@@ -53,5 +69,10 @@ public class LipidAnnotationSummaryModule implements MZmineRunnableModule {
   @Override
   public @NotNull MZmineModuleCategory getModuleCategory() {
     return MZmineModuleCategory.VISUALIZATIONFEATURELIST;
+  }
+
+  private boolean rowHasMatchedLipidSignals(FeatureListRow row) {
+    List<MatchedLipid> matches = row.get(LipidMatchListType.class);
+    return matches != null && !matches.isEmpty();
   }
 }
