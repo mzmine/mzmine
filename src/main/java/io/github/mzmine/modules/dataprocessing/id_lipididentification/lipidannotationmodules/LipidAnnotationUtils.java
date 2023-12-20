ceafc1f0262211b8df6ba3095cec6a5641d19f8d
kyrasolaris01 <kyrasolaris01@gmail.com>
2023-12-20 11:14:42 +0100

    Add license

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_lipididentification/lipidannotationmodules/LipidAnnotationUtils.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_lipididentification/lipidannotationmodules/LipidAnnotationUtils.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2004-2024 The MZmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.github.mzmine.modules.dataprocessing.id_lipididentification.lipidannotationmodules;
 
 import com.google.common.collect.Range;
@@ -193,7 +218,9 @@ public class LipidAnnotationUtils {
           MatchedLipid matchedSpeciesLevelLipid = matchedLipidFactory.validateSpeciesLevelAnnotation(
               row.getAverageMZ(), lipid, annotatedFragments, massList, minMsMsScore, mzToleranceMS2,
               ionization);
-          matchedLipidsInScan.add(matchedSpeciesLevelLipid);
+          if (matchedSpeciesLevelLipid != null) {
+            matchedLipidsInScan.add(matchedSpeciesLevelLipid);
+          }
 
           IMolecularSpeciesLevelMatchedLipidFactory matchedMolecularSpeciesLipidFactory = getMolecularSpeciesLevelMatchedLipidFactory(
               lipidCategory);
