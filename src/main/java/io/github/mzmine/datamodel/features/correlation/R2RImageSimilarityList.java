72812f8c72c28a59eaea189b5bfd712d33f30091
kyrasolaris01 <kyrasolaris01@gmail.com>
2023-12-21 10:39:18 +0100

    Add doc and use DoubleList

--- src/main/java/io/github/mzmine/datamodel/features/correlation/R2RImageSimilarityList.java
+++ src/main/java/io/github/mzmine/datamodel/features/correlation/R2RImageSimilarityList.java
@@ -1,21 +1,30 @@
 package io.github.mzmine.datamodel.features.correlation;
 
 import io.github.mzmine.datamodel.features.FeatureListRow;
-import java.util.ArrayList;
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+import it.unimi.dsi.fastutil.doubles.DoubleList;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Image similarity computed in MZmine.
+ */
 public class R2RImageSimilarityList extends AbstractRowsRelationship {
 
   private final Type type;
-  private final List<Double> similarities = new ArrayList<>();
+  private final DoubleList similarities = new DoubleArrayList();
 
+  /**
+   * @param a    row a
+   * @param b    row b
+   * @param type the similarity type
+   */
   public R2RImageSimilarityList(FeatureListRow a, FeatureListRow b, Type type) {
     super(a, b);
     this.type = type;
   }
 
-  public synchronized void addSimilarity(Double sim) {
+  public synchronized void addSimilarity(double sim) {
     similarities.add(sim);
   }
 
@@ -28,11 +37,11 @@ public class R2RImageSimilarityList extends AbstractRowsRelationship {
   }
 
   public double getMaxSimilarity() {
-    return similarities.stream().mapToDouble(Double::doubleValue).max().orElse(0);
+    return similarities.doubleStream().max().orElse(0.0);
   }
 
   public double getMinSimilarity() {
-    return similarities.stream().mapToDouble(Double::doubleValue).min().orElse(0);
+    return similarities.doubleStream().min().orElse(0.0);
   }
 
   @Override
