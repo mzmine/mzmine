d79f2c2f5db6e570ba8fb6eaa534a8d8b4e8f31e
kyrasolaris01 <kyrasolaris01@gmail.com>
2024-01-09 08:03:48 +0100

    cleaned PhosphoLipidFragmentFactory

--- src/main/java/io/github/mzmine/modules/dataprocessing/id_lipididentification/common/lipididentificationtools/lipidfragmentannotation/GlyceroAndGlyceroPhospholipidFragmentFactory.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/id_lipididentification/common/lipididentificationtools/lipidfragmentannotation/GlyceroAndGlyceroPhospholipidFragmentFactory.java
@@ -3,12 +3,10 @@ package io.github.mzmine.modules.dataprocessing.id_lipididentification.common.li
 import io.github.mzmine.datamodel.IonizationType;
 import io.github.mzmine.datamodel.Scan;
 import io.github.mzmine.modules.dataprocessing.id_lipididentification.common.lipididentificationtools.LipidFragmentationRule;
-import io.github.mzmine.modules.dataprocessing.id_lipididentification.common.lipididentificationtools.LipidFragmentationRuleType;
 import io.github.mzmine.modules.dataprocessing.id_lipididentification.common.lipids.ILipidAnnotation;
 import io.github.mzmine.modules.dataprocessing.id_lipididentification.common.lipids.LipidFragment;
 import io.github.mzmine.modules.dataprocessing.id_lipididentification.lipidannotationmodules.LipidAnnotationChainParameters;
 import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
-import java.util.ArrayList;
 import java.util.List;
 
 public class GlyceroAndGlyceroPhospholipidFragmentFactory extends
@@ -22,49 +20,7 @@ public class GlyceroAndGlyceroPhospholipidFragmentFactory extends
   }
 
   public List<LipidFragment> findLipidFragments() {
-    List<LipidFragment> commonLipidFragments = findCommonLipidFragments();
-    List<LipidFragment> lipidFragments = new ArrayList<>(commonLipidFragments);
-    for (LipidFragmentationRule rule : rules) {
-      if (!ionizationType.equals(rule.getIonizationType())
-          || rule.getLipidFragmentationRuleType() == null) {
-        continue;
-      }
-      List<LipidFragment> detectedFragments = checkForGlyceroAndGlyceroPhospholipidSpecificRuleTypes(
-          rule);
-      if (detectedFragments != null) {
-        lipidFragments.addAll(detectedFragments);
-      }
-    }
-    return lipidFragments;
-  }
-
-  private List<LipidFragment> checkForGlyceroAndGlyceroPhospholipidSpecificRuleTypes(
-      LipidFragmentationRule rule) {
-    LipidFragmentationRuleType ruleType = rule.getLipidFragmentationRuleType();
-    switch (ruleType) {
-
-//      case ALKYLCHAIN_FRAGMENT -> {
-//        // TODO
-//        return null;
-//      }
-//      case ALKYLCHAIN_FRAGMENT_NL -> {
-//        // TODO
-//        return null;
-//      }
-//      case ALKYLCHAIN_PLUS_FORMULA_FRAGMENT_NL -> {
-//        // TODO
-//        return null;
-//      }
-//      case ALKYLCHAIN_MINUS_FORMULA_FRAGMENT -> {
-//        // TODO
-//        return null;
-//      }
-//      case ALKYLCHAIN_MINUS_FORMULA_FRAGMENT_NL -> {
-//        // TODO
-//        return null;
-//      }
-    }
-    return List.of();
+    return findCommonLipidFragments();
   }
 
 }
