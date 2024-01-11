230e2b2cd56669f83d4e5f4affeea140bd8c2448
kyrasolaris01 <kyrasolaris01@gmail.com>
2024-01-11 09:04:04 +0100

    Rename parameter

--- src/main/java/io/github/mzmine/modules/dataprocessing/group_imagecorrelate/ImageCorrelateGroupingParameters.java
+++ src/main/java/io/github/mzmine/modules/dataprocessing/group_imagecorrelate/ImageCorrelateGroupingParameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2023 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -73,7 +73,7 @@ public class ImageCorrelateGroupingParameters extends SimpleParameterSet {
   public static final ComboParameter<SimilarityMeasure> MEASURE = new ComboParameter<>("Measure",
       "Similarity measure", SimilarityMeasure.values(), SimilarityMeasure.PEARSON);
 
-  public static final PercentParameter MIN_R = new PercentParameter("Min image Pearson r",
+  public static final PercentParameter MIN_R = new PercentParameter("Similarity measure",
       "Minimum percentage for image correlation in one raw file.", 0.85, 0d, 1d);
 
 
