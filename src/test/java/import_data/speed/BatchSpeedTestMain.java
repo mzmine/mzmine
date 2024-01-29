9ec4b6bd91445255fb44731b9633ea7714528c9c
robinschmid <robinschmid@uni-muenster.de>
2024-01-29 11:30:35 +0100

    Create batch speed test job and better example

--- src/test/java/import_data/speed/BatchSpeedTestMain.java
+++ src/test/java/import_data/speed/BatchSpeedTestMain.java
@@ -28,7 +28,6 @@ package import_data.speed;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import import_data.local.Dom250Samples;
 import io.github.mzmine.main.MZmineCore;
 import io.github.mzmine.modules.batchmode.BatchModeModule;
 import io.github.mzmine.modules.batchmode.BatchTask;
@@ -51,27 +50,36 @@ import org.junit.jupiter.api.Assertions;
 /**
  * Speed test to log the time required to import files. Just define List of String that point to
  * resources files or absolute paths and add a test to the main method. The results will be appended
- * to {@link #speedTestFile} define a full path here as it will otherwise be relative to
- * build/target path.
+ * to speedTestFile define a full path here as it will otherwise be relative to build/target path.
+ * When working with local files - put those into the import_data/local folder and create a new run
+ * script similar to the main method here
+ * <p>
+ * Be sure to specify VM options -Xms16g -Xmx16g or similar to start with fixed memory
  */
 public class BatchSpeedTestMain {
 
-  static final List<String> thermo = List.of("rawdatafiles/additional/astral.raw");
-  static final List<String> dom = List.of("""
-      rawdatafiles/DOM_a.mzML
-      rawdatafiles/DOM_a_invalid_chars.mzML
-      rawdatafiles/DOM_a_invalid_header.mzML
-          """.split("\n"));
   private static final Logger logger = Logger.getLogger(BatchSpeedTestMain.class.getName());
-  public static String speedTestFile = "D:\\git\\mzmine3\\src\\test\\java\\import_data\\local\\speed.jsonlines";
 
   public static void main(String[] args) {
-    String RAM = "16GB, MZmine4";
+    String speedTestFile = "D:\\git\\mzmine3\\src\\test\\java\\import_data\\local\\speed.jsonlines";
+    String description = "RAM=16GB, MZmine4, smallDOM";
     // keep running and all in memory
     String inMemory = "all";
 //    String inMemory = "none";
     boolean headLess = false;
 
+    String batchFile = "rawdatafiles/test_batch_small.xml";
+    List<String> samples = List.of("rawdatafiles/DOM_a.mzML",
+        "rawdatafiles/DOM_a_invalid_chars.mzML", "rawdatafiles/DOM_a_invalid_header.mzML",
+        "rawdatafiles/DOM_b.mzXML", "rawdatafiles/DOM_b_invalid_header.mzXML");
+
+    List<BatchSpeedJob> jobs = List.of(new BatchSpeedJob(description, 3, batchFile, samples));
+
+    startAndRunTests(speedTestFile, headLess, inMemory, jobs);
+  }
+
+  public static void startAndRunTests(final String outFile, final boolean headLess,
+      final String inMemory, List<BatchSpeedJob> jobs) {
     try (var executor = Executors.newScheduledThreadPool(2)) {
 
       executor.schedule(() -> {
@@ -85,27 +93,24 @@ public class BatchSpeedTestMain {
         }
       }, 0, TimeUnit.SECONDS);
       long delay = headLess ? 1 : 6;
-      executor.schedule(() -> BatchSpeedTestMain.testSpeed(inMemory, RAM, headLess), delay,
-          TimeUnit.SECONDS);
+      executor.schedule(() -> BatchSpeedTestMain.testSpeed(outFile, headLess, inMemory, jobs),
+          delay, TimeUnit.SECONDS);
     }
   }
 
-  private static void testSpeed(final String inMemory, final String RAM, final boolean headLess) {
+  private static void testSpeed(final String outFile, final boolean headLess, final String inMemory,
+      List<BatchSpeedJob> jobs) {
     try {
-      String description = STR."inMemory=\{inMemory}, RAM=\{RAM} \{headLess ? "headless" : "GUI"}";
-      List<String> dom = Dom250Samples.DOM;
-      List<String> dom8 = Dom250Samples.DOM.subList(0, 8);
-      List<String> dom32 = Dom250Samples.DOM.subList(0, 32);
-      List<String> dom64 = Dom250Samples.DOM.subList(0, 64);
-      List<String> dom100 = Dom250Samples.DOM.subList(0, 100);
-      List<String> dom200 = Dom250Samples.DOM.subList(0, 200);
+      int[] iterations = new int[jobs.size()];
+      for (int i = 0; i < jobs.size(); i++) {
+        var job = jobs.get(i);
+        if (iterations[i] < job.iterations()) {
+          iterations[i]++;
 
-      for (int i = 0; i < 3; i++) {
-        runBatch(description, dom8, Dom250Samples.batchFile, speedTestFile);
-        runBatch(description, dom32, Dom250Samples.batchFile, speedTestFile);
-        runBatch(description, dom64, Dom250Samples.batchFile, speedTestFile);
-//        runBatch(description, dom100, Dom250Samples.batchFile, speedTestFile);
-//        runBatch(description, dom200, Dom250Samples.batchFile, speedTestFile);
+          String description = STR."inMemory=\{inMemory}, \{job.description()} \{headLess
+              ? "headless" : "GUI"}";
+          runBatch(description, job.files(), job.batchFile(), outFile);
+        }
       }
 
       System.exit(0);
@@ -117,12 +122,12 @@ public class BatchSpeedTestMain {
   }
 
   private static void runBatch(String description, final List<String> files, final String batchFile,
-      final String speedTestFile) throws InterruptedException, IOException {
+      final String outFile) throws InterruptedException, IOException {
 
     System.gc();
     try {
-      File jsonFile = FileAndPathUtil.getRealFilePath(new File(speedTestFile), ".jsonlines");
-      File tsvFile = FileAndPathUtil.getRealFilePath(new File(speedTestFile), ".tsv");
+      File jsonFile = FileAndPathUtil.getRealFilePath(new File(outFile), ".jsonlines");
+      File tsvFile = FileAndPathUtil.getRealFilePath(new File(outFile), ".tsv");
 
       FileAndPathUtil.createDirectory(tsvFile.getParentFile());
 
