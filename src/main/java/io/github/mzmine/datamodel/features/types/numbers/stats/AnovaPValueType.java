2ef17b2832b9a981752f7328b9c4047a08afa56a
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-02-26 10:01:31 +0100

    make anova type public

--- src/main/java/io/github/mzmine/datamodel/features/types/numbers/stats/AnovaPValueType.java
+++ src/main/java/io/github/mzmine/datamodel/features/types/numbers/stats/AnovaPValueType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2023 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -34,7 +34,7 @@ public class AnovaPValueType extends DoubleType {
 
   private static final DecimalFormat format = new DecimalFormat("0.###");
 
-  protected AnovaPValueType() {
+  public AnovaPValueType() {
     super(format);
   }
 
