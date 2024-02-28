50cc4bd9669d103a5b981a44b195e3640c08a280
Robin Schmid <rschmid1789@gmail.com>
2024-02-28 15:53:41 +0100

    add community module

---
+++ mzmine-community/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "3.9.38"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
