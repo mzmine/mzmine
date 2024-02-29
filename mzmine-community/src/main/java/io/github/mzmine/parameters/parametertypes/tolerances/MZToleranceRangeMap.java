587f956714f65613d625e328f749bfebedc0bf04
Robin Schmid <rschmid1789@gmail.com>
2024-02-29 18:22:28 +0100

    upgrade libs and use lib catalog

--- mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/tolerances/MZToleranceRangeMap.java
+++ mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/tolerances/MZToleranceRangeMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -127,7 +127,7 @@ public class MZToleranceRangeMap<T> implements RangeMap<Double, T> {
   }
 
   @Override
-  public void putAll(RangeMap<Double, T> rangeMap) {
+  public void putAll(final RangeMap<Double, ? extends T> rangeMap) {
     map.putAll(rangeMap);
   }
 
