62f73abb07b05197075d6172ed7636c7c0a6fbf4
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-03-06 10:10:46 +0100

    revert jfreechart version

--- mzmine-community/src/main/java/io/github/mzmine/modules/visualization/chromatogram/TICDataSet.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/chromatogram/TICDataSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -406,14 +406,14 @@ public class TICDataSet extends AbstractTaskXYZDataset {
       processedScans++;
 
       // Refresh every REDRAW_INTERVAL ms.
-      synchronized (TICDataSet.class) {
+      /*synchronized (TICDataSet.class) {
 
         if (System.currentTimeMillis() - lastRedrawTime > REDRAW_INTERVAL) {
 
           refresh();
           lastRedrawTime = System.currentTimeMillis();
         }
-      }
+      }*/
     }
   }
 
