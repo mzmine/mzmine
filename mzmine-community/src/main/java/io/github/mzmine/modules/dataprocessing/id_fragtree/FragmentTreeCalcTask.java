a1e453f4dc0dcad8587c23bebff9e2721afa4bc3
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-04-02 13:06:52 +0200

    fragtree setup

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_fragtree/FragmentTreeCalcTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_fragtree/FragmentTreeCalcTask.java
@@ -28,6 +28,7 @@ package io.github.mzmine.modules.dataprocessing.id_fragtree;
 import com.google.common.collect.Range;
 import io.github.mzmine.datamodel.MergedMassSpectrum.MergingType;
 import io.github.mzmine.datamodel.MergedMsMsSpectrum;
+import io.github.mzmine.datamodel.PolarityType;
 import io.github.mzmine.datamodel.Scan;
 import io.github.mzmine.datamodel.features.FeatureList;
 import io.github.mzmine.datamodel.features.FeatureListRow;
@@ -37,6 +38,7 @@ import io.github.mzmine.parameters.parametertypes.combowithinput.MsLevelFilter;
 import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
 import io.github.mzmine.taskcontrol.AbstractFeatureListTask;
 import io.github.mzmine.util.FeatureUtils;
+import io.github.mzmine.util.FormulaUtils;
 import io.github.mzmine.util.MemoryMapStorage;
 import io.github.mzmine.util.scans.FragmentScanSelection;
 import io.github.mzmine.util.scans.FragmentScanSelection.IncludeInputSpectra;
@@ -45,9 +47,11 @@ import java.time.Instant;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.openscience.cdk.Isotope;
 import org.openscience.cdk.formula.MolecularFormulaGenerator;
 import org.openscience.cdk.formula.MolecularFormulaRange;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IIsotope;
 import org.openscience.cdk.interfaces.IMolecularFormula;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
@@ -59,13 +63,63 @@ public class FragmentTreeCalcTask extends AbstractFeatureListTask {
       IncludeInputSpectra.NONE, IntensityMergingType.MAXIMUM, MsLevelFilter.ALL_LEVELS, null);
   private MZTolerance formulaTolerance = new MZTolerance(0.005, 5);
 
-  private final MolecularFormulaRange elementCounts = new MolecularFormulaRange();
-
   protected FragmentTreeCalcTask(@Nullable MemoryMapStorage storage,
       @NotNull Instant moduleCallDate, ParameterSet parameters, FeatureList flist) {
     super(storage, moduleCallDate, parameters, FragmentTreeCalcModule.class);
 
     this.flist = flist;
+  }
+
+  @NotNull
+  private static MolecularFormulaRange setupFormulaRange(List<IonType> ionTypes) {
+    final MolecularFormulaRange elementCounts = new MolecularFormulaRange();
+    elementCounts.addIsotope(new Isotope("C"), 0, 100);
+    elementCounts.addIsotope(new Isotope("H"), 0, 400);
+    elementCounts.addIsotope(new Isotope("O"), 0, 10);
+    elementCounts.addIsotope(new Isotope("N"), 0, 10);
+    elementCounts.addIsotope(new Isotope("P"), 0, 2);
+    elementCounts.addIsotope(new Isotope("S"), 0, 2);
+
+    for (IonType ionType : ionTypes) {
+      if (ionType.isUndefinedAdduct()) {
+        continue;
+      }
+
+      // check if we have "unusual" adducts (other than m+h and m+nh4) which are not covered by the above
+      reflectIonTypeInFormulaRange(ionType, elementCounts);
+    }
+    return elementCounts;
+  }
+
+  private static void reflectIonTypeInFormulaRange(IonType ionType,
+      MolecularFormulaRange elementCounts) {
+    final IMolecularFormula adductFormula = ionType.getAdduct().getCDKFormula();
+    for (IIsotope isotope : adductFormula.isotopes()) {
+      if (!elementCounts.contains(isotope)) {
+        elementCounts.addIsotope(isotope, 0, 1);
+      } else {
+        increaseMaxIsotopeCountByOne(isotope, elementCounts);
+      }
+    }
+
+    if (ionType.getModification() != null) {
+      final IMolecularFormula modificationFormula = ionType.getModification().getCDKFormula();
+      for (IIsotope isotope : modificationFormula.isotopes()) {
+        if (!elementCounts.contains(isotope)) {
+          elementCounts.addIsotope(isotope, 0, 1);
+        } else {
+          increaseMaxIsotopeCountByOne(isotope, elementCounts);
+        }
+      }
+    }
+  }
+
+  private static void increaseMaxIsotopeCountByOne(IIsotope isotope,
+      MolecularFormulaRange elementCounts) {
+    final int maxCount = elementCounts.getIsotopeCountMax(isotope);
+    final int minCount = elementCounts.getIsotopeCountMin(isotope);
+    elementCounts.removeIsotope(isotope);
+    elementCounts.addIsotope(isotope, minCount, maxCount + 1);
   }
 
   @Override
@@ -92,12 +146,20 @@ public class FragmentTreeCalcTask extends AbstractFeatureListTask {
       return;
     }
 
+    final PolarityType polarity = row.getBestFeature().getRepresentativeScan().getPolarity();
+
     final List<IonType> assignedIonTypes = FeatureUtils.extractAllIonTypes(row);
-    final Range<Double> formulaMassRange = formulaTolerance.getToleranceRange(row.getAverageMZ());
+    final MolecularFormulaRange elementCounts = setupFormulaRange(assignedIonTypes);
+
+    final Double neutralMassWithAdduct = row.getAverageMZ() * row.getRowCharge()
+        + polarity.getSign() * row.getRowCharge() * FormulaUtils.electronMass;
+    final Range<Double> formulaMassRange = formulaTolerance.getToleranceRange(
+        neutralMassWithAdduct);
+
 
     IChemObjectBuilder builder = SilentChemObjectBuilder.getInstance();
-    final MolecularFormulaGenerator generator = new MolecularFormulaGenerator(
-        builder, formulaMassRange.lowerEndpoint(), formulaMassRange.upperEndpoint(), elementCounts);
+    final MolecularFormulaGenerator generator = new MolecularFormulaGenerator(builder,
+        formulaMassRange.lowerEndpoint(), formulaMassRange.upperEndpoint(), elementCounts);
 
     IMolecularFormula cdkFormula;
   }
