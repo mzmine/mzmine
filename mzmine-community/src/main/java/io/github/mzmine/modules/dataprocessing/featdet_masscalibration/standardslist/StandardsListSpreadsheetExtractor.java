a5770e25e85e5c2572caf9263e01ec5418c34ad8
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-04-08 13:18:52 +0200

    remove osermiller csv parser

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_masscalibration/standardslist/StandardsListSpreadsheetExtractor.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_masscalibration/standardslist/StandardsListSpreadsheetExtractor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -134,12 +134,4 @@ public class StandardsListSpreadsheetExtractor implements StandardsListExtractor
     return new StandardsList(extractedData);
   }
 
-  @Override
-  public void closeInputStreams() {
-    try {
-      spreadsheet.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
 }
