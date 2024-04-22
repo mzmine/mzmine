69fcc621a95fb677163062ca0b78737a61410b5d
niekdejonge <niekdejonge@gmail.com>
2024-04-22 12:45:35 +0200

    Update model to work with embeddings instead of predicting similarity

--- mzmine-community/src/main/java/io/github/mzmine/util/scans/similarity/impl/ms2deepscore/MS2DeepscoreModel.java
+++ mzmine-community/src/main/java/io/github/mzmine/util/scans/similarity/impl/ms2deepscore/MS2DeepscoreModel.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2004-2024 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.github.mzmine.util.scans.similarity.impl.ms2deepscore;
 
 import ai.djl.MalformedModelException;
@@ -10,53 +35,52 @@ import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
 import io.github.mzmine.datamodel.Scan;
-
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MS2DeepscoreModel {
-    /**
-     * Predicts the MS2Deepscore similarity
-     */
-    private final ZooModel<NDList, NDList> model;
 
-    public MS2DeepscoreModel(URI modelFilePath) throws ModelNotFoundException, MalformedModelException, IOException {
+  /**
+   * Predicts the MS2Deepscore similarity
+   */
+  private final ZooModel<NDList, NDList> model;
+
+  public MS2DeepscoreModel(URI modelFilePath)
+      throws ModelNotFoundException, MalformedModelException, IOException {
 //        todo load settings as well.
-        Criteria<NDList, NDList> criteria = Criteria.builder()
-                .setTypes(NDList.class, NDList.class)
-                .optModelPath(Paths.get(modelFilePath))
-                .optOption("mapLocation", "true") // this model requires mapLocation for GPU
-                .optProgress(new ProgressBar()).build();
+    Criteria<NDList, NDList> criteria = Criteria.builder().setTypes(NDList.class, NDList.class)
+        .optModelPath(Paths.get(modelFilePath))
+        .optOption("mapLocation", "true") // this model requires mapLocation for GPU
+        .optProgress(new ProgressBar()).build();
 
-        this.model = criteria.loadModel();
-    }
+    this.model = criteria.loadModel();
+  }
 
-    public NDList predict(NDArray spectrumNDArray1,
-                          NDArray spectrumNDArray2,
-                          NDArray metadataNDArray1,
-                          NDArray metadataNDArray2
-    ) throws TranslateException {
+  public NDArray predict(NDArray spectrumNDArray1, NDArray metadataNDArray1)
+      throws TranslateException {
 
-        Predictor<NDList, NDList> predictor = model.newPredictor();
-        return predictor.predict(new NDList(spectrumNDArray1, spectrumNDArray2, metadataNDArray1, metadataNDArray2));
-    }
+    Predictor<NDList, NDList> predictor = model.newPredictor();
+    NDList predictions = predictor.predict(new NDList(spectrumNDArray1, metadataNDArray1));
+    return predictions.getFirst();
+  }
 
-    public NDList tensorizeSpectrum(Scan Spectrum){
+  public NDList tensorizeSpectrum(Scan Spectrum) {
 //        Tensorizes a spectrum to be able to use as input for the model
 //        todo write the method for tensorization, using the settings.
-        return new NDList();
-    }
+    return new NDList();
+  }
 
-    public Double predictPair(Scan Spectrum1, Scan Spectrum2){
+  public Double predictPair(Scan Spectrum1, Scan Spectrum2) {
 //    Predict the similarity between two spectra
 //        todo write method
-        return 1.0;
-    }
-    public Double[][] predictMatrix(ArrayList<Scan> spectra1, ArrayList<Scan> spectra2){
+    return 1.0;
+  }
+
+  public Double[][] predictMatrix(ArrayList<Scan> spectra1, ArrayList<Scan> spectra2) {
 //    Predict the similarity between two lists of spectra
-        //        todo write method
-        return new Double[3][3];
-    }
+    //        todo write method
+    return new Double[3][3];
+  }
 }
--- mzmine-community/src/test/java/io/github/mzmine/util/scans/similarity/impl/ms2deepscore/MS2DeepscoreModelTest.java
+++ mzmine-community/src/test/java/io/github/mzmine/util/scans/similarity/impl/ms2deepscore/MS2DeepscoreModelTest.java
@@ -1,116 +1,117 @@
+/*
+ * Copyright (c) 2004-2024 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.github.mzmine.util.scans.similarity.impl.ms2deepscore;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import ai.djl.MalformedModelException;
-import ai.djl.inference.Predictor;
 import ai.djl.ndarray.NDArray;
-import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.NDManager;
-import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelNotFoundException;
-import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
 import io.github.mzmine.datamodel.PolarityType;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Random;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class MS2DeepscoreModelTest {
-    private static MS2DeepscoreModel model;
 
-    @BeforeAll
-    static void setUp() throws URISyntaxException, ModelNotFoundException, MalformedModelException, IOException {
-        // load model and setup objects that are shared with all tests
-        MassSpecTestData spec = new MassSpecTestData(
-                new double[]{200d, 200.1d}, new double[]{1000d, 2000d},
-                PolarityType.POSITIVE, 221d);
-        URI modelFilePath = MS2DeepscoreModelTest.class.getClassLoader()
-                .getResource("models/java_compatible_ms2deepscore_model.pt").toURI();
-        model = new MS2DeepscoreModel(modelFilePath);
+  private static MS2DeepscoreModel model;
 
+  @BeforeAll
+  static void setUp()
+      throws URISyntaxException, ModelNotFoundException, MalformedModelException, IOException {
+    // load model and setup objects that are shared with all tests
+    MassSpecTestData spec = new MassSpecTestData(new double[]{200d, 200.1d},
+        new double[]{1000d, 2000d}, PolarityType.POSITIVE, 221d);
+    URI modelFilePath = MS2DeepscoreModelTest.class.getClassLoader()
+        .getResource("models/java_embeddings_ms2deepscore_model.pt").toURI();
+    model = new MS2DeepscoreModel(modelFilePath);
+
+  }
+
+
+  @AfterAll
+  static void tearDown() {
+  }
+
+  // Method to generate a list of 1000 random values
+  private float[][] generateRandomList(int listLength, int numberOfArrays) {
+    float[][] listOfList = new float[numberOfArrays][listLength];
+    for (int i = 0; i < numberOfArrays; i++) {
+      Random random = new Random();
+      for (int j = 0; j < listLength; j++) {
+        float randomNumber = random.nextFloat();
+        listOfList[i][j] = randomNumber;
+      }
     }
 
+    return listOfList;
+  }
 
-    @AfterAll
-    static void tearDown() {
+  private NDArray generateNDArray(int listLength, float[] listValues) {
+
+    float[][] listOfList = new float[listValues.length][listLength];
+    for (int j = 0; j < listValues.length; j++) {
+      for (int i = 0; i < listLength; i++) {
+        listOfList[j][i] = listValues[j];
+      }
     }
 
-    // Method to generate a list of 1000 random values
-    private float[][] generateRandomList(int listLength, int numberOfArrays) {
-        float[][] listOfList = new float[numberOfArrays][listLength];
-        for (int i = 0; i < numberOfArrays; i++) {
-            Random random = new Random();
-            for (int j = 0; j < listLength; j++) {
-                float randomNumber = random.nextFloat();
-                listOfList[i][j] = randomNumber;
-            }
-        }
-
-        return listOfList;
+    try (NDManager manager = NDManager.newBaseManager()) {
+      return manager.create(listOfList);
     }
+  }
 
-    private float[][] generateList(int listLength, float listValues) {
-        float[][] listOfList = new float[1][listLength];
-        for (int i = 0; i < listLength; i++) {
-            listOfList[0][i] = listValues;
-        }
-        return listOfList;
+  @Test
+  void test_correct_prediction() {
+    try (NDManager manager = NDManager.newBaseManager()) {
+//      Create test input data
+
+      // Creates a NDArray which contains a matrix of shape 2 by listLength. This represents 2 vectorized spectra.
+      NDArray spectrumNDArray = generateNDArray(990, new float[]{0.1F, 0.2F});
+      // Creates a NDArray which contains a matrix of shape 2 by listLength. This represents the vectorized metadata of 2 spectra
+      NDArray metadataNDArray = generateNDArray(2, new float[]{0.0F, 1.0F});
+
+      NDArray predictions = model.predict(spectrumNDArray, metadataNDArray);
+//      todo @robin if I just used pure list of long, it gave me an error like expected: [J@50de186c<[1, 50]> but was: [J@3f57bcad<[1, 50]>, so I am guessing something like a different array type, but did not figure it out quickly
+      assertEquals("[2, 50]", Arrays.toString(predictions.getShape().getShape()));
+//      Test that the first number in the embedding is correct for the first test spectrum
+      assertEquals(predictions.get(0).getFloat(0), -0.046006925, 0.0001);
+//      Test that the first number in the embedding is correct for the second spectrum
+      assertEquals(predictions.get(1).getFloat(0), -0.03738583, 0.0001);
+
+    } catch (TranslateException e) {
+      throw new RuntimeException(e);
     }
-    @Test
-    void test_correct_prediction(){
-        try (NDManager manager = NDManager.newBaseManager()) {
-//                int nr_of_spectra = 3;
-            float[][] spectrumArray1 = generateList(990, 0.1F);
-            float[][] spectrumArray2 = generateList(990, 0.2F);
-            float[][] metadataArray1 = generateList(2, 0.0F);
-            float[][] metadataArray2 = generateList(2, 1.0F);
-            // Convert input float array to NDArray
-
-            NDArray spectrumNDArray1 = manager.create(spectrumArray1);
-            NDArray spectrumNDArray2 = manager.create(spectrumArray2);
-            NDArray metadataNDArray1 = manager.create(metadataArray1);
-            NDArray metadataNDArray2 = manager.create(metadataArray2);
-
-
-//                System.out.println(spectrumNDArray1);
-            NDList predictions = model.predict(spectrumNDArray1, spectrumNDArray2, metadataNDArray1, metadataNDArray2);
-            assertEquals(predictions.size(), 1);
-            assertEquals(predictions.getFirst().size(), 1);
-            assertEquals(predictions.get(0).getFloat(0), 0.9916, 0.0001);
-
-            System.out.println(Arrays.toString(predictions.toArray()));
-
-        } catch (TranslateException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void test_multiple_predictions() throws ModelNotFoundException, MalformedModelException, IOException {
-        try (NDManager manager = NDManager.newBaseManager()) {
-            int nr_of_spectra = 3;
-            NDArray spectrumNDArray1 = manager.create(generateRandomList(990, nr_of_spectra));
-            NDArray spectrumNDArray2 = manager.create(generateRandomList(990, nr_of_spectra));
-            NDArray metadataNDArray1 = manager.create(generateRandomList(2, nr_of_spectra));
-            NDArray metadataNDArray2 = manager.create(generateRandomList(2, nr_of_spectra));
-
-
-            NDList predictions = model.predict(spectrumNDArray1, spectrumNDArray2, metadataNDArray1, metadataNDArray2);
-            assertEquals(predictions.size(), 1);
-            assertEquals(predictions.getFirst().size(), nr_of_spectra);
-
-        } catch (TranslateException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 }
 
