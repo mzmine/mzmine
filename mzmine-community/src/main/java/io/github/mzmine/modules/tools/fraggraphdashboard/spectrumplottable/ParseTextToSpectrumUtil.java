fa1ed615c2a2a64283144d2e8f5685a2d69463cc
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-05-10 15:11:10 +0200

    format mz values in spectrum

--- mzmine-community/src/main/java/io/github/mzmine/modules/tools/fraggraphdashboard/spectrumplottable/ParseTextToSpectrumUtil.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/tools/fraggraphdashboard/spectrumplottable/ParseTextToSpectrumUtil.java
@@ -1,20 +1,26 @@
 /*
- * Copyright 2006-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
- * This file is part of MZmine.
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
  *
- * MZmine is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- * MZmine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with MZmine; if not,
- * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package io.github.mzmine.modules.tools.fraggraphdashboard.spectrumplottable;
@@ -23,6 +29,8 @@ import io.github.mzmine.datamodel.DataPoint;
 import io.github.mzmine.datamodel.MassSpectrum;
 import io.github.mzmine.datamodel.impl.SimpleDataPoint;
 import io.github.mzmine.datamodel.impl.SimpleMassSpectrum;
+import io.github.mzmine.gui.preferences.NumberFormats;
+import io.github.mzmine.main.ConfigService;
 import io.github.mzmine.util.DataPointUtils;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +40,6 @@ import java.util.logging.Logger;
 public class ParseTextToSpectrumUtil {
 
   private static Logger logger = Logger.getLogger(ParseTextToSpectrumUtil.class.getName());
-  private SimpleMassSpectrum spectrum;
 
   public static MassSpectrum parseStringToSpectrum(String singalList) {
     if (singalList.isBlank()) {
@@ -62,12 +69,14 @@ public class ParseTextToSpectrumUtil {
   }
 
   public static String spectrumToString(MassSpectrum spectrum) {
-    if(spectrum == null) {
+    if (spectrum == null) {
       return "";
     }
+    final NumberFormats formats = ConfigService.getExportFormats();
     StringBuilder b = new StringBuilder();
     for (int i = 0; i < spectrum.getNumberOfDataPoints(); i++) {
-      b.append(spectrum.getMzValue(i)).append("\t").append(spectrum.getIntensityValue(i));
+      b.append(formats.mz(spectrum.getMzValue(i))).append("\t")
+          .append(formats.intensity(spectrum.getIntensityValue(i)));
       if (i < spectrum.getNumberOfDataPoints() - 1) {
         b.append("\n");
       }
