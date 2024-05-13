965d1d1f92931607eff72580cfc118cc35544e0b
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-05-13 22:33:57 +0200

    adapt default visibilities

--- mzmine-community/src/main/java/io/github/mzmine/datamodel/features/types/annotations/iin/IonTypeType.java
+++ mzmine-community/src/main/java/io/github/mzmine/datamodel/features/types/annotations/iin/IonTypeType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -95,10 +95,5 @@ public class IonTypeType extends DataType<IonType> {
     }
 
     return IonType.loadFromXML(reader);
-  }
-
-  @Override
-  public boolean getDefaultVisibility() {
-    return true;
   }
 }
