d25e3d69a60c7a93e198e94ae42151966e28f522
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-05-23 13:17:20 +0200

    make MatchedLipid implement FeatureAnnotation

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_lipidid/common/identification/matched_levels/MatchedLipid.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_lipidid/common/identification/matched_levels/MatchedLipid.java
@@ -27,6 +27,12 @@ package io.github.mzmine.modules.dataprocessing.id_lipidid.common.identification
 
 import io.github.mzmine.datamodel.IonizationType;
 import io.github.mzmine.datamodel.RawDataFile;
+import io.github.mzmine.datamodel.features.ModularFeatureList;
+import io.github.mzmine.datamodel.features.ModularFeatureListRow;
+import io.github.mzmine.datamodel.features.compoundannotations.FeatureAnnotation;
+import io.github.mzmine.datamodel.identities.iontype.IonType;
+import io.github.mzmine.datamodel.identities.iontype.IonTypeParser;
+import io.github.mzmine.datamodel.structures.MolecularStructure;
 import io.github.mzmine.modules.dataprocessing.id_lipidid.common.identification.matched_levels.molecular_species.MolecularSpeciesLevelAnnotation;
 import io.github.mzmine.modules.dataprocessing.id_lipidid.common.identification.matched_levels.species_level.SpeciesLevelAnnotation;
 import io.github.mzmine.modules.dataprocessing.id_lipidid.common.lipids.ILipidAnnotation;
@@ -38,14 +44,20 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.logging.Logger;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
 
-public class MatchedLipid {
+public class MatchedLipid implements FeatureAnnotation {
 
   public static final String XML_ELEMENT = "matchedlipid";
+  private static final Logger logger = Logger.getLogger(MatchedLipid.class.getName());
   private static final String XML_LIPID_ANNOTATION_ELEMENT = "lipidannotation";
   private static final String XML_ACCURATE_MZ = "accuratemz";
   private static final String XML_IONIZATION_TYPE = "ionizationtype";
@@ -77,6 +89,98 @@ public class MatchedLipid {
     this.matchedFragments = matchedFragments;
     this.msMsScore = msMsScore;
     this.status = status;
+  }
+
+  public static MatchedLipid loadFromXML(XMLStreamReader reader,
+      Collection<RawDataFile> possibleFiles) throws XMLStreamException {
+    // differentiate between old and new versions
+    if ((reader.isStartElement() && reader.getLocalName().equals(XML_ELEMENT))) {
+      // is old style start element
+      logger.finest("Found old style lipid annotation entry, will convert to new one on save.");
+    } else if (reader.isStartElement() && reader.getLocalName()
+        .equals(FeatureAnnotation.XML_ELEMENT) && reader.getAttributeValue(null,
+        FeatureAnnotation.XML_TYPE_ATTR).equals(XML_ELEMENT)) {
+      // is new style start element
+      logger.finest("Found new style lipid annotation entry.");
+    } else {
+      throw new IllegalStateException(
+          "Cannot load matched lipid from the current element. Wrong name.");
+    }
+
+    ILipidAnnotation lipidAnnotation = null;
+    Double accurateMz = null;
+    IonizationType ionizationType = null;
+    Set<LipidFragment> lipidFragments = null;
+    Double msMsScore = null;
+    String comment = "";
+    MatchedLipidStatus status = null;
+    while (reader.hasNext() && !(reader.isEndElement() && (reader.getLocalName().equals(XML_ELEMENT)
+        || reader.getLocalName().equals(FeatureAnnotation.XML_ELEMENT)))) {
+      reader.next();
+      if (!reader.isStartElement()) {
+        continue;
+      }
+
+      switch (reader.getLocalName()) {
+        case XML_LIPID_ANNOTATION_ELEMENT -> {
+          if (reader.getAttributeValue(null, XML_LIPID_ANNOTATION_ELEMENT)
+              .equals(LipidAnnotationLevel.SPECIES_LEVEL.name())) {
+            lipidAnnotation = SpeciesLevelAnnotation.loadFromXML(reader);
+          } else if (reader.getAttributeValue(null, XML_LIPID_ANNOTATION_ELEMENT)
+              .equals(LipidAnnotationLevel.MOLECULAR_SPECIES_LEVEL.name())) {
+            lipidAnnotation = MolecularSpeciesLevelAnnotation.loadFromXML(reader);
+          }
+        }
+        case XML_ACCURATE_MZ -> accurateMz = Double.parseDouble(reader.getElementText());
+        case XML_IONIZATION_TYPE ->
+            ionizationType = ParsingUtils.ionizationNameToIonizationType(reader.getElementText());
+        case XML_MATCHED_FRAGMENTS ->
+            lipidFragments = loadLipidFragmentsFromXML(reader, possibleFiles);
+        case XML_MSMS_SCORE -> msMsScore = Double.parseDouble(reader.getElementText());
+        case XML_COMMENT -> {
+          if (reader.hasNext() && reader.next() == XMLStreamConstants.CHARACTERS) {
+            String text = reader.getText();
+            if (!Objects.equals(text, CONST.XML_NULL_VALUE)) {
+              comment = text;
+            }
+          }
+        }
+        case XML_STATUS -> status = MatchedLipidStatus.valueOf(reader.getElementText());
+        default -> {
+        }
+      }
+    }
+
+    MatchedLipid matchedLipid = new MatchedLipid(lipidAnnotation, accurateMz, ionizationType,
+        lipidFragments, msMsScore, status);
+    if (comment != null) {
+      matchedLipid.setComment(comment);
+    }
+    return matchedLipid;
+  }
+
+  private static Set<LipidFragment> loadLipidFragmentsFromXML(XMLStreamReader reader,
+      Collection<RawDataFile> possibleFiles) throws XMLStreamException {
+    if (!(reader.isStartElement() && reader.getLocalName().equals(XML_MATCHED_FRAGMENTS))) {
+      throw new IllegalStateException(
+          "Cannot load matched lipid fragments from the current element. Wrong name.");
+    }
+
+    Set<LipidFragment> lipidFragments = new HashSet<>();
+    while (reader.hasNext() && !(reader.isEndElement() && reader.getLocalName()
+        .equals(XML_MATCHED_FRAGMENTS))) {
+      reader.next();
+      if (!reader.isStartElement()) {
+        continue;
+      }
+      lipidFragments.add(LipidFragment.loadFromXML(reader, possibleFiles));
+    }
+    return lipidFragments;
+  }
+
+  public static double getExactMass(MatchedLipid match) {
+    return MolecularFormulaManipulator.getMass(match.getLipidAnnotation().getMolecularFormula(),
+        AtomContainerManipulator.MonoIsotopic) + match.getIonizationType().getAddedMass();
   }
 
   public ILipidAnnotation getLipidAnnotation() {
@@ -140,8 +244,12 @@ public class MatchedLipid {
     return lipidAnnotation.getAnnotation();
   }
 
-  public void saveToXML(XMLStreamWriter writer) throws XMLStreamException {
-    writer.writeStartElement(XML_ELEMENT);
+
+  @Override
+  public void saveToXML(@NotNull XMLStreamWriter writer, ModularFeatureList flist,
+      ModularFeatureListRow row) throws XMLStreamException {
+    writeOpeningTag(writer);
+
     lipidAnnotation.saveToXML(writer);
     writer.writeStartElement(XML_ACCURATE_MZ);
     writer.writeCharacters(accurateMz.toString());
@@ -171,87 +279,82 @@ public class MatchedLipid {
     writer.writeStartElement(XML_STATUS);
     writer.writeCharacters(status.name());
     writer.writeEndElement();
-    writer.writeEndElement();
 
+    writeClosingTag(writer);
   }
 
-  public static MatchedLipid loadFromXML(XMLStreamReader reader,
-      Collection<RawDataFile> possibleFiles) throws XMLStreamException {
-    if (!(reader.isStartElement() && reader.getLocalName().equals(XML_ELEMENT))) {
-      throw new IllegalStateException(
-          "Cannot load matched lipid from the current element. Wrong name.");
-    }
-
-    ILipidAnnotation lipidAnnotation = null;
-    Double accurateMz = null;
-    IonizationType ionizationType = null;
-    Set<LipidFragment> lipidFragments = null;
-    Double msMsScore = null;
-    String comment = "";
-    MatchedLipidStatus status = null;
-    while (reader.hasNext()
-        && !(reader.isEndElement() && reader.getLocalName().equals(XML_ELEMENT))) {
-      reader.next();
-      if (!reader.isStartElement()) {
-        continue;
-      }
-
-      switch (reader.getLocalName()) {
-        case XML_LIPID_ANNOTATION_ELEMENT -> {
-          if (reader.getAttributeValue(null, XML_LIPID_ANNOTATION_ELEMENT)
-              .equals(LipidAnnotationLevel.SPECIES_LEVEL.name())) {
-            lipidAnnotation = SpeciesLevelAnnotation.loadFromXML(reader);
-          } else if (reader.getAttributeValue(null, XML_LIPID_ANNOTATION_ELEMENT)
-              .equals(LipidAnnotationLevel.MOLECULAR_SPECIES_LEVEL.name())) {
-            lipidAnnotation = MolecularSpeciesLevelAnnotation.loadFromXML(reader);
-          }
-        }
-        case XML_ACCURATE_MZ -> accurateMz = Double.parseDouble(reader.getElementText());
-        case XML_IONIZATION_TYPE ->
-            ionizationType = ParsingUtils.ionizationNameToIonizationType(reader.getElementText());
-        case XML_MATCHED_FRAGMENTS ->
-            lipidFragments = loadLipidFragmentsFromXML(reader, possibleFiles);
-        case XML_MSMS_SCORE -> msMsScore = Double.parseDouble(reader.getElementText());
-        case XML_COMMENT -> {
-          if (reader.hasNext() && reader.next() == XMLStreamConstants.CHARACTERS) {
-            String text = reader.getText();
-            if (!Objects.equals(text, CONST.XML_NULL_VALUE)) {
-              comment = text;
-            }
-          }
-        }
-        case XML_STATUS -> status = MatchedLipidStatus.valueOf(reader.getElementText());
-        default -> {
-        }
-      }
-    }
-
-    MatchedLipid matchedLipid = new MatchedLipid(lipidAnnotation, accurateMz, ionizationType,
-        lipidFragments, msMsScore, status);
-    if (comment != null) {
-      matchedLipid.setComment(comment);
-    }
-    return matchedLipid;
+  @Override
+  public @Nullable Double getPrecursorMZ() {
+    return getExactMass(this);
   }
 
-  private static Set<LipidFragment> loadLipidFragmentsFromXML(XMLStreamReader reader,
-      Collection<RawDataFile> possibleFiles) throws XMLStreamException {
-    if (!(reader.isStartElement() && reader.getLocalName().equals(XML_MATCHED_FRAGMENTS))) {
-      throw new IllegalStateException(
-          "Cannot load matched lipid fragments from the current element. Wrong name.");
-    }
-
-    Set<LipidFragment> lipidFragments = new HashSet<>();
-    while (reader.hasNext()
-        && !(reader.isEndElement() && reader.getLocalName().equals(XML_MATCHED_FRAGMENTS))) {
-      reader.next();
-      if (!reader.isStartElement()) {
-        continue;
-      }
-      lipidFragments.add(LipidFragment.loadFromXML(reader, possibleFiles));
-
-    }
-    return lipidFragments;
+  @Override
+  public @Nullable MolecularStructure getStructure() {
+    return null;
   }
 
+  @Override
+  public @Nullable String getSmiles() {
+    return null;
+  }
+
+  @Override
+  public @Nullable String getInChI() {
+    return null;
+  }
+
+  @Override
+  public @Nullable String getInChIKey() {
+    return null;
+  }
+
+  @Override
+  public @Nullable String getCompoundName() {
+    return getLipidAnnotation().getAnnotation();
+  }
+
+  @Override
+  public @Nullable String getFormula() {
+    return MolecularFormulaManipulator.getString(getLipidAnnotation().getMolecularFormula());
+  }
+
+  @Override
+  public @Nullable IonType getAdductType() {
+    try {
+      return IonTypeParser.parse(getIonizationType().toString());
+    } catch (Exception e) {
+      logger.fine(() -> STR."Error parsing ion type \{getIonizationType().toString()}");
+      return null;
+    }
+  }
+
+  @Override
+  public @Nullable Float getMobility() {
+    return null;
+  }
+
+  @Override
+  public @Nullable Float getCCS() {
+    return null;
+  }
+
+  @Override
+  public @Nullable Float getRT() {
+    return null;
+  }
+
+  @Override
+  public @Nullable Float getScore() {
+    return getMsMsScore() != null ? getMsMsScore().floatValue() : null;
+  }
+
+  @Override
+  public @Nullable String getDatabase() {
+    return "Rule-based lipid fragmentation";
+  }
+
+  @Override
+  public @NotNull String getXmlAttributeKey() {
+    return XML_ELEMENT;
+  }
 }
