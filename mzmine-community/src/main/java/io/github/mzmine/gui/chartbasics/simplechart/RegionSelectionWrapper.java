83537c0ef7db9a716646f8dc163bf0e9179b6154
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-05-27 10:27:49 +0200

    update javadoc

--- mzmine-community/src/main/java/io/github/mzmine/gui/chartbasics/simplechart/RegionSelectionWrapper.java
+++ mzmine-community/src/main/java/io/github/mzmine/gui/chartbasics/simplechart/RegionSelectionWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The MZmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -89,6 +89,10 @@ public class RegionSelectionWrapper<T extends EChartViewer & AllowsRegionSelecti
   private final Paint roiPaint = MZmineCore.getConfiguration().getDefaultColorPalette()
       .getNegativeColorAWT();
 
+  /**
+   *
+   * @param node The chart
+   */
   public RegionSelectionWrapper(T node) {
     this.node = node;
     setCenter(node);
