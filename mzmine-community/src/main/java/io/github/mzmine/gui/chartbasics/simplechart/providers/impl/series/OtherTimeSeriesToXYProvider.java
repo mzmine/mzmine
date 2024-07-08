2fe114f24deebe766eebc0b098bffe430c3795b5
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-07-08 18:07:01 +0200

    add simple visualiser

---
+++ mzmine-community/src/main/java/io/github/mzmine/gui/chartbasics/simplechart/providers/impl/series/OtherTimeSeriesToXYProvider.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2004-2024 The MZmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.gui.chartbasics.simplechart.providers.impl.series;
+
+import io.github.mzmine.datamodel.otherdetectors.OtherTimeSeries;
+import io.github.mzmine.gui.chartbasics.simplechart.providers.PlotXYDataProvider;
+import io.github.mzmine.gui.preferences.NumberFormats;
+import io.github.mzmine.main.ConfigService;
+import io.github.mzmine.taskcontrol.TaskStatus;
+import java.awt.Color;
+import javafx.beans.property.Property;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class OtherTimeSeriesToXYProvider implements PlotXYDataProvider {
+
+  private final javafx.scene.paint.Color colorFx;
+  private final Color colorAwt;
+  @NotNull
+  private final OtherTimeSeries series;
+  private final NumberFormats formats;
+
+  public OtherTimeSeriesToXYProvider(OtherTimeSeries series) {
+    colorFx = series.getOtherDataFile().getRawDataFile().getColor();
+    colorAwt = series.getOtherDataFile().getRawDataFile().getColorAWT();
+    this.series = series;
+    formats = ConfigService.getGuiFormats();
+  }
+
+  @NotNull
+  @Override
+  public Color getAWTColor() {
+    return colorAwt;
+  }
+
+  @NotNull
+  @Override
+  public javafx.scene.paint.Color getFXColor() {
+    return colorFx;
+  }
+
+  @Override
+  public @Nullable String getLabel(int index) {
+    return null;
+  }
+
+  @Override
+  public @NotNull Comparable<?> getSeriesKey() {
+    return STR."\{series.getChromatoogramType()} \{series.getName()}";
+  }
+
+  @Override
+  public @Nullable String getToolTipText(int itemIndex) {
+    return "Intensity: " + formats.intensity(getDomainValue(itemIndex)) + "\nRT: " + formats.rt(
+        getRangeValue(itemIndex));
+  }
+
+  @Override
+  public void computeValues(Property<TaskStatus> status) {
+    return;
+  }
+
+  @Override
+  public double getDomainValue(int index) {
+    return series.getRetentionTime(index);
+  }
+
+  @Override
+  public double getRangeValue(int index) {
+    return series.getIntensity(index);
+  }
+
+  @Override
+  public int getValueCount() {
+    return series.getNumberOfValues();
+  }
+
+  @Override
+  public double getComputationFinishedPercentage() {
+    return 1d;
+  }
+}
---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/otherdetectors/multidetector/MultidetectorVisualizerBuilder.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2004-2024 The MZmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.visualization.otherdetectors.multidetector;
+
+import io.github.mzmine.javafx.components.factories.FxButtons;
+import io.github.mzmine.javafx.components.util.FxLayout;
+import io.github.mzmine.javafx.mvci.FxViewBuilder;
+import io.github.mzmine.javafx.util.FxIcons;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+public class MultidetectorVisualizerBuilder extends FxViewBuilder<MultidetectorVisualizerModel> {
+
+  private VBox content;
+
+  protected MultidetectorVisualizerBuilder(MultidetectorVisualizerModel model) {
+    super(model);
+  }
+
+  @Override
+  public Region build() {
+    final ScrollPane scrollPane = new ScrollPane();
+    content = FxLayout.newVBox();
+    scrollPane.setFitToWidth(true);
+    scrollPane.setFitToHeight(true);
+
+    final Button addButton = FxButtons.createButton(null, FxIcons.ADD, "Add another trace",
+        this::addNewDetector);
+    final HBox addWrapper = FxLayout.newHBox(Pos.TOP_RIGHT, addButton);
+    final VBox contentWrapper = FxLayout.newVBox(content, addWrapper);
+    scrollPane.setContent(contentWrapper);
+    return new BorderPane(scrollPane);
+  }
+
+  private void addNewDetector() {
+    content.getChildren().add(new DetectorPane());
+  }
+}
