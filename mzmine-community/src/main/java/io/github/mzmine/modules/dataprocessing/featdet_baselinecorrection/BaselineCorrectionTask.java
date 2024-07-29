5f72ea1e7872cec4f4f55ef4c385e1094dfe97c3
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-07-29 16:47:55 +0200

    start all java baseline correction

---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_baselinecorrection/BaselineCorrectionTask.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2004-2024 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.dataprocessing.featdet_baselinecorrection;
+
+import io.github.mzmine.datamodel.RawDataFile;
+import io.github.mzmine.datamodel.features.FeatureList;
+import io.github.mzmine.modules.MZmineModule;
+import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.taskcontrol.AbstractSimpleTask;
+import io.github.mzmine.util.MemoryMapStorage;
+import java.time.Instant;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BaselineCorrectionTask extends AbstractSimpleTask {
+
+  /**
+   * @param storage        The {@link MemoryMapStorage} used to store results of this task (e.g.
+   *                       RawDataFiles, MassLists, FeatureLists). May be null if results shall be
+   *                       stored in ram. For now, one storage should be created per module call in
+   * @param moduleCallDate the call date of module to order execution order
+   * @param parameters
+   * @param moduleClass
+   */
+  protected BaselineCorrectionTask(@Nullable MemoryMapStorage storage,
+      @NotNull Instant moduleCallDate, @NotNull ParameterSet parameters,
+      @NotNull Class<? extends MZmineModule> moduleClass) {
+    super(storage, moduleCallDate, parameters, moduleClass);
+
+  }
+
+  @Override
+  protected void process() {
+
+  }
+
+  @Override
+  protected @NotNull List<FeatureList> getProcessedFeatureLists() {
+    return List.of();
+  }
+
+  @Override
+  protected @NotNull List<RawDataFile> getProcessedDataFiles() {
+    return List.of();
+  }
+
+  @Override
+  public String getTaskDescription() {
+    return "";
+  }
+}
