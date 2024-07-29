643e03a1112c26c9a1f6b754e438c647867f4293
Robin Schmid <rschmid1789@gmail.com>
2024-07-29 16:27:44 +0200

    fix ExtensionFilters

--- utils/src/main/java/io/github/mzmine/util/files/ExtensionFilters.java
+++ utils/src/main/java/io/github/mzmine/util/files/ExtensionFilters.java
@@ -57,11 +57,6 @@ public class ExtensionFilters {
   public static final ExtensionFilter CSV_OR_TSV = new ExtensionFilter("CSV or TSV data", "*.csv",
       "*.tsv");
 
-  /**
-   * Executables
-   */
-  public static final ExtensionFilter EXE = new ExtensionFilter("Executable", "*.exe");
-
   // LISTS
   public static final List<ExtensionFilter> CSV_TSV_IMPORT = List.of(CSV_OR_TSV, CSV, TSV,
       ALL_FILES);
