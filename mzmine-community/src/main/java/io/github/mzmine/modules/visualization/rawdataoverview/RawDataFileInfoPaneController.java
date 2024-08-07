4e7aac498fa08fdd36a06a3f8e47edea0cc6bf16
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-08-07 15:56:22 +0200

    fix resizing of raw data info pane

--- mzmine-community/src/main/java/io/github/mzmine/modules/visualization/rawdataoverview/RawDataFileInfoPaneController.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/rawdataoverview/RawDataFileInfoPaneController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -143,7 +143,7 @@ public class RawDataFileInfoPaneController {
     TableColumns.setFormattedCellFactory(injectTimeColumn, rtFormat);
     TableColumns.setFormattedRangeCellFactory(mzRangeColumn, mzFormat);
 
-    TableColumns.autoFitLastColumn(rawDataTableView);
+    rawDataTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN);
   }
 
   /**
