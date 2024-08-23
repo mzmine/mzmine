b04ad0434cb40dba06f8635cb17f835e54946861
MaximilianToe <mtoenies@outlook.de>
2024-08-23 09:01:39 +0200

    Added MLFeatureResolver w/ basic duplicate filter

--- mzmine-community/src/main/java/io/github/mzmine/util/scans/PeakPickingModel/PeakPickingModel.java
+++ mzmine-community/src/main/java/io/github/mzmine/util/scans/PeakPickingModel/PeakPickingModel.java
@@ -1,25 +1,28 @@
 package io.github.mzmine.util.scans.PeakPickingModel;
 
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import ai.djl.Model;
-import ai.djl.translate.Translator;
 import ai.djl.inference.Predictor;
 import ai.djl.ndarray.NDManager;
 import ai.djl.translate.TranslateException;
-import java.util.Map;
-import java.util.List;
-import java.io.InputStream;
+import ai.djl.translate.Translator;
 
 public class PeakPickingModel {
-    private static final String modelDir = "/MLModels/peakPicking.pt";
+    //NEED TO TRACE MODEL AGAIN WITH (1,128) INPUT SHAPE (this is what _test.py is)
+    private static final String modelPath = "./traced_check1_test.pt";
     private final Model model;
     private final NDManager manager;
     private final Translator<double[], Map<String,double[]>> translator;
-    public final Predictor<double[], Map<String,double[]>> predictor;
+    public final Predictor<double[], Map<String,double[]>> predictor; 
     
     public PeakPickingModel(){
         this.model = Model.newInstance("PeakPicking");
         try{
-            final InputStream resourceAsStream = this.getClass().getResourceAsStream(modelDir);
+            final InputStream resourceAsStream = this.getClass().getResourceAsStream(modelPath);
             model.load(resourceAsStream);
         } catch (Exception e) {
             System.out.println("Encountered exception when loading the model.");
@@ -27,7 +30,7 @@ public class PeakPickingModel {
         }
         this.manager = NDManager.newBaseManager();
         this.translator = new PeakPickingTranslator(this.manager);
-        this.predictor = model.newPredictor(this.translator);
+        this.predictor = model.newPredictor(this.translator); 
     }
     
     public void closeModel(){
@@ -63,7 +66,9 @@ public class PeakPickingModel {
             testInput[i] = i;
         }
         Map<String, double[]> testPrediction = testModel.singlePrediction(testInput);
-        System.out.println(testPrediction);
+        System.out.println(Arrays.toString(testPrediction.get("probs")));
+        testModel.closeModel();
+        System.out.println("Main done.");
     }
 
 }
