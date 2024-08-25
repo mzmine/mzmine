9e24cbc600d242ff5f692fe085c9c82a4262348b
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-08-25 17:29:16 +0200

    change crosshair color based on mzmine theme

--- mzmine-community/src/main/java/io/github/mzmine/util/color/ColorUtils.java
+++ mzmine-community/src/main/java/io/github/mzmine/util/color/ColorUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -112,5 +112,9 @@ public class ColorUtils {
 
   public static boolean isDark(Color clr) {
     return getColorDifference(clr, Color.BLACK) < 250;
+  }
+
+  public static boolean isTransparent(Color clr) {
+    return clr.getOpacity() < 0.4;
   }
 }
