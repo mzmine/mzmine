1562fd9fabbf1b60f56d8fcb6f7cd2f75b0cdf9a
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-08-25 17:44:08 +0200

    update OtherFeatureImpl#toString

--- mzmine-community/src/main/java/io/github/mzmine/datamodel/otherdetectors/OtherFeatureImpl.java
+++ mzmine-community/src/main/java/io/github/mzmine/datamodel/otherdetectors/OtherFeatureImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -28,6 +28,7 @@ package io.github.mzmine.datamodel.otherdetectors;
 import io.github.mzmine.datamodel.featuredata.FeatureDataUtils;
 import io.github.mzmine.datamodel.features.types.DataType;
 import io.github.mzmine.datamodel.features.types.otherdectectors.OtherFeatureDataType;
+import io.github.mzmine.datamodel.features.types.otherdectectors.RawTraceType;
 import io.github.mzmine.main.ConfigService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
@@ -54,19 +55,18 @@ public class OtherFeatureImpl implements OtherFeature {
     StringBuilder sb = new StringBuilder();
 
     if (getFeatureData() != null) {
-      sb.append(getFeatureData().getOtherDataFile().getCorrespondingRawDataFile().getName())
-          .append(" ");
+      sb.append(getFeatureData().getName()).append(" ");
+    }
+    if (getWavelength() != null) {
+      sb.append(getWavelength()).append(" nm ");
     }
     if (getChromatogramType() != null) {
       sb.append(getChromatogramType().getDescription()).append(" ");
     }
-    if (getRT() != null) {
+    if (getRT() != null && get(RawTraceType.class) != null) {
       sb.append(ConfigService.getGuiFormats().rt(getRT())).append(" min ");
     }
-    if (getWavelength() != null) {
-      sb.append(getWavelength()).append(" nm");
-    }
 
-    return sb.toString();
+    return sb.toString().trim();
   }
 }
