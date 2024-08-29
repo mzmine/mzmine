0d384608e893e0fe51399de767a109245d1bebc1
MaximilianToe <mtoenies@outlook.de>
2024-08-29 10:53:59 +0200

    added resolver

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_ML/SplitSeries.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_ML/SplitSeries.java
@@ -1,40 +1,52 @@
-package  io.github.mzmine.modules.dataprocessing.featdet_ML;
+package io.github.mzmine.modules.dataprocessing.featdet_ML;
 
-import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SplitSeries {
-    
-    //@brief cuts out a single region of length @regionSize with start point @start
-    public static final double[] extractSingleRegion(double[] fullSeries, int start, int regionSize){
-        double[] standardRegion = new double[regionSize];
-        System.arraycopy(fullSeries, start , standardRegion, 0, regionSize);
 
-        //How should .orElse be handeled?
-        final double maxIntensity = Arrays.stream(standardRegion).max().orElse(Double.NaN);
-        if (maxIntensity != 0){
-            Arrays.stream(standardRegion).map(n -> n/maxIntensity).toArray();
-        }
-        return standardRegion;
-    }    
- 
-    //@brief iterates over the full series and cuts outs regions with overslap @regionSize/2. Adds zero padding to the end to get the right length.
-    public static final List<double[]> extractRegionBatch(double[] fullSeries, int regionSize){
-        int seriesLength = fullSeries.length;
-        int numRegions = seriesLength/(regionSize/2);
-        int paddingLength=(numRegions+1)*(regionSize/2)-seriesLength;
-        double[] paddedSeries = new double[seriesLength + paddingLength];
-        System.arraycopy(fullSeries, 0,paddedSeries,0, seriesLength);
-        List<double[]> regionBatch = new ArrayList<>(); 
-        for (int i=0; i<numRegions;i++){
-           regionBatch.add(extractSingleRegion(fullSeries, i*(regionSize/2), regionSize)); 
-        }
-        return regionBatch;
+  //@brief cuts out a single region of length @regionSize with start point @start
+  public static final double[] extractSingleRegion(double[] fullSeries, int start, int regionSize) {
+    double[] standardRegion = new double[regionSize];
+    System.arraycopy(fullSeries, start, standardRegion, 0, regionSize);
+
+    //How should .orElse be handeled?
+    final double maxIntensity = Arrays.stream(standardRegion).max().orElse(0);
+    if (maxIntensity != 0) {
+      Arrays.stream(standardRegion).map(n -> n / maxIntensity).toArray();
     }
-    
-    //@brief extract batch with default length 128
-    public static final List<double[]> extractRegionBatch(double[] fullSeries){
-        return extractRegionBatch(fullSeries, 128);
+    return standardRegion;
+  }
+
+  //@brief iterates over the full series and cuts outs regions with overslap @regionSize/2. Adds zero padding to the end to get the right length.
+  public static final List<double[]> extractRegionBatch(double[] fullSeries, int regionSize,
+      int overlap, String paddingType) {
+    int seriesLength = fullSeries.length;
+    //The first region takes regionSize from overlap or regionSize-overlap from seriesLength-overlap.
+    //Every additional region takes (regionSize-overlap).
+    //Should check that SeriesLength, regionSize > overlap
+    int numRegions = (seriesLength - overlap) / (regionSize - overlap);
+    int leftover = (seriesLength - overlap) % (regionSize - overlap);
+    int paddingLength = regionSize - leftover - overlap;
+    double[] paddedSeries = new double[seriesLength + paddingLength];
+    System.arraycopy(fullSeries, 0, paddedSeries, 0, seriesLength);
+    if (paddingType == "lastValue") {
+      double lastValue = fullSeries[fullSeries.length - 1];
+      double[] padding = new double[paddingLength];
+      Arrays.fill(padding, lastValue);
+      System.arraycopy(padding, 0, paddedSeries, fullSeries.length, padding.length);
     }
+
+    List<double[]> regionBatch = new ArrayList<>();
+    for (int i = 0; i < numRegions + 1; i++) {
+      regionBatch.add(extractSingleRegion(paddedSeries, i * (regionSize - overlap), regionSize));
+    }
+    return regionBatch;
+  }
+
+  //@brief extract batch with default length 128
+  public static final List<double[]> extractRegionBatch(double[] fullSeries) {
+    return extractRegionBatch(fullSeries, 128, 64, "zero");
+  }
 }
--- mzmine-community/src/main/java/io/github/mzmine/util/scans/PeakPickingModel/PeakPickingModel.java
+++ mzmine-community/src/main/java/io/github/mzmine/util/scans/PeakPickingModel/PeakPickingModel.java
@@ -29,20 +29,16 @@ import ai.djl.Model;
 import ai.djl.inference.Predictor;
 import ai.djl.ndarray.NDManager;
 import ai.djl.translate.TranslateException;
-import ai.djl.translate.Translator;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class PeakPickingModel {
     //NEED TO TRACE MODEL AGAIN WITH (1,128) INPUT SHAPE (this is what _test.py is)
-    private static final String modelPath = "/MLModels/traced_check1_test.pt";
+    private static final String modelPath = "/MLModels/traced_check1.pt";
     private final Model model;
     private final NDManager manager;
-    private final Translator<double[], Map<String,double[]>> translator;
-    public final Predictor<double[], Map<String,double[]>> predictor; 
-    
+    public final Predictor<double[], PeakPickingOutput> predictor;
+
     public PeakPickingModel(){
         this.model = Model.newInstance("PeakPicking");
         try{
@@ -53,17 +49,17 @@ public class PeakPickingModel {
             e.printStackTrace();
         }
         this.manager = NDManager.newBaseManager();
-        this.translator = new PeakPickingTranslator(this.manager);
-        this.predictor = model.newPredictor(this.translator); 
+        PeakPickingTranslator translator = new PeakPickingTranslator();
+        this.predictor = model.newPredictor(translator);
     }
-    
+
     public void closeModel(){
         this.manager.close();
         this.predictor.close();
         this.model.close();
     }
-    
-    public Map<String,double[]> singlePrediction(double[] inputArray){
+
+    public PeakPickingOutput singlePrediction(double[] inputArray){
         try{
             return this.predictor.predict(inputArray);
         } catch(TranslateException e){
@@ -72,8 +68,8 @@ public class PeakPickingModel {
             return null;
         }
     }
-    
-    public List<Map<String, double[]>> batchPrediction(List<double[]> inputBatch){
+
+    public List<PeakPickingOutput> batchPrediction(List<double[]> inputBatch){
        try{
         return this.predictor.batchPredict(inputBatch);
        } catch (TranslateException e){
@@ -82,17 +78,6 @@ public class PeakPickingModel {
         return null;
        }
     }
-    
-    public static void main(String[] args){
-        PeakPickingModel testModel = new PeakPickingModel();
-        double[] testInput = new double[128];
-        for (int i=0; i<128; i++){
-            testInput[i] = i;
-        }
-        Map<String, double[]> testPrediction = testModel.singlePrediction(testInput);
-        System.out.println(Arrays.toString(testPrediction.get("probs")));
-        testModel.closeModel();
-        System.out.println("Main done.");
-    }
+
 
 }
