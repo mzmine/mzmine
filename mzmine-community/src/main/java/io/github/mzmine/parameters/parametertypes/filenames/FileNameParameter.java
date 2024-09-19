6ad39b1e5e35a46d077f2a327d329ca24dc7b442
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-09-19 16:18:00 +0200

    save relative paths in projects
    FileNamesParameter, FileNameParameter, RawDataFilesParameter

--- mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/filenames/FileNameParameter.java
+++ mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/filenames/FileNameParameter.java
@@ -25,11 +25,15 @@
 
 package io.github.mzmine.parameters.parametertypes.filenames;
 
+import io.github.mzmine.datamodel.MZmineProject;
 import io.github.mzmine.parameters.UserParameter;
+import io.github.mzmine.project.ProjectService;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Pattern;
 import javafx.stage.FileChooser.ExtensionFilter;
 import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Document;
@@ -41,6 +45,10 @@ import org.w3c.dom.NodeList;
  * Simple Parameter implementation
  */
 public class FileNameParameter implements UserParameter<File, FileNameComponent> {
+
+  public static final Pattern FILE_PATH_PATTERN = Pattern.compile(
+      "^(?:[a-zA-Z]:\\\\|\\/)?(?:[^<>:\"/\\\\|?*\\x00-\\x1F]+[\\/\\\\]?)*$");
+  public static final String SPLIT_CHARACTER = "||";
 
   protected static final String CURRENT_FILE_ELEMENT = "current_file";
   protected static final String LAST_FILE_ELEMENT = "last_file";
@@ -170,8 +178,21 @@ public class FileNameParameter implements UserParameter<File, FileNameComponent>
   @Override
   public void loadValueFromXML(Element xmlElement) {
     NodeList current = xmlElement.getElementsByTagName(CURRENT_FILE_ELEMENT);
+    final File projectFile = ProjectService.getProject().getProjectFile();
+
     if (current.getLength() == 1) {
-      setValue(new File(current.item(0).getTextContent()));
+      final File absFile = new File(current.item(0).getTextContent());
+
+      final String relPathValue = ((Element) current.item(0)).getAttribute(
+          FileNamesParameter.XML_RELATIVE_PATH_ATTRIBUTE);
+      final File relFile = relPathValue.isBlank() ? null : new File(relPathValue);
+
+      if (!absFile.exists() && relFile != null && projectFile != null && projectFile.toPath()
+          .resolve(relFile.toPath()).toFile().exists()) {
+        setValue(projectFile.toPath().resolve(relFile.toPath()).toFile());
+      } else {
+        setValue(absFile);
+      }
     }
     // add all still existing files
     lastFiles = new ArrayList<>();
@@ -180,9 +201,17 @@ public class FileNameParameter implements UserParameter<File, FileNameComponent>
     for (int i = 0; i < last.getLength(); i++) {
       Node n = last.item(i);
       if (n.getTextContent() != null) {
-        File f = new File(n.getTextContent());
-        if (f.exists()) {
-          lastFiles.add(f);
+        File absFile = new File(n.getTextContent());
+
+        final String relPathAttr = ((Element) n).getAttribute(
+            FileNamesParameter.XML_RELATIVE_PATH_ATTRIBUTE);
+        final File relFile = !relPathAttr.isBlank() ? new File(relPathAttr) : null;
+
+        if (absFile.exists()) {
+          lastFiles.add(absFile);
+        } else if (!absFile.exists() && (relFile != null && projectFile != null
+            && projectFile.toPath().resolve(relFile.toPath()).toFile().exists())) {
+          lastFiles.add(projectFile.toPath().resolve(relFile.toPath()).toFile());
         }
       }
     }
@@ -193,9 +222,17 @@ public class FileNameParameter implements UserParameter<File, FileNameComponent>
   public void saveValueToXML(Element xmlElement) {
     // add new element for each file
     Document parentDocument = xmlElement.getOwnerDocument();
+    final MZmineProject project = ProjectService.getProject();
+
     if (value != null) {
       Element paramElement = parentDocument.createElement(CURRENT_FILE_ELEMENT);
       paramElement.setTextContent(value.getAbsolutePath());
+
+      final Path relativePath = project.getRelativePath(value.toPath());
+      if (relativePath != null) {
+        paramElement.setAttribute(FileNamesParameter.XML_RELATIVE_PATH_ATTRIBUTE,
+            relativePath.toString());
+      }
       xmlElement.appendChild(paramElement);
     }
 
@@ -203,6 +240,12 @@ public class FileNameParameter implements UserParameter<File, FileNameComponent>
       for (File f : lastFiles) {
         Element paramElement = parentDocument.createElement(LAST_FILE_ELEMENT);
         paramElement.setTextContent(f.getAbsolutePath());
+
+        final Path relativePath = project.getRelativePath(f.toPath());
+        if (relativePath != null && relativePath.toFile().exists()) {
+          paramElement.setAttribute(FileNamesParameter.XML_RELATIVE_PATH_ATTRIBUTE,
+              relativePath.toString());
+        }
         xmlElement.appendChild(paramElement);
       }
     }
