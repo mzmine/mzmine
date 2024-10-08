7e6e40b6db1803a6d03ee4a1c0c2f58483cf5335
Adriano Rutz <adriano.rutz@hotmail.com>
2024-10-08 13:24:39 +0200

    added adduct if missing (thx @robinschmid)

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/process_signalsanalysis/CommonMs1Ms2FragmentsAnalysisTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/process_signalsanalysis/CommonMs1Ms2FragmentsAnalysisTask.java
@@ -249,6 +249,11 @@ class CommonMs1Ms2FragmentsAnalysisTask extends AbstractFeatureListTask {
       }
     }
 
+    // Add the target to the list if it's not already there
+    if (!matchingPoints.contains(target)) {
+      matchingPoints.add(target);
+    }
+
     return matchingPoints;
   }
 
