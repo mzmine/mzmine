robinschmid/mzmine3

rename to SharedMs1Ms2FragmentsAnalysisModule

mzmine-community/src/main/java/io/github/mzmine/modules/batchmode/BatchModeModulesList.java
@@ -115,7 +115,7 @@ import io.github.mzmine.modules.dataprocessing.id_spectral_library_match.library
 import io.github.mzmine.modules.dataprocessing.norm_linear.LinearNormalizerModule;
 import io.github.mzmine.modules.dataprocessing.norm_rtcalibration.RTCorrectionModule;
 import io.github.mzmine.modules.dataprocessing.norm_standardcompound.StandardCompoundNormalizerModule;
-import io.github.mzmine.modules.dataprocessing.process_signalsanalysis.SignalsAnalysisModule;
+import io.github.mzmine.modules.dataprocessing.process_signalsanalysis.SharedMs1Ms2FragmentsAnalysisModule;
 import io.github.mzmine.modules.example.export_coding_demo.CodingDemoModule;
 import io.github.mzmine.modules.io.export_ccsbase.CcsBaseExportModule;
 import io.github.mzmine.modules.io.export_compoundAnnotations_csv.CompoundAnnotationsCSVExportModule;
@@ -319,7 +319,7 @@ public class BatchModeModulesList {
       ClearFeatureAnnotationsModule.class, //
       LinearNormalizerModule.class, //
       RTCorrectionModule.class, //
-      SignalsAnalysisModule.class, //
+      SharedMs1Ms2FragmentsAnalysisModule.class, //
       StandardCompoundNormalizerModule.class, //
 
       /*

mzmine/mzmine

mzmine-community/src/main/java/io/github/mzmine/modules/batchmode/BatchModeModulesList.java
@@ -38,7 +38,6 @@ import io.github.mzmine.modules.dataprocessing.align_path.PathAlignerModule;
 import io.github.mzmine.modules.dataprocessing.align_ransac.RansacAlignerModule;
 import io.github.mzmine.modules.dataprocessing.featdet_adap3d.ADAP3DModule;
 import io.github.mzmine.modules.dataprocessing.featdet_adapchromatogrambuilder.ModularADAPChromatogramBuilderModule;
-import io.github.mzmine.modules.dataprocessing.featdet_baselinecorrection.BaselineCorrectionModule;
 import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.minimumsearch.MinimumSearchFeatureResolverModule;
 import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.noiseamplitude.NoiseAmplitudeResolverModule;
 import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.savitzkygolay.SavitzkyGolayResolverModule;
@@ -117,6 +116,7 @@ import io.github.mzmine.modules.dataprocessing.norm_linear.LinearNormalizerModul
 import io.github.mzmine.modules.dataprocessing.norm_rtcalibration.RTCorrectionModule;
 import io.github.mzmine.modules.dataprocessing.norm_standardcompound.StandardCompoundNormalizerModule;
 import io.github.mzmine.modules.dataprocessing.process_signalsanalysis.SignalsAnalysisModule;
+import io.github.mzmine.modules.example.export_coding_demo.CodingDemoModule;
 import io.github.mzmine.modules.io.export_ccsbase.CcsBaseExportModule;
 import io.github.mzmine.modules.io.export_compoundAnnotations_csv.CompoundAnnotationsCSVExportModule;
 import io.github.mzmine.modules.io.export_features_all_speclib_matches.ExportAllIdsGraphicalModule;
@@ -269,7 +269,6 @@ public class BatchModeModulesList {
       ADAP3DModule.class, //
       ImsExpanderModule.class, //
       MaldiSpotFeatureDetectionModule.class, //
-      BaselineCorrectionModule.class, //
 
       /*
        * {@link io.github.mzmine.modules.MZmineModuleCategory.MainCategory#FEATURE_DETECTION}
@@ -450,7 +449,7 @@ public class BatchModeModulesList {
       PCAModule.class, //
       VolcanoPlotModule.class //
 
-//      , CodingDemoModule.class // only test purpose
+      , CodingDemoModule.class // only test purpose
   );
 
 