0f5888cf2700aacbc9adda8716c0d75f4f2ccbe6
MaximilianToe <mtoenies@outlook.de>
2024-10-31 08:17:03 +0100

    initial commit

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/comb_resolver/CombinedResolverModule.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/comb_resolver/CombinedResolverModule.java
@@ -1,31 +1,36 @@
 package io.github.mzmine.modules.dataprocessing.comb_resolver;
 
-import java.time.Instant;
-
+import io.github.mzmine.datamodel.MZmineProject;
+import io.github.mzmine.datamodel.features.FeatureList;
+import io.github.mzmine.modules.MZmineModuleCategory;
+import io.github.mzmine.modules.impl.TaskPerFeatureListModule;
+import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.taskcontrol.Task;
+import io.github.mzmine.util.MemoryMapStorage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import io.github.mzmine.datamodel.MZmineProject;
-import io.github.mzmine.datamodel.features.FeatureList;
-import io.github.mzmine.modules.impl.TaskPerFeatureListModule;
-import io.github.mzmine.parameters.ParameterSet;
-import io.github.mzmine.modules.MZmineModuleCategory;
-import io.github.mzmine.taskcontrol.Task;
-import io.github.mzmine.util.MemoryMapStorage;
-
 public class CombinedResolverModule extends TaskPerFeatureListModule {
-    public final String name;
-    public final String description;
 
-    public CombinedResolverModule(final @NotNull String name, final @NotNull CombinedResolverParameters.class paramterSetClass, final @NotNull MZmineModuleCategory  moduleCategory, final boolean requiresMemoryMapping, final @NotNull String description){
-        this.name = name;
-        this.description = description;
-        }
+  /**
+   * @param name                  name of the module in the menu and quick access
+   * @param parameterSetClass     the class of the parameters
+   * @param moduleCategory        module category for quick access and batch mode
+   * @param requiresMemoryMapping if true and if memory mapping is activated, task will get a memory
+   *                              map storage
+   * @param description           the description of the task
+   */
+  public CombinedResolverModule(@NotNull String name,
+      @Nullable Class<? extends ParameterSet> parameterSetClass,
+      @NotNull MZmineModuleCategory moduleCategory, boolean requiresMemoryMapping,
+      @NotNull String description) {
+    super(name, parameterSetClass, moduleCategory, requiresMemoryMapping, description);
+  }
 
-
-    public createTask(final @NotNull MZmineProject project, final @NotNull ParameterSet parameters, final @NotNull Instant moduleCallDate, @Nullable final MemoryMapStorage storage, @NotNull final FeatureList featureList){
-        return new Task();
-    }
-
-
+  @java.lang.Override
+  public @NotNull Task createTask(@NotNull MZmineProject project, @NotNull ParameterSet parameters,
+      @NotNull Instant moduleCallDate, @Nullable MemoryMapStorage storage,
+      @NotNull FeatureList featureList) {
+    return null;
+  }
 }
