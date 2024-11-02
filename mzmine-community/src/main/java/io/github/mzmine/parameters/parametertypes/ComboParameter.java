1bff713cfafb41a16e24ff63609372c25cc6503a
MaximilianToe <mtoenies@outlook.de>
2024-11-02 09:06:28 +0100

    commit before pull

--- mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/ComboParameter.java
+++ mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/ComboParameter.java
@@ -5,9 +5,7 @@
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
  * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
  * conditions:
  *
  * The above copyright notice and this permission notice shall be
@@ -37,129 +35,129 @@ import org.w3c.dom.Element;
  * Combo Parameter implementation
  */
 public class ComboParameter<ValueType> implements
-    PropertyParameter<ValueType, ComboComponent<ValueType>> {
+        PropertyParameter<ValueType, ComboComponent<ValueType>> {
 
-  private final String name;
-  private final String description;
-  private final ObservableList<ValueType> choices;
-  protected ValueType value;
+    private final String name;
+    private final String description;
+    private final ObservableList<ValueType> choices;
+    protected ValueType value;
 
-  public ComboParameter(String name, String description, ValueType[] choices) {
-    this(name, description, choices, null);
-  }
-
-  public ComboParameter(String name, String description, ValueType[] choices,
-      ValueType defaultValue) {
-    this(name, description, FXCollections.observableArrayList(choices), defaultValue);
-  }
-
-  public ComboParameter(String name, String description, List<ValueType> choices) {
-    this(name, description, choices, null);
-  }
-
-  public ComboParameter(String name, String description, List<ValueType> choices,
-      ValueType defaultValue) {
-    this.name = name;
-    this.description = description;
-    this.choices = FXCollections.observableList(choices);
-    this.value = defaultValue;
-    if (defaultValue == null && !choices.isEmpty()) {
-      this.value = choices.get(0);
+    public ComboParameter(String name, String description, ValueType[] choices) {
+        this(name, description, choices, null);
     }
-  }
 
-  @Override
-  public String getDescription() {
-    return description;
-  }
-
-  @Override
-  public ComboComponent<ValueType> createEditingComponent() {
-    ComboComponent<ValueType> comboComponent = new ComboComponent<>(choices);
-    comboComponent.getSelectionModel().selectFirst();
-    return comboComponent;
-  }
-
-  @Override
-  public ValueType getValue() {
-    return value;
-  }
-
-  @Override
-  public void setValue(ValueType value) {
-    this.value = value;
-  }
-
-  public ObservableList<ValueType> getChoices() {
-    return choices;
-  }
-
-  public void setChoices(ValueType[] newChoices) {
-    choices.clear();
-    choices.addAll(newChoices);
-  }
-
-  @Override
-  public ComboParameter<ValueType> cloneParameter() {
-    ComboParameter<ValueType> copy = new ComboParameter<ValueType>(name, description, choices);
-    copy.value = this.value;
-    return copy;
-  }
-
-  @Override
-  public void setValueFromComponent(ComboComponent<ValueType> component) {
-    value = component.getSelectionModel().getSelectedItem();
-  }
-
-  @Override
-  public void setValueToComponent(ComboComponent<ValueType> component,
-      @Nullable ValueType newValue) {
-    if (newValue == null) {
-      component.getSelectionModel().clearSelection();
-      return;
+    public ComboParameter(String name, String description, ValueType[] choices,
+            ValueType defaultValue) {
+        this(name, description, FXCollections.observableArrayList(choices), defaultValue);
     }
-    component.getSelectionModel().select(newValue);
-  }
 
-  @Override
-  public void loadValueFromXML(Element xmlElement) {
-    String elementString = xmlElement.getTextContent();
-    if (elementString.length() == 0) {
-      return;
+    public ComboParameter(String name, String description, List<ValueType> choices) {
+        this(name, description, choices, null);
     }
-    for (ValueType option : choices) {
-      if (option.toString().equals(elementString)) {
-        value = option;
-        break;
-      }
+
+    public ComboParameter(String name, String description, List<ValueType> choices,
+            ValueType defaultValue) {
+        this.name = name;
+        this.description = description;
+        this.choices = FXCollections.observableList(choices);
+        this.value = defaultValue;
+        if (defaultValue == null && !choices.isEmpty()) {
+            this.value = choices.get(0);
+        }
     }
-  }
 
-  @Override
-  public void saveValueToXML(Element xmlElement) {
-    if (value == null) {
-      return;
+    @Override
+    public String getDescription() {
+        return description;
     }
-    xmlElement.setTextContent(value.toString());
-  }
 
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public String toString() {
-    return name;
-  }
-
-  @Override
-  public boolean checkValue(Collection<String> errorMessages) {
-    if (value == null) {
-      errorMessages.add(name + " is not set properly");
-      return false;
+    @Override
+    public ComboComponent<ValueType> createEditingComponent() {
+        ComboComponent<ValueType> comboComponent = new ComboComponent<>(choices);
+        comboComponent.getSelectionModel().selectFirst();
+        return comboComponent;
     }
-    return true;
-  }
+
+    @Override
+    public ValueType getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(ValueType value) {
+        this.value = value;
+    }
+
+    public ObservableList<ValueType> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ValueType[] newChoices) {
+        choices.clear();
+        choices.addAll(newChoices);
+    }
+
+    @Override
+    public ComboParameter<ValueType> cloneParameter() {
+        ComboParameter<ValueType> copy = new ComboParameter<ValueType>(name, description, choices);
+        copy.value = this.value;
+        return copy;
+    }
+
+    @Override
+    public void setValueFromComponent(ComboComponent<ValueType> component) {
+        value = component.getSelectionModel().getSelectedItem();
+    }
+
+    @Override
+    public void setValueToComponent(ComboComponent<ValueType> component,
+            @Nullable ValueType newValue) {
+        if (newValue == null) {
+            component.getSelectionModel().clearSelection();
+            return;
+        }
+        component.getSelectionModel().select(newValue);
+    }
+
+    @Override
+    public void loadValueFromXML(Element xmlElement) {
+        String elementString = xmlElement.getTextContent();
+        if (elementString.length() == 0) {
+            return;
+        }
+        for (ValueType option : choices) {
+            if (option.toString().equals(elementString)) {
+                value = option;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void saveValueToXML(Element xmlElement) {
+        if (value == null) {
+            return;
+        }
+        xmlElement.setTextContent(value.toString());
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean checkValue(Collection<String> errorMessages) {
+        if (value == null) {
+            errorMessages.add(name + " is not set properly");
+            return false;
+        }
+        return true;
+    }
 
 }
