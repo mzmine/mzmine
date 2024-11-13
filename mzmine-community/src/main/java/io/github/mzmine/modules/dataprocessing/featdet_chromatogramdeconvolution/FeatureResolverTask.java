53725d385d3fd01056c6e7d26f213448754ef061
Steffen Heuckeroth <steffen.heuckeroth@gmx.de>
2024-11-13 14:34:29 +0100

    fix type copying overriding ion series depending types

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/FeatureResolverTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/FeatureResolverTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -12,7 +12,6 @@
  *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -31,6 +30,7 @@ import io.github.mzmine.datamodel.RawDataFile;
 import io.github.mzmine.datamodel.Scan;
 import io.github.mzmine.datamodel.data_access.EfficientDataAccess;
 import io.github.mzmine.datamodel.data_access.FeatureDataAccess;
+import io.github.mzmine.datamodel.featuredata.FeatureDataUtils;
 import io.github.mzmine.datamodel.featuredata.IonMobilogramTimeSeries;
 import io.github.mzmine.datamodel.featuredata.IonTimeSeries;
 import io.github.mzmine.datamodel.features.Feature;
@@ -228,19 +228,12 @@ public class FeatureResolverTask extends AbstractTask {
         final ModularFeatureListRow newRow = new ModularFeatureListRow(resolvedFeatureList,
             peakId++);
         final ModularFeature f = new ModularFeature(resolvedFeatureList,
-            originalFeature.getRawDataFile(), resolved, originalFeature.getFeatureStatus());
-
+            originalFeature.getRawDataFile(), originalFeature.getFeatureStatus());
         DataTypeUtils.copyAllBut(originalFeature, f, excludedTypes);
 
-        /*if (originalFeature.getMobilityUnit() != null) {
-          f.set(MobilityUnitType.class, originalFeature.getMobilityUnit());
-        }
-        if (originalFeature.get(ImageType.class) != null) {
-          f.set(ImageType.class, true);
-        }
-        if (originalFeature.get(MaldiSpotType.class) != null) {
-          f.set(MaldiSpotType.class, originalFeature.get(MaldiSpotType.class));
-        }*/
+        f.set(FeatureDataType.class, resolved);
+        FeatureDataUtils.recalculateIonSeriesDependingTypes(f);
+
         newRow.addFeature(originalFeature.getRawDataFile(), f);
         resolvedFeatureList.addRow(newRow);
         if (resolved.getSpectra().size() <= 3) {
