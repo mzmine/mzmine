e06740623b93660ce6a85b04b409f57f4e2836e5
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-11-27 18:07:29 +0100

    fix exception if mzml chromatograms with 0 data points were imported

--- mzmine-community/src/main/java/io/github/mzmine/modules/io/import_rawdata_mzml/ConversionUtils.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/io/import_rawdata_mzml/ConversionUtils.java
@@ -401,6 +401,14 @@ public class ConversionUtils {
         otherFile.setDescription(unit + "_" + chromType.getDescription());
 
         for (MzMLChromatogram chrom : unitChromEntry.getValue()) {
+          if (chrom.getNumberOfDataPoints() == 0) {
+            // drop empty chromatograms
+            logger.finest(
+                () -> "%s: Zero data points detected for chromatogram %s. Dropping".formatted(
+                    file.getName(), chrom.getId()));
+            continue;
+          }
+
           final SimpleOtherTimeSeries timeSeries = new SimpleOtherTimeSeries(
               file.getMemoryMapStorage(), chrom.getRetentionTimes(), chrom.getIntensities(),
               chrom.getId(), timeSeriesData);
