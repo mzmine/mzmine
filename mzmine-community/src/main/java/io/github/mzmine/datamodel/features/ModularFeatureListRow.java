942c988cda0accf775ef418a02ffca541f765091
SteffenHeu <steffen.heuckeroth@gmx.de>
2024-11-29 12:06:48 +0100

    make ion annotation not count as annotated

--- mzmine-community/src/main/java/io/github/mzmine/datamodel/features/ModularFeatureListRow.java
+++ mzmine-community/src/main/java/io/github/mzmine/datamodel/features/ModularFeatureListRow.java
@@ -245,7 +245,7 @@ public class ModularFeatureListRow implements FeatureListRow {
     }
     if (!flist.equals(feature.getFeatureList())) {
       throw new IllegalArgumentException("Cannot add feature with different feature list to this "
-                                         + "row. Create feature with the correct feature list as an argument.");
+          + "row. Create feature with the correct feature list as an argument.");
     }
     if (raw == null) {
       throw new IllegalArgumentException("Raw file cannot be null");
@@ -535,7 +535,8 @@ public class ModularFeatureListRow implements FeatureListRow {
   public boolean isIdentified() {
     for (Entry<DataType, Object> entry : getMap().entrySet()) {
       final DataType dt = entry.getKey();
-      if (dt instanceof ListWithSubsType<?> listType && dt instanceof AnnotationType) {
+      if (dt instanceof ListWithSubsType<?> listType && dt instanceof AnnotationType
+          && !(dt instanceof IonIdentityListType)) {
         final List<?> list = get(listType);
         if (list != null && !list.isEmpty()) {
           return true;
@@ -680,7 +681,7 @@ public class ModularFeatureListRow implements FeatureListRow {
   @Override
   public IsotopePattern getBestIsotopePattern() {
     return streamFeatures().filter(f -> f != null && f.getIsotopePattern() != null
-                                        && f.getFeatureStatus() != FeatureStatus.UNKNOWN)
+            && f.getFeatureStatus() != FeatureStatus.UNKNOWN)
         .max(Comparator.comparingDouble(ModularFeature::getHeight))
         .map(ModularFeature::getIsotopePattern).orElse(null);
   }
