08096197a80bbf90402cf7dd69cb6464a4f5a344
Robin Schmid <rschmid1789@gmail.com>
2024-12-20 16:57:09 +0100

    merging options in spectral networking

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/group_spectral_networking/modified_cosine/ModifiedCosineSpectralNetworkingParameters.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/group_spectral_networking/modified_cosine/ModifiedCosineSpectralNetworkingParameters.java
@@ -27,12 +27,13 @@ package io.github.mzmine.modules.dataprocessing.group_spectral_networking.modifi
 
 
 import io.github.mzmine.main.MZmineCore;
+import io.github.mzmine.modules.dataprocessing.filter_scan_merge_select.SpectraMergeSelectParameter;
+import io.github.mzmine.modules.dataprocessing.filter_scan_merge_select.options.SpectraMergeSelectPresets;
 import io.github.mzmine.modules.dataprocessing.group_spectral_networking.SignalFiltersParameters;
 import io.github.mzmine.modules.dataprocessing.group_spectral_networking.SpectralSignalFilter;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.parameters.impl.IonMobilitySupport;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
-import io.github.mzmine.parameters.parametertypes.BooleanParameter;
 import io.github.mzmine.parameters.parametertypes.DoubleParameter;
 import io.github.mzmine.parameters.parametertypes.IntegerParameter;
 import io.github.mzmine.parameters.parametertypes.OptionalParameter;
@@ -40,6 +41,7 @@ import io.github.mzmine.parameters.parametertypes.submodules.ParameterSetParamet
 import io.github.mzmine.parameters.parametertypes.tolerances.MZTolerance;
 import io.github.mzmine.parameters.parametertypes.tolerances.MZToleranceParameter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * MS/MS similarity check based on difference and signal comparison
@@ -47,6 +49,8 @@ import org.jetbrains.annotations.NotNull;
  * @author Robin Schmid (robinschmid@uni-muenster.de)
  */
 public class ModifiedCosineSpectralNetworkingParameters extends SimpleParameterSet {
+
+  public static final SpectraMergeSelectParameter spectraMergeSelect = SpectraMergeSelectParameter.createMolecularNetworkingDefault();
 
   // MZ-tolerance: deisotoping, adducts
   public static final MZToleranceParameter MZ_TOLERANCE = new MZToleranceParameter("m/z tolerance",
@@ -56,9 +60,6 @@ public class ModifiedCosineSpectralNetworkingParameters extends SimpleParameterS
   public static final DoubleParameter MIN_COSINE_SIMILARITY = new DoubleParameter(
       "Min cosine similarity", "Minimum spectral cosine similarity (scaled 0-1). Default is 0.7",
       MZmineCore.getConfiguration().getScoreFormat(), 0.7, 0d, 1d);
-
-  public static final BooleanParameter ONLY_BEST_MS2_SCAN = new BooleanParameter(
-      "Only best MS2 scan", "Compares only the best MS2 scan (or all MS2 scans)", true);
 
   public static final IntegerParameter MIN_MATCH = new IntegerParameter("Minimum matched signals",
       "Minimum matched signals or neutral losses (m/z differences). Default is 4 for small molecules but the higher the more confident.",
@@ -76,16 +77,17 @@ public class ModifiedCosineSpectralNetworkingParameters extends SimpleParameterS
   public ModifiedCosineSpectralNetworkingParameters() {
     super(
         "https://mzmine.github.io/mzmine_documentation/module_docs/group_spectral_net/molecular_networking.html",
-        MZ_TOLERANCE, ONLY_BEST_MS2_SCAN, MAX_MZ_DELTA, MIN_MATCH, MIN_COSINE_SIMILARITY,
+        MZ_TOLERANCE, spectraMergeSelect, MAX_MZ_DELTA, MIN_MATCH, MIN_COSINE_SIMILARITY,
         signalFilters);
   }
 
 
-  public static void setAll(final ParameterSet param, final boolean onlyBestMs2,
+  public static void setAll(final ParameterSet param, final MZTolerance mergingTolerance,
       final boolean removePrecursor, final double maxMzDelta, final int minMatched,
       final double minCosine, final MZTolerance mzTol, final SpectralSignalFilter filter) {
     param.setParameter(MAX_MZ_DELTA, removePrecursor, maxMzDelta);
-    param.setParameter(ONLY_BEST_MS2_SCAN, onlyBestMs2);
+    param.getParameter(spectraMergeSelect)
+        .setSimplePreset(SpectraMergeSelectPresets.SINGLE_MERGED_SCAN, mergingTolerance);
     param.setParameter(MIN_MATCH, minMatched);
     param.setParameter(MIN_COSINE_SIMILARITY, minCosine);
     param.setParameter(MZ_TOLERANCE, mzTol);
@@ -98,7 +100,17 @@ public class ModifiedCosineSpectralNetworkingParameters extends SimpleParameterS
   }
 
   @Override
+  public @Nullable String getVersionMessage(final int version) {
+    return switch (version) {
+      case 3 -> """
+          From mzmine version > 4.4.3 the scan selection and merging has been harmonized across modules.
+          Please check and configure the %s parameter.""".formatted(spectraMergeSelect.getName());
+      default -> null;
+    };
+  }
+
+  @Override
   public int getVersion() {
-    return 2;
+    return 3;
   }
 }
