7f786a09deeb9703852e9bd1bd7299a7fc31c14f
Robin Schmid <rschmid1789@gmail.com>
2025-01-03 16:00:33 +0100

    hide mzml export

--- mzmine-community/src/main/java/io/github/mzmine/modules/io/export_scans/ScanFormats.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/io/export_scans/ScanFormats.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -26,5 +26,5 @@
 package io.github.mzmine.modules.io.export_scans;
 
 public enum ScanFormats {
-  mgf, txt, mzML, msp;
+  mgf, txt, msp;
 }
