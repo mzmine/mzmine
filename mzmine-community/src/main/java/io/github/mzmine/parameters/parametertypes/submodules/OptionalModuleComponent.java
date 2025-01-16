edc87a85ebaf27d7399a85ac6a02ee2f04f00429
Robin Schmid <rschmid1789@gmail.com>
2025-01-16 12:50:16 +0100

    put embedded params in center for better layout

--- mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/submodules/OptionalModuleComponent.java
+++ mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/submodules/OptionalModuleComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2024 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -81,7 +81,7 @@ public class OptionalModuleComponent extends BorderPane implements EstimatedComp
         boolean toggledHidden = !hidden.get();
         // change text
         setButton.setText(toggledHidden ? "Show" : "Hide");
-        setBottom(toggledHidden ? null : paramPane);
+        setCenter(toggledHidden ? null : paramPane);
         // events
         hidden.set(toggledHidden);
 
