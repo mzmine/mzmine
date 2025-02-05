a0ef5c813a2f1368d48351696175a16d866a6f8d
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-02-05 15:22:50 +0100

    set up specific mzmine pro version

--- utils/src/main/java/io/github/mzmine/util/io/SemverVersionReader.java
+++ utils/src/main/java/io/github/mzmine/util/io/SemverVersionReader.java
@@ -52,4 +52,23 @@ public class SemverVersionReader {
       return new Semver("4-SNAPSHOT", SemverType.LOOSE);
     }
   }
+
+  @NotNull
+  public static Semver getMZmineProVersion(ClassLoader classLoader) {
+    try {
+      InputStream inStream = classLoader.getResourceAsStream("mzmineproversion.properties");
+      if (inStream == null) {
+        return new Semver("4-SNAPSHOT", SemverType.LOOSE);
+      }
+      Properties properties = new Properties();
+      properties.load(inStream);
+      String versionString = properties.getProperty("version.semver");
+      if ((versionString == null) || (versionString.startsWith("$"))) {
+        return new Semver("4-SNAPSHOT", SemverType.LOOSE);
+      }
+      return new Semver(versionString, SemverType.LOOSE);
+    } catch (Exception e) {
+      return new Semver("4-SNAPSHOT", SemverType.LOOSE);
+    }
+  }
 }
