be765ae72f7fa41b1b317f3548eaee97753350bc
William Chu <wchu@BCCRC.CA>
2025-02-13 10:12:43 -0800

    Undid accidental comment

--- mzmine-community/src/main/java/io/github/mzmine/modules/io/import_rawdata_mzml/msdk/data/BuildingMzMLMsScan.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/io/import_rawdata_mzml/msdk/data/BuildingMzMLMsScan.java
@@ -518,7 +518,6 @@ public class BuildingMzMLMsScan extends MetadataOnlyScan {
     }
 
     return retentionTime == null ? -1 : retentionTime;
-    // float is default for RI but handle Double in case wrong value was present
   }
 
   @Override
