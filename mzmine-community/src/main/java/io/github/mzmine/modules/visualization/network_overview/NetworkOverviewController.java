6853d6023e1dc52335ddb929cc0b097ba780f33c
robinschmid <rschmid1789@gmail.com>
2025-03-19 13:49:08 +0100

    remove lipid annotation tab from network - in annotation tab

--- mzmine-community/src/main/java/io/github/mzmine/modules/visualization/network_overview/NetworkOverviewController.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/network_overview/NetworkOverviewController.java
@@ -32,7 +32,6 @@ import io.github.mzmine.modules.visualization.compdb.CompoundDatabaseMatchTab;
 import io.github.mzmine.modules.visualization.featurelisttable_modular.FeatureTableFX;
 import io.github.mzmine.modules.visualization.featurelisttable_modular.FeatureTableTab;
 import io.github.mzmine.modules.visualization.networking.visual.FeatureNetworkController;
-import io.github.mzmine.modules.visualization.spectra.matchedlipid.LipidAnnotationMatchTabOld;
 import io.github.mzmine.modules.visualization.spectra.simplespectra.mirrorspectra.MirrorScanWindowController;
 import io.github.mzmine.modules.visualization.spectra.simplespectra.mirrorspectra.MirrorScanWindowFXML;
 import io.github.mzmine.modules.visualization.spectra.spectra_stack.SpectraStackVisualizerPane;
@@ -67,7 +66,6 @@ public class NetworkOverviewController {
   public ToggleSwitch cbBindToExternalTable;
   public BorderPane pnNetwork;
   public Tab tabAnnotations;
-  public Tab tabLipidAnnotations;
   public Tab tabSimilarity;
   public Tab tabAllMs2;
   public Tab tabNodes;
@@ -114,8 +112,6 @@ public class NetworkOverviewController {
     // create annotations tab
     spectralMatchesController = new SpectraIdentificationResultsWindowFX(internalTable);
     CompoundDatabaseMatchTab compoundMatchController = new CompoundDatabaseMatchTab(internalTable);
-    LipidAnnotationMatchTabOld lipidAnnotationMatchTabOld = new LipidAnnotationMatchTabOld(
-        internalTable);
 
     // create mirror scan tab
     var mirrorScanTab = new MirrorScanWindowFXML();
@@ -126,21 +122,18 @@ public class NetworkOverviewController {
 
     tabSimilarity.setContent(mirrorScanController.getMainPane());
     tabAnnotations.setContent(gridAnnotations);
-    tabLipidAnnotations.setContent(lipidAnnotationMatchTabOld.getContent());
     tabAllMs2.setContent(allMs2Pane);
 
     // all content that listens to selected feature changes
-    featureRowInterfaces = List.of(spectralMatchesController, compoundMatchController,
-        lipidAnnotationMatchTabOld, allMs2Pane, mirrorScanController);
+    featureRowInterfaces = List.of(spectralMatchesController, compoundMatchController, allMs2Pane,
+        mirrorScanController);
     // only annotation interfaces to control visibility
-    annotationInterfaces = List.of(spectralMatchesController, compoundMatchController,
-        lipidAnnotationMatchTabOld);
+    annotationInterfaces = List.of(spectralMatchesController, compoundMatchController);
     layoutAnnotations();
 
     // add callbacks
     weak.addListChangeListener(networkController,
-        networkController.getNetworkPane().getSelectedNodes(),
-        c -> handleSelectedNodesChanged(c));
+        networkController.getNetworkPane().getSelectedNodes(), c -> handleSelectedNodesChanged(c));
 
     // set focussed rows last
     if (focussedRows != null) {
@@ -190,14 +183,14 @@ public class NetworkOverviewController {
     var tabController = tempTab.getController();
     weak.addListChangeListener(networkController,
         networkController.getNetworkPane().getVisibleRows(), c -> {
-      if (weak.isDisposed()) {
-        return;
-      }
-      ObservableList<? extends FeatureListRow> visible = c.getList();
-      tabController.getIdSearchField().setText(
-          visible.stream().map(FeatureListRow::getID).map(Object::toString)
-              .collect(Collectors.joining(",")));
-    });
+          if (weak.isDisposed()) {
+            return;
+          }
+          ObservableList<? extends FeatureListRow> visible = c.getList();
+          tabController.getIdSearchField().setText(
+              visible.stream().map(FeatureListRow::getID).map(Object::toString)
+                  .collect(Collectors.joining(",")));
+        });
   }
 
 
