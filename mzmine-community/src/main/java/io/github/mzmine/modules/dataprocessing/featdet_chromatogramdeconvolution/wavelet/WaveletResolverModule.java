333ccf03eff2e9f9df721c69cc027e6f48606d14
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-03-28 14:46:44 +0100

    start wavelet resolver

---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/wavelet/WaveletResolverModule.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2004-2022 The MZmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.wavelet;
+
+import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.FeatureResolverModule;
+import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.minimumsearch.MinimumSearchFeatureResolverParameters;
+import io.github.mzmine.parameters.ParameterSet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class WaveletResolverModule extends FeatureResolverModule {
+
+  public static final String NAME = "Wavelet feature resolver";
+
+  @NotNull
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Nullable
+  @Override
+  public Class<? extends ParameterSet> getParameterSetClass() {
+    return MinimumSearchFeatureResolverParameters.class;
+  }
+
+  @NotNull
+  @Override
+  public String getDescription() {
+    return "Resolves EICs to features by wavelet transform. Supports retention time and mobility resolving.";
+  }
+
+}
---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/wavelet/WaveletResolverParameters.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2004-2025 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.wavelet;
+
+import io.github.mzmine.datamodel.features.ModularFeatureList;
+import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.GeneralResolverParameters;
+import io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.Resolver;
+import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.parameters.parametertypes.DoubleParameter;
+import io.github.mzmine.parameters.parametertypes.IntegerParameter;
+import io.github.mzmine.parameters.parametertypes.PercentParameter;
+import java.text.DecimalFormat;
+import org.jetbrains.annotations.Nullable;
+
+public class WaveletResolverParameters extends GeneralResolverParameters {
+
+  public static final DoubleParameter snr = new DoubleParameter("Signal to noise threshold", "",
+      new DecimalFormat("#.#"), 3d, 0d, Double.MAX_VALUE);
+
+  public static final DoubleParameter minHeight = new DoubleParameter("Minimum height", "",
+      new DecimalFormat("#.#"), 1E3, 0d, Double.MAX_VALUE);
+
+  public static final PercentParameter boundaryThreshold = new PercentParameter(
+      "Boundary detection", "", 0.05);
+
+  public static final IntegerParameter maxWidthPoints = new IntegerParameter("Max data points", "",
+      30, 0, Integer.MAX_VALUE);
+
+  public WaveletResolverParameters() {
+    super(GeneralResolverParameters.PEAK_LISTS, GeneralResolverParameters.dimension,
+        GeneralResolverParameters.groupMS2Parameters, snr, boundaryThreshold, maxWidthPoints,
+
+        GeneralResolverParameters.MIN_NUMBER_OF_DATAPOINTS, GeneralResolverParameters.SUFFIX,
+        GeneralResolverParameters.handleOriginal);
+  }
+
+  @Override
+  public @Nullable Resolver getResolver(ParameterSet parameterSet, ModularFeatureList flist) {
+    return null;
+  }
+}
