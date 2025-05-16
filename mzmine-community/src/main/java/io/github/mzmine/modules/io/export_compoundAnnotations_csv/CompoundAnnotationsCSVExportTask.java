b16b7ca40ddf584948edec4094ed5aa43857c8b4
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-05-16 11:44:15 +0200

    add database entry id to CompoundAnnotationsCSVExportTask

--- mzmine-community/src/main/java/io/github/mzmine/modules/io/export_compoundAnnotations_csv/CompoundAnnotationsCSVExportTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/io/export_compoundAnnotations_csv/CompoundAnnotationsCSVExportTask.java
@@ -32,6 +32,7 @@ import io.github.mzmine.datamodel.features.types.DataType;
 import io.github.mzmine.datamodel.features.types.DataTypes;
 import io.github.mzmine.datamodel.features.types.MethodType;
 import io.github.mzmine.datamodel.features.types.annotations.CompoundNameType;
+import io.github.mzmine.datamodel.features.types.annotations.DatabaseEntryIdType;
 import io.github.mzmine.datamodel.features.types.annotations.InChIKeyStructureType;
 import io.github.mzmine.datamodel.features.types.annotations.InChIStructureType;
 import io.github.mzmine.datamodel.features.types.annotations.SmilesStructureType;
@@ -172,7 +173,7 @@ public class CompoundAnnotationsCSVExportTask extends AbstractTask {
       var columns = Stream.of(IDType.class, CompoundNameType.class, IonTypeType.class,
               ScoreType.class, PrecursorMZType.class, MobilityType.class, CCSType.class, RTType.class,
               FormulaType.class, SmilesStructureType.class, InChIStructureType.class,
-              InChIKeyStructureType.class, MethodType.class, UsiType.class)
+              InChIKeyStructureType.class, MethodType.class, UsiType.class, DatabaseEntryIdType.class)
           .map(c -> DataTypes.get((Class) c)).toList();
 
       // Create a header string by joining the unique IDs of the DataTypes with commas
@@ -208,12 +209,14 @@ public class CompoundAnnotationsCSVExportTask extends AbstractTask {
             String inchikey = annotation.getInChIKey();
             String formula = annotation.getFormula();
             String usi = null;
+            String entryId = null;
             if (annotation instanceof SpectralDBAnnotation spec) {
               usi = spec.getEntry().getAsString(DBEntryField.USI).orElse("");
+              entryId = spec.getEntry().getAsString(DBEntryField.ENTRY_ID).orElse("");
             }
 
             String result = Stream.of(rowId, compoundName, adductType, scoreType, precursorMZ,
-                    mobility, getCCS, getRT, formula, smiles, inchi, inchikey, method, usi)
+                    mobility, getCCS, getRT, formula, smiles, inchi, inchikey, method, usi, entryId)
                 .map(o -> (o == null) ? "" : CSVUtils.escape(o.toString(), fieldSeparator))
                 .collect(Collectors.joining(","));
 
