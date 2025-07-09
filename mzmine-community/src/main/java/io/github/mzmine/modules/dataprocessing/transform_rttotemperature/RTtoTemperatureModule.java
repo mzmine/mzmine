128149d8379491803c71f996aa970cafb95d7df7
DomiDotso <dotson14@purdue.edu>
2025-07-09 12:44:59 -0400

    Added support for converting retention time values to temperature values to assist in temperature programmed desorption experiments.

---
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/transform_rttotemperature/RTtoTemperatureModule.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2004-2024 The MZmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules.dataprocessing.transform_rttotemperature;
+
+import io.github.mzmine.datamodel.MZmineProject;
+import io.github.mzmine.datamodel.RawDataFile;
+import io.github.mzmine.modules.MZmineModuleCategory;
+import io.github.mzmine.modules.MZmineProcessingModule;
+import io.github.mzmine.parameters.ParameterSet;
+import io.github.mzmine.taskcontrol.Task;
+import io.github.mzmine.util.ExitCode;
+import io.github.mzmine.util.MemoryMapStorage;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Instant;
+import java.util.Collection;
+
+public class RTtoTemperatureModule implements MZmineProcessingModule {
+
+    private static final String MODULE_NAME = "Retention Time → Temperature";
+    private static final String MODULE_DESCRIPTION =
+            "Converts scan retention times into oven temperatures based on the user-defined program.";
+
+    @Override
+    public @NotNull String getName() {
+        return MODULE_NAME;
+    }
+
+    @Override
+    public @NotNull String getDescription() {
+        return MODULE_DESCRIPTION;
+    }
+
+    @Override
+    public @NotNull ExitCode runModule(@NotNull MZmineProject project,
+                                       @NotNull ParameterSet parameters,
+                                       @NotNull Collection<Task> tasks,
+                                       @NotNull Instant moduleCallDate) {
+        // Use one shared storage for all tasks in this module invocation
+        MemoryMapStorage storage = MemoryMapStorage.forRawDataFile();
+
+        // Retrieve the selected raw data files via the static parameter key
+        RawDataFile[] files = parameters
+                .getValue(RTtoTemperatureParameters.dataFiles)
+                .getMatchingRawDataFiles();
+
+        for (RawDataFile file : files) {
+            Task task = new RTtoTemperatureTask(
+                    project,
+                    file,
+                    parameters,
+                    storage,
+                    moduleCallDate);
+            tasks.add(task);
+        }
+        return ExitCode.OK;
+    }
+
+    @Override
+    public @NotNull MZmineModuleCategory getModuleCategory() {
+        return MZmineModuleCategory.RAWDATAFILTERING;
+    }
+
+    @Override
+    public @NotNull Class<? extends ParameterSet> getParameterSetClass() {
+        return RTtoTemperatureParameters.class;
+    }
+}
