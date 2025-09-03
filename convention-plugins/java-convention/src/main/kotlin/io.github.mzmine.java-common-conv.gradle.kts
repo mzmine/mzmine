9bd1c0cc5e02211c62cd55638df61f4904873378
SteffenHeu <steffen.heuckeroth@gmx.de>
2025-09-03 14:42:07 +0200

    include batik xml dependencies

--- convention-plugins/java-convention/src/main/kotlin/io.github.mzmine.java-common-conv.gradle.kts
+++ convention-plugins/java-convention/src/main/kotlin/io.github.mzmine.java-common-conv.gradle.kts
@@ -48,10 +48,12 @@ configurations.all {
         }
 
         if (requested.group == "org.apache.xmlgraphics") {
-            val goodVersion = libs.findVersion("batik").get().requiredVersion
+            if (requested.name != "xmlgraphics-commons") { // different dependency version than batik itself
+                val goodVersion = libs.findVersion("batik").get().requiredVersion
 //            System.out.println("Replacing batik version " + requested.version + " with " + goodVersion)
-            useVersion(goodVersion) // strict version
-            because("Replacing batik version " + requested.version + " with " + goodVersion)
+                useVersion(goodVersion) // strict version
+                because("Replacing batik version " + requested.version + " with " + goodVersion)
+            }
         }
     }
 }
