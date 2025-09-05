59337c39dd04759ec9e62f0375396472a831a7ad
robinschmid <rschmid1789@gmail.com>
2025-09-05 15:45:57 +0200

    fix npe when using StringConverter and unmodifiable list in ComboBox

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_smoothing/SmoothingSetupDialog.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_smoothing/SmoothingSetupDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The MZmine Development Team
+ * Copyright (c) 2004-2025 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -94,7 +94,8 @@ public class SmoothingSetupDialog extends ParameterSetupDialogWithPreview {
             fBox.setItems(FXCollections.observableArrayList(
                 newValue.getFeatures(newValue.getRawDataFile(0))));
           } else {
-            fBox.setItems(FXCollections.emptyObservableList());
+            // needs to be modifiable when using stringconverter
+            fBox.setItems(FXCollections.observableArrayList());
           }
         }));
 
