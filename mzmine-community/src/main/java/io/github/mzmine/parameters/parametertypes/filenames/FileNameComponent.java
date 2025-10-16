2ffd4a539d5bc5ddce5b23d6c97e0bad8255b5e4
robinschmid <rschmid1789@gmail.com>
2025-10-16 12:57:48 +0200

    Filename parameter avoid empty file

--- mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/filenames/FileNameComponent.java
+++ mzmine-community/src/main/java/io/github/mzmine/parameters/parametertypes/filenames/FileNameComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2024 The mzmine Development Team
+ * Copyright (c) 2004-2025 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -173,8 +173,7 @@ public class FileNameComponent extends HBox implements LastFilesComponent {
 
   public File getValue() {
     String fileName = txtFilename.getText();
-    File file = new File(fileName);
-    return file;
+    return fileName.isBlank() ? null : new File(fileName);
   }
 
   public void setValue(File value) {
