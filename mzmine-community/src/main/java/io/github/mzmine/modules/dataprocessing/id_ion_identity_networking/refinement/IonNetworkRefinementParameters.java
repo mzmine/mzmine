ed4cf804d19f7819bbeed976f9de5bc1270ac2a4
robinschmid <rschmid1789@gmail.com>
2025-12-17 19:27:58 +0100

    redo ion refinement

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_ion_identity_networking/refinement/IonNetworkRefinementParameters.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_ion_identity_networking/refinement/IonNetworkRefinementParameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2025 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -26,18 +26,21 @@
 package io.github.mzmine.modules.dataprocessing.id_ion_identity_networking.refinement;
 
 
+import io.github.mzmine.datamodel.identities.IonLibraries;
 import io.github.mzmine.parameters.Parameter;
 import io.github.mzmine.parameters.impl.IonMobilitySupport;
 import io.github.mzmine.parameters.impl.SimpleParameterSet;
 import io.github.mzmine.parameters.parametertypes.BooleanParameter;
 import io.github.mzmine.parameters.parametertypes.IntegerParameter;
 import io.github.mzmine.parameters.parametertypes.OptionalParameter;
+import io.github.mzmine.parameters.parametertypes.ionidentity.IonLibraryParameter;
 import io.github.mzmine.parameters.parametertypes.selectors.FeatureListsParameter;
+import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Refinement to MS annotation
- * 
+ *
  * @author Robin Schmid (robinschmid@uni-muenster.de)
  *
  */
@@ -48,27 +51,36 @@ public class IonNetworkRefinementParameters extends SimpleParameterSet {
   public static final FeatureListsParameter PEAK_LISTS = new FeatureListsParameter();
 
 
-  public static final OptionalParameter<IntegerParameter> MIN_NETWORK_SIZE =
-      new OptionalParameter<>(new IntegerParameter("Minimum size", "Minimum network size", 2));
+  public static final OptionalParameter<IntegerParameter> MIN_NETWORK_SIZE = new OptionalParameter<>(
+      new IntegerParameter("Minimum size", "Minimum network size", 2), true);
   public static final OptionalParameter<IntegerParameter> TRUE_THRESHOLD = // todo - better description
       new OptionalParameter<>(new IntegerParameter("Delete smaller networks: Link threshold",
-          "links>=true threshold, then delete all other occurance in annotation networks", 4));
+          "links>=true threshold, then delete all other occurance in annotation networks", 4),
+          true);
 
-  public static final BooleanParameter DELETE_SMALL_NO_MAJOR = new BooleanParameter(
-      "Delete small networks without major ion",
-      "Delete small networks without H+, Na+, NH4+, H-, Cl-, formic acid- adduct. \nMaximum allowed in-source modifications: H+(3, e.g., -H2O), Na(0), NH4(1), H-(2), Cl(0), FA(0).",
-      false);
-  public static final BooleanParameter DELETE_ROWS_WITHOUT_ID =
-      new BooleanParameter("Delete rows witout ion id", "Keeps only rows with ion ids", false);
-  public static final BooleanParameter DELETE_WITHOUT_MONOMER =
-      new BooleanParameter("Delete networks without monomer",
-          "Deletes all networks without monomer or with 1 monomer and >=3 multimers", true);
+  public static final BooleanParameter DELETE_ROWS_WITHOUT_ID = new BooleanParameter(
+      "Only keep rows with ion ID", "Keeps only rows with ion ids", false);
+  public static final BooleanParameter DELETE_WITHOUT_MONOMER = new BooleanParameter(
+      "Delete networks without monomer",
+      "Deletes all networks without monomer or with 1 monomer and >=3 multimers", true);
   // public static final BooleanParameter DELETE_XMERS_ON_MSMS = new BooleanParameter(
   // "Use MS/MS xmer verification to exclude",
   // "If an xmer was identified by MS/MS annotation of fragment xmers, then use this identification
   // and delete rest",
   // true);
 
+  public static final OptionalParameter<IonLibraryParameter> mainIonLibrary = new OptionalParameter<>(
+      new IonLibraryParameter("Require main ions library", """
+          The main ions library describes ions that are well expected in the analysis.
+          Each final ion identity network requires at least 1 main ion.
+          Uses internal default main ions if this parameter is unchecked.""",
+          IonLibraries.MZMINE_DEFAULT_DUAL_POLARITY_MAIN), false);
+
+
+  private final BooleanParameter DELETE_SMALL_NO_MAJOR_LEGACY = new BooleanParameter(
+      "Delete small networks without major ion",
+      "Delete small networks without H+, Na+, NH4+, H-, Cl-, formic acid- adduct. \nMaximum allowed in-source modifications: H+(3, e.g., -H2O), Na(0), NH4(1), H-(2), Cl(0), FA(0).",
+      false);
 
   // Constructor
   public IonNetworkRefinementParameters() {
@@ -77,14 +89,30 @@ public class IonNetworkRefinementParameters extends SimpleParameterSet {
 
   public IonNetworkRefinementParameters(boolean isSub) {
     super(isSub ? // no peak list and rt tolerance
-        new Parameter[] {MIN_NETWORK_SIZE, DELETE_SMALL_NO_MAJOR, TRUE_THRESHOLD,
-            DELETE_WITHOUT_MONOMER, DELETE_ROWS_WITHOUT_ID}
-        : new Parameter[] {PEAK_LISTS, MIN_NETWORK_SIZE, DELETE_SMALL_NO_MAJOR, TRUE_THRESHOLD,
+        new Parameter[]{MIN_NETWORK_SIZE, mainIonLibrary, TRUE_THRESHOLD, DELETE_WITHOUT_MONOMER,
+            DELETE_ROWS_WITHOUT_ID}
+        : new Parameter[]{PEAK_LISTS, MIN_NETWORK_SIZE, mainIonLibrary, TRUE_THRESHOLD,
             DELETE_WITHOUT_MONOMER, DELETE_ROWS_WITHOUT_ID});
   }
 
   @Override
   public @NotNull IonMobilitySupport getIonMobilitySupport() {
     return IonMobilitySupport.SUPPORTED;
+  }
+
+  @Override
+  public Map<String, Parameter<?>> getNameParameterMap() {
+    final Map<String, Parameter<?>> map = super.getNameParameterMap();
+    map.put(DELETE_SMALL_NO_MAJOR_LEGACY.getName(), DELETE_SMALL_NO_MAJOR_LEGACY);
+    map.put("Delete rows witout ion id", getParameter(DELETE_ROWS_WITHOUT_ID));
+    return map;
+  }
+
+  @Override
+  public void handleLoadedParameters(Map<String, Parameter<?>> loadedParams, int loadedVersion) {
+    if (loadedParams.containsKey(DELETE_SMALL_NO_MAJOR_LEGACY.getName())) {
+      final Boolean selected = DELETE_SMALL_NO_MAJOR_LEGACY.getValue();
+      setParameter(mainIonLibrary, selected, IonLibraries.MZMINE_DEFAULT_DUAL_POLARITY_MAIN);
+    }
   }
 }
--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_ion_identity_networking/refinement/IonNetworkRefinementTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/id_ion_identity_networking/refinement/IonNetworkRefinementTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2022 The MZmine Development Team
+ * Copyright (c) 2004-2025 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -27,46 +27,49 @@ package io.github.mzmine.modules.dataprocessing.id_ion_identity_networking.refin
 
 
 import io.github.mzmine.datamodel.MZmineProject;
-import io.github.mzmine.datamodel.features.FeatureList;
 import io.github.mzmine.datamodel.features.FeatureListRow;
 import io.github.mzmine.datamodel.features.ModularFeatureList;
+import io.github.mzmine.datamodel.features.SimpleFeatureListAppliedMethod;
+import io.github.mzmine.datamodel.identities.IonLibrary;
+import io.github.mzmine.datamodel.identities.IonType;
+import io.github.mzmine.datamodel.identities.IonType.IonTypeStringFlavor;
 import io.github.mzmine.datamodel.identities.iontype.IonIdentity;
-import io.github.mzmine.datamodel.identities.iontype.IonModification;
 import io.github.mzmine.datamodel.identities.iontype.IonNetwork;
 import io.github.mzmine.datamodel.identities.iontype.IonNetworkLogic;
-import io.github.mzmine.datamodel.identities.iontype.IonType;
-import io.github.mzmine.javafx.concurrent.threading.FxThread;
+import io.github.mzmine.datamodel.identities.iontype.IonNetworkNode;
 import io.github.mzmine.parameters.ParameterSet;
 import io.github.mzmine.taskcontrol.AbstractTask;
 import io.github.mzmine.taskcontrol.TaskStatus;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
 
 public class IonNetworkRefinementTask extends AbstractTask {
+
   // Logger.
   private static final Logger LOG = Logger.getLogger(IonNetworkRefinementTask.class.getName());
+  private final Set<String> mainIonNames;
 
   private int finishedRows;
-  private int totalRows;
+  private final int totalRows;
+  @NotNull
+  private final ParameterSet parameters;
   private final ModularFeatureList featureList;
 
-  private final ParameterSet parameters;
-  private final MZmineProject project;
-
   // >= trueThreshold delete all other occurance in networks
-  private int trueThreshold = 4;
+  private final int trueThreshold;
   // delete all other xmers when one was confirmed in MSMS
-  private boolean deleteWithoutMonomer = true;
-  private boolean deleteSmallerNets = true;
-  private boolean filterMinSize = true;
-  private boolean deleteSmallNoMajor = false;
-  private boolean keepRowOnlyWithID = false;
-  private int minNetworkSize = 2;
+  private final boolean requireMonomer;
+  private final boolean deleteSmallerNets;
+  private final boolean filterMinSize;
+  private final boolean requireMajorIon;
+  private final boolean keepRowOnlyWithID;
+  private final int minNetworkSize;
 
   /**
    * Create the task.
@@ -76,30 +79,45 @@ public class IonNetworkRefinementTask extends AbstractTask {
   public IonNetworkRefinementTask(final MZmineProject project, final ParameterSet parameterSet,
       final ModularFeatureList featureLists, @NotNull Instant moduleCallDate) {
     super(featureLists.getMemoryMapStorage(), moduleCallDate);
-
-    this.project = project;
+    this.parameters = parameterSet;
     this.featureList = featureLists;
-    parameters = parameterSet;
 
     finishedRows = 0;
-    totalRows = 0;
+    totalRows = featureLists.getNumberOfRows();
 
     // tolerances
-    deleteWithoutMonomer =
-        parameterSet.getParameter(IonNetworkRefinementParameters.DELETE_WITHOUT_MONOMER).getValue();
-    deleteSmallerNets =
-        parameterSet.getParameter(IonNetworkRefinementParameters.TRUE_THRESHOLD).getValue();
-    trueThreshold = parameterSet.getParameter(IonNetworkRefinementParameters.TRUE_THRESHOLD)
-        .getEmbeddedParameter().getValue();
+    requireMonomer = parameterSet.getValue(IonNetworkRefinementParameters.DELETE_WITHOUT_MONOMER);
+    deleteSmallerNets = parameterSet.getValue(IonNetworkRefinementParameters.TRUE_THRESHOLD);
+    trueThreshold = parameterSet.getEmbeddedParameterValueIfSelectedOrElse(
+        IonNetworkRefinementParameters.TRUE_THRESHOLD, 10);
 
-    filterMinSize =
-        parameterSet.getParameter(IonNetworkRefinementParameters.MIN_NETWORK_SIZE).getValue();
-    minNetworkSize = parameterSet.getParameter(IonNetworkRefinementParameters.MIN_NETWORK_SIZE)
-        .getEmbeddedParameter().getValue();
-    deleteSmallNoMajor =
-        parameterSet.getParameter(IonNetworkRefinementParameters.DELETE_SMALL_NO_MAJOR).getValue();
-    keepRowOnlyWithID =
-        parameterSet.getParameter(IonNetworkRefinementParameters.DELETE_ROWS_WITHOUT_ID).getValue();
+    filterMinSize = parameterSet.getValue(IonNetworkRefinementParameters.MIN_NETWORK_SIZE);
+    minNetworkSize = parameterSet.getEmbeddedParameterValueIfSelectedOrElse(
+        IonNetworkRefinementParameters.MIN_NETWORK_SIZE, 1);
+
+    final Optional<IonLibrary> main = parameters.getOptionalValue(
+        IonNetworkRefinementParameters.mainIonLibrary);
+    requireMajorIon = main.isPresent();
+    if (requireMajorIon) {
+      // use the string names to ignore the mass that may be slightly different
+      mainIonNames = main.get().ions().stream().map(IonNetworkRefinementTask::uniqueID)
+          .collect(Collectors.toSet());
+    } else {
+      mainIonNames = Set.of();
+    }
+
+    keepRowOnlyWithID = parameterSet.getValue(
+        IonNetworkRefinementParameters.DELETE_ROWS_WITHOUT_ID);
+  }
+
+  /**
+   * Use name as unique ID to ignore the mass which may be slightly different depending on ion type
+   * definition.
+   *
+   * @return name as unique id
+   */
+  private static String uniqueID(IonType n) {
+    return n.toString(IonTypeStringFlavor.SIMPLE_DEFAULT);
   }
 
   @Override
@@ -114,128 +132,122 @@ public class IonNetworkRefinementTask extends AbstractTask {
 
   @Override
   public void run() {
+    setStatus(TaskStatus.PROCESSING);
+
     refine();
+
+    featureList.addDescriptionOfAppliedTask(
+        new SimpleFeatureListAppliedMethod(IonNetworkRefinementModule.class, parameters,
+            getModuleCallDate()));
+
     setStatus(TaskStatus.FINISHED);
   }
 
 
-  public void refine() {
-    // sort and refine
-    refine(featureList, deleteSmallerNets, deleteWithoutMonomer, trueThreshold, deleteSmallNoMajor,
-        filterMinSize, minNetworkSize, keepRowOnlyWithID);
-  }
-
   /**
    * Delete all without monomer or 1 monomer and >=3 multimers. Delete all smaller networks if one
    * network is the best for all
-   * 
-   * @param trueThreshold
-   * @param minNetworkSize
-   * @param filterMinSize
-   * @param deleteSmallNoMajor
    */
-  public static void refine(ModularFeatureList pkl, boolean deleteSmallerNets, boolean deleteWithoutMonomer,
-      int trueThreshold, boolean deleteSmallNoMajor, boolean filterMinSize, int minNetworkSize,
-      boolean keepRowOnlyWithID) {
-    // sort
-    IonNetworkLogic.sortIonIdentities(pkl, true);
+  public void refine() {
+    if (keepRowOnlyWithID) {
+      final List<FeatureListRow> toRemove = featureList.getRows().stream()
+          .filter(r -> !r.hasIonIdentity()).toList();
+      featureList.removeRows(toRemove);
+      LOG.info("Removed %d rows keeping %d with ion identity from %s".formatted(toRemove.size(),
+          featureList.getNumberOfRows(), featureList.getName()));
+    }
 
-    long count = IonNetworkLogic.streamNetworks(pkl).count();
+    // sort
+    IonNetworkLogic.sortIonIdentities(featureList, true);
+
+    List<IonNetwork> nets = IonNetworkLogic.getAllNetworksList(featureList.getRows(), null, false);
+
+    int count = nets.size();
     LOG.info("Ion identity networks before refinement: " + count);
 
-    // min size
-    deleteSmallOrWithoutMajor(pkl, filterMinSize, minNetworkSize, deleteSmallNoMajor,
-        keepRowOnlyWithID);
-
-    IonNetwork[] nets = IonNetworkLogic.getAllNetworks(pkl, false);
-    if (deleteWithoutMonomer)
-      deleteAllWithoutMonomer(nets);
-    if (deleteSmallerNets)
+    if (deleteSmallerNets) {
       deleteSmallerNetworks(nets, trueThreshold);
+    }
 
-    // remove all identities where networks were marked as deleted
-    deleteAllIonsOfDeletedNetworks(pkl.getRows());
-    // TODO new network refinement
+    for (IonNetwork net : nets) {
+      final boolean keep = checkKeepNetwork(net);
+      if (!keep) {
+        net.delete();
+      }
+    }
 
-    count = IonNetworkLogic.streamNetworks(pkl).count();
+    count = IonNetworkLogic.getAllNetworksList(featureList.getRows(), null, false).size();
+    IonNetworkLogic.sortIonIdentities(featureList, true);
     LOG.info("Ion identity networks after refinement: " + count);
   }
 
-  /**
-   * Delete all ion identities of as deleted marked networks
-   *
-   * @param rows
-   */
-  private static void deleteAllIonsOfDeletedNetworks(List<FeatureListRow> rows) {
-    for (FeatureListRow row : rows) {
-      if (row.hasIonIdentity()) {
-        for (int i = 0; i < row.getIonIdentities().size(); i++) {
-          IonIdentity id = row.getIonIdentities().get(i);
-          if (id.isDeleted() || id.getNetwork() == null || id.getNetwork().isEmpty()) {
-            id.delete(row);
-            i--;
-          }
-        }
+  private boolean checkKeepNetwork(IonNetwork net) {
+    return checkNetSize(net) //
+        && (!requireMonomer || hasMonomer(net)) //
+        && (!requireMajorIon || hasMajorIon(net));
+  }
+
+  private boolean hasMajorIon(IonNetwork net) {
+    for (IonNetworkNode node : net.getNodes()) {
+      if (mainIonNames.contains(uniqueID(node.ion().getIonType()))) {
+        return true;
       }
     }
+    return false;
+  }
+
+  /**
+   *
+   * @param net
+   * @return true if multiple monomers or if 1 monomer and not too many multimers
+   */
+  public static boolean hasMonomer(IonNetwork net) {
+    int mono = 0;
+    for (IonNetworkNode node : net.getNodes()) {
+      if (node.ion().getIonType().molecules() == 1) {
+        mono++;
+      }
+    }
+    return mono > 1 || (mono == 1 && net.size() < 4);
+  }
+
+
+  private boolean checkNetSize(IonNetwork net) {
+    return net.size() >= minNetworkSize;
   }
 
   /**
    * Delete all smaller networks if one network is the preferred in all rows
-   * 
+   *
    * @param nets
    * @param trueThreshold
    */
-  private static void deleteSmallerNetworks(IonNetwork[] nets, int trueThreshold) {
+  private static void deleteSmallerNetworks(List<IonNetwork> nets, int trueThreshold) {
+    if (trueThreshold < 2) {
+      return;
+    }
     for (IonNetwork net : nets) {
-      // not deleted
-      if (net.size() > 0) {
-        // delete small ones
-        if (net.size() >= trueThreshold && trueThreshold > 1) {
-          if (isBestNet(net)) {
-            deleteAllOther(net);
-          }
+      // delete small ones
+      if (net.size() >= trueThreshold) {
+        if (isBestNet(net)) {
+          deleteAllOther(net);
         }
       }
     }
   }
 
-  /**
-   * Delete all networks without monomer or with 1 monomer and >=3 multimers
-   * 
-   * @param nets
-   */
-  private static void deleteAllWithoutMonomer(IonNetwork[] nets) {
-    for (IonNetwork net : nets) {
-      // not deleted
-      if (net.size() > 0) {
-        int monomer = 0;
-        int multimer = 0;
-        for (Entry<FeatureListRow, IonIdentity> e : net.entrySet()) {
-          if (e.getValue().getIonType().getMolecules() == 1)
-            monomer++;
-          else if (e.getValue().getIonType().getMolecules() > 1)
-            multimer++;
-        }
-        // no monomer
-        // 1 monomer and >=3 multimers --> delete
-        if (monomer == 0 || (monomer == 1 && multimer >= 3))
-          net.delete();
-      }
-    }
-  }
 
   /**
    * Is best network in all rows?
-   * 
+   *
    * @param net
    * @return
    */
   private static boolean isBestNet(IonNetwork net) {
-    for (FeatureListRow row : net.keySet()) {
-      IonIdentity id = row.getBestIonIdentity();
+    for (var node : net.getNodes()) {
+      IonIdentity id = node.row().getBestIonIdentity();
       // is best of all rows
-      if (id != null && !id.getNetwork().equals(net)) {
+      if (id != null && !net.equals(id.getNetwork())) {
         return false;
       }
     }
@@ -244,114 +256,17 @@ public class IonNetworkRefinementTask extends AbstractTask {
 
   /**
    * Keep net but delete all other networks from all rows
-   * 
+   *
    * @param net
    */
   private static void deleteAllOther(IonNetwork net) {
-    for (FeatureListRow row : net.keySet()) {
-      Stream.of(IonNetworkLogic.getAllNetworks(row)).forEach(o -> {
+    for (var node : net.getNodes()) {
+      Stream.of(IonNetworkLogic.getAllNetworks(node.row())).forEach(o -> {
         if (net.getID() != o.getID()) {
           o.delete();
         }
       });
     }
   }
-
-  private static int getLinks(IonIdentity best) {
-    int links = best.getPartnerRows().size();
-    if (best.getMSMSMultimerCount() > 0)
-      links++;
-    return links;
-  }
-
-
-  /**
-   * Delete all networks smaller min size
-   * 
-   * @param pkl
-   * @param minNetSize
-   * @param deleteSmallNoMajor
-   * @throws Exception
-   */
-  public static void deleteSmallOrWithoutMajor(FeatureList pkl, boolean filterMinSize, int minNetSize,
-      boolean deleteSmallNoMajor, boolean deleteRowsWithoutIon) {
-    if (filterMinSize || deleteSmallNoMajor) {
-      // need to convert to array first to avoid concurren mod exception
-      IonNetwork[] nets = IonNetworkLogic.getAllNetworks(pkl, false);
-      Arrays.stream(nets).forEach(net -> {
-        if ((filterMinSize && net.size() < minNetSize)
-            || (deleteSmallNoMajor && !hasMajorIonID(net)))
-          net.delete();
-      });
-    }
-
-    // remove all rows without ion identity?
-    if (deleteRowsWithoutIon)
-      FxThread.runLater(() -> {
-        for (int i = 0; i < pkl.getNumberOfRows();)
-          if (pkl.getRow(i).hasIonIdentity())
-            i++;
-          else
-            pkl.removeRow(i);
-      });
-  }
-
-  private static boolean hasMajorIonID(IonNetwork net) {
-    return net.values().stream().map(IonIdentity::getIonType).anyMatch(ion -> {
-      return equalsType(ion, IonModification.H, 3) || equalsType(ion, IonModification.NA, 0)
-          || equalsType(ion, IonModification.NH4, 1) || equalsType(ion, IonModification.H_NEG, 2)
-          || equalsType(ion, IonModification.CL, 0) || equalsType(ion, IonModification.FA, 0);
-    });
-  }
-
-  /**
-   * @param testedIon        tested ion
-   * @param adduct           the target adduct
-   * @param maxModifications maximum number of ion modifications (-2H2O == 2)
-   * @return true if adducts equal and modification is within max
-   */
-  private static boolean equalsType(IonType testedIon, IonModification adduct,
-      int maxModifications) {
-    return testedIon.getAdduct().equals(adduct) && testedIon.getModCount() <= maxModifications;
-  }
-
-
-  /**
-   * Delete xmers if one was verified by msms
-   *
-   * @param row
-   * @param best
-   * @param all
-   * @return
-   */
-  private static boolean deleteXmersOnMSMS(FeatureListRow row, IonIdentity best, List<IonIdentity> all,
-      int trueThreshold) {
-    // check best first
-    if (best.getMSMSMultimerCount() > 0) {
-      // delete rest of annotations
-      for (int i = 1; i < all.size();)
-        row.getIonIdentities().get(i).delete(row);
-
-      row.setBestIonIdentity(best);
-      return true;
-    } else {
-      // check rest
-      for (IonIdentity other : all) {
-        if (other.getMSMSMultimerCount() > 0) {
-          row.setBestIonIdentity(other);
-
-          // delete rest of annotations
-          for (int i = 1; i < row.getIonIdentities().size(); i++) {
-            IonIdentity e = row.getIonIdentities().get(i);
-            if (!other.equals(e) && (trueThreshold <= 1 || getLinks(e) < trueThreshold))
-              e.delete(row);
-          }
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
 
 }
