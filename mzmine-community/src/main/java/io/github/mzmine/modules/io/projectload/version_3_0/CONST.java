89ca981105f9dc7bf2d726e6677aa724d2b73b67
Steffen Heuckeroth <steffen.heuckeroth@mzio.io>
2026-01-14 17:13:14 +0100

    specmatch-modular: review

--- mzmine-community/src/main/java/io/github/mzmine/modules/io/projectload/version_3_0/CONST.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/io/projectload/version_3_0/CONST.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2025 The mzmine Development Team
+ * Copyright (c) 2004-2026 The mzmine Development Team
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -86,7 +86,7 @@ public class CONST {
   /**
    *
    */
-  public static final String XML_VERSION_ATTR = "v";
+  public static final String XML_VERSION_ATTR = "version";
 
   private CONST() {
   }
