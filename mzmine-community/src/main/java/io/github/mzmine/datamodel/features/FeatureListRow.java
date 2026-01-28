3839b2335edaae57497c2e1a1ea48db641c4546d
robinschmid <rschmid1789@gmail.com>
2026-01-28 12:51:02 +0100

    NPE in lipid annotation utils without representative MS1

--- mzmine-community/src/main/java/io/github/mzmine/datamodel/features/FeatureListRow.java
+++ mzmine-community/src/main/java/io/github/mzmine/datamodel/features/FeatureListRow.java
@@ -569,14 +569,11 @@ public interface FeatureListRow extends ModularDataModel {
   @Nullable String getPreferredAnnotationName();
 
   /**
-   * @return The polarity of this row. Based on {@link Feature#getRepresentativeScan()}.
+   * @return The polarity of this row. Based on {@link Feature#getRepresentativeScan()} or MS2 scans.
    */
   @Nullable
   default PolarityType getRepresentativePolarity() {
-    final Feature bestFeature = getBestFeature();
-    if (bestFeature != null && bestFeature.getRepresentativePolarity() != null) {
-      return bestFeature.getRepresentativePolarity();
-    }
+    // checks best feature first
     return streamFeatures().sorted(Comparator.comparingDouble(Feature::getHeight).reversed())
         .map(Feature::getRepresentativePolarity).filter(Objects::nonNull).findFirst().orElse(null);
   }
