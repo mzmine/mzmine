8cb74d7027d94bb9370a224b75074325cdc6a442
Steffen Heuckeroth <steffen.heuckeroth@mzio.io>
2026-02-20 15:10:44 +0100

    wavelet: update slope detector

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/wavelet/SlopeEdgeDetector.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataprocessing/featdet_chromatogramdeconvolution/wavelet/SlopeEdgeDetector.java
@@ -1,13 +1,36 @@
+/*
+ * Copyright (c) 2004-2026 The mzmine Development Team
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.github.mzmine.modules.dataprocessing.featdet_chromatogramdeconvolution.wavelet;
 
 import java.util.LinkedList;
-import java.util.Queue;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 public class SlopeEdgeDetector implements EdgeDetector {
 
   private final int windowSize;
-  private final Queue<Data> history = new LinkedList<>();
+  private final LinkedList<Data> history = new LinkedList<>();
 
   public SlopeEdgeDetector(int windowSize) {
     this.windowSize = windowSize;
@@ -66,9 +89,9 @@ public class SlopeEdgeDetector implements EdgeDetector {
     regression.clear();
     final double normFactor = 1; //y[startIndex];
 
-    if (startIndex - windowSize < 0) {
-      return 0;
-    }
+//    if (startIndex - windowSize < 0) {
+//      return 0;
+//    }
 
     // fill initial points
     for (int i = startIndex; i < startIndex + windowSize && i < y.length; i++) {
@@ -78,7 +101,11 @@ public class SlopeEdgeDetector implements EdgeDetector {
     }
 
     if (regression.getSlope() > 0) {
-      return startIndex - windowSize;
+      while (regression.getN() > 2) {
+        Data last = history.pollLast();
+        regression.removeData(last.x, last.y);
+      }
+      return Math.toIntExact(startIndex + regression.getN());
     }
 
     int absMinIndex = startIndex;
