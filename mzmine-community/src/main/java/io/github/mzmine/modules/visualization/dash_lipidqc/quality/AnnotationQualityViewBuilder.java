96a65164b33314ff72dd2bc876ed59e64668c646
Steffen Heuckeroth <steffen.heuckeroth@mzio.io>
2026-05-06 16:36:30 +0200

    lipid-qc: add option for SetLipidAnnotationLevelModule to dashboard

--- mzmine-community/src/main/java/io/github/mzmine/modules/visualization/dash_lipidqc/quality/AnnotationQualityViewBuilder.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/visualization/dash_lipidqc/quality/AnnotationQualityViewBuilder.java
@@ -95,7 +95,10 @@ class AnnotationQualityViewBuilder extends FxViewBuilder<AnnotationQualityModel>
 
     final Button removeMultiRowAnnotationsButton = FxButtons.createButton(
         "Remove multi-row annotations", interactor::removeMultiRowAnnotations);
-    final VBox actionBox = new VBox(6, removeMultiRowAnnotationsButton);
+    final Button setPreferredAnnotationLevelButton = FxButtons.createButton(
+        "Set preferred annotation level (global)", interactor::setPreferredLipidAnnotationLevel);
+    final VBox actionBox = new VBox(6, removeMultiRowAnnotationsButton,
+        setPreferredAnnotationLevelButton);
     actionBox.setAlignment(Pos.TOP_LEFT);
     final TitledPane actionsPane = new TitledPane("Annotation actions", actionBox);
     actionsPane.setCollapsible(true);
