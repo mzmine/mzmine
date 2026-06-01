23fd525f56454376e9e5ad8fc927c4c3c2157b32
robinschmid <rschmid1789@gmail.com>
2026-06-01 17:27:22 +0200

    label color in ms2 scan

--- mzmine-community/src/main/java/io/github/mzmine/modules/dataanalysis/compounddashboard/CompoundDashboardSpectraTask.java
+++ mzmine-community/src/main/java/io/github/mzmine/modules/dataanalysis/compounddashboard/CompoundDashboardSpectraTask.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2004-2026 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.github.mzmine.modules.dataanalysis.compounddashboard;
 
 import io.github.mzmine.datamodel.RawDataFile;
@@ -183,8 +208,10 @@ public class CompoundDashboardSpectraTask extends FxUpdateTask<CompoundDashboard
     final String label = "MS2 " + (CompoundDashboardColoring.ionTypeLabel(ms2Row) != null
         ? CompoundDashboardColoring.ionTypeLabel(ms2Row) : "");
     final Color awt = FxColorUtil.fxColorToAWT(colors.colorFor(ms2Row));
-    return List.of(new DatasetAndRenderer(new MassSpectrumProvider(ms2Scan, label, awt),
-        new ColoredXYBarRenderer(false)));
+    final ColoredXYBarRenderer renderer = new ColoredXYBarRenderer(false);
+    // match the on-plot stick label color to the dataset color
+    renderer.setDefaultItemLabelPaint(awt);
+    return List.of(new DatasetAndRenderer(new MassSpectrumProvider(ms2Scan, label, awt), renderer));
   }
 
   private static @NotNull String buildMs2Title(@NotNull final Scan ms2) {
