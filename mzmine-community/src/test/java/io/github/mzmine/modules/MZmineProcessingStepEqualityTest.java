70e87d47cacdc18676640451ff4f6ecdeb67dad8
Steffen Heuckeroth <steffen.heuckeroth@mzio.io>
2026-08-21 17:04:20 +0200

    batch: fix step duplication with drag & drop

---
+++ mzmine-community/src/test/java/io/github/mzmine/modules/MZmineProcessingStepEqualityTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2004-2026 The mzmine Development Team
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.mzmine.modules;
+
+import io.github.mzmine.modules.batchmode.BatchQueue;
+import io.github.mzmine.modules.batchmode.BatchQueueParameter;
+import io.github.mzmine.modules.impl.MZmineProcessingStepImpl;
+import io.github.mzmine.parameters.ParameterUtils;
+import io.github.mzmine.parameters.impl.SimpleParameterSet;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class MZmineProcessingStepEqualityTest {
+
+  @Test
+  void duplicateConfigurationsRetainObjectIdentity() {
+    final MZmineProcessingModule module = Mockito.mock(MZmineProcessingModule.class);
+    final MZmineProcessingStep<MZmineProcessingModule> first = new MZmineProcessingStepImpl<>(
+        module, new SimpleParameterSet());
+    final MZmineProcessingStep<MZmineProcessingModule> second = new MZmineProcessingStepImpl<>(
+        module, new SimpleParameterSet());
+
+    Assertions.assertNotEquals(first, second);
+    Assertions.assertTrue(ParameterUtils.equalValues(first, second));
+
+    final BatchQueue queue = new BatchQueue();
+    queue.addAll(first, second);
+    queue.removeAll(List.of(first));
+
+    Assertions.assertEquals(1, queue.size());
+    Assertions.assertSame(second, queue.getFirst());
+  }
+
+  @Test
+  void batchQueueParametersCompareStepValues() {
+    final MZmineProcessingModule module = Mockito.mock(MZmineProcessingModule.class);
+    final BatchQueue firstQueue = new BatchQueue();
+    firstQueue.add(new MZmineProcessingStepImpl<>(module, new SimpleParameterSet()));
+    final BatchQueue secondQueue = new BatchQueue();
+    secondQueue.add(new MZmineProcessingStepImpl<>(module, new SimpleParameterSet()));
+
+    final BatchQueueParameter first = new BatchQueueParameter();
+    first.setValue(firstQueue);
+    final BatchQueueParameter second = new BatchQueueParameter();
+    second.setValue(secondQueue);
+
+    Assertions.assertTrue(first.valueEquals(second));
+  }
+
+}
